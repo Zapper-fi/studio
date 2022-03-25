cdca2da6c105a7e75419a56a68efae1211f8a5b7
Justin D'Errico <justin.derrico1991@gmail.com>
2022-03-25 16:39:57 -0400

    Add position module, stubbing the position service (#6)

--- src/app-toolkit/app-toolkit.module.ts
+++ src/app-toolkit/app-toolkit.module.ts
@@ -3,12 +3,13 @@ import { Module } from '@nestjs/common';
 import { ContractModule } from '~contract/contract.module';
 import { MulticallModule } from '~multicall/multicall.module';
 import { NetworkProviderModule } from '~network-provider/network-provider.module';
+import { PositionModule } from '~position/position.module';
 import { TokenModule } from '~token/token.module';
 
 import { AppToolkit } from './app-toolkit.service';
 
 @Module({
-  imports: [ContractModule, MulticallModule, NetworkProviderModule, TokenModule],
+  imports: [ContractModule, MulticallModule, NetworkProviderModule, TokenModule, PositionModule],
   providers: [AppToolkit],
   exports: [AppToolkit],
 })
