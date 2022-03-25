3a8aba4051abdf53075409c7b0f01246e168c45c
Justin D'Errico <justin.derrico1991@gmail.com>
2022-03-25 16:49:33 -0400

    Add app toolkit decorators

---
+++ src/app-toolkit/decorators/index.ts
@@ -0,0 +1,5 @@
+import { AppDefinition } from './app-defintion.decorator';
+import { BalanceFetcher } from './balance-fetcher.decorator';
+import { PositionFetcher } from './position-fetcher.decorator';
+
+export { BalanceFetcher, PositionFetcher, AppDefinition };
---
+++ src/app-toolkit/decorators/position-fetcher.decorator.ts
@@ -0,0 +1,29 @@
+import { applyDecorators, Injectable, SetMetadata } from '@nestjs/common';
+
+import { ContractType } from '~position/contract.interface';
+import { Network } from '~types/network.interface';
+
+export const POSITION_FETCHER_APP = 'POSITION_FETCHER_APP';
+export const POSITION_FETCHER_GROUP = 'POSITION_FETCHER_GROUP';
+export const POSITION_FETCHER_NETWORK = 'POSITION_FETCHER_NETWORK';
+export const POSITION_FETCHER_TYPE = 'POSITION_FETCHER_TYPE';
+
+export const PositionFetcher = ({
+  appId,
+  groupId,
+  network,
+  type,
+}: {
+  appId: string;
+  groupId: string;
+  network: Network;
+  type: ContractType;
+}) => {
+  return applyDecorators(
+    SetMetadata(POSITION_FETCHER_APP, appId),
+    SetMetadata(POSITION_FETCHER_GROUP, groupId),
+    SetMetadata(POSITION_FETCHER_NETWORK, network),
+    SetMetadata(POSITION_FETCHER_TYPE, type),
+    Injectable,
+  );
+};
