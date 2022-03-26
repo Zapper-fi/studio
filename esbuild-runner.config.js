6ee0143b4ec26e3f0566543071f2ddc22d6057c6
Xavier Drdak <1198051+xdrdak@users.noreply.github.com>
2022-03-25 20:06:07 -0400

    Remove esr config

--- esbuild-runner.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  type: 'bundle', // bundle or transform (see description above)
-  esbuild: {
-    // Any esbuild build or transform options go here
-    target: 'esnext',
-  },
-};
