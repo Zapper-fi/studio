37bea0ae83be275f2f8e79057c235bce9d4c733d
Justin D'Errico <justin.derrico1991@gmail.com>
2022-03-28 21:36:08 -0400

    Fetch positions from both the registry and the API (if not present locally) (#11)

--- src/position/position.service.ts
+++ src/position/position.service.ts
@@ -6,7 +6,7 @@ import { ContractType } from './contract.interface';
 import { DefaultDataProps } from './display.interface';
 import { ApiPositionSource } from './position-source/position-source.api';
 import { RegistryPositionSource } from './position-source/position-source.registry';
-import { AppTokenPosition, ContractPosition } from './position.interface';
+import { AbstractPosition, AppTokenPosition, ContractPosition } from './position.interface';
 
 export type AppGroupsDefinition = {
   appId: string;
@@ -20,11 +20,25 @@ export class PositionService {
     @Inject(ApiPositionSource) private readonly apiPositionSource: ApiPositionSource,
     @Inject(RegistryPositionSource) private readonly registryPositionSource: RegistryPositionSource,
   ) {}
+
+  private async getPositions<T extends AbstractPosition<any>>(
+    appGroupDefinitions: AppGroupsDefinition[],
+    contractType: ContractType,
+  ): Promise<T[]> {
+    const { supported, unsupported } = this.registryPositionSource.getSupported(appGroupDefinitions, contractType);
+    const positions = await Promise.all([
+      this.registryPositionSource.getPositions<T>(supported, contractType),
+      this.apiPositionSource.getPositions<T>(unsupported, contractType),
+    ]);
+
+    return positions.flat();
+  }
+
   async getAppContractPositions<T = DefaultDataProps>(...appGroupDefinitions: AppGroupsDefinition[]) {
-    return this.registryPositionSource.getPositions<ContractPosition<T>>(appGroupDefinitions, ContractType.POSITION);
+    return this.getPositions<ContractPosition<T>>(appGroupDefinitions, ContractType.POSITION);
   }
 
   async getAppTokenPositions<T = DefaultDataProps>(...appGroupDefinitions: AppGroupsDefinition[]) {
-    return this.registryPositionSource.getPositions<AppTokenPosition<T>>(appGroupDefinitions, ContractType.APP_TOKEN);
+    return this.getPositions<AppTokenPosition<T>>(appGroupDefinitions, ContractType.APP_TOKEN);
   }
 }
