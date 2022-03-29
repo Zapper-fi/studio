76deb41531994e0457ba70eff32cafa7001e1453
Justin D'Errico <justin.derrico1991@gmail.com>
2022-03-29 17:39:06 -0400

    Expose the right components for the public lib

--- index.ts
+++ index.ts
@@ -1,1 +1,4 @@
-export { AppsModule } from './src/apps/apps.module';
+export { APP_TOOLKIT } from '~app-toolkit/app-toolkit.constants';
+export { AppsModule } from '~apps/apps.module';
+export { IAppToolkit } from '~app-toolkit/app-toolkit.interface';
+export { AppToolkitHelperRegistry } from '~app-toolkit/app-toolkit.helpers';
