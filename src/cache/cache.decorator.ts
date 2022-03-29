40d4aebaf11f4575ac4bcc7212f9aa391226c54c
Justin D'Errico <justin.derrico1991@gmail.com>
2022-03-29 10:46:54 -0400

    Add cache decorator for local caching (#13)

--- src/cache/cache.decorator.ts
+++ src/cache/cache.decorator.ts
@@ -1,52 +1,29 @@
 import { applyDecorators, SetMetadata } from '@nestjs/common';
-import { Class } from 'type-fest';
 
-export const CACHE_ON_INTERVAL_KEY = 'CACHE_ON_INTERVAL_KEY';
-export const CACHE_ON_INTERVAL_TIMEOUT = 'CACHE_ON_INTERVAL_TIMEOUT';
-export const CACHE_ON_INTERVAL_INSTANCE = 'CACHE_ON_INTERVAL_INSTANCE';
-export const CACHE_ON_INTERVAL_FAIL_ON_MISSING_CACHE = 'CACHE_ON_INTERVAL_FAIL_ON_MISSING_CACHE';
-export const CACHE_ON_INTERVAL_DESERIALIZE_INTO = 'CACHE_ON_INTERVAL_DESERIALIZE_INTO';
+export const CACHE_KEY = 'CACHE_KEY';
+export const CACHE_TTL = 'CACHE_TTL';
+export const CACHE_INSTANCE = 'CACHE_INSTANCE';
+export const CACHE_FAIL_ON_MISSING_CACHE = 'CACHE_FAIL_ON_MISSING_CACHE';
 
-export type CacheOnIntervalOptions = {
-  key: string;
-  instance: 'business';
-  timeout: number;
+type CacheKeyBuilder = (...args: any) => string;
+type CacheTtlBuilder = (...args: any) => number;
+
+export type CacheOptions = {
+  key: string | CacheKeyBuilder;
+  ttl?: number | null | CacheTtlBuilder;
+  instance?: 'business' | 'user';
   failOnMissingData?: boolean;
 };
 
-type CacheOnIntervalBuilderOptions = CacheOnIntervalOptions & {
-  targetMethod: string;
-};
-
-export function CacheOnIntervalBuilder<T>({
-  targetMethod,
-  key,
-  instance,
-  timeout,
-  failOnMissingData,
-}: CacheOnIntervalBuilderOptions) {
-  return (target: Class<T>) => {
-    const method = target.prototype[targetMethod];
-    if (!method) throw new Error(`Method ${targetMethod} not found!`);
-
-    return CacheOnInterval({
-      instance,
-      key,
-      timeout,
-      failOnMissingData,
-    })(method);
-  };
-}
-
-export const CacheOnInterval = (options: CacheOnIntervalOptions) => {
-  const { failOnMissingData = false } = options;
+export const Cache = (options: CacheOptions) => {
+  const { failOnMissingData = false, ttl = null, instance = 'business' } = options;
 
   return applyDecorators(
-    SetMetadata(CACHE_ON_INTERVAL_KEY, options.key),
-    SetMetadata(CACHE_ON_INTERVAL_INSTANCE, options.instance),
-    SetMetadata(CACHE_ON_INTERVAL_TIMEOUT, options.timeout),
+    SetMetadata(CACHE_KEY, options.key),
+    SetMetadata(CACHE_INSTANCE, instance),
+    SetMetadata(CACHE_TTL, ttl),
     SetMetadata(
-      CACHE_ON_INTERVAL_FAIL_ON_MISSING_CACHE,
+      CACHE_FAIL_ON_MISSING_CACHE,
       // Do not throw locally if cached data is missing, try to fetch it
       // to ease development
       process.env.NODE_ENV !== 'production' ? failOnMissingData : false,
