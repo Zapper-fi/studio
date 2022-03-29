e116c91de2e2accbabc051818d9d5cf7f0c511f6
Justin D'Errico <justin.derrico1991@gmail.com>
2022-03-28 22:25:13 -0400

    Add balance fetcher support and cache metadata

--- src/apps/my-app/my-app.module.ts
+++ src/apps/my-app/my-app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 
+import { EthereumMyAppBalanceFetcher } from './ethereum/my-app.balance-fetcher';
 import { EthereumMyAppPoolTokenFetcher } from './ethereum/my-app.pool.token-fetcher';
 import { MyAppAppDefinition } from './my-app.definition';
 
 @Module({
-  providers: [MyAppAppDefinition, EthereumMyAppPoolTokenFetcher],
+  providers: [MyAppAppDefinition, EthereumMyAppPoolTokenFetcher, EthereumMyAppBalanceFetcher],
 })
 export class MyAppAppModule {}
