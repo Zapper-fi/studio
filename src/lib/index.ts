155966d8665bd6a6edead4511e572a8582da302a
Justin D'Errico <justin.derrico1991@gmail.com>
2022-03-30 15:15:45 -0400

    Expose App Toolkit Helpers (#52)

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,3 @@
 export { AppsModule } from '~apps/apps.module';
 export { IAppToolkit, APP_TOOLKIT } from '~app-toolkit/app-toolkit.interface';
-export { AppToolkitHelperRegistry } from '~app-toolkit/app-toolkit.helpers';
+export { AppToolkitHelperRegistry, AppToolkitHelpers } from '~app-toolkit/app-toolkit.helpers';
