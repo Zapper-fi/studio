096e01b2f7de87ee4ef07231390e90ce4c8978d0
Karan Shahani <karan.shahani91@gmail.com>
2022-04-02 18:10:39 -0400

    Add indexes to apps for easier access (#121)

---
+++ src/apps/aelin/index.ts
@@ -0,0 +1,3 @@
+export { AELIN_DEFINITION, AelinAppDefinition } from './aelin.definition';
+export { AelinAppModule } from './aelin.module';
+export { AelinContractFactory } from './contracts';
---
+++ src/apps/unagii/index.ts
@@ -0,0 +1,3 @@
+export { UNAGII_DEFINITION, UnagiiAppDefinition } from './unagii.definition';
+export { UnagiiAppModule } from './unagii.module';
+export { UnagiiContractFactory } from './contracts';
