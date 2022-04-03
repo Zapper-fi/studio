eda241c69fbdb7a2939ae80dd7887e175e9ae792
Karan Shahani <karan.shahani91@gmail.com>
2022-04-02 21:17:37 -0400

    Export contract types

--- src/apps/aelin/index.ts
+++ src/apps/aelin/index.ts
@@ -1,3 +1,6 @@
 export { AELIN_DEFINITION, AelinAppDefinition } from './aelin.definition';
 export { AelinAppModule } from './aelin.module';
 export { AelinContractFactory } from './contracts';
+
+export type { AelinPool } from './contracts';
+export type { AelinStaking } from './contracts';
--- src/apps/unagii/index.ts
+++ src/apps/unagii/index.ts
@@ -1,3 +1,5 @@
 export { UNAGII_DEFINITION, UnagiiAppDefinition } from './unagii.definition';
 export { UnagiiAppModule } from './unagii.module';
 export { UnagiiContractFactory } from './contracts';
+
+export type { UnagiiV2Vault } from './contracts';
