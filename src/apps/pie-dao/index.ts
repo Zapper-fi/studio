7b388ff2e95ed002387743a8a3eddbd367dc7b46
Karan Shahani <karan.shahani91@gmail.com>
2022-04-12 14:52:10 -0400

    Add missing index to PieDAO

---
+++ src/apps/pie-dao/index.ts
@@ -0,0 +1,7 @@
+export { PIE_DAO_DEFINITION, PieDaoAppDefinition } from './pie-dao.definition';
+export { PieDaoAppModule } from './pie-dao.module';
+export { PieDaoContractFactory } from './contracts';
+
+export type { PieDaoReferralRewards } from './contracts';
+export type { PieDaoStaking } from './contracts';
+export type { PieDaoVoteLockedDough } from './contracts';
