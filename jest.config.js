eb478eeafda20bb37c15ca7f041d4cb8c78d3c8d
Justin D'Errico <justin.derrico1991@gmail.com>
2022-04-19 12:51:52 -0400

    Integration test setup to fail on faulty builds (#150)

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  name: 'studio',
+  testEnvironment: 'node',
+  clearMocks: true,
+  transformIgnorePatterns: ['node_modules'],
+  testPathIgnorePatterns: ['/node_modules/', '/src/app/'],
+  testMatch: ['<rootDir>/src/**/__tests__/**/+(*.)+(spec|test).+(ts|js)?(x)'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.json',
+    },
+  },
+  moduleNameMapper: {
+    '^~(.*)$': '<rootDir>/src/$1',
+  },
+  transform: {
+    '^.+\\.(ts|js|html)$': 'ts-jest',
+  },
+};
