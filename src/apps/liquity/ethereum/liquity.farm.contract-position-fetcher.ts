d971b9357c95cccbc07b7136731cd6ddf2c2f49f
William Poulin <williampoulin01@gmail.com>
2022-04-20 11:02:19 -0400

    migrated liquity in studio (#158)

---
+++ src/apps/liquity/ethereum/liquity.farm.contract-position-fetcher.ts
@@ -0,0 +1,46 @@
+import { Inject } from '@nestjs/common';
+
+import { SingleStakingFarmContractPositionHelper } from '~app-toolkit';
+import { ZERO_ADDRESS } from '~app-toolkit/constants/address';
+import { Register } from '~app-toolkit/decorators';
+import { PositionFetcher } from '~position/position-fetcher.interface';
+import { ContractPosition } from '~position/position.interface';
+import { Network } from '~types/network.interface';
+
+import { LiquityContractFactory, LiquityStaking } from '../contracts';
+import { LIQUITY_DEFINITION } from '../liquity.definition';
+
+const FARMS = [
+  {
+    address: '0x4f9fbb3f1e99b56e0fe2892e623ed36a76fc605d',
+    stakedTokenAddress: '0x6dea81c8171d0ba574754ef6f8b412f2ed88c54d',
+    rewardTokenAddresses: ['0x5f98805a4e8be255a32880fdec7f6728c6568ba0', ZERO_ADDRESS], // LUSD and ETH
+  },
+];
+
+const appId = LIQUITY_DEFINITION.id;
+const groupId = LIQUITY_DEFINITION.groups.farm.id;
+const network = Network.ETHEREUM_MAINNET;
+
+@Register.ContractPositionFetcher({ appId, groupId, network })
+export class EthereumLiquityFarmContractPositionFetcher implements PositionFetcher<ContractPosition> {
+  constructor(
+    @Inject(LiquityContractFactory)
+    private readonly liquityContractFactory: LiquityContractFactory,
+    @Inject(SingleStakingFarmContractPositionHelper)
+    private readonly singleStakingFarmContractPositionHelper: SingleStakingFarmContractPositionHelper,
+  ) {}
+
+  async getPositions() {
+    return this.singleStakingFarmContractPositionHelper.getContractPositions<LiquityStaking>({
+      appId,
+      groupId,
+      network,
+      dependencies: [],
+      resolveFarmDefinitions: async () => FARMS,
+      resolveFarmContract: ({ address, network }) => this.liquityContractFactory.liquityStaking({ address, network }),
+      resolveIsActive: async () => true,
+      resolveRois: async () => ({ dailyROI: 0, weeklyROI: 0, yearlyROI: 0 }),
+    });
+  }
+}
---
+++ src/apps/liquity/liquity.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+
+import { AbstractDynamicApp } from '~app/app.dynamic-module';
+
+import { LiquityContractFactory } from './contracts';
+import { EthereumLiquityBalanceFetcher } from './ethereum/liquity.balance-fetcher';
+import { EthereumLiquityFarmContractPositionFetcher } from './ethereum/liquity.farm.contract-position-fetcher';
+import { LiquityStabilityPoolBalanceHelper } from './helpers/liquity.stability-pool.balance-helper';
+import { LiquityTroveBalanceHelper } from './helpers/liquity.trove.balance-helper';
+import { LiquityAppDefinition } from './liquity.definition';
+
+@Module({
+  providers: [
+    LiquityAppDefinition,
+    LiquityContractFactory,
+    EthereumLiquityBalanceFetcher,
+    EthereumLiquityFarmContractPositionFetcher,
+    LiquityTroveBalanceHelper,
+    LiquityStabilityPoolBalanceHelper,
+  ],
+})
+export class LiquityAppModule extends AbstractDynamicApp<LiquityAppModule>() {}
