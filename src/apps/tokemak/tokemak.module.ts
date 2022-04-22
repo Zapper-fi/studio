2d4e54a639cb4ea4ff9a02be8e705f38850d6ae1
Justin D'Errico <justin.derrico1991@gmail.com>
2022-04-22 19:07:45 -0400

    fix(tokemak): Use App Toolkit instead of position service (#239)

--- src/apps/tokemak/tokemak.module.ts
+++ src/apps/tokemak/tokemak.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 
 import { AbstractDynamicApp } from '~app/app.dynamic-module';
 import { SynthetixAppModule } from '~apps/synthetix';
-import { PositionModule } from '~position/position.module';
 
 import { TokemakContractFactory } from './contracts';
 import { EthereumTokemakBalanceFetcher } from './ethereum/tokemak.balance-fetcher';
@@ -12,7 +11,7 @@ import { EthereumTokemakTvlFetcher } from './ethereum/tokemak.tvl-fetcher';
 import { TokemakAppDefinition } from './tokemak.definition';
 
 @Module({
-  imports: [SynthetixAppModule.externallyConfigured(SynthetixAppModule, 0), PositionModule],
+  imports: [SynthetixAppModule.externallyConfigured(SynthetixAppModule, 0)],
   providers: [
     TokemakAppDefinition,
     TokemakContractFactory,
