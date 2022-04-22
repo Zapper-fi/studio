ecd118ab298925be5a8fbcc911617cb43d70794b
Xavier Drdak <1198051+xdrdak@users.noreply.github.com>
2022-04-22 11:55:26 -0400

    feat: add tokemak tvl fetcher

--- src/apps/tokemak/tokemak.module.ts
+++ src/apps/tokemak/tokemak.module.ts
@@ -2,21 +2,24 @@ import { Module } from '@nestjs/common';
 
 import { AbstractDynamicApp } from '~app/app.dynamic-module';
 import { SynthetixAppModule } from '~apps/synthetix';
+import { PositionModule } from '~position/position.module';
 
 import { TokemakContractFactory } from './contracts';
 import { EthereumTokemakBalanceFetcher } from './ethereum/tokemak.balance-fetcher';
 import { EthereumTokemakFarmContractPositionFetcher } from './ethereum/tokemak.farm.contract-position-fetcher';
 import { EthereumTokemakReactorTokenFetcher } from './ethereum/tokemak.reactor.token-fetcher';
+import { EthereumTokemakTvlFetcher } from './ethereum/tokemak.tvl-fetcher';
 import { TokemakAppDefinition } from './tokemak.definition';
 
 @Module({
-  imports: [SynthetixAppModule.externallyConfigured(SynthetixAppModule, 0)],
+  imports: [SynthetixAppModule.externallyConfigured(SynthetixAppModule, 0), PositionModule],
   providers: [
     TokemakAppDefinition,
     TokemakContractFactory,
     EthereumTokemakBalanceFetcher,
     EthereumTokemakReactorTokenFetcher,
     EthereumTokemakFarmContractPositionFetcher,
+    EthereumTokemakTvlFetcher,
   ],
 })
 export class TokemakAppModule extends AbstractDynamicApp<TokemakAppModule>() {}
