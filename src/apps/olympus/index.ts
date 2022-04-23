38110fbfc5765ac87790cd8caee6d8318d0ba936
brightiron <brightiron@protonmail.com>
2022-04-23 10:45:07 -0500

    updating export

--- src/apps/olympus/index.ts
+++ src/apps/olympus/index.ts
@@ -2,7 +2,7 @@ export { OLYMPUS_DEFINITION, OlympusAppDefinition } from './olympus.definition';
 export { OlympusAppModule } from './olympus.module';
 export { OlympusContractFactory } from './contracts';
 
-export type { OlympusBondDepository } from './contracts';
+export type { OlympusV2BondDepository } from './contracts';
 export type { OlympusGOhmToken } from './contracts';
 export type { OlympusSOhmToken } from './contracts';
 export type { OlympusSOhmV1Token } from './contracts';
