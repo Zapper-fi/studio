dfb00cea82c3c5bf63e6e9e2c779d24243bae8db
immasandwich <karan.shahani91@gmail.com>
2022-04-25 11:10:54 -0400

    fix: Create index files for Uniswap V2 and Olympus modules (#255)

--- src/apps/olympus/index.ts
+++ src/apps/olympus/index.ts
@@ -2,6 +2,11 @@ export { OLYMPUS_DEFINITION, OlympusAppDefinition } from './olympus.definition';
 export { OlympusAppModule } from './olympus.module';
 export { OlympusContractFactory } from './contracts';
 
+export { OlympusBondV1ContractPositionBalanceHelper } from './helpers/olympus.bond-v1.contract-position-balance-helper';
+export { OlympusBondV2ContractPositionBalanceHelper } from './helpers/olympus.bond-v2.contract-position-balance-helper';
+export { OlympusBondContractPositionHelper } from './helpers/olympus.bond.contract-position-helper';
+export { OlympusBridgeTokenHelper } from './helpers/olympus.bridge-token-helper';
+
 export type { OlympusV2BondDepository } from './contracts';
 export type { OlympusGOhmToken } from './contracts';
 export type { OlympusSOhmToken } from './contracts';
---
+++ src/apps/uniswap-v2/index.ts
@@ -0,0 +1,16 @@
+export { UNISWAP_V2_DEFINITION, UniswapV2AppDefinition } from './uniswap-v2.definition';
+export { UniswapV2AppModule } from './uniswap-v2.module';
+export { UniswapV2ContractFactory } from './contracts';
+
+export { UniswapV2OnChainPoolTokenAddressStrategy } from './helpers/uniswap-v2.on-chain.pool-token-address-strategy';
+export { UniswapV2PoolTokenHelper } from './helpers/uniswap-v2.pool.token-helper';
+export { UniswapV2TheGraphPoolTokenAddressStrategy } from './helpers/uniswap-v2.the-graph.pool-token-address-strategy';
+export { UniswapV2TheGraphPoolTokenBalanceHelper } from './helpers/uniswap-v2.the-graph.pool-token-balance-helper';
+export { UniswapV2TheGraphPoolVolumeStrategy } from './helpers/uniswap-v2.the-graph.pool-volume-strategy';
+
+export type { UniswapFactory } from './contracts';
+export type { UniswapPair } from './contracts';
+export type {
+  UniswapV2PoolTokenDataProps,
+  UniswapV2PoolTokenHelperParams,
+} from './helpers/uniswap-v2.pool.token-helper';
