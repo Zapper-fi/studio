895562fe9eb1edc63f359b410dde1a0987705047
Karan Shahani <karan.shahani91@gmail.com>
2022-04-25 11:47:48 -0400

    fix(olympus): Add V1 bond depository contract

--- src/apps/olympus/contracts/ethers/factories/index.ts
+++ src/apps/olympus/contracts/ethers/factories/index.ts
@@ -4,6 +4,7 @@
 export { OlympusGOhmToken__factory } from './OlympusGOhmToken__factory';
 export { OlympusSOhmToken__factory } from './OlympusSOhmToken__factory';
 export { OlympusSOhmV1Token__factory } from './OlympusSOhmV1Token__factory';
+export { OlympusV1BondDepository__factory } from './OlympusV1BondDepository__factory';
 export { OlympusV2BondDepository__factory } from './OlympusV2BondDepository__factory';
 export { OlympusWsOhmV1Token__factory } from './OlympusWsOhmV1Token__factory';
 export { OlympusZapperZap__factory } from './OlympusZapperZap__factory';
