a6d3588b293b36d479214b82e66fe1398942d553
Justin D'Errico <justin.derrico1991@gmail.com>
2022-04-25 11:26:19 -0400

    feat(yearn): Migrate Yearn over

---
+++ src/apps/yearn/ethereum/yearn.balance-fetcher.ts
@@ -0,0 +1,72 @@
+import { Inject } from '@nestjs/common';
+
+import { Register } from '~app-toolkit/decorators';
+import { presentBalanceFetcherResponse } from '~app-toolkit/helpers/presentation/balance-fetcher-response.present';
+import { SynthetixContractFactory, SynthetixRewards } from '~apps/synthetix';
+import { BalanceFetcher } from '~balance/balance-fetcher.interface';
+import { APP_TOOLKIT, IAppToolkit } from '~lib';
+import { Network } from '~types/network.interface';
+
+import { YEARN_DEFINITION } from '../yearn.definition';
+
+@Register.BalanceFetcher(YEARN_DEFINITION.id, Network.ETHEREUM_MAINNET)
+export class EthereumYearnBalanceFetcher implements BalanceFetcher {
+  constructor(
+    @Inject(APP_TOOLKIT) private readonly appToolkit: IAppToolkit,
+    @Inject(SynthetixContractFactory)
+    private readonly synthetixContractFactory: SynthetixContractFactory,
+  ) {}
+
+  private async getYieldTokens(address: string) {
+    return await this.appToolkit.helpers.tokenBalanceHelper.getTokenBalances({
+      network: Network.ETHEREUM_MAINNET,
+      appId: YEARN_DEFINITION.id,
+      groupId: YEARN_DEFINITION.groups.yield.id,
+      address,
+    });
+  }
+
+  private async getVaultTokenBalances(address: string) {
+    return await this.appToolkit.helpers.tokenBalanceHelper.getTokenBalances({
+      network: Network.ETHEREUM_MAINNET,
+      appId: YEARN_DEFINITION.id,
+      groupId: YEARN_DEFINITION.groups.vault.id,
+      address,
+    });
+  }
+
+  private async getGovernanceBalances(address: string) {
+    return this.appToolkit.helpers.singleStakingContractPositionBalanceHelper.getBalances<SynthetixRewards>({
+      address,
+      appId: YEARN_DEFINITION.id,
+      groupId: YEARN_DEFINITION.groups.farm.id,
+      network: Network.ETHEREUM_MAINNET,
+      resolveContract: ({ address, network }) => this.synthetixContractFactory.synthetixRewards({ address, network }),
+      resolveStakedTokenBalance: ({ contract, address, multicall }) => multicall.wrap(contract).balanceOf(address),
+      resolveRewardTokenBalances: ({ contract, address, multicall }) => multicall.wrap(contract).earned(address),
+    });
+  }
+
+  async getBalances(address: string) {
+    const [yieldTokenBalances, vaultTokenBalances, governanceBalances] = await Promise.all([
+      this.getYieldTokens(address),
+      this.getVaultTokenBalances(address),
+      this.getGovernanceBalances(address),
+    ]);
+
+    return presentBalanceFetcherResponse([
+      {
+        label: 'Yield Tokens',
+        assets: yieldTokenBalances,
+      },
+      {
+        label: 'Vaults',
+        assets: vaultTokenBalances,
+      },
+      {
+        label: 'Governance',
+        assets: governanceBalances,
+      },
+    ]);
+  }
+}
