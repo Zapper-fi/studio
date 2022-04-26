afc2dd8dc166f37169b8e0e024f5bf60b7e5489c
William Poulin <williampoulin01@gmail.com>
2022-04-26 09:25:12 -0400

    feat: Implemented Jpeg'd's pools and bonds (#272)
    * implemented contract-positions for jpegd pools and bonds 
    
    * implemented balance-fetching of pools and bonds for jpegd

---
+++ src/apps/jpegd/jpegd.definition.ts
@@ -0,0 +1,29 @@
+import { Register } from '~app-toolkit/decorators';
+import { AppDefinition } from '~app/app.definition';
+import { GroupType, ProtocolAction, ProtocolTag } from '~app/app.interface';
+import { Network } from '~types/network.interface';
+
+export const JPEGD_DEFINITION = {
+  id: 'jpegd',
+  name: 'Jpegd',
+  description: 'JPEGd is the leading NFT lending platform in the decentralized finance space.',
+  url: 'https://jpegd.io/',
+  groups: {
+    pool: { id: 'pool', type: GroupType.POSITION },
+    bond: { id: 'bond', type: GroupType.POSITION },
+  },
+  tags: [ProtocolTag.LENDING],
+  supportedNetworks: {
+    [Network.ETHEREUM_MAINNET]: [ProtocolAction.VIEW],
+  },
+  primaryColor: '#fff',
+};
+
+@Register.AppDefinition(JPEGD_DEFINITION.id)
+export class JpegdAppDefinition extends AppDefinition {
+  constructor() {
+    super(JPEGD_DEFINITION);
+  }
+}
+
+export default JPEGD_DEFINITION;
