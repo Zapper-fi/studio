25634f0c9da3024e00d6f70ffe3a9739f9666569
Xavier Drdak <1198051+xdrdak@users.noreply.github.com>
2022-04-28 18:07:47 -0400

    chore: update studio npm script to target indirection file

--- scripts/run-cli.js
+++ scripts/run-cli.js
@@ -4,7 +4,7 @@ const isWin = process.platform === 'win32';
 const args = process.argv.slice(2).join(' ');
 
 if (isWin) {
-  execSync(`./studio.bat ${args}`, { stdio: 'inherit' });
+  execSync(`studio.bat ${args}`, { stdio: 'inherit' });
 } else {
   execSync(`./studio.sh ${args}`, { stdio: 'inherit' });
 }
