6d0019b9d3a7ed7850d29b4371b1526c8c3df3c7
Justin D'Errico <justin.derrico1991@gmail.com>
2022-04-28 17:06:43 -0400

    feat(pancakeswap): Port pancakeswap into Studio (#305)

---
+++ src/apps/pancakeswap/binance/pancakeswap.auto-cake.contract-position-fetcher.ts
@@ -0,0 +1,54 @@
+import { Inject } from '@nestjs/common';
+import { BigNumber } from 'ethers';
+
+import { Register } from '~app-toolkit/decorators';
+import { RewardRateUnit } from '~app-toolkit/helpers/master-chef/master-chef.contract-position-helper';
+import { APP_TOOLKIT, IAppToolkit } from '~lib';
+import { PositionFetcher } from '~position/position-fetcher.interface';
+import { ContractPosition } from '~position/position.interface';
+import { Network } from '~types/network.interface';
+
+import { PancakeswapCakeChef, PancakeswapContractFactory } from '../contracts';
+import { PANCAKESWAP_DEFINITION } from '../pancakeswap.definition';
+
+const appId = PANCAKESWAP_DEFINITION.id;
+const groupId = PANCAKESWAP_DEFINITION.groups.autoCake.id;
+const network = Network.BINANCE_SMART_CHAIN_MAINNET;
+
+@Register.ContractPositionFetcher({ appId, groupId, network })
+export class BinanceSmartChainPancakeswapAutoCakeContractPositionFetcher implements PositionFetcher<ContractPosition> {
+  constructor(
+    @Inject(APP_TOOLKIT) private readonly appToolkit: IAppToolkit,
+    @Inject(PancakeswapContractFactory)
+    private readonly contractFactory: PancakeswapContractFactory,
+  ) {}
+
+  getPositions() {
+    const chefContract = this.contractFactory.pancakeswapChef({
+      network,
+      address: '0x73feaa1ee314f8c655e354234017be2193c9e24e',
+    });
+    return this.appToolkit.helpers.masterChefContractPositionHelper.getContractPositions<PancakeswapCakeChef>({
+      network,
+      groupId,
+      appId,
+      minimumTvl: 10000,
+      address: '0xa80240eb5d7e05d3f250cf000eec0891d00b51cc',
+      resolveContract: opts => this.contractFactory.pancakeswapCakeChef(opts),
+      resolvePoolLength: async () => BigNumber.from(1),
+      resolveDepositTokenAddress: ({ multicall, contract }) => multicall.wrap(contract).token(),
+      resolveTotalValueLocked: ({ multicall, contract }) => multicall.wrap(contract).balanceOf(),
+      resolveRewardTokenAddresses: ({ multicall, contract }) => multicall.wrap(contract).token(),
+      rewardRateUnit: RewardRateUnit.BLOCK,
+      resolveRewardRate: this.appToolkit.helpers.masterChefDefaultRewardsPerBlockStrategy.build({
+        resolvePoolAllocPoints: ({ multicall }) =>
+          multicall
+            .wrap(chefContract)
+            .poolInfo(0)
+            .then(i => i.allocPoint),
+        resolveTotalAllocPoints: ({ multicall }) => multicall.wrap(chefContract).totalAllocPoint(),
+        resolveTotalRewardRate: ({ multicall }) => multicall.wrap(chefContract).cakePerBlock(),
+      }),
+    });
+  }
+}
