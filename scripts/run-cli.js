9930852c719355f84e0a6864db9726b93f4dafa8
Xavier Drdak <1198051+xdrdak@users.noreply.github.com>
2022-04-28 18:03:33 -0400

    chore: small indirection to exec studio cli universally

---
+++ scripts/run-cli.js
@@ -0,0 +1,10 @@
+const { execSync } = require('child_process');
+
+const isWin = process.platform === 'win32';
+const args = process.argv.slice(2).join(' ');
+
+if (isWin) {
+  execSync(`./studio.bat ${args}`, { stdio: 'inherit' });
+} else {
+  execSync(`./studio.sh ${args}`, { stdio: 'inherit' });
+}
