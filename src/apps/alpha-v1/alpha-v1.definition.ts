6c49f3448950e87f01e96cfdc9cc5b80d4c2c106
William Poulin <williampoulin01@gmail.com>
2022-04-29 15:38:43 -0400

    added links to alpha-v1 definition

--- src/apps/alpha-v1/alpha-v1.definition.ts
+++ src/apps/alpha-v1/alpha-v1.definition.ts
@@ -11,6 +11,11 @@ export const ALPHA_V1_DEFINITION = {
     lending: { id: 'lending', type: GroupType.TOKEN },
   },
   url: 'https://homora.alphafinance.io/',
+  links: {
+    twitter: 'https://twitter.com/alphafinancelab',
+    discord: 'https://discord.com/invite/2My6wKt',
+    telegram: 'https://t.me/AlphaFinanceLab',
+  },
   tags: [ProtocolTag.LENDING],
   supportedNetworks: {
     [Network.ETHEREUM_MAINNET]: [ProtocolAction.VIEW],
