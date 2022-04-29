167d5fcf2a0a317226231e16730cef2c161dde34
farhaan-ali <farhaan@gro.xyz>
2022-04-29 17:00:34 +0100

    [feat] FA - 29.04.22 - Balance Fetcher for AVAX and ETH

--- src/apps/gro/avalanche/gro.balance-fetcher.ts
+++ src/apps/gro/avalanche/gro.balance-fetcher.ts
@@ -14,7 +14,22 @@ const network = Network.AVALANCHE_MAINNET;
 export class AvalancheGroBalanceFetcher implements BalanceFetcher {
   constructor(@Inject(APP_TOOLKIT) private readonly appToolkit: IAppToolkit) {}
 
+  async getLabsTokenBalances(address: string) {
+    return this.appToolkit.helpers.tokenBalanceHelper.getTokenBalances({
+      address,
+      appId: GRO_DEFINITION.id,
+      groupId: GRO_DEFINITION.groups.labs.id,
+      network: Network.AVALANCHE_MAINNET,
+    });
+  }
+
   async getBalances(address: string) {
-    return presentBalanceFetcherResponse([]);
+    const [labsTokenBalances] = await Promise.all([this.getLabsTokenBalances(address)]);
+    return presentBalanceFetcherResponse([
+      {
+        label: 'Labs',
+        assets: labsTokenBalances,
+      },
+    ]);
   }
 }
--- src/apps/gro/ethereum/gro.balance-fetcher.ts
+++ src/apps/gro/ethereum/gro.balance-fetcher.ts
@@ -6,15 +6,50 @@ import { presentBalanceFetcherResponse } from '~app-toolkit/helpers/presentation
 import { BalanceFetcher } from '~balance/balance-fetcher.interface';
 import { Network } from '~types/network.interface';
 
+import { GroContractFactory, GroLpTokenStaker } from '../contracts';
 import { GRO_DEFINITION } from '../gro.definition';
 
 const network = Network.ETHEREUM_MAINNET;
 
 @Register.BalanceFetcher(GRO_DEFINITION.id, network)
 export class EthereumGroBalanceFetcher implements BalanceFetcher {
-  constructor(@Inject(APP_TOOLKIT) private readonly appToolkit: IAppToolkit) {}
+  constructor(
+    @Inject(APP_TOOLKIT) private readonly appToolkit: IAppToolkit,
+    @Inject(GroContractFactory) private readonly groContractFactory: GroContractFactory,
+  ) {}
+
+  private async getPoolsBalances(address: string) {
+    return this.appToolkit.helpers.masterChefContractPositionBalanceHelper.getBalances<GroLpTokenStaker>({
+      address,
+      appId: GRO_DEFINITION.id,
+      groupId: GRO_DEFINITION.groups.pools.id,
+      network,
+      resolveChefContract: ({ contractAddress }) =>
+        this.groContractFactory.groLpTokenStaker({ network, address: contractAddress }),
+      resolveStakedTokenBalance: this.appToolkit.helpers.masterChefDefaultStakedBalanceStrategy.build({
+        resolveStakedBalance: ({ contract, multicall, contractPosition }) =>
+          multicall
+            .wrap(contract)
+            .userInfo(contractPosition.dataProps.poolIndex, address)
+            .then(v => v.amount),
+      }),
+      resolveClaimableTokenBalances: this.appToolkit.helpers.masterChefDefaultClaimableBalanceStrategy.build({
+        resolveClaimableBalance: ({ multicall, contract, contractPosition, address }) =>
+          multicall
+            .wrap(contract)
+            .userInfo(contractPosition.dataProps.poolIndex, address)
+            .then(v => v.rewardDebt),
+      }),
+    });
+  }
 
   async getBalances(address: string) {
-    return presentBalanceFetcherResponse([]);
+    const [poolsBalances] = await Promise.all([this.getPoolsBalances(address)]);
+    return presentBalanceFetcherResponse([
+      {
+        label: 'Pools',
+        assets: poolsBalances,
+      },
+    ]);
   }
 }
