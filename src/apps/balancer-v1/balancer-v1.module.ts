e59c1942e1fc60b40d01026e126e49d4f5f409a8
William Poulin <williampoulin01@gmail.com>
2022-04-29 12:14:25 -0400

    fix: updated app module decorator of balancer-v1

--- src/apps/balancer-v1/balancer-v1.module.ts
+++ src/apps/balancer-v1/balancer-v1.module.ts
@@ -1,12 +1,12 @@
-import { Module } from '@nestjs/common';
-
+import { Register } from '~app-toolkit/decorators';
 import { AbstractApp } from '~app/app.dynamic-module';
 
-import { BalancerV1AppDefinition } from './balancer-v1.definition';
+import BALANCER_V1_DEFINITION, { BalancerV1AppDefinition } from './balancer-v1.definition';
 import { EthereumBalancerV1BalanceFetcher } from './ethereum/balancer-v1.balance-fetcher';
 import { EthereumBalancerV1PoolTokenFetcher } from './ethereum/balancer-v1.token-fetcher';
 
-@Module({
+@Register.AppModule({
+  appId: BALANCER_V1_DEFINITION.id,
   providers: [BalancerV1AppDefinition, EthereumBalancerV1PoolTokenFetcher, EthereumBalancerV1BalanceFetcher],
 })
 export class BalancerV1AppModule extends AbstractApp() {}
