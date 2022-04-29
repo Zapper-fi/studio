ef15509c98898559fa17a5bfb9c96367f2563906
William Poulin <williampoulin01@gmail.com>
2022-04-29 13:43:09 -0400

    feat: migrate balancer-v1 to studio (#236)
    * feat: migrate balancer-v1 to studio
    
    * feat: add logo balancer-v1
    
    * fix: updated export class balancerV1AppModule
    
    * fix: updated app module decorator of balancer-v1
    
    * added links in balancer-v1 definition

---
+++ src/apps/balancer-v1/balancer-v1.definition.ts
@@ -0,0 +1,39 @@
+import { Register } from '~app-toolkit/decorators';
+import { AppDefinition } from '~app/app.definition';
+import { GroupType, ProtocolAction, ProtocolTag } from '~app/app.interface';
+import { Network } from '~types/network.interface';
+
+export const BALANCER_V1_DEFINITION = {
+  id: 'balancer-v1',
+  name: 'Balancer V1',
+  description:
+    'Balancer is a flexible and versatile Automated Market Maker, giving developers customizability over the weights of the underlying tokens in AMM pools. This project is deprecated in favour of V2.',
+  url: 'https://pools.balancer.exchange/',
+  links: {
+    github: 'https://github.com/balancer-labs/',
+    twitter: 'https://twitter.com/BalancerLabs',
+    discord: 'https://discord.balancer.fi/',
+    medium: 'https://medium.com/balancer-protocol',
+  },
+  groups: {
+    pool: { id: 'pool', type: GroupType.TOKEN },
+  },
+  tags: [ProtocolTag.LIQUIDITY_POOL],
+  supportedNetworks: {
+    [Network.ETHEREUM_MAINNET]: [ProtocolAction.VIEW],
+  },
+  token: {
+    address: '0xba100000625a3754423978a60c9317c58a424e3d',
+    network: Network.ETHEREUM_MAINNET,
+  },
+  primaryColor: '#1c1d26',
+};
+
+@Register.AppDefinition(BALANCER_V1_DEFINITION.id)
+export class BalancerV1AppDefinition extends AppDefinition {
+  constructor() {
+    super(BALANCER_V1_DEFINITION);
+  }
+}
+
+export default BALANCER_V1_DEFINITION;
