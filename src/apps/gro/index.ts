833e81d008b0e012408518a6e573b3bbd93a5777
immasandwich <karan.shahani91@gmail.com>
2022-05-08 11:19:39 -0400

    fix(gro): Add logo and index (#396)

---
+++ src/apps/gro/index.ts
@@ -0,0 +1,9 @@
+export { GRO_DEFINITION, GroAppDefinition } from './gro.definition';
+export { GroAppModule } from './gro.module';
+export { GroContractFactory } from './contracts';
+
+/* Contracts */
+export type { GroDistributor } from './contracts';
+export type { GroLabsVault } from './contracts';
+export type { GroLpTokenStaker } from './contracts';
+export type { GroVesting } from './contracts';
