455b395a15a3b4c03bd9a3a32c2eafa0277eb4fd
Karan Shahani <karan.shahani91@gmail.com>
2022-05-09 10:55:37 -0400

    fix(multicall): Resolve type errors

--- src/apps/keeper-dao/contracts/ethers/factories/KeeperDaoLiquidityPoolDistributor__factory.ts
+++ src/apps/keeper-dao/contracts/ethers/factories/KeeperDaoLiquidityPoolDistributor__factory.ts
@@ -2,16 +2,12 @@
 /* tslint:disable */
 /* eslint-disable */
 
-import { Contract, Signer } from 'ethers';
-import { Provider } from '@ethersproject/providers';
-
-import type { KeeperDaoLiquidityPoolDistributor } from '../KeeperDaoLiquidityPoolDistributor';
-
-export class KeeperDaoLiquidityPoolDistributor__factory {
-  static connect(address: string, signerOrProvider: Signer | Provider): KeeperDaoLiquidityPoolDistributor {
-    return new Contract(address, _abi, signerOrProvider) as KeeperDaoLiquidityPoolDistributor;
-  }
-}
+import { Contract, Signer, utils } from 'ethers';
+import type { Provider } from '@ethersproject/providers';
+import type {
+  KeeperDaoLiquidityPoolDistributor,
+  KeeperDaoLiquidityPoolDistributorInterface,
+} from '../KeeperDaoLiquidityPoolDistributor';
 
 const _abi = [
   {
@@ -255,3 +251,13 @@ const _abi = [
     type: 'function',
   },
 ];
+
+export class KeeperDaoLiquidityPoolDistributor__factory {
+  static readonly abi = _abi;
+  static createInterface(): KeeperDaoLiquidityPoolDistributorInterface {
+    return new utils.Interface(_abi) as KeeperDaoLiquidityPoolDistributorInterface;
+  }
+  static connect(address: string, signerOrProvider: Signer | Provider): KeeperDaoLiquidityPoolDistributor {
+    return new Contract(address, _abi, signerOrProvider) as KeeperDaoLiquidityPoolDistributor;
+  }
+}
