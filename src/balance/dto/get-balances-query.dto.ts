675025df954e5a6c459ade252912afd87226a5e3
Karan Shahani <karan.shahani91@gmail.com>
2022-05-12 12:30:49 -0400

    fix(swagger): Fix addresses query parameter in Swagger

--- src/balance/dto/get-balances-query.dto.ts
+++ src/balance/dto/get-balances-query.dto.ts
@@ -4,13 +4,11 @@ import { ArrayMinSize, IsEnum, IsString } from 'class-validator';
 import { Network } from '~types/network.interface';
 
 export class GetBalancesQuery {
-  @ApiProperty({
-    enum: Network,
-  })
+  @ApiProperty({ description: 'Network for which to retrieve balances', enum: Network, required: false })
   @IsEnum(Network)
   network: Network = Network.ETHEREUM_MAINNET;
 
-  @ApiProperty()
+  @ApiProperty({ description: 'Addresses for which to retrieve balances', type: [String], name: 'addresses[]' })
   @IsString({ each: true })
   @ArrayMinSize(1)
   addresses: string[];
