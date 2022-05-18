269f721340090519cccd3e70feea5ba8577f0851
unknown <mingji1116@live.com>
2022-05-18 16:41:28 -0400

    fix: add token image from underlying token

--- src/apps/sturdy/fantom/sturdy.lending.token-fetcher.ts
+++ src/apps/sturdy/fantom/sturdy.lending.token-fetcher.ts
@@ -4,6 +4,7 @@ import axios from 'axios';
 import { IAppToolkit, APP_TOOLKIT } from '~app-toolkit/app-toolkit.interface';
 import { ZERO_ADDRESS } from '~app-toolkit/constants/address';
 import { Register } from '~app-toolkit/decorators';
+import { getImagesFromToken } from '~app-toolkit/helpers/presentation/image.present';
 import { ContractType } from '~position/contract.interface';
 import { PositionFetcher } from '~position/position-fetcher.interface';
 import { AppTokenPosition } from '~position/position.interface';
@@ -78,7 +79,7 @@ export class FantomSturdyLendingTokenFetcher implements PositionFetcher<AppToken
         },
         displayProps: {
           label: symbol,
-          images: []
+          images: getImagesFromToken(underlyingTokens[0])
         }
       };
       return token;
