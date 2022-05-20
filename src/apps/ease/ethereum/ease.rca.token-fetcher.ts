76c5da2383d8250ebad497db9a2e3f89f9888715
dominikpre <dominik.pre@gmail.com>
2022-05-20 13:06:28 +0200

    fix(ease) reimplement token-fetcher according to recipe

--- src/apps/ease/ethereum/ease.rca.token-fetcher.ts
+++ src/apps/ease/ethereum/ease.rca.token-fetcher.ts
@@ -1,122 +1,67 @@
 import { Inject } from '@nestjs/common';
 import Axios from 'axios';
-import _ from 'lodash';
+import _, { add } from 'lodash';
 
 import { IAppToolkit, APP_TOOLKIT } from '~app-toolkit/app-toolkit.interface';
 import { Register } from '~app-toolkit/decorators';
 import { PositionFetcher } from '~position/position-fetcher.interface';
 import { AppTokenPosition } from '~position/position.interface';
 import { Network } from '~types/network.interface';
-import { ContractType } from '~position/contract.interface';
-import { getLabelFromToken, getTokenImg } from '~app-toolkit/helpers/presentation/image.present';
+import { YEARN_DEFINITION } from '~apps/yearn/yearn.definition';
 
-import { EaseContractFactory } from '../contracts';
+import { EaseContractFactory, EaseRcaShield } from '../contracts';
+import { EaseRcaShieldOnsen, EaseRcaShield__factory } from '../contracts/ethers';
 import { EASE_DEFINITION } from '../ease.definition';
-
+import { getLabelFromToken } from '~app-toolkit/helpers/presentation/image.present';
 
 const appId = EASE_DEFINITION.id;
 const groupId = EASE_DEFINITION.groups.rca.id;
 const network = Network.ETHEREUM_MAINNET;
 
 export type EaseRcaVaultDetails = {
-	symbol: string;
-	name: string;
-	address: string;
-	apy: number;
+  symbol: string;
+  name: string;
+  address: string;
+  apy: number;
+  token: [];
 };
 
 @Register.TokenPositionFetcher({ appId, groupId, network })
 export class EthereumEaseRcaTokenFetcher implements PositionFetcher<AppTokenPosition> {
-	constructor(
-		@Inject(APP_TOOLKIT) private readonly appToolkit: IAppToolkit,
-		@Inject(EaseContractFactory) private readonly easeContractFactory: EaseContractFactory,
-	) { }
+  constructor(
+    @Inject(APP_TOOLKIT) private readonly appToolkit: IAppToolkit,
+    @Inject(EaseContractFactory) private readonly easeContractFactory: EaseContractFactory,
+  ) { }
 
-	async getPositions() {
+  async getPositions() {
+    const endpoint = 'https://app.ease.org/api/v1/vaults';
+    const ethData = await Axios.get<EaseRcaVaultDetails[]>(endpoint).then(v => v.data);
+    const rcaAddressToDetails = _.keyBy(ethData, v => v.address.toLowerCase());
 
-		const endpoint = 'https://app.ease.org/api/v1/vaults';
-		const ethData = await Axios.get<EaseRcaVaultDetails[]>(endpoint).then(v => v.data);
-		// const ethData = data.filter(({ network }) => network === 'eth');
-		const addresses = ethData.map(({ address }) => address.toLowerCase());
-		const rcaAddressToDetails = _.keyBy(ethData, v => v.address.toLowerCase());
-
-		const multicall = this.appToolkit.getMulticall(network);
-
-
-		const baseTokenDependencies = await this.appToolkit.getBaseTokenPrices(network);
-
-		const appTokenDependencies = await this.appToolkit.getAppTokenPositions(
-			//TODO: auto insert appIds for new protocols
-			{ appId: 'sushiswap', groupIds: ['pool'], network },
-			{ appId: 'convex', groupIds: ['deposit'], network },
-			{ appId: 'yearn', groupIds: ['vault'], network },
-			{ appId: 'aave-v2', groupIds: ['supply'], network },
-			{ appId: 'compound', groupIds: ['supply'], network },
-		)
-		// console.log(appTokenDependencies);
-
-		const allTokenDependencies = [...appTokenDependencies, ...baseTokenDependencies];
-		// console.log(allTokenDependencies);
-
-		const tokens = await Promise.all(
-			addresses.map(async address => {
-				//TODO: Compound, Sushi etc have their own variation of RcaShieldContracts. Is this important for the zapper integration?
-				const contract = this.easeContractFactory.easeRcaShield({ address: address, network });
-
-				const [symbol, decimals, supplyRaw] = await Promise.all([
-					multicall.wrap(contract).symbol(),
-					multicall.wrap(contract).decimals(),
-					multicall.wrap(contract).totalSupply(),
-				]);
-
-				const supply = Number(supplyRaw) / 10 ** decimals;
-
-
-				const [underlyingTokenAddressRaw, ratioRaw] = await Promise.all([
-					multicall.wrap(contract).uToken().catch(() => ''),
-					1
-				]);
-
-				const underlyingTokenAddress = underlyingTokenAddressRaw.toLowerCase();
-				const underlyingToken = allTokenDependencies.find(v => v.address === underlyingTokenAddress)
-				if (!underlyingToken) return null;
-				const tokens = [underlyingToken]
-
-				//TODO: is pricePerShare = underlyingToken.price? Maybe not, because we gat rounding issues and deposits gave different amounts back
-				const pricePerShare = underlyingToken.price;
-				const price = underlyingToken.price
-
-				// console.log(underlyingToken);
-				// console.log(price);
-				// console.log(pricePerShare);
-
-				const label = `ez-${getLabelFromToken(underlyingToken)}`;
-
-				// Create the token object
-				const token: AppTokenPosition = {
-					type: ContractType.APP_TOKEN,
-					appId,
-					groupId,
-					address: address,
-					network,
-					symbol,
-					decimals,
-					supply,
-					pricePerShare,
-					price,
-					tokens,
-					dataProps: {
-						//TODO: APY, TVL etc. ask team what else is needed
-					},
-					displayProps: {
-						label: label,
-						images: [getTokenImg(address, network)]
-					}
-				};
-				return token;
-			}),
-		);
-
-		return _.compact(tokens);
-	}
+    //TODO: Is there the need to implement TokenPositionFetcher for all Shields? (RCAShieldNormalized / RcaShield / RcaShieldBase)
+    return this.appToolkit.helpers.vaultTokenHelper.getTokens<EaseRcaShield>({
+      appId: EASE_DEFINITION.id,
+      groupId: EASE_DEFINITION.groups.rca.id,
+      network: Network.ETHEREUM_MAINNET,
+      dependencies: [
+        { appId: YEARN_DEFINITION.id, groupIds: [YEARN_DEFINITION.groups.vault.id], network },
+        //TODO: migrate
+        { appId: 'sushiswap', groupIds: ['pool'], network },
+        { appId: 'convex', groupIds: ['deposit'], network },
+        { appId: 'aave-v2', groupIds: ['supply'], network },
+        { appId: 'compound', groupIds: ['supply'], network },
+      ],
+      //TODO: maybe easeRcaShieldNormalized? Or for every token own contract?
+      resolveContract: ({ address, network }) => this.easeContractFactory.easeRcaShield({ address, network }),
+      resolveVaultAddresses: async () => ethData.map(({ address }) => address.toLowerCase()),
+      resolveUnderlyingTokenAddress: ({ multicall, contract }) => multicall.wrap(contract).uToken().catch(() => ''),
+      resolveReserve: async ({ underlyingToken, multicall, address }) => multicall
+        .wrap(this.appToolkit.globalContracts.erc20(underlyingToken))
+        .balanceOf(address)
+        .then(v => Number(v) / 10 ** underlyingToken.decimals),
+      resolvePricePerShare: () => 1,
+      resolveApy: async ({ vaultAddress }) => await (rcaAddressToDetails[vaultAddress]?.token['apy'] ?? 0) / 100,
+      // resolvePrimaryLabel
+    });
+  }
 }
