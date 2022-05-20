ec02c965f12e86b5570396f34b6c474a81eb65d9
dominikpre <dominik.pre@gmail.com>
2022-05-20 13:20:42 +0200

    feat(ease) implement tvl-fetcher

--- src/apps/ease/ease.module.ts
+++ src/apps/ease/ease.module.ts
@@ -2,19 +2,19 @@ import { Register } from '~app-toolkit/decorators';
 import { AbstractApp } from '~app/app.dynamic-module';
 
 import { EaseContractFactory } from './contracts';
-import { EaseAppDefinition } from './ease.definition';
+import { EaseAppDefinition, EASE_DEFINITION } from './ease.definition';
 import { EthereumEaseBalanceFetcher } from './ethereum/ease.balance-fetcher';
 import { EthereumEaseRcaTokenFetcher } from './ethereum/ease.rca.token-fetcher';
-import { EthereumEaseRcaTokenFetcher } from './ethereum/ease.rca.token-fetcher';
+import { EthereumEaseTvlFetcher } from './ethereum/ease.tvl-fetcher';
 
 @Register.AppModule({
-  appId: 'ease',
+  appId: EASE_DEFINITION.id,
   providers: [
     EaseAppDefinition,
     EaseContractFactory,
     EthereumEaseBalanceFetcher,
     EthereumEaseRcaTokenFetcher,
-    EthereumEaseRcaTokenFetcher,
+    EthereumEaseTvlFetcher,
   ],
 })
 export class EaseAppModule extends AbstractApp() {}
