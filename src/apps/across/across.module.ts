6912e3bf2c38a5511b1fb5182ee2f0e4a096142e
Karan Shahani <karan.shahani91@gmail.com>
2022-05-20 08:20:05 -0400

    fix(across): Fix Across balance fetcher and token fetcher

--- src/apps/across/across.module.ts
+++ src/apps/across/across.module.ts
@@ -3,10 +3,18 @@ import { AbstractApp } from '~app/app.dynamic-module';
 
 import { AcrossAppDefinition, ACROSS_DEFINITION } from './across.definition';
 import { AcrossContractFactory } from './contracts';
+import { EthereumAcrossBalanceFetcher } from './ethereum/across.pool.balance-fetcher';
+import { EthereumAcrossPoolTokenFetcher } from './ethereum/across.pool.token-fetcher';
 import { EthereumAcrossTvlFetcher } from './ethereum/across.tvl-fetcher';
 
 @Register.AppModule({
   appId: ACROSS_DEFINITION.id,
-  providers: [AcrossAppDefinition, AcrossContractFactory, EthereumAcrossTvlFetcher],
+  providers: [
+    AcrossAppDefinition,
+    AcrossContractFactory,
+    EthereumAcrossTvlFetcher,
+    EthereumAcrossBalanceFetcher,
+    EthereumAcrossPoolTokenFetcher,
+  ],
 })
 export class AcrossAppModule extends AbstractApp() {}
