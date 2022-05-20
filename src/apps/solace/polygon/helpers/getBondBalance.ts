738691c7ece480dc438067baa6d3ef8917362fbd
Karan Shahani <karan.shahani91@gmail.com>
2022-05-20 09:19:07 -0400

    fix(solace): Adjust Solace implementation

--- src/apps/solace/polygon/helpers/getBondBalance.ts
+++ src/apps/solace/polygon/helpers/getBondBalance.ts
@@ -1,48 +1,49 @@
-import { SolaceContractFactory } from '../../contracts';
-import { Network } from '~types/network.interface';
-import { SOLACE_DEFINITION } from '../../solace.definition';
+import { ethers } from 'ethers';
+import { range } from 'lodash';
+
 import { drillBalance } from '~app-toolkit';
 import { IAppToolkit } from '~app-toolkit/app-toolkit.interface';
-import { Token } from '~position/position.interface';
-import { WithMetaType } from '~position/display.interface';
+import { MetaType } from '~position/position.interface';
+import { Network } from '~types/network.interface';
 
-import { ethers } from 'ethers';
+import { SolaceContractFactory } from '../../contracts';
+import { SOLACE_DEFINITION } from '../../solace.definition';
+
 const BN = ethers.BigNumber;
-import { range } from '~apps/solace/utils';
 
-export default async function getBondBalance(address: string, appToolkit: IAppToolkit, solaceContractFactory: SolaceContractFactory) {
+export default async function getBondBalance(
+  address: string,
+  appToolkit: IAppToolkit,
+  solaceContractFactory: SolaceContractFactory,
+) {
   const network = Network.POLYGON_MAINNET;
   return appToolkit.helpers.contractPositionBalanceHelper.getContractPositionBalances({
-      address,
-      appId: SOLACE_DEFINITION.id,
-      groupId: SOLACE_DEFINITION.groups.bonds.id,
-      network,
-      resolveBalances: async ({ address, contractPosition, multicall }) => {
+    address,
+    appId: SOLACE_DEFINITION.id,
+    groupId: SOLACE_DEFINITION.groups.bonds.id,
+    network,
+    resolveBalances: async ({ address, contractPosition, multicall }) => {
+      // Resolve the staked token and reward token from the contract position object
+      const stakedToken = contractPosition.tokens.find(t => t.metaType === MetaType.SUPPLIED)!;
+      const rewardToken = contractPosition.tokens.find(t => t.metaType === MetaType.CLAIMABLE)!;
 
-        // Resolve the staked token and reward token from the contract position object
-        const stakedToken = contractPosition.tokens.find((t:WithMetaType<Token>) => t.metaType === 'supplied')!;
-        const rewardToken = contractPosition.tokens.find((t:WithMetaType<Token>) => t.metaType === 'claimable')!;
+      const teller = solaceContractFactory.bondTellerErc20({ address: contractPosition.address, network });
 
-        const teller = solaceContractFactory.bondTellerErc20({ address: contractPosition.address, network });
+      const mct = multicall.wrap(teller);
 
-        const mct = multicall.wrap(teller);
+      const balance = await mct.balanceOf(address);
+      const indices = range(0, balance.toNumber());
+      const tokenIDs = await Promise.all(indices.map((i: number) => mct.tokenOfOwnerByIndex(address, i)));
+      const bonds = await Promise.all(tokenIDs.map(id => mct.bonds(id)));
 
-        const balance = await mct.balanceOf(address);
-        const indices = range(0, balance.toNumber());
-        const tokenIDs = await Promise.all(indices.map((i:number) => mct.tokenOfOwnerByIndex(address, i)));
-        const bonds = await Promise.all(tokenIDs.map(id => mct.bonds(id)));
+      let supplySum = BN.from(0);
+      let rewardSum = BN.from(0);
+      indices.forEach((i: number) => {
+        supplySum = supplySum.add(bonds[i].principalPaid);
+        rewardSum = rewardSum.add(bonds[i].payoutAmount.sub(bonds[i].payoutAlreadyClaimed));
+      });
 
-        let supplySum = BN.from(0);
-        let rewardSum = BN.from(0);
-        indices.forEach((i:number) => {
-          supplySum = supplySum.add(bonds[i].principalPaid);
-          rewardSum = rewardSum.add(bonds[i].payoutAmount.sub(bonds[i].payoutAlreadyClaimed));
-        });
-
-        return [
-          drillBalance(stakedToken, supplySum.toString()),
-          drillBalance(rewardToken, rewardSum.toString()),
-        ];
-      },
-    });
+      return [drillBalance(stakedToken, supplySum.toString()), drillBalance(rewardToken, rewardSum.toString())];
+    },
+  });
 }
--- src/apps/solace/polygon/solace.xslocker.contract-position-fetcher.ts
+++ src/apps/solace/polygon/solace.xslocker.contract-position-fetcher.ts
@@ -2,44 +2,45 @@ import { Inject } from '@nestjs/common';
 
 import { IAppToolkit, APP_TOOLKIT } from '~app-toolkit/app-toolkit.interface';
 import { Register } from '~app-toolkit/decorators';
+import { getImagesFromToken } from '~app-toolkit/helpers/presentation/image.present';
+import { ContractType } from '~position/contract.interface';
 import { PositionFetcher } from '~position/position-fetcher.interface';
 import { ContractPosition } from '~position/position.interface';
-import { ContractType } from '~position/contract.interface';
 import { claimable, supplied } from '~position/position.utils';
 import { Network } from '~types/network.interface';
-import { Token } from '~position/position.interface';
-import { WithMetaType } from '~position/display.interface';
 
-import { SolaceContractFactory } from '../contracts';
 import { SOLACE_DEFINITION } from '../solace.definition';
 
 const appId = SOLACE_DEFINITION.id;
 const groupId = SOLACE_DEFINITION.groups.xslocker.id;
 const network = Network.POLYGON_MAINNET;
 
-const SOLACE_ADDRESS          = "0x501ace9c35e60f03a2af4d484f49f9b1efde9f40";
-const XSLOCKER_ADDRESS        = "0x501ace47c5b0c2099c4464f681c3fa2ecd3146c1";
+const SOLACE_ADDRESS = '0x501ace9c35e60f03a2af4d484f49f9b1efde9f40';
+const XSLOCKER_ADDRESS = '0x501ace47c5b0c2099c4464f681c3fa2ecd3146c1';
 
 @Register.ContractPositionFetcher({ appId, groupId, network })
 export class PolygonSolaceXslockerContractPositionFetcher implements PositionFetcher<ContractPosition> {
-  constructor(
-    @Inject(APP_TOOLKIT) private readonly appToolkit: IAppToolkit,
-    @Inject(SolaceContractFactory) private readonly solaceContractFactory: SolaceContractFactory,
-  ) {}
+  constructor(@Inject(APP_TOOLKIT) private readonly appToolkit: IAppToolkit) {}
 
   async getPositions() {
     const baseTokens = await this.appToolkit.getBaseTokenPrices(network);
-    const solace = baseTokens.find((t:WithMetaType<Token>) => t.address === SOLACE_ADDRESS);
-    const tokens = ((!!solace)
-      ? [supplied(solace), claimable(solace)]
-      : []);
-    return [{
+    const solace = baseTokens.find(t => t.address === SOLACE_ADDRESS)!;
+    if (!solace) return [];
+
+    const position: ContractPosition = {
       type: ContractType.POSITION,
+      address: XSLOCKER_ADDRESS,
       appId,
       groupId,
-      address: XSLOCKER_ADDRESS,
       network,
-      tokens
-    }];
+      tokens: [supplied(solace), claimable(solace)],
+      dataProps: {},
+      displayProps: {
+        label: `xsLOCK`,
+        images: getImagesFromToken(solace),
+      },
+    };
+
+    return [position];
   }
 }
