1b55ec8e15e5737bb83a7742e1a3a676fe5ef11d
liuyepiaoxiang <liujia19891217@126.com>
2022-05-20 20:02:59 +0800

    feat(across): Add across protocol on Ethereum mainnet (#395)

---
+++ src/apps/across/across.module.ts
@@ -0,0 +1,19 @@
+import { Register } from '~app-toolkit/decorators';
+import { AbstractApp } from '~app/app.dynamic-module';
+
+import { AcrossAppDefinition, ACROSS_DEFINITION } from './across.definition';
+
+import { AcrossContractFactory } from './contracts';
+
+import { EthereumAcrossTvlFetcher } from './ethereum/across.tvl-fetcher';
+
+
+@Register.AppModule({
+  appId: ACROSS_DEFINITION.id,
+  providers: [
+    AcrossAppDefinition,
+    AcrossContractFactory,
+    EthereumAcrossTvlFetcher,
+  ],
+})
+export class AcrossAppModule extends AbstractApp() { }
---
+++ src/apps/across/ethereum/across.pool.definitions.ts
@@ -0,0 +1,105 @@
+import {AcrossPoolDefinition } from '../across.types';
+
+export const ACROSS_V1_POOL_DEFINITIONS: AcrossPoolDefinition[] = [
+  {
+    queryKey: 'badger',
+    label: 'Across Badger LP',
+    poolAddress: '0x43298f9f91a4545df64748e78a2c777c580573d6',
+    tokenAddress: '0x3472a5a71965499acd81997a54bba8d852c6e53d',
+    isLegacy: true,
+  },
+  {
+    queryKey: 'boba',
+    label: 'Across BOBA LP',
+    poolAddress: '0x4841572daA1f8E4Ce0f62570877c2D0CC18C9535',
+    tokenAddress: '0x42bbfa2e77757c645eeaad1655e0911a7553efbc',
+    isLegacy: true,
+  },
+  {
+    queryKey: 'dai',
+    label: 'Across DAI LP',
+    poolAddress: '0x43f133FE6fDFA17c417695c476447dc2a449Ba5B',
+    tokenAddress: '0x6b175474e89094c44da98b954eedeac495271d0f',
+    isLegacy: true,
+  },
+  {
+    queryKey: 'uma',
+    label: 'Across UMA LP',
+    poolAddress: '0xdfe0ec39291e3b60ACa122908f86809c9eE64E90',
+    tokenAddress: '0x04fa0d235c4abf4bcf4787af4cf447de572ef828',
+    isLegacy: true,
+  },
+  {
+    queryKey: 'usdc',
+    label: 'Across USDC LP',
+    poolAddress: '0x256C8919CE1AB0e33974CF6AA9c71561Ef3017b6',
+    tokenAddress: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+    isLegacy: true,
+  },
+  {
+    queryKey: 'wbtc',
+    label: 'Across WBTC LP',
+    poolAddress: '0x02fbb64517E1c6ED69a6FAa3ABf37Db0482f1152',
+    tokenAddress: '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599',
+    isLegacy: true,
+  },
+  {
+    queryKey: 'weth',
+    label: 'Across WETH LP',
+    poolAddress: '0x7355Efc63Ae731f584380a9838292c7046c1e433',
+    tokenAddress: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+    isLegacy: true,
+  },
+];
+
+export const ACROSS_V2_POOL_DEFINITIONS: AcrossPoolDefinition[] = [
+  {
+    queryKey: 'badger',
+    label: 'Across Badger LP',
+    poolAddress: '0x43298f9f91a4545df64748e78a2c777c580573d6',
+    tokenAddress: '0x3472a5a71965499acd81997a54bba8d852c6e53d',
+    isLegacy: true,
+  },
+  {
+    queryKey: 'boba',
+    label: 'Across BOBA LP',
+    poolAddress: '0x4841572daA1f8E4Ce0f62570877c2D0CC18C9535',
+    tokenAddress: '0x42bbfa2e77757c645eeaad1655e0911a7553efbc',
+    isLegacy: true,
+  },
+  {
+    queryKey: 'dai',
+    label: 'Across DAI LP',
+    poolAddress: '0x43f133FE6fDFA17c417695c476447dc2a449Ba5B',
+    tokenAddress: '0x6b175474e89094c44da98b954eedeac495271d0f',
+    isLegacy: true,
+  },
+  {
+    queryKey: 'uma',
+    label: 'Across UMA LP',
+    poolAddress: '0xdfe0ec39291e3b60ACa122908f86809c9eE64E90',
+    tokenAddress: '0x04fa0d235c4abf4bcf4787af4cf447de572ef828',
+    isLegacy: true,
+  },
+  {
+    queryKey: 'usdc',
+    label: 'Across USDC LP',
+    poolAddress: '0x256C8919CE1AB0e33974CF6AA9c71561Ef3017b6',
+    tokenAddress: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+    isLegacy: true,
+  },
+  {
+    queryKey: 'wbtc',
+    label: 'Across WBTC LP',
+    poolAddress: '0x02fbb64517E1c6ED69a6FAa3ABf37Db0482f1152',
+    tokenAddress: '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599',
+    isLegacy: true,
+  },
+  {
+    queryKey: 'weth',
+    label: 'Across WETH LP',
+    poolAddress: '0x7355Efc63Ae731f584380a9838292c7046c1e433',
+    tokenAddress: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+    isLegacy: true,
+  },
+];
