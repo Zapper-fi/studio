8c47c9318f9141e4e121e5d144d7ca3b1101d1d3
immasandwich <karan.shahani91@gmail.com>
2022-05-21 16:21:56 -0400

    feat(jones-dao): Add staking V2, get staking V1 dynamically (#489)

--- src/apps/jones-dao/arbitrum/jones-dao.farm.contract-position-fetcher.ts
+++ src/apps/jones-dao/arbitrum/jones-dao.farm.contract-position-fetcher.ts
@@ -1,57 +1,15 @@
 import { Inject } from '@nestjs/common';
+import { compact, range } from 'lodash';
 
 import { APP_TOOLKIT, IAppToolkit } from '~app-toolkit/app-toolkit.interface';
 import { Register } from '~app-toolkit/decorators';
-import {
-  SynthetixContractFactory,
-  SynthetixRewards,
-  SynthetixSingleStakingIsActiveStrategy,
-  SynthetixSingleStakingRoiStrategy,
-} from '~apps/synthetix';
+import { SynthetixSingleStakingIsActiveStrategy, SynthetixSingleStakingRoiStrategy } from '~apps/synthetix';
 import { PositionFetcher } from '~position/position-fetcher.interface';
 import { ContractPosition } from '~position/position.interface';
 import { Network } from '~types/network.interface';
 
+import { JonesDaoContractFactory, JonesStakingRewards } from '../contracts';
 import { JONES_DAO_DEFINITION } from '../jones-dao.definition';
-
-const FARMS = [
-  // JONES StakingRewards
-  {
-    address: '0xf1a26Cf6309a59794da29B5b2E6fABD3070d470f',
-    stakedTokenAddress: '0x10393c20975cf177a3513071bc110f7962cd67da',
-    rewardTokenAddresses: ['0x10393c20975cf177a3513071bc110f7962cd67da'],
-  },
-  // JONES-ETH LP StakingRewards
-  {
-    address: '0x360a766f30f0ba57d2865efb32502fb800b14dd3',
-    stakedTokenAddress: '0xe8ee01ae5959d3231506fcdef2d5f3e85987a39c',
-    rewardTokenAddresses: ['0x10393c20975cf177a3513071bc110f7962cd67da'],
-  },
-  // JONES-USDC LP StakingRewards
-  {
-    address: '0x13f6a63867046107780bc3febdee90e7afcdfd99',
-    stakedTokenAddress: '0xa6efc26daa4bb2b9bf5d23a0bc202a2badc2b59e',
-    rewardTokenAddresses: ['0x10393c20975cf177a3513071bc110f7962cd67da'],
-  },
-  // jETH-ETH LP StakingRewards
-  {
-    address: '0xbac58e8b57935a0b60d5cb4cd9f6c21049595f04',
-    stakedTokenAddress: '0xdf1a6dd4e5b77d7f2143ed73074be26c806754c5',
-    rewardTokenAddresses: ['0x10393c20975cf177a3513071bc110f7962cd67da'],
-  },
-  // jgOHM-gOHM LP StakingRewards
-  {
-    address: '0x7ece38dbe9d61d0d9bf2d804a87a7d21b5937a56',
-    stakedTokenAddress: '0x292d1587a6bb37e34574c9ad5993f221d8a5616c',
-    rewardTokenAddresses: ['0x10393c20975cf177a3513071bc110f7962cd67da'],
-  },
-  // jDPX-DPX LP StakingRewards
-  {
-    address: '0x5723be83199c9ec68ed0ac979e98381224870e7f',
-    stakedTokenAddress: '0xeeb24360c8c7a87933d16b0075e10e1a30ad65b7',
-    rewardTokenAddresses: ['0x10393c20975cf177a3513071bc110f7962cd67da'],
-  },
-];
 
 const appId = JONES_DAO_DEFINITION.id;
 const groupId = JONES_DAO_DEFINITION.groups.farm.id;
@@ -61,8 +19,8 @@ const network = Network.ARBITRUM_MAINNET;
 export class ArbitrumJonesDaoFarmContractPositionFetcher implements PositionFetcher<ContractPosition> {
   constructor(
     @Inject(APP_TOOLKIT) private readonly appToolkit: IAppToolkit,
-    @Inject(SynthetixContractFactory)
-    private readonly synthetixContractFactory: SynthetixContractFactory,
+    @Inject(JonesDaoContractFactory)
+    private readonly jonesDaoContractFactory: JonesDaoContractFactory,
     @Inject(SynthetixSingleStakingIsActiveStrategy)
     private readonly synthetixSingleStakingIsActiveStrategy: SynthetixSingleStakingIsActiveStrategy,
     @Inject(SynthetixSingleStakingRoiStrategy)
@@ -70,19 +28,31 @@ export class ArbitrumJonesDaoFarmContractPositionFetcher implements PositionFetc
   ) {}
 
   async getPositions() {
-    return this.appToolkit.helpers.singleStakingFarmContractPositionHelper.getContractPositions<SynthetixRewards>({
+    const multicall = this.appToolkit.getMulticall(network);
+    const factoryAddress = '0x2c2082e4062bfd02141adc86cbd5e437201a1cf3';
+    const factory = this.jonesDaoContractFactory.jonesStakingRewardsFactory({ address: factoryAddress, network });
+    const mcf = multicall.wrap(factory);
+
+    const maybeStakingIds = await Promise.all(range(0, 50).map(i => mcf.stakingID(i).catch(() => null)));
+    const stakingIds = compact(maybeStakingIds).map(v => Number(v));
+    const stakingInfo = await Promise.all(stakingIds.map(i => mcf.stakingRewardsInfoByStakingToken(i)));
+    const stakingAddresses = stakingInfo.map(v => v.stakingRewards);
+
+    return this.appToolkit.helpers.singleStakingFarmContractPositionHelper.getContractPositions<JonesStakingRewards>({
       network,
       appId,
       groupId,
       dependencies: [{ appId: 'sushiswap', groupIds: ['pool'], network }],
-      resolveFarmDefinitions: async () => FARMS,
       resolveFarmContract: ({ network, address }) =>
-        this.synthetixContractFactory.synthetixRewards({ network, address }),
+        this.jonesDaoContractFactory.jonesStakingRewards({ network, address }),
+      resolveFarmAddresses: async () => stakingAddresses,
+      resolveStakedTokenAddress: async ({ multicall, contract }) => multicall.wrap(contract).stakingToken(),
+      resolveRewardTokenAddresses: async () => '0x10393c20975cf177a3513071bc110f7962cd67da',
       resolveIsActive: this.synthetixSingleStakingIsActiveStrategy.build({
         resolvePeriodFinish: ({ contract, multicall }) => multicall.wrap(contract).periodFinish(),
       }),
       resolveRois: this.synthetixSingleStakingRoiStrategy.build({
-        resolveRewardRates: ({ contract, multicall }) => multicall.wrap(contract).rewardRate(),
+        resolveRewardRates: ({ contract, multicall }) => multicall.wrap(contract).rewardRateJONES(),
       }),
     });
   }
--- src/apps/jones-dao/contracts/index.ts
+++ src/apps/jones-dao/contracts/index.ts
@@ -5,6 +5,9 @@ import { ContractFactory } from '~contract/contracts';
 import { Network } from '~types/network.interface';
 
 import { JonesAsset__factory } from './ethers';
+import { JonesMillinerV2__factory } from './ethers';
+import { JonesStakingRewards__factory } from './ethers';
+import { JonesStakingRewardsFactory__factory } from './ethers';
 
 // eslint-disable-next-line
 type ContractOpts = { address: string; network: Network };
@@ -18,6 +21,18 @@ export class JonesDaoContractFactory extends ContractFactory {
   jonesAsset({ address, network }: ContractOpts) {
     return JonesAsset__factory.connect(address, this.appToolkit.getNetworkProvider(network));
   }
+  jonesMillinerV2({ address, network }: ContractOpts) {
+    return JonesMillinerV2__factory.connect(address, this.appToolkit.getNetworkProvider(network));
+  }
+  jonesStakingRewards({ address, network }: ContractOpts) {
+    return JonesStakingRewards__factory.connect(address, this.appToolkit.getNetworkProvider(network));
+  }
+  jonesStakingRewardsFactory({ address, network }: ContractOpts) {
+    return JonesStakingRewardsFactory__factory.connect(address, this.appToolkit.getNetworkProvider(network));
+  }
 }
 
 export type { JonesAsset } from './ethers';
+export type { JonesMillinerV2 } from './ethers';
+export type { JonesStakingRewards } from './ethers';
+export type { JonesStakingRewardsFactory } from './ethers';
