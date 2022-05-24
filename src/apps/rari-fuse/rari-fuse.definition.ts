b2096deb4a8a714c6c9b75f56473fe8cde7572d1
immasandwich <karan.shahani91@gmail.com>
2022-05-23 22:46:00 -0400

    feat(rari-fuse): Move Rari Fuse to Studio (#499)

---
+++ src/apps/rari-fuse/rari-fuse.definition.ts
@@ -0,0 +1,42 @@
+import { Register } from '~app-toolkit/decorators';
+import { appDefinition, AppDefinition } from '~app/app.definition';
+import { AppAction, AppTag, GroupType } from '~app/app.interface';
+import { Network } from '~types/network.interface';
+
+export const RARI_FUSE_DEFINITION = appDefinition({
+  id: 'rari-fuse',
+  name: 'Rari Fuse',
+  description: `Fuse is the first truly open interest rate protocol. Lend, borrow, and create isolated lending markets with unlimited flexibility.`,
+
+  groups: {
+    supply: {
+      id: 'supply',
+      type: GroupType.TOKEN,
+      label: 'Lending',
+    },
+
+    borrow: {
+      id: 'borrow',
+      type: GroupType.POSITION,
+      label: 'Lending',
+    },
+  },
+
+  url: 'https://rari.capital/',
+  tags: [AppTag.LIQUIDITY_POOL],
+  keywords: [],
+  links: {},
+
+  supportedNetworks: {
+    [Network.ETHEREUM_MAINNET]: [AppAction.VIEW],
+  },
+});
+
+@Register.AppDefinition(RARI_FUSE_DEFINITION.id)
+export class RariFuseAppDefinition extends AppDefinition {
+  constructor() {
+    super(RARI_FUSE_DEFINITION);
+  }
+}
+
+export default RARI_FUSE_DEFINITION;
