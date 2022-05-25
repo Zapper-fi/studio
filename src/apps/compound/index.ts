cd9abef729f65ac4f1710e3e5fd28da458f87843
Karan Shahani <karan.shahani91@gmail.com>
2022-05-25 15:41:13 -0400

    fix(compound): Export helpers in Compound index

--- src/apps/compound/index.ts
+++ src/apps/compound/index.ts
@@ -2,6 +2,15 @@ export { COMPOUND_DEFINITION, CompoundAppDefinition } from './compound.definitio
 export { CompoundAppModule } from './compound.module';
 export { CompoundContractFactory } from './contracts';
 
+/* Helpers */
+export { CompoundBorrowBalanceHelper } from './helper/compound.borrow.balance-helper';
+export { CompoundBorrowContractPositionHelper } from './helper/compound.borrow.contract-position-helper';
+export { CompoundClaimableBalanceHelper } from './helper/compound.claimable.balance-helper';
+export { CompoundLendingMetaHelper } from './helper/compound.lending.meta-helper';
+export { CompoundSupplyBalanceHelper } from './helper/compound.supply.balance-helper';
+export { CompoundSupplyTokenHelper } from './helper/compound.supply.token-helper';
+export { CompoundTvlHelper } from './helper/compound.tvl-helper';
+
 /* Contracts */
 export type { CompoundCToken } from './contracts';
 export type { CompoundComptroller } from './contracts';
