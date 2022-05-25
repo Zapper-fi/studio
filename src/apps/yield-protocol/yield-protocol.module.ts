50f082bb9814a46cd218f2c8bc13c8e263340d3e
marcomariscal <marco.a.mariscal@gmail.com>
2022-05-25 17:43:24 -0400

    feat: lend token fetcher module

--- src/apps/yield-protocol/yield-protocol.module.ts
+++ src/apps/yield-protocol/yield-protocol.module.ts
@@ -2,10 +2,11 @@ import { Register } from '~app-toolkit/decorators';
 import { AbstractApp } from '~app/app.dynamic-module';
 
 import { YieldProtocolContractFactory } from './contracts';
+import { EthereumYieldProtocolLendTokenFetcher } from './ethereum/yield-protocol.lend.token-fetcher';
 import { YieldProtocolAppDefinition, YIELD_PROTOCOL_DEFINITION } from './yield-protocol.definition';
 
 @Register.AppModule({
   appId: YIELD_PROTOCOL_DEFINITION.id,
-  providers: [YieldProtocolAppDefinition, YieldProtocolContractFactory],
+  providers: [EthereumYieldProtocolLendTokenFetcher, YieldProtocolAppDefinition, YieldProtocolContractFactory],
 })
 export class YieldProtocolAppModule extends AbstractApp() {}
