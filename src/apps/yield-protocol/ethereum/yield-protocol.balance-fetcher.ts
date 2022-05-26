f44259846f72d8c69780485d04993e8a50228a06
marcomariscal <marco.a.mariscal@gmail.com>
2022-05-26 13:25:59 -0400

    feat: add lend account balance

---
+++ src/apps/yield-protocol/ethereum/yield-protocol.balance-fetcher.ts
@@ -0,0 +1,30 @@
+import { Inject } from '@nestjs/common';
+
+import { IAppToolkit, APP_TOOLKIT } from '~app-toolkit/app-toolkit.interface';
+import { Register } from '~app-toolkit/decorators';
+import { presentBalanceFetcherResponse } from '~app-toolkit/helpers/presentation/balance-fetcher-response.present';
+import { BalanceFetcher } from '~balance/balance-fetcher.interface';
+import { Network } from '~types/network.interface';
+
+import { YIELD_PROTOCOL_DEFINITION } from '../yield-protocol.definition';
+
+const network = Network.ETHEREUM_MAINNET;
+
+@Register.BalanceFetcher(YIELD_PROTOCOL_DEFINITION.id, network)
+export class EthereumYieldProtocolBalanceFetcher implements BalanceFetcher {
+  constructor(@Inject(APP_TOOLKIT) private readonly appToolkit: IAppToolkit) {}
+
+  async getLendBalances(address: string) {
+    return this.appToolkit.helpers.tokenBalanceHelper.getTokenBalances({
+      address,
+      appId: YIELD_PROTOCOL_DEFINITION.id,
+      groupId: YIELD_PROTOCOL_DEFINITION.groups.lend.id,
+      network: Network.ETHEREUM_MAINNET,
+    });
+  }
+
+  async getBalances(address: string) {
+    const [lendBalances] = await Promise.all([this.getLendBalances(address)]);
+    return presentBalanceFetcherResponse([{ label: 'Lend', assets: lendBalances }]);
+  }
+}
