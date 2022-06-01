e62a3379b5c8853ae44c7bdabb052797583deb18
a <a@gmail.com>
2022-06-01 12:13:25 -0700

    Style Fix:Change addresses to lower case

--- src/apps/kinesis-labs/evmos/kinesis-labs.pool.definitions.ts
+++ src/apps/kinesis-labs/evmos/kinesis-labs.pool.definitions.ts
@@ -3,12 +3,12 @@ import { CurvePoolDefinition } from '~apps/curve/curve.types';
 export const KINESIS_LABS_BASEPOOL_DEFINITIONS: CurvePoolDefinition[] = [
   // Nomad Base Pool madUSDC/madUSDT/FRAX
   {
-    swapAddress: '0x49b97224655AaD13832296b8f6185231AFB8aaCc',
-    tokenAddress: '0xfB25679ff0651cde4Cf58887be266B68326dDaB6',
+    swapAddress: '0x49b97224655aad13832296b8f6185231afb8aacc',
+    tokenAddress: '0xfb25679ff0651cde4cf58887be266b68326ddab6',
   },
   // Celer Base Pool celerUSDC/celerUSDT/FRAX
   {
-    swapAddress: '0xbBD5a7AE45a484BD8dAbdfeeeb33E4b859D2c95C',
-    tokenAddress: '0xF6b65B88A9e7846ED0DE503E682cC230F892C2fa',
+    swapAddress: '0xbbd5a7ae45a484bd8dabdfeeeb33e4b859d2c95c',
+    tokenAddress: '0xf6b65b88a9e7846ed0de503e682cc230f892c2fa',
   },
 ];
