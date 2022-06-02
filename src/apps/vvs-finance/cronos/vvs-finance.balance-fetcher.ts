115f61c8ad9bbaca62e095f2451bd7b21d3f2b18
CronosLabsDev <dev@cronoslabs.org>
2022-06-03 01:30:23 +0800

    feat: add vvs-finance app integration
    * pool (token) fetcher and cache manager
    * farm contract position fetcher
    * farmV2 contract position fetcher
    * balance fetcher
    * TVL fetcher

---
+++ src/apps/vvs-finance/cronos/vvs-finance.balance-fetcher.ts
@@ -0,0 +1,108 @@
+import { Inject } from '@nestjs/common';
+
+import { APP_TOOLKIT, IAppToolkit } from '~app-toolkit/app-toolkit.interface';
+import { Register } from '~app-toolkit/decorators';
+import { presentBalanceFetcherResponse } from '~app-toolkit/helpers/presentation/balance-fetcher-response.present';
+import { BalanceFetcher } from '~balance/balance-fetcher.interface';
+import { Network } from '~types/network.interface';
+
+import {
+  VvsFinanceContractFactory,
+  VvsCraftsman,
+  VvsCraftsmanV2,
+} from '../contracts';
+import { VVS_FINANCE_DEFINITION } from '../vvs-finance.definition';
+
+const appId = VVS_FINANCE_DEFINITION.id;
+const network = Network.CRONOS_MAINNET;
+
+@Register.BalanceFetcher(appId, network)
+export class CronosVvsFinanceBalanceFetcher implements BalanceFetcher {
+  constructor(
+    @Inject(APP_TOOLKIT) private readonly appToolkit: IAppToolkit,
+    @Inject(VvsFinanceContractFactory) private readonly contractFactory: VvsFinanceContractFactory,
+  ) {}
+
+  private async getPoolBalances(address: string) {
+    return this.appToolkit.helpers.tokenBalanceHelper.getTokenBalances({
+      address,
+      appId,
+      network,
+      groupId: VVS_FINANCE_DEFINITION.groups.pool.id,
+    });
+  }
+
+  private async getFarmBalances(address: string) {
+    // LP and Manual VVS Farms
+    return this.appToolkit.helpers.masterChefContractPositionBalanceHelper.getBalances<VvsCraftsman>({
+      address,
+      appId,
+      network,
+      groupId: VVS_FINANCE_DEFINITION.groups.farm.id,
+      resolveChefContract: ({ contractAddress }) =>
+      this.contractFactory.vvsCraftsman({ network, address: contractAddress }),
+      resolveStakedTokenBalance: this.appToolkit.helpers.masterChefDefaultStakedBalanceStrategy.build({
+        resolveStakedBalance: async ({ multicall, contract, contractPosition }) => (
+          multicall
+          .wrap(contract)
+          .userInfo(contractPosition.dataProps.poolIndex, address)
+          .then(v => v.amount)
+        ),
+      }),
+      resolveClaimableTokenBalances: this.appToolkit.helpers.masterChefDefaultClaimableBalanceStrategy.build({
+        resolveClaimableBalance: ({ multicall, contract, contractPosition }) =>
+        multicall.wrap(contract).pendingVVS(contractPosition.dataProps.poolIndex, address),
+      }),
+    });
+  }
+
+  private async getFarmV2Balances(address: string) {
+    // LP and Manual VVS Farms
+    return this.appToolkit.helpers.masterChefContractPositionBalanceHelper.getBalances<VvsCraftsmanV2>({
+      address,
+      appId,
+      network,
+      groupId: VVS_FINANCE_DEFINITION.groups.farmV2.id,
+      resolveChefContract: ({ contractAddress }) =>
+        this.contractFactory.vvsCraftsmanV2({ network, address: contractAddress }),
+      resolveStakedTokenBalance: this.appToolkit.helpers.masterChefDefaultStakedBalanceStrategy.build({
+        resolveStakedBalance: ({ multicall, contract, contractPosition }) =>
+          multicall
+            .wrap(contract)
+            .userInfo(contractPosition.dataProps.poolIndex, address)
+            .then(v => v.amount),
+      }),
+      resolveClaimableTokenBalances: this.appToolkit.helpers.masterChefDefaultClaimableBalanceStrategy.build({
+        resolveClaimableBalance: ({ multicall, contract, contractPosition }) =>
+          multicall.wrap(contract).pendingVVS(contractPosition.dataProps.poolIndex, address),
+      }),
+    });
+  }
+
+  async getBalances(address: string) {
+    const [
+      poolBalances,
+      farmBalances,
+      farmV2Balances,
+    ] = await Promise.all([
+      this.getPoolBalances(address),
+      this.getFarmBalances(address),
+      this.getFarmV2Balances(address),
+    ]);
+
+    return presentBalanceFetcherResponse([
+      {
+        label: 'Pools',
+        assets: poolBalances,
+      },
+      {
+        label: 'Farms',
+        assets: farmBalances,
+      },
+      {
+        label: 'FarmsV2',
+        assets: farmV2Balances,
+      },
+    ]);
+  }
+}
---
+++ src/apps/vvs-finance/cronos/vvs-finance.pool.cache-manager.ts
@@ -0,0 +1,148 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { BigNumber } from 'ethers';
+import { compact, range, uniq } from 'lodash';
+
+import { APP_TOOLKIT, IAppToolkit } from '~app-toolkit/app-toolkit.interface';
+import { CacheOnInterval } from '~cache/cache-on-interval.decorator';
+import { Network } from '~types/network.interface';
+
+import { VvsFinanceContractFactory } from '../contracts';
+import { VVS_FINANCE_DEFINITION } from '../vvs-finance.definition';
+
+const network = Network.CRONOS_MAINNET;
+
+@Injectable()
+export class CronosVvsFinancePoolAddressCacheManager {
+  constructor(
+    @Inject(APP_TOOLKIT) private readonly appToolkit: IAppToolkit,
+    @Inject(VvsFinanceContractFactory) protected readonly contractFactory: VvsFinanceContractFactory,
+  ) {}
+
+  @CacheOnInterval({
+    key: `apps-v3:${VVS_FINANCE_DEFINITION.id}:graph-top-pool-addresses`,
+    timeout: 15 * 60 * 1000,
+  })
+  private async getTopPoolAddresses() {
+    return [];
+  }
+
+  @CacheOnInterval({
+    key: `apps-v3:${VVS_FINANCE_DEFINITION.id}:craftsman-pool-addresses`,
+    timeout: 15 * 60 * 1000,
+  })
+  private async getCraftsmanPoolAddresses() {
+    const craftsmanContract = this.contractFactory.vvsCraftsman({
+      address: '0xDccd6455AE04b03d785F12196B492b18129564bc',
+      network,
+    });
+
+    const provider = this.appToolkit.getNetworkProvider(network);
+    const multicall = this.appToolkit.getMulticall(network);
+    const numPools = await multicall.wrap(craftsmanContract).poolLength();
+
+    const allAddresses = await Promise.all(
+      range(0, Number(numPools)).map(async v => {
+        const poolInfo = await multicall.wrap(craftsmanContract).poolInfo(v);
+        const lpTokenAddress = poolInfo.lpToken.toLowerCase();
+        const lpTokenContract = this.contractFactory.vvsPair({ address: lpTokenAddress, network });
+
+        // Some EOAs exist on the MasterChef contract; calling these breaks multicall
+        const code = await provider.getCode(lpTokenAddress);
+        if (code === '0x') return false;
+
+        const symbol = await multicall.wrap(lpTokenContract).symbol().catch(_err => '');
+
+        const isVvsLp = symbol === 'VVS-LP';
+        if (!isVvsLp) return null;
+
+        return lpTokenAddress;
+      }),
+    );
+
+    return compact(allAddresses);
+  }
+
+  @CacheOnInterval({
+    key: `apps-v3:${VVS_FINANCE_DEFINITION.id}:craftsman-v2-pools`,
+    timeout: 15 * 60 * 1000,
+  })
+  async getCraftsmanV2Pools() {
+    const multicall = this.appToolkit.getMulticall(network);
+    const craftsmanV2Contract = multicall.wrap(
+      this.contractFactory.vvsCraftsmanV2({
+        address: '0xbc149c62EFe8AFC61728fC58b1b66a0661712e76',
+        network,
+      })
+    );
+
+    const resolvedPoolIds: BigNumber[] = [];
+    let index = 0;
+    while(true) {
+      try {
+        const poolId = await craftsmanV2Contract.poolIds(index)
+        resolvedPoolIds.push(poolId);
+        index++;
+      } catch (_error) {
+        // index out of range
+        break;
+      }
+    }
+
+    const pools = await Promise.all(resolvedPoolIds.map(async poolId => {
+      const { lpToken, accVVSPerShare } = await craftsmanV2Contract.poolInfo(poolId);
+
+      return {
+        poolId, lpToken, accVVSPerShare,
+      }
+    }));
+
+    return pools;
+  }
+
+  @CacheOnInterval({
+    key: `apps-v3:${VVS_FINANCE_DEFINITION.id}:craftsman-v2-pool-addresses`,
+    timeout: 15 * 60 * 1000,
+  })
+  private async getCraftsmanV2PoolAddresses() {
+    const pools = await this.getCraftsmanV2Pools();
+    if (!pools) return [];
+
+    const provider = this.appToolkit.getNetworkProvider(network);
+    const multicall = this.appToolkit.getMulticall(network);
+
+    const craftsmanV2PoolAddresses = await Promise.all(
+      pools.map(async pool => {
+        const lpTokenAddress = pool.lpToken.toLowerCase();
+        const lpTokenContract = this.contractFactory.vvsPair({ address: lpTokenAddress, network });
+
+        // Some EOAs exist on the MasterChef contract; calling these breaks multicall
+        const code = await provider.getCode(lpTokenAddress);
+        if (code === '0x') return false;
+
+        const symbol = await multicall.wrap(lpTokenContract).symbol().catch(_err => '');
+
+        const isVvsLp = symbol === 'VVS-LP';
+        if (!isVvsLp) return null;
+
+        return lpTokenAddress;
+      }),
+    );
+
+    return compact(craftsmanV2PoolAddresses);
+  }
+
+  private async getStaticPoolAddresses() {
+    return [];
+  }
+
+  async getPoolAddresses(): Promise<string[]> {
+    const [topPoolAddresses, craftsmanPoolAddresses, craftsmanV2PoolAddresses, staticPoolAddresses] = await Promise.all([
+      this.getTopPoolAddresses(),
+      this.getCraftsmanPoolAddresses(),
+      this.getCraftsmanV2PoolAddresses(),
+      this.getStaticPoolAddresses(),
+    ]);
+
+    return uniq([...topPoolAddresses, ...craftsmanPoolAddresses, ...craftsmanV2PoolAddresses, ...staticPoolAddresses]);
+  }
+}
