74e9418055aee7e83beac9ab9a380d52eea0d2a7
The Ol' Dirty Bubble <theonepichael@gmail.com>
2022-06-03 17:20:18 -0400

    reverts to untouched state

--- src/apps/pika-protocol/contracts/index.ts
+++ src/apps/pika-protocol/contracts/index.ts
@@ -1,13 +1,10 @@
-import { Provider } from '@ethersproject/providers';
 import { Injectable, Inject } from '@nestjs/common';
-import { ethers } from 'ethers';
 
 import { IAppToolkit, APP_TOOLKIT } from '~app-toolkit/app-toolkit.interface';
 import { ContractFactory } from '~contract/contracts';
-import { EthersMulticall } from '~multicall';
 import { Network } from '~types/network.interface';
 
-import { PikaProtocolVault, PikaProtocolVault__factory } from './ethers';
+import { PikaProtocolVault__factory } from './ethers';
 
 // eslint-disable-next-line
 type ContractOpts = { address: string; network: Network };
@@ -21,26 +18,6 @@ export class PikaProtocolContractFactory extends ContractFactory {
   pikaProtocolVault({ address, network }: ContractOpts) {
     return PikaProtocolVault__factory.connect(address, this.appToolkit.getNetworkProvider(network));
   }
-
-  pikaProtocolVaultRewards({ address, network }: ContractOpts) {
-    const abi = [
-      "function getClaimableReward(address account) external view returns(uint256)"
-    ]
-
-    const contract = new ethers.Contract(address, abi, this.appToolkit.getNetworkProvider(network))
-    return contract;
-  }
-
-  // Defines helper function since Contract lacks method for returning its current balance
-  async getVaultBalance(vaultContractAddress: string, depositTokenAddress: string, network: Network) {
-    const multicall = this.appToolkit.getMulticall(network);
-    return await Promise.all([multicall.wrap(this.appToolkit.globalContracts.erc20({ network, address: depositTokenAddress })).balanceOf(vaultContractAddress)
-    ]);
-  }
-
-
 }
-
-
 
 export type { PikaProtocolVault } from './ethers';
