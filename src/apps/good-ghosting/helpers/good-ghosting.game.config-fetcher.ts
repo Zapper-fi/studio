a075d9cba296a43acae249bfea98d74938a8ff33
Karan Shahani <karan.shahani91@gmail.com>
2022-06-03 21:37:36 -0400

    fix(good-ghosting): Cache games API data for 15m to prevent 502 errors when trashing the endpoint

--- src/apps/good-ghosting/helpers/good-ghosting.game.config-fetcher.ts
+++ src/apps/good-ghosting/helpers/good-ghosting.game.config-fetcher.ts
@@ -2,18 +2,28 @@ import { Injectable } from '@nestjs/common';
 import axios from 'axios';
 
 import { SingleStakingFarmDefinition } from '~app-toolkit';
+import { CacheOnInterval } from '~cache/cache-on-interval.decorator';
+import { Network } from '~types';
+
+import GOOD_GHOSTING_DEFINITION from '../good-ghosting.definition';
 
 import { GamesResponse } from './constants';
 
 @Injectable()
 export class GoodGhostingGameConfigFetcherHelper {
-  async getGameConfigs(networkIdParam: string) {
+  @CacheOnInterval({
+    key: `studio:${GOOD_GHOSTING_DEFINITION.id}:${GOOD_GHOSTING_DEFINITION.groups.game}:${Network.POLYGON_MAINNET}:addresses`,
+    timeout: 15 * 60 * 1000,
+  })
+  async getCachedGameConfigsData() {
     const url = `https://goodghosting-api.com/v1/games`;
-
     const response = await axios.get<GamesResponse>(url);
-    const gameConfigs = response.data;
-    const farms: (SingleStakingFarmDefinition & { contractVersion: string })[] = [];
+    return response.data;
+  }
 
+  async getGameConfigs(networkIdParam: string) {
+    const farms: (SingleStakingFarmDefinition & { contractVersion: string })[] = [];
+    const gameConfigs = await this.getCachedGameConfigsData();
     const gameContractAddresses = Object.keys(gameConfigs);
 
     for (let i = 0; i < gameContractAddresses.length; i += 1) {
