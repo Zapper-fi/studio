9d820a05210568b2bbec76a281b80dca8e646203
The Ol' Dirty Bubble <106809061+theonepichael@users.noreply.github.com>
2022-06-03 21:39:00 -0400

    feat(pika-protocol): Support Pika Protocol (#559)

---
+++ src/apps/pika-protocol/pika-protocol.module.ts
@@ -0,0 +1,20 @@
+import { Register } from '~app-toolkit/decorators';
+import { AbstractApp } from '~app/app.dynamic-module';
+
+import { PikaProtocolContractFactory } from './contracts';
+import { OptimismPikaProtocolBalanceFetcher } from './optimism/pika-protocol.balance-fetcher';
+import { OptimismPikaProtocolTvlFetcher } from './optimism/pika-protocol.tvl-fetcher';
+import { OptimismPikaProtocolVaultContractPositionFetcher } from './optimism/pika-protocol.vault.contract-position-fetcher';
+import { PikaProtocolAppDefinition, PIKA_PROTOCOL_DEFINITION } from './pika-protocol.definition';
+
+@Register.AppModule({
+  appId: PIKA_PROTOCOL_DEFINITION.id,
+  providers: [
+    OptimismPikaProtocolBalanceFetcher,
+    OptimismPikaProtocolTvlFetcher,
+    OptimismPikaProtocolVaultContractPositionFetcher,
+    PikaProtocolAppDefinition,
+    PikaProtocolContractFactory,
+  ],
+})
+export class PikaProtocolAppModule extends AbstractApp() {}
