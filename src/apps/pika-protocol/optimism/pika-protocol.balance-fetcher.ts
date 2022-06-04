d2c3f98d821a3fa6284b7fc49c9c249af6988681
Karan Shahani <karan.shahani91@gmail.com>
2022-06-03 21:41:30 -0400

    fix(pika-protocol): Few linting errors because we absolutely need a pre-commit hook

--- src/apps/pika-protocol/optimism/pika-protocol.balance-fetcher.ts
+++ src/apps/pika-protocol/optimism/pika-protocol.balance-fetcher.ts
@@ -1,7 +1,7 @@
 import { Inject } from '@nestjs/common';
 import { ethers } from 'ethers';
-import { drillBalance } from '~app-toolkit';
 
+import { drillBalance } from '~app-toolkit';
 import { IAppToolkit, APP_TOOLKIT } from '~app-toolkit/app-toolkit.interface';
 import { Register } from '~app-toolkit/decorators';
 import { presentBalanceFetcherResponse } from '~app-toolkit/helpers/presentation/balance-fetcher-response.present';
@@ -18,53 +18,51 @@ const network = Network.OPTIMISM_MAINNET;
 export class OptimismPikaProtocolBalanceFetcher implements BalanceFetcher {
   constructor(
     @Inject(APP_TOOLKIT) private readonly appToolkit: IAppToolkit,
-    @Inject(PikaProtocolContractFactory) private readonly pikaProtocolContractFactory: PikaProtocolContractFactory
-  ) { }
+    @Inject(PikaProtocolContractFactory) private readonly pikaProtocolContractFactory: PikaProtocolContractFactory,
+  ) {}
 
   async getBalances(address: string) {
-    const [vaultBalances] = await Promise.all([this.getFarmBalances(address)])
-    return presentBalanceFetcherResponse([
-      { label: 'Vaults', assets: vaultBalances }
-    ]);
+    const [vaultBalances] = await Promise.all([this.getFarmBalances(address)]);
+    return presentBalanceFetcherResponse([{ label: 'Vaults', assets: vaultBalances }]);
   }
 
   pikaProtocolVaultRewards(address: string) {
-    const abi = [
-      "function getClaimableReward(address account) external view returns(uint256)"
-    ]
+    const abi = ['function getClaimableReward(address account) external view returns(uint256)'];
 
     const provider = this.appToolkit.getNetworkProvider(network);
 
-    const contract = new ethers.Contract(address, abi, provider)
+    const contract = new ethers.Contract(address, abi, provider);
 
     return contract;
-
   }
   async getFarmBalances(address: string) {
     return this.appToolkit.helpers.contractPositionBalanceHelper.getContractPositionBalances({
-      address, appId: PIKA_PROTOCOL_DEFINITION.id, groupId: PIKA_PROTOCOL_DEFINITION.groups.vault.id,
+      address,
+      appId: PIKA_PROTOCOL_DEFINITION.id,
+      groupId: PIKA_PROTOCOL_DEFINITION.groups.vault.id,
       network: Network.OPTIMISM_MAINNET,
 
       resolveBalances: async ({ address, contractPosition, multicall }) => {
         const rewardAddress = '0x58488bB666d2da33F8E8938Dbdd582D2481D4183'.toLowerCase();
-        const contract = this.pikaProtocolContractFactory.pikaProtocolVault({ address: contractPosition.address, network })
+        const contract = this.pikaProtocolContractFactory.pikaProtocolVault({
+          address: contractPosition.address,
+          network,
+        });
         const rewardContract = this.pikaProtocolVaultRewards(rewardAddress);
 
         const stakedToken = contractPosition.tokens.find(isSupplied)!;
         const rewardToken = contractPosition.tokens.find(isClaimable)!;
 
-
-        const [stakedBalanceRaw, rewardBalanceRaw] = await Promise.all(
-          [
-            multicall.wrap(contract).getShareBalance(address),
-            multicall.wrap(rewardContract).getClaimableReward(address)
-          ])
+        const [stakedBalanceRaw, rewardBalanceRaw] = await Promise.all([
+          multicall.wrap(contract).getShareBalance(address),
+          multicall.wrap(rewardContract).getClaimableReward(address),
+        ]);
 
         return [
           drillBalance(stakedToken, stakedBalanceRaw.toString()),
-          drillBalance(rewardToken, rewardBalanceRaw.toString())
-        ]
-      }
-    })
+          drillBalance(rewardToken, rewardBalanceRaw.toString()),
+        ];
+      },
+    });
   }
 }
--- src/apps/pika-protocol/optimism/pika-protocol.tvl-fetcher.ts
+++ src/apps/pika-protocol/optimism/pika-protocol.tvl-fetcher.ts
@@ -7,6 +7,7 @@ import { TvlFetcher } from '~stats/tvl/tvl-fetcher.interface';
 import { Network } from '~types/network.interface';
 
 import { PIKA_PROTOCOL_DEFINITION } from '../pika-protocol.definition';
+
 import { PikaProtocolContractPositionDataProps } from './pika-protocol.vault.contract-position-fetcher';
 
 const appId = PIKA_PROTOCOL_DEFINITION.id;
@@ -15,10 +16,14 @@ const network = Network.OPTIMISM_MAINNET;
 
 @Register.TvlFetcher({ appId, network })
 export class OptimismPikaProtocolTvlFetcher implements TvlFetcher {
-  constructor(@Inject(APP_TOOLKIT) private readonly appToolkit: IAppToolkit) { }
+  constructor(@Inject(APP_TOOLKIT) private readonly appToolkit: IAppToolkit) {}
 
   async getTvl() {
-    const tokens = await this.appToolkit.getAppContractPositions<PikaProtocolContractPositionDataProps>({ appId, groupIds: [groupId], network });
+    const tokens = await this.appToolkit.getAppContractPositions<PikaProtocolContractPositionDataProps>({
+      appId,
+      groupIds: [groupId],
+      network,
+    });
 
     return sumBy(tokens, v => v.dataProps.totalValueLocked);
   }
