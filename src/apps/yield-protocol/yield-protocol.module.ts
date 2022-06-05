2fc8bfa23492b8f66a3ccf4d3d7855e82cace8c7
marcomariscal <marco.a.mariscal@gmail.com>
2022-06-04 20:04:22 -0400

    feat: borrow contract position shell

--- src/apps/yield-protocol/yield-protocol.module.ts
+++ src/apps/yield-protocol/yield-protocol.module.ts
@@ -6,6 +6,7 @@ import { ArbitrumYieldProtocolLendTokenFetcher } from './arbitrum/yield-protocol
 import { ArbitrumYieldProtocolPoolTokenFetcher } from './arbitrum/yield-protocol.pool.token-fetcher';
 import { YieldProtocolContractFactory } from './contracts';
 import { EthereumYieldProtocolBalanceFetcher } from './ethereum/yield-protocol.balance-fetcher';
+import { EthereumYieldProtocolBorrowContractPositionFetcher } from './ethereum/yield-protocol.borrow.contract-position-fetcher';
 import { EthereumYieldProtocolLendTokenFetcher } from './ethereum/yield-protocol.lend.token-fetcher';
 import { EthereumYieldProtocolPoolTokenFetcher } from './ethereum/yield-protocol.pool.token-fetcher';
 import { YieldProtocolAppDefinition, YIELD_PROTOCOL_DEFINITION } from './yield-protocol.definition';
@@ -17,6 +18,7 @@ import { YieldProtocolAppDefinition, YIELD_PROTOCOL_DEFINITION } from './yield-p
     ArbitrumYieldProtocolLendTokenFetcher,
     ArbitrumYieldProtocolPoolTokenFetcher,
     EthereumYieldProtocolBalanceFetcher,
+    EthereumYieldProtocolBorrowContractPositionFetcher,
     EthereumYieldProtocolLendTokenFetcher,
     EthereumYieldProtocolPoolTokenFetcher,
     YieldProtocolAppDefinition,
