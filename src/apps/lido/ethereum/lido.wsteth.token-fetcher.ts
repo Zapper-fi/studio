801ecf7c4d81f5339d42c5c6ff5c424e808c77f1
Xavier Drdak <1198051+xdrdak@users.noreply.github.com>
2022-06-07 13:46:30 -0400

    fix: Add liquidity stats item to lido

--- src/apps/lido/ethereum/lido.wsteth.token-fetcher.ts
+++ src/apps/lido/ethereum/lido.wsteth.token-fetcher.ts
@@ -35,6 +35,7 @@ export class EthereumLidoWstethTokenFetcher implements PositionFetcher<AppTokenP
       multicall.wrap(contract).totalSupply(),
     ]);
     const supply = Number(totalSupply) / 10 ** decimals;
+    const liquidity = wstethToken.price * Number(totalSupply);
     const token: AppTokenPosition = {
       address,
       network,
@@ -52,6 +53,12 @@ export class EthereumLidoWstethTokenFetcher implements PositionFetcher<AppTokenP
         label: symbol,
         secondaryLabel: buildDollarDisplayItem(wstethToken.price),
         images: [getTokenImg(address, network)],
+        statsItems: [
+          {
+            label: 'Liquidity',
+            value: buildDollarDisplayItem(liquidity),
+          },
+        ],
       },
     };
 
