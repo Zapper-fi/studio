931e79a2cec8a20e20a4565ad73230d2109a2c08
Papa Sougou Wele <papis.wele@gmail.com>
2022-06-07 13:12:26 -0400

    feat(compound): Add resolver in helper method

--- src/apps/compound/helper/compound.borrow.contract-position-helper.ts
+++ src/apps/compound/helper/compound.borrow.contract-position-helper.ts
@@ -1,4 +1,5 @@
 import { Inject, Injectable } from '@nestjs/common';
+import { BigNumberish } from 'ethers';
 import { isNumber } from 'lodash';
 
 import { APP_TOOLKIT, IAppToolkit } from '~app-toolkit/app-toolkit.interface';
@@ -7,12 +8,13 @@ import {
   buildPercentageDisplayItem,
 } from '~app-toolkit/helpers/presentation/display-item.present';
 import { getLabelFromToken } from '~app-toolkit/helpers/presentation/image.present';
+import { EthersMulticall } from '~multicall';
 import { ContractType } from '~position/contract.interface';
 import { ContractPosition } from '~position/position.interface';
 import { borrowed } from '~position/position.utils';
 import { Network } from '~types/network.interface';
 
-import { CompoundContractFactory } from '../contracts';
+import { CompoundContractFactory, CompoundCToken } from '../contracts';
 
 import { CompoundSupplyTokenDataProps } from './compound.supply.token-helper';
 
@@ -26,6 +28,12 @@ type CompoundBorrowContractPositionHelperParams = {
   appId: string;
   groupId: string;
   supplyGroupId: string;
+  resolveCashRaw?: (opts: {
+    contract: CompoundCToken;
+    address: string;
+    network: Network;
+    multicall: EthersMulticall;
+  }) => Promise<BigNumberish>;
 };
 
 @Injectable()
@@ -35,7 +43,13 @@ export class CompoundBorrowContractPositionHelper {
     @Inject(APP_TOOLKIT) private readonly appToolkit: IAppToolkit,
   ) {}
 
-  async getPositions({ network, appId, groupId, supplyGroupId }: CompoundBorrowContractPositionHelperParams) {
+  async getPositions({
+    network,
+    appId,
+    groupId,
+    supplyGroupId,
+    resolveCashRaw = ({ contract, multicall }) => multicall.wrap(contract).getCash(),
+  }: CompoundBorrowContractPositionHelperParams) {
     const appTokens = await this.appToolkit.getAppTokenPositions<CompoundSupplyTokenDataProps>({
       appId,
       groupIds: [supplyGroupId],
@@ -49,13 +63,10 @@ export class CompoundBorrowContractPositionHelper {
       // Compound has a `getCash` method which returns the total liquidity
       // of a given borrowed contract position. This is typically what defilama
       // would use to display a TVL
-      const [cash] = await Promise.all([multicall.wrap(contract).getCash()]);
+      const cashRaw = await resolveCashRaw({ multicall, contract, address: appToken.address, network }).catch(() => 0);
       // The "cash" needs to be converted back into a proper number format.
       // We use the underlying token as the basis for the conversion.
-      const cashSupply = this.appToolkit
-        .getBigNumber(cash)
-        .div(10 ** appToken.tokens[0].decimals)
-        .toNumber();
+      const cashSupply = Number(cashRaw) / 10 ** appToken.tokens[0].decimals;
 
       const tokens = [borrowed(appToken.tokens[0])];
       // The underlying token liquidity actually represents the TOTAL SUPPLY of a borrowed
