516123ccaef605c5e1e057e4200d0269026bc7b0
Weston <westontnelson@outlook.com>
2022-06-08 18:44:45 -0500

    Update

--- src/apps/saddle/evmos/saddle.pool.token-fetcher.ts
+++ src/apps/saddle/evmos/saddle.pool.token-fetcher.ts
@@ -1,26 +1,61 @@
-import { Inject } from '@nestjs/common';
 
-import { IAppToolkit, APP_TOOLKIT } from '~app-toolkit/app-toolkit.interface';
+import { Inject } from '@nestjs/common';
+import { BigNumber } from 'ethers';
+import { uniqBy } from 'lodash';
+
 import { Register } from '~app-toolkit/decorators';
+import { CurvePoolTokenHelper, CurveVirtualPriceStrategy } from '~apps/curve';
+import { Erc20 } from '~contract/contracts';
 import { PositionFetcher } from '~position/position-fetcher.interface';
 import { AppTokenPosition } from '~position/position.interface';
 import { Network } from '~types/network.interface';
 
-import { SaddleContractFactory } from '../contracts';
+import { SaddleContractFactory, SaddleSwap } from '../contracts';
+import { SaddleOnChainCoinStrategy } from '../helpers/saddle.on-chain.coin-strategy';
+import { SaddleOnChainReserveStrategy } from '../helpers/saddle.on-chain.reserve-strategy';
 import { SADDLE_DEFINITION } from '../saddle.definition';
 
-const appId = SADDLE_DEFINITION.id;
-const groupId = SADDLE_DEFINITION.groups.pool.id;
-const network = Network.EVMOS_MAINNET;
+import { SADDLE_BASEPOOL_DEFINITIONS } from './saddle.pool.definitions';
 
-@Register.TokenPositionFetcher({ appId, groupId, network })
-export class EvmosSaddlePoolTokenFetcher implements PositionFetcher<AppTokenPosition> {
+@Register.TokenPositionFetcher({
+  appId: SADDLE_DEFINITION.id,
+  groupId: SADDLE_DEFINITION.groups.pool.id,
+  network: Network.EVMOS_MAINNET,
+})
+export class EthereumSaddlePoolTokenFetcher implements PositionFetcher<AppTokenPosition> {
   constructor(
-    @Inject(APP_TOOLKIT) private readonly appToolkit: IAppToolkit,
-    @Inject(SaddleContractFactory) private readonly saddleContractFactory: SaddleContractFactory,
+    @Inject(CurvePoolTokenHelper)
+    private readonly curvePoolTokenHelper: CurvePoolTokenHelper,
+    @Inject(SaddleOnChainCoinStrategy)
+    private readonly saddleOnChainCoinStrategy: SaddleOnChainCoinStrategy,
+    @Inject(SaddleOnChainReserveStrategy)
+    private readonly saddleOnChainReserveStrategy: SaddleOnChainReserveStrategy,
+    @Inject(CurveVirtualPriceStrategy)
+    private readonly curveVirtualPriceStrategy: CurveVirtualPriceStrategy,
+    @Inject(SaddleContractFactory)
+    private readonly saddleContractFactory: SaddleContractFactory,
   ) {}
 
   async getPositions() {
-    return [];
+    const basePools = await this.curvePoolTokenHelper.getTokens<SaddleSwap, Erc20>({
+      network: Network.EVMOS_MAINNET,
+      appId: SADDLE_DEFINITION.id,
+      groupId: SADDLE_DEFINITION.groups.pool.id,
+      resolvePoolDefinitions: async () => SADDLE_BASEPOOL_DEFINITIONS,
+      resolvePoolContract: ({ network, definition }) =>
+        this.saddleContractFactory.saddleSwap({ network, address: definition.swapAddress }),
+      resolvePoolTokenContract: ({ network, definition }) =>
+        this.saddleContractFactory.erc20({ network, address: definition.tokenAddress }),
+      resolvePoolCoinAddresses: this.saddleOnChainCoinStrategy.build(),
+      resolvePoolReserves: this.saddleOnChainReserveStrategy.build(),
+      resolvePoolFee: async () => BigNumber.from('4000000'),
+      resolvePoolTokenSymbol: ({ multicall, poolTokenContract }) => multicall.wrap(poolTokenContract).symbol(),
+      resolvePoolTokenSupply: ({ multicall, poolTokenContract }) => multicall.wrap(poolTokenContract).totalSupply(),
+      resolvePoolTokenPrice: this.curveVirtualPriceStrategy.build({
+        resolveVirtualPrice: ({ multicall, poolContract }) => multicall.wrap(poolContract).getVirtualPrice(),
+      }),
+    });
+
+    return uniqBy([basePools].flat(), v => v.address);
   }
 }
