5f5c41f44f7f425ddf4c6e3aaf62a89c3c35bd6a
Weston <westontnelson@outlook.com>
2022-06-08 18:36:10 -0500

    Note: rm yarn files and updates Farm address.

--- src/apps/saddle/evmos/saddle.communal-farm.contract-fetcher.ts
+++ src/apps/saddle/evmos/saddle.communal-farm.contract-fetcher.ts
@@ -13,7 +13,7 @@ import SADDLE_DEFINITION from '../saddle.definition';
 const FARMS = [
   // Saddle Frax 3 Pool Farm
   {
-    address: '0x21d4365834b7c61447e142ef6bcf01136cbd01c6',
+    address: '0x0232e0b6df048c8cc4037c52bc90cf943c9c8cc6',
     stakedTokenAddress: '0x2801fE8f9DE3a4aD6098a5B95d5165676bb01f82',
     rewardTokenAddresses: ['0x3344e55c6dde2a01f4ed893f97bac1f99ec24f8b'],
   },
@@ -43,7 +43,7 @@ export class EvmosSaddleCommunalFarmContractPositionFetcher implements PositionF
         {
           appId: SADDLE_DEFINITION.id,
           groupIds: [SADDLE_DEFINITION.groups.pool.id],
-          network: Network.ETHEREUM_MAINNET,
+          network: Network.EVMOS_MAINNET,
         },
       ],
       resolveFarmDefinitions: async () => FARMS,
