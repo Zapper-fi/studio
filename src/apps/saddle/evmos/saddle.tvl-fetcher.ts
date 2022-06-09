e5b9851c5bd8f2fdaa5f088ed0622ec659ca6b00
Weston <westontnelson@outlook.com>
2022-06-09 03:39:54 -0500

    Updates and add logo .png file

--- src/apps/saddle/evmos/saddle.tvl-fetcher.ts
+++ src/apps/saddle/evmos/saddle.tvl-fetcher.ts
@@ -2,19 +2,19 @@ import { Inject } from '@nestjs/common';
 
 import { IAppToolkit, APP_TOOLKIT } from '~app-toolkit/app-toolkit.interface';
 import { Register } from '~app-toolkit/decorators';
-import { presentBalanceFetcherResponse } from '~app-toolkit/helpers/presentation/balance-fetcher-response.present';
-import { BalanceFetcher } from '~balance/balance-fetcher.interface';
+import { TvlFetcher } from '~stats/tvl/tvl-fetcher.interface';
 import { Network } from '~types/network.interface';
 
 import { SADDLE_DEFINITION } from '../saddle.definition';
 
+const appId = SADDLE_DEFINITION.id;
 const network = Network.EVMOS_MAINNET;
 
-@Register.BalanceFetcher(SADDLE_DEFINITION.id, network)
-export class EvmosSaddleBalanceFetcher implements BalanceFetcher {
+@Register.TvlFetcher({ appId, network })
+export class EvmosSaddleTvlFetcher implements TvlFetcher {
   constructor(@Inject(APP_TOOLKIT) private readonly appToolkit: IAppToolkit) {}
 
-  async getBalances(address: string) {
-    return presentBalanceFetcherResponse([]);
+  async getTvl() {
+    return 0;
   }
 }
