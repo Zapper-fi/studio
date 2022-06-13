74a5027c5b5f72fcba0705d32cb8a5f9b6c34b74
Provider <mateyasu@bastionprotocol.com>
2022-06-13 22:53:38 +0800

    refactor(bastion-protocol): used the correct data prop

--- src/apps/bastion-protocol/aurora/bastion-protocol.tvl-fetcher.ts
+++ src/apps/bastion-protocol/aurora/bastion-protocol.tvl-fetcher.ts
@@ -3,16 +3,16 @@ import { sumBy } from 'lodash';
 
 import { IAppToolkit, APP_TOOLKIT } from '~app-toolkit/app-toolkit.interface';
 import { Register } from '~app-toolkit/decorators';
+import { CompoundSupplyTokenDataProps } from '~apps/compound/helper/compound.supply.token-helper';
 import { TvlFetcher } from '~stats/tvl/tvl-fetcher.interface';
 import { Network } from '~types/network.interface';
 
 import { BASTION_PROTOCOL_DEFINITION } from '../bastion-protocol.definition';
-import { BastionSupplyTokenDataProps } from './bastion-protocol.supply.token-fetcher';
 
 const appId = BASTION_PROTOCOL_DEFINITION.id;
 const network = Network.AURORA_MAINNET;
 
-export type BastionBorrowContractPositionDataProps = BastionSupplyTokenDataProps & {
+export type BastionBorrowContractPositionDataProps = CompoundSupplyTokenDataProps & {
   supply: number;
   borrow: number;
 };
@@ -23,7 +23,7 @@ export class AuroraBastionProtocolTvlFetcher implements TvlFetcher {
 
 
   async getTvl() {
-    const appTokens = await this.appToolkit.getAppTokenPositions<BastionSupplyTokenDataProps>({
+    const appTokens = await this.appToolkit.getAppTokenPositions<CompoundSupplyTokenDataProps>({
       appId,
       groupIds: [
         BASTION_PROTOCOL_DEFINITION.groups.supply.id,
