dce61375964bf598a3068fbf9dfbff1455fd4aff
David Esposto <desposto@uwo.ca>
2022-06-13 16:17:48 -0400

    feat:Implemented-Honeyswap

---
+++ src/apps/honeyswap/index.ts
@@ -0,0 +1,3 @@
+export { HONEYSWAP_DEFINITION, HoneyswapAppDefinition } from './honeyswap.definition';
+export { HoneyswapAppModule } from './honeyswap.module';
+export { HoneyswapContractFactory } from './contracts';
