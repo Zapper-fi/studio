badfa3334a323eb4d215cb50cbd122c59b5f4ae5
CronosLabsDev <106642922+CronosLabsDev@users.noreply.github.com>
2022-06-13 20:20:36 +0800

    feat(tectonic): Add Tectonic Integration (#609)

---
+++ src/apps/tectonic/index.ts
@@ -0,0 +1,3 @@
+export { TECTONIC_DEFINITION, TectonicAppDefinition } from './tectonic.definition';
+export { TectonicAppModule } from './tectonic.module';
+export { TectonicContractFactory } from './contracts';
