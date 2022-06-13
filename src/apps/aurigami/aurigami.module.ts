c79f1155ad1b3679025cc643641b1613cbec82ab
Takao <takao@aurigami.finance>
2022-06-13 15:02:46 +0700

    feat(aurigami): Add contract position fetcher for borrowing

--- src/apps/aurigami/aurigami.module.ts
+++ src/apps/aurigami/aurigami.module.ts
@@ -3,12 +3,18 @@ import { AbstractApp } from '~app/app.dynamic-module';
 import { CompoundAppModule } from '~apps/compound';
 
 import { AurigamiAppDefinition, AURIGAMI_DEFINITION } from './aurigami.definition';
+import { AuroraAurigamiBorrowContractPositionFetcher } from './aurora/aurigami.borrow.contract-position-fetcher';
 import { AuroraAurigamiSupplyTokenFetcher } from './aurora/aurigami.supply.token-fetcher';
 import { AurigamiContractFactory } from './contracts';
 
 @Register.AppModule({
   appId: AURIGAMI_DEFINITION.id,
   imports: [CompoundAppModule],
-  providers: [AurigamiAppDefinition, AurigamiContractFactory, AuroraAurigamiSupplyTokenFetcher],
+  providers: [
+    AurigamiAppDefinition,
+    AurigamiContractFactory,
+    AuroraAurigamiBorrowContractPositionFetcher,
+    AuroraAurigamiSupplyTokenFetcher,
+  ],
 })
 export class AurigamiAppModule extends AbstractApp() {}
