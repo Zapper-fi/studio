43efda7008f75a75feb5649b838e852e55a84cf4
Dawg679 <tanjunmeng@gmail.com>
2022-06-15 00:27:15 +0800

    feat: :fire: Add token and contract position fetcher

---
+++ src/apps/argo-finance/cronos/argo-finance.pledging.contract-position-fetcher.ts
@@ -0,0 +1,73 @@
+import { Inject } from '@nestjs/common';
+import { XArgoPledging } from '../contracts';
+import { IAppToolkit, APP_TOOLKIT } from '~app-toolkit/app-toolkit.interface';
+import { Register } from '~app-toolkit/decorators';
+import { PositionFetcher } from '~position/position-fetcher.interface';
+import { buildDollarDisplayItem } from '~app-toolkit/helpers/presentation/display-item.present';
+import { getImagesFromToken } from '~app-toolkit/helpers/presentation/image.present';
+import { ContractType } from '~position/contract.interface';
+import { ContractPosition } from '~position/position.interface';
+import { claimable, supplied } from "~position/position.utils";
+import { Network } from '~types/network.interface';
+
+import { ARGO_FINANCE_DEFINITION } from '../argo-finance.definition';
+import { ArgoFinanceContractFactory } from '../contracts';
+import { ADDRESSES } from './consts';
+
+const appId = ARGO_FINANCE_DEFINITION.id;
+const groupId = ARGO_FINANCE_DEFINITION.groups.pledging.id;
+const network = Network.CRONOS_MAINNET;
+
+@Register.ContractPositionFetcher({ appId, groupId, network })
+export class CronosArgoFinancePledgingContractPositionFetcher implements PositionFetcher<ContractPosition> {
+  constructor(
+    @Inject(APP_TOOLKIT) private readonly appToolkit: IAppToolkit,
+    @Inject(ArgoFinanceContractFactory) private readonly argoFinanceContractFactory: ArgoFinanceContractFactory,
+  ) {}
+
+  async getVePosition(address: string, baseAddress: string) {
+    const multicall = this.appToolkit.getMulticall(network);
+    const baseTokens = await this.appToolkit.getBaseTokenPrices(network);
+    let baseToken = baseTokens.find(t => t.address === baseAddress)!;
+    let croToken = baseTokens.find(t => t.symbol === "WCRO")!;
+    baseToken.symbol = "xARGO";
+    baseToken.address = "0xb966B5D6A0fCd5b373B180Bbe072BBFbbEe10552";
+    const veToken = multicall.wrap(this.appToolkit.globalContracts.erc20({ address, network }));
+    const [supplyRaw, decimals, symbol] = await Promise.all([
+      veToken.totalSupply(),
+      veToken.decimals(),
+      veToken.symbol(),
+    ]);
+    const supply = Number(supplyRaw) / 10 ** decimals;
+    const pricePerShare = 1; // Note: Consult liquidity pools for peg once set up
+    const price = baseToken.price * pricePerShare;
+    const liquidity = supply * price;
+
+    const tokens = [supplied(baseToken), claimable(baseToken), claimable(croToken)];
+    const position: ContractPosition =
+    {
+      type: ContractType.POSITION,
+      appId,
+      groupId,
+      address,
+      network,
+      tokens,
+      dataProps: {
+     
+      },
+      displayProps: {
+        label: "xARGO",
+        secondaryLabel: buildDollarDisplayItem(price),
+        images:getImagesFromToken(baseToken),
+      },
+    };
+    return position;
+  }
+
+  async getPositions() {
+    const [argo] = await Promise.all([
+      this.getVePosition(ADDRESSES.xargo, ADDRESSES.argo),
+    ]);
+    return [argo];
+  }
+}
---
+++ src/apps/argo-finance/cronos/consts.ts
@@ -0,0 +1,4 @@
+export const ADDRESSES = {
+  argo: '0x47a9d630dc5b28f75d3af3be3aaa982512cd89aa'.toLowerCase(),
+  xargo: '0xb966B5D6A0fCd5b373B180Bbe072BBFbbEe10552'.toLowerCase(),
+};
