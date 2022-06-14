3a152a75958e435b02bffaa1bb976167f4c9da21
Kosiso <kosiso.gaxion@gmail.com>
2022-06-14 21:16:48 +0100

    feat: adds support for Celo and bug fixes (#642)
    * feat: adds good-ghosting integration to zapper
    
    * fix: adds games  type to axios fetch, filtered games response according to network
    
    * feat: adds support for CELO and bug fixes
    
    * feat: adds support for Celo and bug fixes
    
    * feat: adds support for Celo and bug fixes
    
    * feat: adds support for Celo and bug fixes
    
    * feat: adds support for Celo and bug fixes
    
    * feat: adds support for Celo and bug fixes
    
    * feat: adds support for Celo and bug fixes
    
    * feat: adds support for Celo and bug fixes
    
    * feat: adds support for Celo and bug fixes
    
    * feat: adds support for Celo and bug fixes
    
    * fix: show only incentives for winners

---
+++ src/apps/good-ghosting/celo/good-ghosting.game.contract-position-fetcher.ts
@@ -0,0 +1,32 @@
+import { Inject } from '@nestjs/common';
+
+import { IAppToolkit, APP_TOOLKIT } from '~app-toolkit/app-toolkit.interface';
+import { Register } from '~app-toolkit/decorators';
+import { PositionFetcher } from '~position/position-fetcher.interface';
+import { ContractPosition } from '~position/position.interface';
+import { Network } from '~types/network.interface';
+
+import { GOOD_GHOSTING_DEFINITION } from '../good-ghosting.definition';
+import { NetworkId } from '../helpers/constants';
+import { GoodGhostingContractFactory } from '../contracts';
+import { GoodGhostingGameContractPositionFetcherHelper } from '../helpers/good-ghosting.game.contract-position-fetcher-helper';
+
+const appId = GOOD_GHOSTING_DEFINITION.id;
+const groupId = GOOD_GHOSTING_DEFINITION.groups.game.id;
+const network = Network.CELO_MAINNET;
+const networkId = NetworkId.CeloMainnet;
+
+@Register.ContractPositionFetcher({ appId, groupId, network })
+export class CeloGoodGhostingGameContractPositionFetcher implements PositionFetcher<ContractPosition> {
+  constructor(
+    @Inject(APP_TOOLKIT) private readonly appToolkit: IAppToolkit,
+    @Inject(GoodGhostingContractFactory) private readonly goodGhostingContractFactory: GoodGhostingContractFactory,
+
+    @Inject(GoodGhostingGameContractPositionFetcherHelper)
+    private readonly helper: GoodGhostingGameContractPositionFetcherHelper,
+  ) {}
+
+  async getPositions() {
+    return this.helper.getContractPosition(network, networkId, appId, groupId);
+  }
+}
