05eef48957267f877cee651ac0aa9c062707dc10
reganwrench <98139806+reganwrench@users.noreply.github.com>
2022-06-16 19:45:29 +0800

    feat(iron-bank): Added Iron Bank to Zapper  (#617)

---
+++ src/apps/iron-bank/index.ts
@@ -0,0 +1,8 @@
+export { IRON_BANK_DEFINITION, IronBankAppDefinition } from './iron-bank.definition';
+export { IronBankAppModule } from './iron-bank.module';
+export { IronBankContractFactory } from './contracts';
+
+/* Contracts */
+export type { IronBankCToken } from './contracts';
+export type { IronBankComptroller } from './contracts';
+export type { IronBankLens } from './contracts';
