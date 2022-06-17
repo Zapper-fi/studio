2409305166261b07e14e9f742d7d23adb926a1e6
Sébastien Audet <seb.audet26@gmail.com>
2022-06-17 16:41:20 -0400

    lint fixes

--- src/apps/pancakeswap/binance/pancakeswap.syrup-staking.contract-position-fetcher.ts
+++ src/apps/pancakeswap/binance/pancakeswap.syrup-staking.contract-position-fetcher.ts
@@ -287,14 +287,14 @@ const FARMS = [
   '0xcec2671c81a0ecf7f8ee796efa6dbdc5cb062693',
   '0x2d17ec6cd0af737b2ade40ea527d41ceeedc166f',
   '0x0f96e19bdc787e767ba1e8f1add0f62cbdad87c8',
-  '0xa79d37ce9df9443ef4b6dec2e38a8ecd35303adc'
+  '0xa79d37ce9df9443ef4b6dec2e38a8ecd35303adc',
 ];
 
 const appId = PANCAKESWAP_DEFINITION.id;
 const groupId = PANCAKESWAP_DEFINITION.groups.syrupStaking.id;
 const network = Network.BINANCE_SMART_CHAIN_MAINNET;
 
-@Register.ContractPositionFetcher({ appId, groupId, network, options: { includeInTvl: true })
+@Register.ContractPositionFetcher({ appId, groupId, network, options: { includeInTvl: true } })
 export class BinanceSmartChainPancakeswapSyrupStakingContractPositionFetcher
   implements PositionFetcher<ContractPosition>
 {
