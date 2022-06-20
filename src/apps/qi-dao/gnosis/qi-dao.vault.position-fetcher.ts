8f86a0c1d8f978258b07eb1348820b028c992e14
Anson <git@anson-cheung.com>
2022-06-21 00:17:25 +0800

    chore: lowercase addresses

--- src/apps/qi-dao/gnosis/qi-dao.vault.position-fetcher.ts
+++ src/apps/qi-dao/gnosis/qi-dao.vault.position-fetcher.ts
@@ -19,11 +19,11 @@ export class GnosisQiDaoVaultPositionFetcher implements PositionFetcher<Contract
   getPositions() {
     return this.qiDaoVaultPositionHelper.getPositions({
       network,
-      debtTokenAddress: '0x3F56e0c36d275367b8C502090EDF38289b3dEa0d',
+      debtTokenAddress: '0x3f56e0c36d275367b8c502090edf38289b3dea0d',
       vaults: [
         {
-          nftAddress: '0x5c49b268c9841AFF1Cc3B0a418ff5c3442eE3F3b', // WETH
-          vaultInfoAddress: '0x5c49b268c9841AFF1Cc3B0a418ff5c3442eE3F3b',
+          nftAddress: '0x5c49b268c9841aff1cc3b0a418ff5c3442ee3f3b', // WETH
+          vaultInfoAddress: '0x5c49b268c9841aff1cc3b0a418ff5c3442ee3f3b',
         },
         {
           nftAddress: '0x014a177e9642d1b4e970418f894985dc1b85657f', // GNO
