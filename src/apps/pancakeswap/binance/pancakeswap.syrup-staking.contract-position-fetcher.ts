3d2216dcdf93f910fbcc0911a36e7fb06b680f80
Papa Sougou Wele <papis.wele@gmail.com>
2022-06-27 11:40:56 -0400

    fix(pancakeswap): Update Syrup pools label (#768)

--- src/apps/pancakeswap/binance/pancakeswap.syrup-staking.contract-position-fetcher.ts
+++ src/apps/pancakeswap/binance/pancakeswap.syrup-staking.contract-position-fetcher.ts
@@ -4,6 +4,7 @@ import { BigNumber } from 'ethers';
 import { APP_TOOLKIT, IAppToolkit } from '~app-toolkit/app-toolkit.interface';
 import { Register } from '~app-toolkit/decorators';
 import { RewardRateUnit } from '~app-toolkit/helpers/master-chef/master-chef.contract-position-helper';
+import { getLabelFromToken } from '~app-toolkit/helpers/presentation/image.present';
 import { PositionFetcher } from '~position/position-fetcher.interface';
 import { ContractPosition } from '~position/position.interface';
 import { Network } from '~types/network.interface';
@@ -350,6 +351,7 @@ export class BinanceSmartChainPancakeswapSyrupStakingContractPositionFetcher
           return multicall.wrap(contract).rewardPerBlock();
         },
       }),
+      resolveLabel: ({ rewardTokens }) => `Earn ${getLabelFromToken(rewardTokens[0])}`,
     });
   }
 }
