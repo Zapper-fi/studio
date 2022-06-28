b9573a46ec0adc9ebb23af17dc5e6f1e57336885
FiboApe <fiboape@gmail.com>
2022-06-28 18:01:57 -0300

    feat(position-utils): improve generic type contrains

--- src/position/position.utils.ts
+++ src/position/position.utils.ts
@@ -8,9 +8,9 @@ export const isClaimable = (token: WithMetaType<Token>) => token.metaType === Me
 export const isVesting = (token: WithMetaType<Token>) => token.metaType === MetaType.VESTING;
 export const isLocked = (token: WithMetaType<Token>) => token.metaType === MetaType.LOCKED;
 
-export const wallet = <T>(token: T): WithMetaType<T> => ({ metaType: MetaType.WALLET, ...token });
-export const supplied = <T>(token: T): WithMetaType<T> => ({ metaType: MetaType.SUPPLIED, ...token });
-export const borrowed = <T>(token: T): WithMetaType<T> => ({ metaType: MetaType.BORROWED, ...token });
-export const claimable = <T>(token: T): WithMetaType<T> => ({ metaType: MetaType.CLAIMABLE, ...token });
-export const vesting = <T>(token: T): WithMetaType<T> => ({ metaType: MetaType.VESTING, ...token });
-export const locked = <T>(token: T): WithMetaType<T> => ({ metaType: MetaType.LOCKED, ...token });
+export const wallet = <T extends Token>(token: T): WithMetaType<T> => ({ metaType: MetaType.WALLET, ...token });
+export const supplied = <T extends Token>(token: T): WithMetaType<T> => ({ metaType: MetaType.SUPPLIED, ...token });
+export const borrowed = <T extends Token>(token: T): WithMetaType<T> => ({ metaType: MetaType.BORROWED, ...token });
+export const claimable = <T extends Token>(token: T): WithMetaType<T> => ({ metaType: MetaType.CLAIMABLE, ...token });
+export const vesting = <T extends Token>(token: T): WithMetaType<T> => ({ metaType: MetaType.VESTING, ...token });
+export const locked = <T extends Token>(token: T): WithMetaType<T> => ({ metaType: MetaType.LOCKED, ...token });
