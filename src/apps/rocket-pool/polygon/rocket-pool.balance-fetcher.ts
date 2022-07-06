eb94fd138d157a72137f99e224562d64360c1f67
peteris <peteris@rocketscan.io>
2022-07-06 21:01:44 +0200

    feat: Rocket Pool

---
+++ src/apps/rocket-pool/polygon/rocket-pool.balance-fetcher.ts
@@ -0,0 +1,22 @@
+import { Inject } from '@nestjs/common';
+
+import { Register } from '~app-toolkit/decorators';
+import { presentBalanceFetcherResponse } from '~app-toolkit/helpers/presentation/balance-fetcher-response.present';
+import { BalanceFetcher } from '~balance/balance-fetcher.interface';
+import { Network } from '~types/network.interface';
+import { RocketPoolRethBalanceHelper } from '../helpers/rocket-pool.reth.balance-helper';
+
+import { ROCKET_POOL_DEFINITION } from '../rocket-pool.definition';
+
+const network = Network.POLYGON_MAINNET;
+
+@Register.BalanceFetcher(ROCKET_POOL_DEFINITION.id, network)
+export class PolygonRocketPoolBalanceFetcher implements BalanceFetcher {
+  constructor(
+    @Inject(RocketPoolRethBalanceHelper) private readonly rocketPoolRethBalanceHelper: RocketPoolRethBalanceHelper,
+  ) {}
+
+  async getBalances(address: string) {
+    return presentBalanceFetcherResponse(await this.rocketPoolRethBalanceHelper.getBalances(address));
+  }
+}
