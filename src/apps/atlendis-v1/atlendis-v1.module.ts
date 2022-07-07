ae187e61968c6dda393689004fb5c444926f27ed
FredCoen <friedrich.kaspar.coen@gmail.com>
2022-07-07 23:21:45 +0200

    remove contract position fetcher

--- src/apps/atlendis-v1/atlendis-v1.module.ts
+++ src/apps/atlendis-v1/atlendis-v1.module.ts
@@ -4,15 +4,9 @@ import { AbstractApp } from '~app/app.dynamic-module';
 import { AtlendisV1AppDefinition, ATLENDIS_V_1_DEFINITION } from './atlendis-v1.definition';
 import { AtlendisV1ContractFactory } from './contracts';
 import { PolygonAtlendisV1BalanceFetcher } from './polygon/atlendis-v1.balance-fetcher';
-import { PolygonAtlendisV1PositionContractPositionFetcher } from './polygon/atlendis-v1.position.contract-position-fetcher';
 
 @Register.AppModule({
   appId: ATLENDIS_V_1_DEFINITION.id,
-  providers: [
-    AtlendisV1AppDefinition,
-    AtlendisV1ContractFactory,
-    PolygonAtlendisV1BalanceFetcher,
-    PolygonAtlendisV1PositionContractPositionFetcher,
-  ],
+  providers: [AtlendisV1AppDefinition, AtlendisV1ContractFactory, PolygonAtlendisV1BalanceFetcher],
 })
 export class AtlendisV1AppModule extends AbstractApp() {}
--- src/apps/atlendis-v1/polygon/atlendis-v1.balance-fetcher.ts
+++ src/apps/atlendis-v1/polygon/atlendis-v1.balance-fetcher.ts
@@ -1,14 +1,20 @@
 import { Inject } from '@nestjs/common';
 import { BigNumber } from 'ethers';
+import { sumBy } from 'lodash';
 
 import { drillBalance } from '~app-toolkit';
 import { IAppToolkit, APP_TOOLKIT } from '~app-toolkit/app-toolkit.interface';
 import { Register } from '~app-toolkit/decorators';
 import { presentBalanceFetcherResponse } from '~app-toolkit/helpers/presentation/balance-fetcher-response.present';
+import { getImagesFromToken, getLabelFromToken } from '~app-toolkit/helpers/presentation/image.present';
 import { BalanceFetcher } from '~balance/balance-fetcher.interface';
+import { ContractType } from '~position/contract.interface';
+import { ContractPositionBalance } from '~position/position-balance.interface';
+import { supplied } from '~position/position.utils';
 import { Network } from '~types/network.interface';
 
 import { ATLENDIS_V_1_DEFINITION } from '../atlendis-v1.definition';
+import { GET_USER_POSITIONS } from '../graphql/getPositions';
 
 const network = Network.POLYGON_MAINNET;
 
@@ -17,28 +23,49 @@ export class PolygonAtlendisV1BalanceFetcher implements BalanceFetcher {
   constructor(@Inject(APP_TOOLKIT) private readonly appToolkit: IAppToolkit) {}
 
   async getPositionBalances(address: string) {
-    return this.appToolkit.helpers.contractPositionBalanceHelper.getContractPositionBalances({
-      address,
-      groupId: ATLENDIS_V_1_DEFINITION.groups.position.id,
-      appId: ATLENDIS_V_1_DEFINITION.id,
-      network: Network.POLYGON_MAINNET,
-      resolveBalances: async ({ contractPosition }) => {
-        const underlyingToken = contractPosition.tokens[0];
-        const position = contractPosition.dataProps as {
-          owner: string;
-          amount: string;
-          tokenId: string;
-        };
-
-        const isOwner = position.owner.toLowerCase() === address.toLowerCase();
-
-        const wad = BigNumber.from('0xde0b6b3a7640000');
-        const tokenDecimals = BigNumber.from((10 ** underlyingToken.decimals).toString());
-
-        const balance = isOwner ? BigNumber.from(position.amount).div(wad).mul(tokenDecimals).toString() : '0';
-        return [drillBalance(underlyingToken, balance)];
-      },
+    const graphHelper = this.appToolkit.helpers.theGraphHelper;
+    const data = await graphHelper.requestGraph({
+      endpoint: 'https://atlendis.herokuapp.com/graphql',
+      query: GET_USER_POSITIONS,
+      variables: { address },
     });
+    const baseTokens = await this.appToolkit.getBaseTokenPrices(network);
+
+    const positions = data.positions.map(position => {
+      const underlyingToken = baseTokens.find(
+        t => t.address.toLowerCase() === position.pool.parameters.underlyingToken.toLowerCase(),
+      );
+      if (!underlyingToken) return null;
+      const positionManagerAddress = '0x55E4e70a725C1439dac6B9412B71fC8372Bd73e9';
+
+      const positionContract = {
+        type: ContractType.POSITION,
+        appId: ATLENDIS_V_1_DEFINITION.id,
+        groupId: ATLENDIS_V_1_DEFINITION.groups.position.id,
+        address: positionManagerAddress,
+        network,
+        tokens: [supplied(underlyingToken)],
+        dataProps: {
+          tokenId: position.tokenId,
+        },
+        displayProps: {
+          label: getLabelFromToken(underlyingToken),
+          images: getImagesFromToken(underlyingToken),
+        },
+      };
+
+      const tokenBalances = [
+        drillBalance(positionContract.tokens[0], BigNumber.from(position.normalizedAmount).toString()),
+      ];
+      const contractPositionBalance: ContractPositionBalance = {
+        ...position,
+        tokens: tokenBalances,
+        balanceUSD: sumBy(tokenBalances, v => v.balanceUSD),
+      };
+
+      return contractPositionBalance;
+    });
+    return positions;
   }
 
   async getBalances(address: string) {
