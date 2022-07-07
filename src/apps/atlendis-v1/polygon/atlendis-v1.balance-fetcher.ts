709a01ad3bec84d45361d9c009c225d65f26f2ae
VGLoic <vincent.genod.loic@gmail.com>
2022-07-07 17:11:39 +0200

    feat: first iteration on implementation

--- src/apps/atlendis-v1/polygon/atlendis-v1.balance-fetcher.ts
+++ src/apps/atlendis-v1/polygon/atlendis-v1.balance-fetcher.ts
@@ -1,5 +1,7 @@
 import { Inject } from '@nestjs/common';
+import { BigNumber } from 'ethers';
 
+import { drillBalance } from '~app-toolkit';
 import { IAppToolkit, APP_TOOLKIT } from '~app-toolkit/app-toolkit.interface';
 import { Register } from '~app-toolkit/decorators';
 import { presentBalanceFetcherResponse } from '~app-toolkit/helpers/presentation/balance-fetcher-response.present';
@@ -15,10 +17,37 @@ export class PolygonAtlendisV1BalanceFetcher implements BalanceFetcher {
   constructor(@Inject(APP_TOOLKIT) private readonly appToolkit: IAppToolkit) {}
 
   async getPositionBalances(address: string) {
-    return this.appToolkit.helpers.contractPositionBalanceHelper.getContractPositionBalances();
+    return this.appToolkit.helpers.contractPositionBalanceHelper.getContractPositionBalances({
+      address,
+      groupId: ATLENDIS_V_1_DEFINITION.groups.position.id,
+      appId: ATLENDIS_V_1_DEFINITION.id,
+      network: Network.POLYGON_MAINNET,
+      resolveBalances: async ({ contractPosition }) => {
+        const underlyingToken = contractPosition.tokens[0];
+        const position = contractPosition.dataProps as {
+          owner: string;
+          amount: string;
+          tokenId: string;
+        };
+
+        const isOwner = position.owner.toLowerCase() === address.toLowerCase();
+
+        const wad = BigNumber.from('0xde0b6b3a7640000');
+        const tokenDecimals = BigNumber.from((10 ** underlyingToken.decimals).toString());
+
+        const balance = isOwner ? BigNumber.from(position.amount).div(wad).mul(tokenDecimals).toString() : '0';
+        return [drillBalance(underlyingToken, balance)];
+      },
+    });
   }
 
   async getBalances(address: string) {
-    return presentBalanceFetcherResponse([]);
+    const assets = await this.getPositionBalances(address);
+    return presentBalanceFetcherResponse([
+      {
+        label: 'Positions',
+        assets,
+      },
+    ]);
   }
 }
--- src/apps/atlendis-v1/polygon/atlendis-v1.position.contract-position-fetcher.ts
+++ src/apps/atlendis-v1/polygon/atlendis-v1.position.contract-position-fetcher.ts
@@ -1,9 +1,12 @@
 import { Inject } from '@nestjs/common';
+import { gql, GraphQLClient } from 'graphql-request';
 
 import { IAppToolkit, APP_TOOLKIT } from '~app-toolkit/app-toolkit.interface';
 import { Register } from '~app-toolkit/decorators';
+import { ContractType } from '~position/contract.interface';
 import { PositionFetcher } from '~position/position-fetcher.interface';
 import { ContractPosition } from '~position/position.interface';
+import { borrowed } from '~position/position.utils';
 import { Network } from '~types/network.interface';
 
 import { ATLENDIS_V_1_DEFINITION } from '../atlendis-v1.definition';
@@ -21,13 +24,77 @@ export class PolygonAtlendisV1PositionContractPositionFetcher implements Positio
   ) {}
 
   async getPositions() {
-    const contract = this.atlendisV1ContractFactory.positionManager({
-      address: '0x55E4e70a725C1439dac6B9412B71fC8372Bd73e9',
-      network,
-    });
+    const positionManagerAddress = '0x55E4e70a725C1439dac6B9412B71fC8372Bd73e9';
+
+    const baseTokens = await this.appToolkit.getBaseTokenPrices(network);
 
     const endpoint = 'https://atlendis.herokuapp.com/graphql';
+    const client = new GraphQLClient(endpoint);
 
-    return [];
+    const query = gql`
+      query {
+        pools {
+          hash
+          parameters {
+            underlyingToken
+          }
+          positions {
+            tokenId
+            normalizedAmount
+            lender {
+              address
+            }
+          }
+        }
+      }
+    `;
+    const { pools } = await client.request<{
+      pools: {
+        hash: string;
+        parameters: {
+          underlyingToken: string;
+        };
+        positions: {
+          tokenId: string;
+          normalizedAmount: string;
+          lender: {
+            address: string;
+          };
+        }[];
+      }[];
+    }>(query);
+
+    const tokens = pools.map(pool => {
+      const token = baseTokens.find(t => t.address.toLowerCase() === pool.parameters.underlyingToken.toLowerCase());
+      if (!token) {
+        throw new Error("Oh no, it's broken");
+      }
+      return token;
+    });
+
+    const positionsPerPool = pools.map((pool, poolIndex) => {
+      const positions = pool.positions.map(
+        (position): ContractPosition => ({
+          type: ContractType.POSITION,
+          appId,
+          groupId,
+          address: positionManagerAddress,
+          network,
+          tokens: [borrowed(tokens[poolIndex])],
+          dataProps: {
+            tokenId: position.tokenId,
+            amount: position.normalizedAmount,
+            owner: position.lender.address,
+          },
+          displayProps: {
+            label: position.tokenId,
+            images: [],
+          },
+        }),
+      );
+      return positions;
+    });
+
+    return positionsPerPool.reduce((acc, positions) => [...acc, ...positions], []);
   }
 }
