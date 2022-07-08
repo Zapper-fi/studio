926bbbd8e48f6643d3717cbcfbe7bf0b4640bce3
Karan Shahani <karan.shahani91@gmail.com>
2022-07-08 11:53:13 -0400

    fix(lyra-avalon): Add unique keys on Lyra Avalon positions

--- src/position/position.module.ts
+++ src/position/position.module.ts
@@ -5,14 +5,21 @@ import { CacheModule } from '~cache/cache.module';
 
 import { PositionBalanceFetcherRegistry } from './position-balance-fetcher.registry';
 import { PositionFetcherRegistry } from './position-fetcher.registry';
+import { PositionKeyService } from './position-key.service';
 import { PositionSources } from './position-source';
 import { PositionController } from './position.controller';
 import { PositionService } from './position.service';
 
 @Module({
   imports: [DiscoveryModule, CacheModule],
-  providers: [...PositionSources, PositionService, PositionFetcherRegistry, PositionBalanceFetcherRegistry],
+  providers: [
+    ...PositionSources,
+    PositionService,
+    PositionFetcherRegistry,
+    PositionBalanceFetcherRegistry,
+    PositionKeyService,
+  ],
   controllers: [PositionController],
-  exports: [PositionService, PositionFetcherRegistry, PositionBalanceFetcherRegistry],
+  exports: [PositionService, PositionFetcherRegistry, PositionBalanceFetcherRegistry, PositionKeyService],
 })
 export class PositionModule {}
