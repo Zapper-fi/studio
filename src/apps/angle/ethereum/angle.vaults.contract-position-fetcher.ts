33186f91b54aa8ea33453a138f0f2f4aedecb492
William Poulin <williampoulin01@gmail.com>
2022-07-12 17:54:05 -0400

    chore(angle): refactor vault contract position using on chain data

--- src/apps/angle/ethereum/angle.vaults.contract-position-fetcher.ts
+++ src/apps/angle/ethereum/angle.vaults.contract-position-fetcher.ts
@@ -12,7 +12,12 @@ import { Network } from '~types/network.interface';
 
 import { ANGLE_DEFINITION } from '../angle.definition';
 import { AngleContractFactory } from '../contracts';
-import { AngleApiHelper } from '../helpers/angle.api';
+
+const vaultAddresses = [
+  '0x241d7598bd1eb819c0e9ded456acb24aca623679', // wBTC
+  '0x1bece8193f8dc2b170135da9f1fa8b81c7ad18b1', // wETH
+  '0x73aaf8694ba137a7537e7ef544fcf5e2475f227b', // wsETH
+];
 
 const appId = ANGLE_DEFINITION.id;
 const groupId = ANGLE_DEFINITION.groups.vaults.id;
@@ -23,44 +28,68 @@ export class EthereumAngleVaultsContractPositionFetcher implements PositionFetch
   constructor(
     @Inject(APP_TOOLKIT) private readonly appToolkit: IAppToolkit,
     @Inject(AngleContractFactory) private readonly angleContractFactory: AngleContractFactory,
-    @Inject(AngleApiHelper)
-    private readonly angleApiHelper: AngleApiHelper,
   ) {}
 
   async getPositions() {
-    const baseTokenDependencies = await this.appToolkit.getBaseTokenPrices(network);
+    const multicall = this.appToolkit.getMulticall(network);
+    const baseTokens = await this.appToolkit.getBaseTokenPrices(network);
 
-    const vaultManagers = Object.values(await this.angleApiHelper.getVaultManagers());
+    const positions = await Promise.all(
+      vaultAddresses.map(async address => {
+        const angleVaultContract = this.angleContractFactory.angleVaultManager({
+          address,
+          network,
+        });
 
-    const positions = vaultManagers.map(vaultManager => {
-      const collateralToken = baseTokenDependencies.find(
-        v => v.address.toLowerCase() === vaultManager.collateral.toLowerCase(),
-      );
-      const stableToken = baseTokenDependencies.find(
-        v => v.address.toLowerCase() === vaultManager.stablecoin.toLowerCase(),
-      );
-      if (!collateralToken || !stableToken) return null;
+        const [baseFactor, collateralTokenAddressRaw, stableTokenAddressRaw, totalDebtRaw, collateralFactorRaw] =
+          await Promise.all([
+            multicall.wrap(angleVaultContract).BASE_PARAMS(),
+            multicall.wrap(angleVaultContract).collateral(),
+            multicall.wrap(angleVaultContract).stablecoin(),
+            multicall.wrap(angleVaultContract).getTotalDebt(),
+            multicall.wrap(angleVaultContract).collateralFactor(),
+          ]);
 
-      const position: ContractPosition = {
-        type: ContractType.POSITION,
-        appId,
-        groupId,
-        address: vaultManager.address,
-        network,
-        tokens: [supplied(collateralToken), borrowed(stableToken)],
-        dataProps: {
-          totalDebt: vaultManager.totalDebt,
-          totalCollateral: vaultManager.totalCollateral,
-          maxLTV: vaultManager.maxLTV,
-        },
-        displayProps: {
-          label: `${getLabelFromToken(collateralToken)} - ${getLabelFromToken(stableToken)} vault`,
-          images: [...getImagesFromToken(collateralToken), ...getImagesFromToken(stableToken)],
-        },
-      };
+        const collateralToken = baseTokens.find(v => v.address === collateralTokenAddressRaw.toLowerCase());
+        const stableToken = baseTokens.find(v => v.address === stableTokenAddressRaw.toLowerCase());
+        if (!collateralToken || !stableToken) return null;
 
-      return position;
-    });
+        const totalDebt = Number(totalDebtRaw) / 10 ** collateralToken.decimals;
+        const maxLTV = Number(collateralFactorRaw) / Number(baseFactor);
+
+        const collateralTokenContract = this.angleContractFactory.erc20({
+          address: collateralTokenAddressRaw.toLowerCase(),
+          network,
+        });
+
+        const [balanceOfRaw, decimals] = await Promise.all([
+          multicall.wrap(collateralTokenContract).balanceOf(address),
+          multicall.wrap(collateralTokenContract).decimals(),
+        ]);
+
+        const totalCollateral = Number(balanceOfRaw) / 10 ** decimals;
+
+        const position: ContractPosition = {
+          type: ContractType.POSITION,
+          appId,
+          groupId,
+          address,
+          network,
+          tokens: [supplied(collateralToken), borrowed(stableToken)],
+          dataProps: {
+            totalDebt,
+            totalCollateral,
+            maxLTV,
+          },
+          displayProps: {
+            label: getLabelFromToken(collateralToken),
+            images: [...getImagesFromToken(collateralToken), ...getImagesFromToken(stableToken)],
+          },
+        };
+
+        return position;
+      }),
+    );
 
     return compact(positions);
   }
