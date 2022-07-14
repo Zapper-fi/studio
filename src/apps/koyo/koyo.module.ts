d1bfb86f9ee411f665e8cb3b9457609b673c3f96
p7m <perpetuum7@protonmail.com>
2022-07-14 14:50:47 +0200

    fix: only import required modules

--- src/apps/koyo/koyo.module.ts
+++ src/apps/koyo/koyo.module.ts
@@ -1,24 +1,26 @@
 import { Register } from '~app-toolkit/decorators';
 import { AbstractApp } from '~app/app.dynamic-module';
-import { BalancerV2AppModule } from '~apps/balancer-v2';
+import { BalancerV2ContractFactory } from '~apps/balancer-v2/contracts/index';
+import { BalancerV2PoolTokensHelper } from '~apps/balancer-v2/helpers/balancer-v2.pool.token-helper';
+import { BalancerV2TheGraphPoolTokenDataStrategy } from '~apps/balancer-v2/helpers/balancer-v2.the-graph.pool-token-address-strategy';
 
 import { AuroraKoyoBalanceFetcher } from './aurora/koyo.balance-fetcher';
 import { AuroraKoyoPoolTokenFetcher } from './aurora/koyo.pool.token-fetcher';
-import { KoyoContractFactory } from './contracts';
 import { KoyoAppDefinition, KOYO_DEFINITION } from './koyo.definition';
 import { MoonriverKoyoBalanceFetcher } from './moonriver/koyo.balance-fetcher';
 import { MoonriverKoyoPoolTokenFetcher } from './moonriver/koyo.pool.token-fetcher';
 
 @Register.AppModule({
   appId: KOYO_DEFINITION.id,
-  imports: [BalancerV2AppModule],
   providers: [
+    BalancerV2ContractFactory,
+    BalancerV2TheGraphPoolTokenDataStrategy,
+    BalancerV2PoolTokensHelper,
     AuroraKoyoPoolTokenFetcher,
     AuroraKoyoBalanceFetcher,
     MoonriverKoyoPoolTokenFetcher,
     MoonriverKoyoBalanceFetcher,
     KoyoAppDefinition,
-    KoyoContractFactory,
   ],
 })
 export class KoyoAppModule extends AbstractApp() {}
