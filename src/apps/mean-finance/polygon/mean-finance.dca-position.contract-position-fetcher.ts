60a546fc430901a3ec51c0de5759134b998783c9
FiboApe <fiboape@gmail.com>
2022-07-19 16:18:03 -0300

    fix(mean-finance): fix mean-finance tvl view

--- src/apps/mean-finance/polygon/mean-finance.dca-position.contract-position-fetcher.ts
+++ src/apps/mean-finance/polygon/mean-finance.dca-position.contract-position-fetcher.ts
@@ -1,5 +1,6 @@
 import { Inject } from '@nestjs/common';
-import { sumBy } from 'lodash';
+import { BigNumber } from 'ethers';
+import { keyBy, sumBy } from 'lodash';
 
 import { IAppToolkit, APP_TOOLKIT } from '~app-toolkit/app-toolkit.interface';
 import { Register } from '~app-toolkit/decorators';
@@ -10,97 +11,130 @@ import { WithMetaType } from '~position/display.interface';
 import { BaseTokenBalance } from '~position/position-balance.interface';
 import { PositionFetcher } from '~position/position-fetcher.interface';
 import { ContractPosition } from '~position/position.interface';
-import { claimable } from '~position/position.utils';
+import { BaseToken } from '~position/token.interface';
 import { Network } from '~types/network.interface';
 
-import { getPositions } from '../helpers/graph';
-import { STRING_SWAP_INTERVALS } from '../helpers/intervals';
+import { getPairs, getPositions } from '../helpers/graph';
+import { sortTokens } from '../helpers/tokens';
 import { MEAN_FINANCE_DEFINITION } from '../mean-finance.definition';
 
 const appId = MEAN_FINANCE_DEFINITION.id;
 const groupId = MEAN_FINANCE_DEFINITION.groups.dcaPosition.id;
 const network = Network.POLYGON_MAINNET;
 
+interface ExtendedContractPosition extends ContractPosition {
+  tokenA?: BaseToken;
+  tokenB?: BaseToken;
+  tokenALiquidity: BigNumber;
+  tokenBLiquidity: BigNumber;
+  positions: number;
+  id: string;
+}
+
 @Register.ContractPositionFetcher({ appId, groupId, network })
 export class PolygonMeanFinanceDcaPositionContractPositionFetcher implements PositionFetcher<ContractPosition> {
-  constructor(@Inject(APP_TOOLKIT) private readonly appToolkit: IAppToolkit) {}
+  constructor(@Inject(APP_TOOLKIT) private readonly appToolkit: IAppToolkit) { }
 
   async getPositions() {
     const graphHelper = this.appToolkit.helpers.theGraphHelper;
-    const data = await getPositions(Network.POLYGON_MAINNET, graphHelper);
+    const positionsData = await getPositions(network, graphHelper);
+    const pairsData = await getPairs(network, graphHelper);
     const baseTokens = await this.appToolkit.getBaseTokenPrices(network);
-    const positions = data.positions;
+    const pairs = pairsData.pairs;
+    const positions = positionsData.positions;
     const dcaHubAddress = '0x059d306a25c4ce8d7437d25743a8b94520536bd5';
 
-    const contractPositions: ContractPosition[] = positions.map(dcaPosition => {
-      const toWithdraw = dcaPosition.current.idleSwapped;
-      const remainingLiquidity = dcaPosition.current.remainingLiquidity;
-      const remainingSwaps = Number(dcaPosition.current.remainingSwaps);
-      const swapInterval = Number(dcaPosition.swapInterval.interval) as keyof typeof STRING_SWAP_INTERVALS;
-      const rawRate = dcaPosition.current.rate;
-      const rate = Number(rawRate) / 10 ** Number(dcaPosition.from.decimals);
-      let formattedRate = rate.toFixed(3);
+    const contractPositionsByKey: Record<string, ExtendedContractPosition> = keyBy(pairs.map(pair => {
+      const tokenA = baseTokens.find(v => v.address === pair.tokenA.address);
+      const tokenB = baseTokens.find(v => v.address === pair.tokenB.address);
 
-      if (rate < 0.001) {
-        formattedRate = '<0.001';
-      }
-
-      const from = baseTokens.find(v => v.address === dcaPosition.from.address);
-      const to = baseTokens.find(v => v.address === dcaPosition.to.address);
-
-      const tokens: WithMetaType<BaseTokenBalance>[] = [];
       let images: string[] = [];
-      if (from) {
-        from.network = network;
-        tokens.push(drillBalance(from, remainingLiquidity));
-        images = [...images, ...getImagesFromToken(from)];
+
+      if (tokenA) {
+        tokenA.network = network;
+        images = [...images, ...getImagesFromToken(tokenA)];
       }
-      if (to) {
-        to.network = network;
-        tokens.push(drillBalance(claimable(to), toWithdraw));
-        images = [...images, ...getImagesFromToken(to)];
+      if (tokenB) {
+        tokenB.network = network;
+        images = [...images, ...getImagesFromToken(tokenB)];
       }
-
-      const balanceUSD = sumBy(tokens, t => t.balanceUSD);
-
-      const swapIntervalAdverb = STRING_SWAP_INTERVALS[swapInterval].adverb;
-      let label = '';
-
-      if (remainingSwaps > 0) {
-        label = `Swapping ~${formattedRate} ${from?.symbol || dcaPosition.from.symbol} ${swapIntervalAdverb} to ${
-          to?.symbol || dcaPosition.from.symbol
-        }`;
-      } else {
-        label = `Swapping ${from?.symbol || dcaPosition.from.symbol} to ${to?.symbol || dcaPosition.from.symbol}`;
-      }
-      const secondaryLabel =
-        remainingSwaps && STRING_SWAP_INTERVALS[swapInterval]
-          ? `${STRING_SWAP_INTERVALS[swapInterval].plural(remainingSwaps)} left`
-          : 'Position finished';
-
-      const position: ContractPosition = {
+      const position: ExtendedContractPosition = {
         type: ContractType.POSITION,
         address: dcaHubAddress,
         appId,
         groupId,
         network,
-        tokens,
+        tokens: [],
+        id: pair.id,
+        tokenA,
+        tokenB,
+        tokenALiquidity: BigNumber.from('0'),
+        tokenBLiquidity: BigNumber.from('0'),
+        positions: 0,
         dataProps: {
-          id: dcaPosition.id,
-          toWithdraw,
-          remainingLiquidity,
-          remainingSwaps,
-          liquidity: balanceUSD,
+          id: pair.id,
         },
         displayProps: {
-          label,
-          secondaryLabel,
+          label: `${pair.tokenA.symbol}:${pair.tokenB.symbol} pair`,
           images,
         },
       };
 
       position.key = this.appToolkit.getPositionKey(position, ['id']);
       return position;
+    }), 'id');
+
+    positions.forEach(dcaPosition => {
+      const [tokenAAddress, tokenBAddress] = sortTokens(dcaPosition.from.address, dcaPosition.to.address);
+
+      const toWithdraw = dcaPosition.current.idleSwapped;
+      const remainingLiquidity = dcaPosition.current.remainingLiquidity;
+
+      contractPositionsByKey[`${tokenAAddress}-${tokenBAddress}`].positions += 1;
+
+      if (dcaPosition.from.address === tokenAAddress) {
+        contractPositionsByKey[`${tokenAAddress}-${tokenBAddress}`].tokenALiquidity = contractPositionsByKey[`${tokenAAddress}-${tokenBAddress}`].tokenALiquidity.add(BigNumber.from(remainingLiquidity));
+      } else {
+        contractPositionsByKey[`${tokenAAddress}-${tokenBAddress}`].tokenBLiquidity = contractPositionsByKey[`${tokenAAddress}-${tokenBAddress}`].tokenBLiquidity.add(BigNumber.from(remainingLiquidity));
+      }
+      if (dcaPosition.to.address === tokenAAddress) {
+        contractPositionsByKey[`${tokenAAddress}-${tokenBAddress}`].tokenALiquidity = contractPositionsByKey[`${tokenAAddress}-${tokenBAddress}`].tokenALiquidity.add(BigNumber.from(toWithdraw));
+      } else {
+        contractPositionsByKey[`${tokenAAddress}-${tokenBAddress}`].tokenBLiquidity = contractPositionsByKey[`${tokenAAddress}-${tokenBAddress}`].tokenBLiquidity.add(BigNumber.from(toWithdraw));
+      }
+    });
+
+    const contractPositions: ContractPosition[] = Object.values(contractPositionsByKey).map(position => {
+      const tokens: WithMetaType<BaseTokenBalance>[] = [];
+
+      if (position.tokenA) {
+        tokens.push(drillBalance(position.tokenA, position.tokenALiquidity.toString()));
+      }
+      if (position.tokenB) {
+        tokens.push(drillBalance(position.tokenB, position.tokenBLiquidity.toString()));
+      }
+
+      const balanceUSD = sumBy(tokens, t => t.balanceUSD);
+
+      return {
+        type: ContractType.POSITION,
+        address: dcaHubAddress,
+        appId,
+        groupId,
+        network,
+        tokens,
+        positions: 0,
+        dataProps: {
+          id: position.dataProps.id,
+          liquidity: balanceUSD,
+          tokenALiquidity: position.tokenALiquidity.toString(),
+          tokenBLiquidity: position.tokenBLiquidity.toString(),
+        },
+        displayProps: {
+          ...position.displayProps,
+          secondaryLabel: `${position.positions} active positions`
+        },
+      }
     });
 
     return contractPositions;
