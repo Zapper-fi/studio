4ed36769d680e4531e2e8f26bb5239bad95b6b46
Papa Sougou Wele <papis.wele@gmail.com>
2022-07-19 10:16:35 -0400

    feat(bastion): split markets into different Realm (#900)

---
+++ src/apps/bastion-protocol/aurora/bastion-protocol.borrow-aurora-ecosystem.contract-position-fetcher.ts
@@ -0,0 +1,32 @@
+import { Inject } from '@nestjs/common';
+
+import { Register } from '~app-toolkit/decorators';
+import { PositionFetcher } from '~position/position-fetcher.interface';
+import { ContractPosition } from '~position/position.interface';
+import { Network } from '~types/network.interface';
+
+import { BASTION_PROTOCOL_DEFINITION } from '../bastion-protocol.definition';
+import { BastionBorrowContractPositionHelper } from '../helper/bastion-protocol.borrow.contract-position-helper';
+
+const appId = BASTION_PROTOCOL_DEFINITION.id;
+const groupId = BASTION_PROTOCOL_DEFINITION.groups.borrowAuroraEcosystem.id;
+const network = Network.AURORA_MAINNET;
+
+@Register.ContractPositionFetcher({ appId, groupId, network })
+export class AuroraBastionProtocolBorrowAuroraEcosystemContractPositionFetcher
+  implements PositionFetcher<ContractPosition>
+{
+  constructor(
+    @Inject(BastionBorrowContractPositionHelper)
+    private readonly bastionBorrowContractPositionHelper: BastionBorrowContractPositionHelper,
+  ) {}
+
+  async getPositions() {
+    return this.bastionBorrowContractPositionHelper.getPositions({
+      network,
+      appId,
+      groupId,
+      supplyGroupId: BASTION_PROTOCOL_DEFINITION.groups.supplyAuroraEcosystem.id,
+    });
+  }
+}
---
+++ src/apps/bastion-protocol/aurora/bastion-protocol.borrow-multichain.contract-position-fetcher.ts
@@ -0,0 +1,30 @@
+import { Inject } from '@nestjs/common';
+
+import { Register } from '~app-toolkit/decorators';
+import { PositionFetcher } from '~position/position-fetcher.interface';
+import { ContractPosition } from '~position/position.interface';
+import { Network } from '~types/network.interface';
+
+import { BASTION_PROTOCOL_DEFINITION } from '../bastion-protocol.definition';
+import { BastionBorrowContractPositionHelper } from '../helper/bastion-protocol.borrow.contract-position-helper';
+
+const appId = BASTION_PROTOCOL_DEFINITION.id;
+const groupId = BASTION_PROTOCOL_DEFINITION.groups.borrowMultichain.id;
+const network = Network.AURORA_MAINNET;
+
+@Register.ContractPositionFetcher({ appId, groupId, network })
+export class AuroraBastionProtocolBorrowMultichainContractPositionFetcher implements PositionFetcher<ContractPosition> {
+  constructor(
+    @Inject(BastionBorrowContractPositionHelper)
+    private readonly bastionBorrowContractPositionHelper: BastionBorrowContractPositionHelper,
+  ) {}
+
+  async getPositions() {
+    return this.bastionBorrowContractPositionHelper.getPositions({
+      network,
+      appId,
+      groupId,
+      supplyGroupId: BASTION_PROTOCOL_DEFINITION.groups.supplyMultichain.id,
+    });
+  }
+}
