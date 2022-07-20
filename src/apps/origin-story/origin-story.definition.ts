021d455c64a3b07a7630b9e654bb6fefc13b2ead
Mike Shultz <shultzm@gmail.com>
2022-07-19 21:00:28 -0600

    feat(origin-story): adds origin-story app (OGN staking) (#907)
    Co-authored-by: immasandwich <karan.shahani91@gmail.com>

---
+++ src/apps/origin-story/origin-story.definition.ts
@@ -0,0 +1,38 @@
+import { Register } from '~app-toolkit/decorators';
+import { appDefinition, AppDefinition } from '~app/app.definition';
+import { AppAction, AppTag, GroupType } from '~app/app.interface';
+import { Network } from '~types/network.interface';
+
+export const ORIGIN_STORY_DEFINITION = appDefinition({
+  id: 'origin-story',
+  name: 'Origin Story',
+  description: 'Origin Story is where top creators launch their NFTs',
+  url: 'https://www.story.xyz',
+
+  groups: {
+    series: {
+      id: 'series',
+      type: GroupType.POSITION,
+      label: 'Series',
+    },
+  },
+
+  tags: [AppTag.FARMING, AppTag.NFT_MARKETPLACE, AppTag.STAKING],
+  keywords: [],
+  links: {},
+
+  supportedNetworks: {
+    [Network.ETHEREUM_MAINNET]: [AppAction.VIEW],
+  },
+
+  primaryColor: '#fff',
+});
+
+@Register.AppDefinition(ORIGIN_STORY_DEFINITION.id)
+export class OriginStoryAppDefinition extends AppDefinition {
+  constructor() {
+    super(ORIGIN_STORY_DEFINITION);
+  }
+}
+
+export default ORIGIN_STORY_DEFINITION;
