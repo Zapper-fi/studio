968e48d8b7620909b939c3c1e66dd93d776f46ff
peteris <104527020+peteriscan@users.noreply.github.com>
2022-07-22 15:24:35 +0200

    feat(rocket-pool): Add Rocket Pool staked ETH (#930)

--- src/apps/rocket-pool/contracts/index.ts
+++ src/apps/rocket-pool/contracts/index.ts
@@ -5,6 +5,7 @@ import { ContractFactory } from '~contract/contracts';
 import { Network } from '~types/network.interface';
 
 import { RocketDaoNodeTrusted__factory } from './ethers';
+import { RocketMinipoolManager__factory } from './ethers';
 import { RocketNodeStaking__factory } from './ethers';
 
 // eslint-disable-next-line
@@ -19,10 +20,14 @@ export class RocketPoolContractFactory extends ContractFactory {
   rocketDaoNodeTrusted({ address, network }: ContractOpts) {
     return RocketDaoNodeTrusted__factory.connect(address, this.appToolkit.getNetworkProvider(network));
   }
+  rocketMinipoolManager({ address, network }: ContractOpts) {
+    return RocketMinipoolManager__factory.connect(address, this.appToolkit.getNetworkProvider(network));
+  }
   rocketNodeStaking({ address, network }: ContractOpts) {
     return RocketNodeStaking__factory.connect(address, this.appToolkit.getNetworkProvider(network));
   }
 }
 
 export type { RocketDaoNodeTrusted } from './ethers';
+export type { RocketMinipoolManager } from './ethers';
 export type { RocketNodeStaking } from './ethers';
--- src/apps/rocket-pool/ethereum/rocket-pool.staking.contract-position-fetcher.ts
+++ src/apps/rocket-pool/ethereum/rocket-pool.staking.contract-position-fetcher.ts
@@ -1,6 +1,7 @@
 import { Inject } from '@nestjs/common';
 
 import { IAppToolkit, APP_TOOLKIT } from '~app-toolkit/app-toolkit.interface';
+import { ZERO_ADDRESS } from '~app-toolkit/constants/address';
 import { Register } from '~app-toolkit/decorators';
 import { getImagesFromToken } from '~app-toolkit/helpers/presentation/image.present';
 import { ContractType } from '~position/contract.interface';
@@ -11,11 +12,13 @@ import { Network } from '~types/network.interface';
 
 import { ROCKET_POOL_DEFINITION } from '../rocket-pool.definition';
 
+export const rocketNodeStakingAddress = '0x3019227b2b8493e45bf5d25302139c9a2713bf15';
+export const rocketMinipoolManagerAddress = '0x6293B8abC1F36aFB22406Be5f96D893072A8cF3a';
+export const rocketTokenRPLAddress = '0xd33526068d116ce69f19a9ee46f0bd304f21a51f';
+
 const appId = ROCKET_POOL_DEFINITION.id;
 const groupId = ROCKET_POOL_DEFINITION.groups.staking.id;
 const network = Network.ETHEREUM_MAINNET;
-const rocketNodeStakingAddress = '0x3019227b2b8493e45bf5d25302139c9a2713bf15';
-const rocketTokenRPLAddress = '0xd33526068d116ce69f19a9ee46f0bd304f21a51f';
 
 @Register.ContractPositionFetcher({ appId, groupId, network })
 export class EthereumRocketPoolStakingContractPositionFetcher implements PositionFetcher<ContractPosition> {
@@ -23,9 +26,24 @@ export class EthereumRocketPoolStakingContractPositionFetcher implements Positio
 
   async getPositions() {
     const baseTokens = await this.appToolkit.getBaseTokenPrices(network);
+    const eth = baseTokens.find(v => v.address === ZERO_ADDRESS)!;
     const rpl = baseTokens.find(v => v.address === rocketTokenRPLAddress)!;
 
-    const position: ContractPosition = {
+    const ethposition: ContractPosition = {
+      type: ContractType.POSITION,
+      address: rocketMinipoolManagerAddress,
+      network,
+      appId,
+      groupId,
+      tokens: [supplied(eth)],
+      dataProps: {},
+      displayProps: {
+        label: `Staked ETH`,
+        images: getImagesFromToken(eth),
+      },
+    };
+
+    const rplposition: ContractPosition = {
       type: ContractType.POSITION,
       address: rocketNodeStakingAddress,
       network,
@@ -39,6 +57,6 @@ export class EthereumRocketPoolStakingContractPositionFetcher implements Positio
       },
     };
 
-    return [position];
+    return [ethposition, rplposition];
   }
 }
