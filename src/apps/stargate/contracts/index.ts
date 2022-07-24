d3a9d47888124b0c28aa2e45956371e8aa353448
immasandwich <karan.shahani91@gmail.com>
2022-07-24 18:30:53 -0400

    feat(stargate): Extract Stargate to Studio (#943)

---
+++ src/apps/stargate/contracts/index.ts
@@ -0,0 +1,33 @@
+import { Injectable, Inject } from '@nestjs/common';
+
+import { IAppToolkit, APP_TOOLKIT } from '~app-toolkit/app-toolkit.interface';
+import { ContractFactory } from '~contract/contracts';
+import { Network } from '~types/network.interface';
+
+import { StargateChef__factory } from './ethers';
+import { StargateFactory__factory } from './ethers';
+import { StargatePool__factory } from './ethers';
+
+// eslint-disable-next-line
+type ContractOpts = { address: string; network: Network };
+
+@Injectable()
+export class StargateContractFactory extends ContractFactory {
+  constructor(@Inject(APP_TOOLKIT) protected readonly appToolkit: IAppToolkit) {
+    super((network: Network) => appToolkit.getNetworkProvider(network));
+  }
+
+  stargateChef({ address, network }: ContractOpts) {
+    return StargateChef__factory.connect(address, this.appToolkit.getNetworkProvider(network));
+  }
+  stargateFactory({ address, network }: ContractOpts) {
+    return StargateFactory__factory.connect(address, this.appToolkit.getNetworkProvider(network));
+  }
+  stargatePool({ address, network }: ContractOpts) {
+    return StargatePool__factory.connect(address, this.appToolkit.getNetworkProvider(network));
+  }
+}
+
+export type { StargateChef } from './ethers';
+export type { StargateFactory } from './ethers';
+export type { StargatePool } from './ethers';
