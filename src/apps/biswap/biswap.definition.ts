2ec731d1984bcc7a25a368886731ad41cfce6f0a
William Poulin <williampoulin01@gmail.com>
2022-07-25 10:24:49 -0400

    feat(biswap): Added Biswap on BSC (pools and farms) (#945)

---
+++ src/apps/biswap/biswap.definition.ts
@@ -0,0 +1,29 @@
+import { Register } from '~app-toolkit/decorators';
+import { appDefinition, AppDefinition } from '~app/app.definition';
+import { GroupType, AppTag, AppAction } from '~app/app.interface';
+import { Network } from '~types/network.interface';
+
+export const BISWAP_DEFINITION = appDefinition({
+  id: 'biswap',
+  name: 'Biswap',
+  description: `Enjoy the lowest exchange fee, profitable features & Multi-type Referral program`,
+  groups: {
+    pool: { id: 'pool', type: GroupType.TOKEN, label: 'Pools' },
+    farm: { id: 'farm', type: GroupType.POSITION, label: 'Farms' },
+  },
+  url: 'https://app.beets.fi/',
+  links: {
+    github: 'https://github.com/biswap-org',
+    twitter: 'https://twitter.com/Biswap_DEX',
+    medium: 'https://biswap-dex.medium.com/',
+  },
+  tags: [AppTag.DECENTRALIZED_EXCHANGE],
+  supportedNetworks: { [Network.BINANCE_SMART_CHAIN_MAINNET]: [AppAction.VIEW] },
+});
+
+@Register.AppDefinition(BISWAP_DEFINITION.id)
+export class BiswapAppDefinition extends AppDefinition {
+  constructor() {
+    super(BISWAP_DEFINITION);
+  }
+}
