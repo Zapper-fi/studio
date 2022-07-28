16d506028dc37a77d7b67d94e576429565c361ab
Justin D'Errico <justin.derrico1991@gmail.com>
2022-07-28 17:26:23 -0400

    fix

--- src/cache/cache-on-interval.service.ts
+++ src/cache/cache-on-interval.service.ts
@@ -26,7 +26,7 @@ export class CacheOnIntervalService implements OnModuleInit, OnModuleDestroy {
     @Inject(Reflector) private readonly reflector: Reflector,
   ) {}
 
-  async onModuleInit() {
+  onModuleInit() {
     const instanceWrappers = this.discoveryService.getProviders();
     instanceWrappers
       .filter(wrapper => wrapper.isDependencyTreeStatic() && !!wrapper.instance)
