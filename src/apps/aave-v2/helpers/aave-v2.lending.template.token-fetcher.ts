d3a8a521008999f7eff6f8f5aa49b6f782e9a637
Karan Shahani <karan.shahani91@gmail.com>
2022-08-03 15:25:36 -0400

    feat(positions): Implement AppToken template and use it in Aave V2 forks

---
+++ src/apps/aave-v2/helpers/aave-v2.lending.template.token-fetcher.ts
@@ -0,0 +1,144 @@
+import { Inject } from '@nestjs/common';
+
+import { APP_TOOLKIT, IAppToolkit } from '~app-toolkit/app-toolkit.interface';
+import { getLabelFromToken } from '~app-toolkit/helpers/presentation/image.present';
+import {
+  AppTokenTemplatePositionFetcher,
+  DataPropsStageParams,
+  DisplayPropsStageParams,
+} from '~position/template/app-token.template.position-fetcher';
+
+import { AaveV2ContractFactory } from '../contracts';
+import { AaveV2AToken } from '../contracts/ethers/AaveV2AToken';
+
+export type AaveAmmTemplateTokenDataProps = {
+  apy: number;
+  enabledAsCollateral: boolean;
+  liquidity: number;
+  liquidationThreshold: number;
+  isActive: boolean;
+};
+
+export type AaveAmmReserveTokenAddressesData = {
+  aTokenAddress: string;
+  stableDebtTokenAddress: string;
+  variableDebtTokenAddress: string;
+};
+
+export type AaveAmmReserveApyData = {
+  supplyApy: number;
+  stableBorrowApy: number;
+  variableBorrowApy: number;
+};
+
+export type AaveAmmReserveConfigurationData = {
+  enabledAsCollateral: boolean;
+  liquidationThreshold: number;
+};
+
+export type AaveV2LendingTokenDataProps = {
+  apy: number;
+  enabledAsCollateral: boolean;
+  liquidity: number;
+  liquidationThreshold: number;
+};
+
+export abstract class AaveV2LendingTemplateTokenFetcher extends AppTokenTemplatePositionFetcher<
+  AaveV2AToken,
+  AaveV2LendingTokenDataProps
+> {
+  constructor(
+    @Inject(APP_TOOLKIT) protected readonly appToolkit: IAppToolkit,
+    @Inject(AaveV2ContractFactory) protected readonly contractFactory: AaveV2ContractFactory,
+  ) {
+    super(appToolkit);
+  }
+
+  abstract providerAddress: string;
+  abstract getTokenAddress(reserveTokenAddressesData: AaveAmmReserveTokenAddressesData): string;
+  abstract getApy(reserveApyData: AaveAmmReserveApyData): number;
+
+  getContract(address: string): AaveV2AToken {
+    return this.contractFactory.aaveV2AToken({ network: this.network, address });
+  }
+
+  async getAddresses(): Promise<string[]> {
+    const multicall = this.appToolkit.getMulticall(this.network);
+    const pool = multicall.wrap(
+      this.contractFactory.aaveProtocolDataProvider({
+        network: this.network,
+        address: this.providerAddress,
+      }),
+    );
+
+    const reserveTokens = await pool.getAllReservesTokens();
+    const reserveTokenAddreses = reserveTokens.map(v => v[1]);
+    const reserveTokensData = await Promise.all(reserveTokenAddreses.map(r => pool.getReserveTokensAddresses(r)));
+
+    const reserveTokenAddressesData = reserveTokensData.map(v => ({
+      aTokenAddress: v.aTokenAddress.toLowerCase(),
+      stableDebtTokenAddress: v.stableDebtTokenAddress.toLowerCase(),
+      variableDebtTokenAddress: v.variableDebtTokenAddress.toLowerCase(),
+    }));
+
+    return reserveTokenAddressesData.map(v => this.getTokenAddress(v));
+  }
+
+  async getReserveApy({
+    appToken,
+    multicall,
+  }: DataPropsStageParams<AaveV2AToken, AaveV2LendingTokenDataProps>): Promise<number> {
+    const pool = multicall.wrap(
+      this.contractFactory.aaveProtocolDataProvider({
+        network: this.network,
+        address: this.providerAddress,
+      }),
+    );
+
+    const reservesData = await pool.getReserveData(appToken.tokens[0].address);
+
+    return this.getApy({
+      supplyApy: Number(reservesData.liquidityRate) / 10 ** 27,
+      stableBorrowApy: Number(reservesData.stableBorrowRate) / 10 ** 27,
+      variableBorrowApy: Number(reservesData.variableBorrowRate) / 10 ** 27,
+    });
+  }
+
+  async getReserveConfigurationData({
+    appToken,
+    multicall,
+  }: DataPropsStageParams<AaveV2AToken, AaveAmmTemplateTokenDataProps>): Promise<AaveAmmReserveConfigurationData> {
+    const pool = this.contractFactory.aaveProtocolDataProvider({
+      network: this.network,
+      address: this.providerAddress,
+    });
+
+    const mcPool = multicall.wrap(pool);
+    const reserveConfigurationData = await mcPool.getReserveConfigurationData(appToken.tokens[0].address);
+    const liquidationThreshold = Number(reserveConfigurationData.liquidationThreshold) / 10 ** 4;
+    const enabledAsCollateral = reserveConfigurationData.usageAsCollateralEnabled;
+
+    return { liquidationThreshold, enabledAsCollateral };
+  }
+
+  async getDataProps(opts: DataPropsStageParams<AaveV2AToken, AaveV2LendingTokenDataProps>) {
+    const reserveConfigData = await this.getReserveConfigurationData(opts);
+    const apy = await this.getReserveApy(opts);
+
+    const { appToken } = opts;
+    const liquidity = -1 * appToken.price * appToken.supply;
+    const isActive = liquidity > 0;
+
+    return { liquidity, isActive, apy, ...reserveConfigData };
+  }
+
+  async getLabel({ appToken }: DisplayPropsStageParams<AaveV2AToken, AaveV2LendingTokenDataProps>): Promise<string> {
+    return getLabelFromToken(appToken.tokens[0]);
+  }
+
+  async getLabelDetailed({
+    appToken,
+  }: DisplayPropsStageParams<AaveV2AToken, AaveV2LendingTokenDataProps>): Promise<string> {
+    return appToken.symbol;
+  }
+}
---
+++ src/position/template/app-token.template.position-fetcher.ts
@@ -0,0 +1,183 @@
+import { Inject } from '@nestjs/common';
+import { BigNumberish, Contract } from 'ethers/lib/ethers';
+import { compact, sum } from 'lodash';
+
+import { APP_TOOLKIT, IAppToolkit } from '~app-toolkit/app-toolkit.interface';
+import {
+  buildDollarDisplayItem,
+  buildPercentageDisplayItem,
+} from '~app-toolkit/helpers/presentation/display-item.present';
+import { getImagesFromToken } from '~app-toolkit/helpers/presentation/image.present';
+import { Erc20 } from '~contract/contracts';
+import { IMulticallWrapper } from '~multicall';
+import { ContractType } from '~position/contract.interface';
+import { DefaultDataProps, DisplayProps, StatsItem } from '~position/display.interface';
+import { PositionFetcher } from '~position/position-fetcher.interface';
+import { AppTokenPosition } from '~position/position.interface';
+import { Network } from '~types/network.interface';
+
+export type StageParams<T extends Contract, V, K extends keyof AppTokenPosition> = {
+  multicall: IMulticallWrapper;
+  contract: T;
+  appToken: Omit<AppTokenPosition<V>, K>;
+};
+
+export type PriceStageParams<T extends Contract, V> = StageParams<T, V, 'price' | 'dataProps' | 'displayProps'>;
+export type DataPropsStageParams<T extends Contract, V> = StageParams<T, V, 'dataProps' | 'displayProps'>;
+export type DisplayPropsStageParams<T extends Contract, V> = StageParams<T, V, 'displayProps'>;
+
+export abstract class AppTokenTemplatePositionFetcher<T extends Contract, V extends DefaultDataProps = DefaultDataProps>
+  implements PositionFetcher<AppTokenPosition<V>>
+{
+  abstract appId: string;
+  abstract groupId: string;
+  abstract network: Network;
+
+  constructor(@Inject(APP_TOOLKIT) protected readonly appToolkit: IAppToolkit) {}
+
+  abstract getContract(address: string): T;
+  abstract getAddresses(): Promise<string[]>;
+
+  // Token Props
+  async getSymbol(contract: T): Promise<string> {
+    return (contract as unknown as Erc20).symbol();
+  }
+
+  async getDecimals(contract: T): Promise<number> {
+    return (contract as unknown as Erc20).decimals();
+  }
+
+  async getSupply(contract: T): Promise<BigNumberish> {
+    return (contract as unknown as Erc20).totalSupply();
+  }
+
+  // Price Properties
+  async getUnderlyingTokenAddresses(_contract: T): Promise<string[]> {
+    return [];
+  }
+
+  async getPricePerShare(_contract: T): Promise<number | number[]> {
+    return 1;
+  }
+
+  async getPrice({ appToken }: PriceStageParams<T, V>): Promise<number> {
+    return sum(appToken.tokens.map((v, i) => v.price * appToken.pricePerShare[i]));
+  }
+
+  // Data Properties
+  async getDataProps(_params: DataPropsStageParams<T, V>): Promise<V> {
+    return {} as V;
+  }
+
+  // Display Properties
+  async getLabel({ appToken }: DisplayPropsStageParams<T, V>): Promise<DisplayProps['label']> {
+    return appToken.symbol;
+  }
+
+  async getLabelDetailed(_params: DisplayPropsStageParams<T, V>): Promise<DisplayProps['labelDetailed']> {
+    return undefined;
+  }
+
+  async getSecondaryLabel({ appToken }: DisplayPropsStageParams<T, V>): Promise<DisplayProps['secondaryLabel']> {
+    return buildDollarDisplayItem(appToken.price);
+  }
+
+  async getTertiaryLabel({ appToken }: DisplayPropsStageParams<T, V>): Promise<DisplayProps['tertiaryLabel']> {
+    if (typeof appToken.dataProps.apy === 'number') return `${appToken.dataProps.apy.toFixed(3)}% APY`;
+    return '';
+  }
+
+  async getImages({ appToken }: DisplayPropsStageParams<T, V>): Promise<DisplayProps['images']> {
+    return appToken.tokens.flatMap(v => getImagesFromToken(v));
+  }
+
+  async getStatsItems({ appToken }: DisplayPropsStageParams<T, V>): Promise<DisplayProps['statsItems']> {
+    const statsItems: StatsItem[] = [];
+
+    // Standardized Fields
+    if (typeof appToken.dataProps.liquidity === 'number')
+      statsItems.push({ label: 'Liquidity', value: buildDollarDisplayItem(appToken.dataProps.liquidity) });
+    if (typeof appToken.dataProps.apy === 'number')
+      statsItems.push({ label: 'APY', value: buildPercentageDisplayItem(appToken.dataProps.apy) });
+
+    return statsItems;
+  }
+
+  // Default (adapted) Template Runner
+  // Note: This will be removed in favour of an orchestrator at a higher level once all groups are migrated
+  async getPositions(): Promise<AppTokenPosition<V>[]> {
+    const multicall = this.appToolkit.getMulticall(this.network);
+    const tokenLoader = this.appToolkit.getBaseTokenPriceSelector();
+    const addresses = await this.getAddresses();
+
+    const skeletons = await Promise.all(
+      addresses.map(async address => {
+        const contract = multicall.wrap(this.getContract(address));
+        const underlyingTokenAddressesRaw = await this.getUnderlyingTokenAddresses(contract);
+        const underlyingTokenAddresses = underlyingTokenAddressesRaw.map(v => v.toLowerCase());
+        return { address, underlyingTokenAddresses };
+      }),
+    );
+
+    const skeletonsWithResolvedTokens = await Promise.all(
+      skeletons.map(async ({ address, underlyingTokenAddresses }) => {
+        const maybeTokens = await tokenLoader.getMany(
+          underlyingTokenAddresses.map(v => ({ address: v, network: this.network })),
+        );
+
+        const tokens = compact(maybeTokens);
+        if (maybeTokens.length !== tokens.length) return null;
+        return { address, tokens };
+      }),
+    );
+
+    const appTokens = await Promise.all(
+      compact(skeletonsWithResolvedTokens).map(async ({ address, tokens }) => {
+        const contract = multicall.wrap(this.getContract(address));
+        const [symbol, decimals, totalSupplyRaw] = await Promise.all([
+          this.getSymbol(contract),
+          this.getDecimals(contract),
+          this.getSupply(contract),
+        ]);
+
+        const supply = Number(totalSupplyRaw) / 10 ** decimals;
+        const pricePerShare = await this.getPricePerShare(contract);
+
+        const fragment: PriceStageParams<T, V>['appToken'] = {
+          type: ContractType.APP_TOKEN,
+          appId: this.appId,
+          groupId: this.groupId,
+          network: this.network,
+          address,
+          symbol,
+          decimals,
+          supply,
+          pricePerShare,
+          tokens,
+        };
+
+        // Resolve Price Stage
+        const priceStageParams = { appToken: fragment, contract, multicall };
+        const price = await this.getPrice(priceStageParams);
+
+        // Resolve Data Props Stage
+        const dataPropsStageParams = { appToken: { ...fragment, price }, contract, multicall };
+        const dataProps = await this.getDataProps(dataPropsStageParams);
+
+        // Resolve Display Props Stage
+        const displayPropsStageParams = { appToken: { ...fragment, price, dataProps }, contract, multicall };
+        const displayProps = {
+          label: await this.getLabel(displayPropsStageParams),
+          secondarylabel: await this.getSecondaryLabel(displayPropsStageParams),
+          tertiaryLabel: await this.getTertiaryLabel(displayPropsStageParams),
+          images: await this.getImages(displayPropsStageParams),
+          statsItems: await this.getStatsItems(displayPropsStageParams),
+        };
+
+        return { ...fragment, price, dataProps, displayProps };
+      }),
+    );
+
+    return appTokens;
+  }
+}
