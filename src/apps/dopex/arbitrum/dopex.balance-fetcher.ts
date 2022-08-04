0a79258ab14607018630116b46cdd5682625f49d
William Poulin <williampoulin01@gmail.com>
2022-08-04 01:13:14 -0400

    chore(dopex): moved dopex to studio

---
+++ src/apps/dopex/arbitrum/dopex.balance-fetcher.ts
@@ -0,0 +1,190 @@
+import { Inject } from '@nestjs/common';
+import { padEnd } from 'lodash';
+import Web3 from 'web3';
+
+import { APP_TOOLKIT, IAppToolkit } from '~app-toolkit/app-toolkit.interface';
+import { Register } from '~app-toolkit/decorators';
+import { presentBalanceFetcherResponse } from '~app-toolkit/helpers/presentation/balance-fetcher-response.present';
+import { BalanceFetcher } from '~balance/balance-fetcher.interface';
+import { Network } from '~types/network.interface';
+
+import {
+  DopexContractFactory,
+  DopexDpxSsov,
+  DopexEthSsov,
+  DopexGmxSsov,
+  DopexGOhmSsov,
+  DopexRdpxSsov,
+  DopexStaking,
+} from '../contracts';
+import { DOPEX_DEFINITION } from '../dopex.definition';
+import { DopexSsovClaimableBalancesStrategy } from '../helpers/dopex.ssov.claimable-balances-strategy';
+import { DopexSsovContractPositionBalanceHelper } from '../helpers/dopex.ssov.contract-position-balance-helper';
+import { DopexSsovDepositBalanceStrategy } from '../helpers/dopex.ssov.deposit-balance-strategy';
+
+const network = Network.ARBITRUM_MAINNET;
+
+@Register.BalanceFetcher(DOPEX_DEFINITION.id, network)
+export class ArbitrumDopexBalanceFetcher implements BalanceFetcher {
+  constructor(
+    @Inject(APP_TOOLKIT) private readonly appToolkit: IAppToolkit,
+    @Inject(DopexSsovContractPositionBalanceHelper)
+    private readonly dopexSsovContractPositionBalanceHelper: DopexSsovContractPositionBalanceHelper,
+    @Inject(DopexContractFactory)
+    private readonly dopexContractFactory: DopexContractFactory,
+    @Inject(DopexSsovDepositBalanceStrategy)
+    private readonly dopexSsovDepositBalanceStrategy: DopexSsovDepositBalanceStrategy,
+    @Inject(DopexSsovClaimableBalancesStrategy)
+    private readonly dopexSsovClaimableBalancesStrategy: DopexSsovClaimableBalancesStrategy,
+  ) {}
+
+  private async getSsovBalances(address: string) {
+    return Promise.all([
+      this.dopexSsovContractPositionBalanceHelper.getBalances<DopexDpxSsov>({
+        address,
+        appId: DOPEX_DEFINITION.id,
+        groupId: DOPEX_DEFINITION.groups.dpxSsov.id,
+        network,
+        resolveSsovContract: ({ address, network }) => this.dopexContractFactory.dopexDpxSsov({ address, network }),
+        resolveDepositBalance: this.dopexSsovDepositBalanceStrategy.build({
+          resolveFinalEpochStrikeBalance: ({ contract, multicall, contractPosition }) =>
+            multicall
+              .wrap(contract)
+              .totalEpochStrikeBalance(contractPosition.dataProps.epoch, contractPosition.dataProps.strike),
+        }),
+        resolveExtraClaimableBalances: this.dopexSsovClaimableBalancesStrategy.build({
+          resolveTotalEpochStrikeBalance: ({ contract, multicall, contractPosition }) =>
+            multicall
+              .wrap(contract)
+              .totalEpochStrikeRdpxBalance(contractPosition.dataProps.epoch, contractPosition.dataProps.strike),
+        }),
+      }),
+      this.dopexSsovContractPositionBalanceHelper.getBalances<DopexRdpxSsov>({
+        address,
+        appId: DOPEX_DEFINITION.id,
+        groupId: DOPEX_DEFINITION.groups.rdpxSsov.id,
+        network,
+        resolveSsovContract: ({ address, network }) => this.dopexContractFactory.dopexRdpxSsov({ address, network }),
+        resolveDepositBalance: this.dopexSsovDepositBalanceStrategy.build({
+          resolveFinalEpochStrikeBalance: ({ contract, multicall, contractPosition }) =>
+            multicall
+              .wrap(contract)
+              .totalEpochStrikeBalance(contractPosition.dataProps.epoch, contractPosition.dataProps.strike),
+        }),
+        resolveExtraClaimableBalances: this.dopexSsovClaimableBalancesStrategy.build({
+          resolveTotalEpochStrikeBalance: ({ contract, multicall, contractPosition }) =>
+            multicall
+              .wrap(contract)
+              .totalEpochStrikeDpxBalance(contractPosition.dataProps.epoch, contractPosition.dataProps.strike),
+        }),
+      }),
+      this.dopexSsovContractPositionBalanceHelper.getBalances<DopexEthSsov>({
+        address,
+        appId: DOPEX_DEFINITION.id,
+        groupId: DOPEX_DEFINITION.groups.ethSsov.id,
+        network,
+        resolveSsovContract: ({ address, network }) => this.dopexContractFactory.dopexEthSsov({ address, network }),
+        resolveDepositBalance: this.dopexSsovDepositBalanceStrategy.build({
+          resolveFinalEpochStrikeBalance: ({ contract, multicall, contractPosition }) =>
+            multicall
+              .wrap(contract)
+              .totalEpochStrikeEthBalance(contractPosition.dataProps.epoch, contractPosition.dataProps.strike),
+        }),
+        resolveExtraClaimableBalances: this.dopexSsovClaimableBalancesStrategy.build({
+          resolveTotalEpochStrikeBalance: async ({ contract, multicall, contractPosition }) => {
+            const rdName = Web3.utils.asciiToHex('RewardsDistribution');
+            const rdNamePadded = padEnd(rdName, 66, '0');
+            const rdAddress = await multicall.wrap(contract).getAddress(rdNamePadded);
+            const rdContract = this.dopexContractFactory.dopexRewardDistribution({
+              address: rdAddress,
+              network: contractPosition.network,
+            });
+
+            return Promise.all([
+              multicall.wrap(rdContract).dpxReceived(contractPosition.dataProps.epoch),
+              multicall.wrap(rdContract).rdpxReceived(contractPosition.dataProps.epoch),
+            ]);
+          },
+        }),
+      }),
+      this.dopexSsovContractPositionBalanceHelper.getBalances<DopexGOhmSsov>({
+        address,
+        appId: DOPEX_DEFINITION.id,
+        groupId: DOPEX_DEFINITION.groups.gohmSsov.id,
+        network,
+        resolveSsovContract: ({ address, network }) => this.dopexContractFactory.dopexGOhmSsov({ address, network }),
+        resolveDepositBalance: this.dopexSsovDepositBalanceStrategy.build({
+          resolveFinalEpochStrikeBalance: ({ contract, multicall, contractPosition }) =>
+            multicall
+              .wrap(contract)
+              .totalEpochStrikeGohmBalance(contractPosition.dataProps.epoch, contractPosition.dataProps.strike),
+        }),
+      }),
+      this.dopexSsovContractPositionBalanceHelper.getBalances<DopexGmxSsov>({
+        address,
+        appId: DOPEX_DEFINITION.id,
+        groupId: DOPEX_DEFINITION.groups.gmxSsov.id,
+        network,
+        resolveSsovContract: ({ address, network }) => this.dopexContractFactory.dopexGmxSsov({ address, network }),
+        resolveDepositBalance: this.dopexSsovDepositBalanceStrategy.build({
+          resolveFinalEpochStrikeBalance: async ({ contract, multicall, contractPosition }) =>
+            multicall
+              .wrap(contract)
+              .totalEpochStrikeGmxBalance(contractPosition.dataProps.epoch, contractPosition.dataProps.strike),
+        }),
+        resolveExtraClaimableBalances: this.dopexSsovClaimableBalancesStrategy.build({
+          resolveTotalEpochStrikeBalance: async ({ contract, multicall, contractPosition }) => {
+            const [claimedFees, totalEpochStrikeDeposits, totalEpochDeposits] = await Promise.all([
+              multicall.wrap(contract).totalGmxFeesClaimed(contractPosition.dataProps.strike),
+              multicall
+                .wrap(contract)
+                .totalEpochStrikeDeposits(contractPosition.dataProps.epoch, contractPosition.dataProps.strike),
+              multicall.wrap(contract).totalEpochDeposits(contractPosition.dataProps.epoch),
+            ]);
+
+            const totalFeesClaimableForStrike = (
+              (Number(claimedFees) * Number(totalEpochStrikeDeposits)) /
+              Number(totalEpochDeposits)
+            ).toFixed(0);
+
+            return totalFeesClaimableForStrike;
+          },
+        }),
+      }),
+    ]).then(v => v.flat());
+  }
+
+  private async getStakedBalances(address: string) {
+    return this.appToolkit.helpers.singleStakingContractPositionBalanceHelper.getBalances<DopexStaking>({
+      address,
+      appId: DOPEX_DEFINITION.id,
+      groupId: DOPEX_DEFINITION.groups.farm.id,
+      network,
+      resolveContract: ({ address, network }) => this.dopexContractFactory.dopexStaking({ address, network }),
+      resolveStakedTokenBalance: ({ contract, address, multicall }) => multicall.wrap(contract).balanceOf(address),
+      resolveRewardTokenBalances: ({ contract, address, multicall }) =>
+        multicall
+          .wrap(contract)
+          .earned(address)
+          .then(v => [v.DPXtokensEarned, v.RDPXtokensEarned]),
+    });
+  }
+
+  async getBalances(address: string) {
+    const [ssovBalances, stakedBalances] = await Promise.all([
+      this.getSsovBalances(address),
+      this.getStakedBalances(address),
+    ]);
+
+    return presentBalanceFetcherResponse([
+      {
+        label: 'SSOVs',
+        assets: [...ssovBalances],
+      },
+      {
+        label: 'Staking',
+        assets: [...stakedBalances],
+      },
+    ]);
+  }
+}
---
+++ src/apps/dopex/arbitrum/dopex.gohm-ssov.contract-position-fetcher.ts
@@ -0,0 +1,54 @@
+import { Inject } from '@nestjs/common';
+
+import { Register } from '~app-toolkit/decorators';
+import { OLYMPUS_DEFINITION } from '~apps/olympus';
+import { PositionFetcher } from '~position/position-fetcher.interface';
+import { ContractPosition } from '~position/position.interface';
+import { Network } from '~types/network.interface';
+
+import { DOPEX_DEFINITION } from '../dopex.definition';
+import { DopexSsovContractPositionHelper } from '../helpers/dopex.ssov.contract-position-helper';
+
+const appId = DOPEX_DEFINITION.id;
+const groupId = DOPEX_DEFINITION.groups.gohmSsov.id;
+const network = Network.ARBITRUM_MAINNET;
+
+@Register.TokenPositionFetcher({ appId, groupId, network })
+export class ArbitrumDopexGOhmSsovContractPositionFetcher implements PositionFetcher<ContractPosition> {
+  constructor(
+    @Inject(DopexSsovContractPositionHelper)
+    private readonly dopexSsovContractPositionHelper: DopexSsovContractPositionHelper,
+  ) {}
+
+  async getPositions() {
+    return this.dopexSsovContractPositionHelper.getPositions({
+      network,
+      appId,
+      groupId,
+      dependencies: [
+        {
+          appId: OLYMPUS_DEFINITION.id,
+          groupIds: [OLYMPUS_DEFINITION.groups.gOhm.id],
+          network,
+        },
+      ],
+      definitions: [
+        // gOHM December Epoch (Legacy)
+        {
+          address: '0x89836d5f178141aaf013412b12abd754802d2318',
+          depositTokenAddress: '0x8d9ba570d6cb60c7e3e0f31343efe75ab8e65fb1',
+        },
+        // gOHM January Epoch (Legacy)
+        {
+          address: '0x460f95323a32e26c8d32346abe73eb94d7db08d6',
+          depositTokenAddress: '0x8d9ba570d6cb60c7e3e0f31343efe75ab8e65fb1',
+        },
+        // gOHM February Epoch (Active)
+        {
+          address: '0x54552cb564f4675bceda644e47de3e35d1c88e1b',
+          depositTokenAddress: '0x8d9ba570d6cb60c7e3e0f31343efe75ab8e65fb1',
+        },
+      ],
+    });
+  }
+}
