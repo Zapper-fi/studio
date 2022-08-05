451c65335acac5dc44a0011d29bdddcc9d7ac7b7
William Poulin <williampoulin01@gmail.com>
2022-08-05 11:30:36 -0400

    chore(dopex): moved dopex to studio (#1063)

---
+++ src/apps/dopex/dopex.definition.ts
@@ -0,0 +1,36 @@
+import { Register } from '~app-toolkit/decorators';
+import { appDefinition, AppDefinition } from '~app/app.definition';
+import { AppAction, AppTag, GroupType } from '~app/app.interface';
+import { Network } from '~types/network.interface';
+
+export const DOPEX_DEFINITION = appDefinition({
+  id: 'dopex',
+  name: 'Dopex',
+  description: `Dopex is a maximum liquidity and minimal exposure options protocol`,
+  url: 'https://dopex.io/',
+
+  groups: {
+    farm: { id: 'farm', type: GroupType.POSITION, label: 'Staking', groupLabel: 'Farms' },
+    dpxSsov: { id: 'dpx-ssov', type: GroupType.POSITION, label: 'SSOVs' },
+    rdpxSsov: { id: 'rdpx-ssov', type: GroupType.POSITION, label: 'SSOVs' },
+    ethSsov: { id: 'eth-ssov', type: GroupType.POSITION, label: 'SSOVs' },
+    gohmSsov: { id: 'gohm-ssov', type: GroupType.POSITION, label: 'SSOVs' },
+    gmxSsov: { id: 'gmx-ssov', type: GroupType.POSITION, label: 'SSOVs' },
+  },
+  links: {
+    twitter: 'https://twitter.com/dopex_io',
+    discord: 'https://discord.com/invite/dopex',
+    github: 'https://github.com/dopex-io',
+  },
+  tags: [AppTag.OPTIONS],
+  supportedNetworks: {
+    [Network.ARBITRUM_MAINNET]: [AppAction.VIEW],
+  },
+});
+
+@Register.AppDefinition(DOPEX_DEFINITION.id)
+export class DopexAppDefinition extends AppDefinition {
+  constructor() {
+    super(DOPEX_DEFINITION);
+  }
+}
---
+++ src/apps/dopex/dopex.module.ts
@@ -0,0 +1,40 @@
+import { Register } from '~app-toolkit/decorators';
+import { AbstractApp } from '~app/app.dynamic-module';
+import { SynthetixAppModule } from '~apps/synthetix';
+
+import { ArbitrumDopexBalanceFetcher } from './arbitrum/dopex.balance-fetcher';
+import { ArbitrumDopexDpxSsovContractPositionFetcher } from './arbitrum/dopex.dpx-ssov.contract-position-fetcher';
+import { ArbitrumDopexEthSsovContractPositionFetcher } from './arbitrum/dopex.eth-ssov.contract-position-fetcher';
+import { ArbitrumDopexFarmContractPositionFetcher } from './arbitrum/dopex.farm.contract-position-fetcher';
+import { ArbitrumDopexGmxSsovContractPositionFetcher } from './arbitrum/dopex.gmx-ssov.contract-position-fetcher';
+import { ArbitrumDopexGOhmSsovContractPositionFetcher } from './arbitrum/dopex.gohm-ssov.contract-position-fetcher';
+import { ArbitrumDopexRdpxSsovContractPositionFetcher } from './arbitrum/dopex.rdpx-ssov.contract-position-fetcher';
+import { DopexContractFactory } from './contracts';
+import { DopexAppDefinition, DOPEX_DEFINITION } from './dopex.definition';
+import { DopexDualRewardFarmRoiStrategy } from './helpers/dopex.dual-reward-farm.roi-strategy';
+import { DopexSsovClaimableBalancesStrategy } from './helpers/dopex.ssov.claimable-balances-strategy';
+import { DopexSsovContractPositionBalanceHelper } from './helpers/dopex.ssov.contract-position-balance-helper';
+import { DopexSsovContractPositionHelper } from './helpers/dopex.ssov.contract-position-helper';
+import { DopexSsovDepositBalanceStrategy } from './helpers/dopex.ssov.deposit-balance-strategy';
+
+@Register.AppModule({
+  appId: DOPEX_DEFINITION.id,
+  imports: [SynthetixAppModule],
+  providers: [
+    DopexAppDefinition,
+    DopexContractFactory,
+    DopexDualRewardFarmRoiStrategy,
+    DopexSsovClaimableBalancesStrategy,
+    DopexSsovContractPositionBalanceHelper,
+    DopexSsovContractPositionHelper,
+    DopexSsovDepositBalanceStrategy,
+    ArbitrumDopexBalanceFetcher,
+    ArbitrumDopexDpxSsovContractPositionFetcher,
+    ArbitrumDopexRdpxSsovContractPositionFetcher,
+    ArbitrumDopexEthSsovContractPositionFetcher,
+    ArbitrumDopexFarmContractPositionFetcher,
+    ArbitrumDopexGmxSsovContractPositionFetcher,
+    ArbitrumDopexGOhmSsovContractPositionFetcher,
+  ],
+})
+export class DopexAppModule extends AbstractApp() {}
