8ae191ec560c1b3f44c7e30ebb885ef4cf203030
Karan Shahani <karan.shahani91@gmail.com>
2022-08-08 11:10:15 -0400

    feat(furucombo): Remove balance fetcher

--- src/apps/furucombo/furucombo.module.ts
+++ src/apps/furucombo/furucombo.module.ts
@@ -3,16 +3,10 @@ import { AbstractApp } from '~app/app.dynamic-module';
 
 import { FurucomboContractFactory } from './contracts';
 import { FurucomboAppDefinition, FURUCOMBO_DEFINITION } from './furucombo.definition';
-import { PolygonFurucomboBalanceFetcher } from './polygon/furucombo.balance-fetcher';
 import { PolygonFurucomboFundTokenFetcher } from './polygon/furucombo.fund.token-fetcher';
 
 @Register.AppModule({
   appId: FURUCOMBO_DEFINITION.id,
-  providers: [
-    FurucomboAppDefinition,
-    FurucomboContractFactory,
-    PolygonFurucomboBalanceFetcher,
-    PolygonFurucomboFundTokenFetcher,
-  ],
+  providers: [FurucomboAppDefinition, FurucomboContractFactory, PolygonFurucomboFundTokenFetcher],
 })
 export class FurucomboAppModule extends AbstractApp() {}
