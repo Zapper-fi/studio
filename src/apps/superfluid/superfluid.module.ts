a57953daa64e19e8913a4f432ae9c9dd2ae27241
William Poulin <williampoulin01@gmail.com>
2022-08-09 16:02:38 -0400

    chore(superfluid): remove balance fetcher

--- src/apps/superfluid/superfluid.module.ts
+++ src/apps/superfluid/superfluid.module.ts
@@ -2,17 +2,11 @@ import { Register } from '~app-toolkit/decorators';
 import { AbstractApp } from '~app/app.dynamic-module';
 
 import { SuperfluidContractFactory } from './contracts';
-import { PolygonSuperfluidBalanceFetcher } from './polygon/superfluid.balance-fetcher';
 import { PolygonSuperfluidVaultTokenFetcher } from './polygon/superfluid.vault.token-fetcher';
 import { SuperfluidAppDefinition, SUPERFLUID_DEFINITION } from './superfluid.definition';
 
 @Register.AppModule({
   appId: SUPERFLUID_DEFINITION.id,
-  providers: [
-    SuperfluidAppDefinition,
-    SuperfluidContractFactory,
-    PolygonSuperfluidBalanceFetcher,
-    PolygonSuperfluidVaultTokenFetcher,
-  ],
+  providers: [SuperfluidAppDefinition, SuperfluidContractFactory, PolygonSuperfluidVaultTokenFetcher],
 })
 export class SuperfluidAppModule extends AbstractApp() {}
