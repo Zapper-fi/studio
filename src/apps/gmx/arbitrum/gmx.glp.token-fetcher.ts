a08b67f834e79e7a0e5a346b46066bb99b16df4b
William Poulin <williampoulin01@gmail.com>
2022-08-10 11:53:09 -0400

    fix(gmx): remove MIM from GLP index

--- src/apps/gmx/arbitrum/gmx.glp.token-fetcher.ts
+++ src/apps/gmx/arbitrum/gmx.glp.token-fetcher.ts
@@ -21,6 +21,7 @@ export class ArbitrumGmxGlpTokenFetcher implements PositionFetcher<AppTokenPosit
       glmManagerAddress: '0x321f653eed006ad1c29d174e17d96351bde22649',
       glpTokenAddress: '0x4277f8f2c384827b5273592ff7cebd9f2c1ac258',
       network,
+      blockedTokenAddresses: ['0xfea7a6a0b346362bf88a9e4a88416b77a57d6c2a'],
     });
   }
 }
