8f85d8960b1460434fea3410c3d865bbaae8178c
Justin D'Errico <justin.derrico1991@gmail.com>
2022-08-12 15:50:05 -0400

    feat(app-toolkit): Sunset other token selectors in favor of token deps

--- src/app-toolkit/app-toolkit.interface.ts
+++ src/app-toolkit/app-toolkit.interface.ts
@@ -8,14 +8,11 @@ import { IMulticallWrapper } from '~multicall/multicall.interface';
 import { DefaultDataProps } from '~position/display.interface';
 import { AppTokenPosition, ContractPosition, NonFungibleToken } from '~position/position.interface';
 import { AppGroupsDefinition } from '~position/position.service';
-import { AppTokenSelector, CreateAppTokenSelectorOptions } from '~position/selectors/app-token-selector.interface';
 import {
   CreateTokenDependencySelectorOptions,
   TokenDependencySelector,
 } from '~position/selectors/token-dependency-selector.interface';
 import { BaseToken } from '~position/token.interface';
-import { CreatePriceSelectorOptions } from '~token/selectors/token-price-selector.interface';
-import { PriceSelector } from '~token/selectors/token-price-selector.interface';
 import { Network } from '~types/network.interface';
 
 import { AppToolkitHelperRegistry } from './app-toolkit.helpers';
@@ -37,8 +34,6 @@ export interface IAppToolkit {
 
   // Base Tokens
 
-  getBaseTokenPriceSelector(opts?: CreatePriceSelectorOptions): PriceSelector;
-
   getBaseTokenPrices(network: Network): Promise<BaseToken[]>;
 
   getBaseTokenPrice(opts: { network: Network; address: string }): Promise<BaseToken | null>;
@@ -46,8 +41,6 @@ export interface IAppToolkit {
   // Positions
 
   getTokenDependencySelector(opts?: CreateTokenDependencySelectorOptions): TokenDependencySelector;
-
-  getAppTokenSelector(opts?: CreateAppTokenSelectorOptions): AppTokenSelector;
 
   getAppTokenPositions<T = DefaultDataProps>(
     ...appTokenDefinition: AppGroupsDefinition[]
