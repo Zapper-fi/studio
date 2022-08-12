31a615b848353b95326de235c08821f8e9164bda
Karan Shahani <karan.shahani91@gmail.com>
2022-08-11 22:52:36 -0400

    feat(koyo): Migrate to template pattern

--- src/apps/balancer-v2/balancer-v2.module.ts
+++ src/apps/balancer-v2/balancer-v2.module.ts
@@ -6,11 +6,7 @@ import { BalancerV2ContractFactory } from './contracts';
 import { EthereumBalancerV2PoolTokenFetcher } from './ethereum/balancer-v2.pool.token-fetcher';
 import { BalancerV2CacheManager } from './helpers/balancer-v2.cache-manager';
 import { BalancerV2ClaimableContractPositionBalanceHelper } from './helpers/balancer-v2.claimable.contract-position-balance-helper';
-import { BalancerV2EventsPoolTokenDataStrategy } from './helpers/balancer-v2.events.pool-token-address-strategy';
-import { BalancerV2PoolTokensHelper } from './helpers/balancer-v2.pool.token-helper';
-import { BalancerV2GaugeRewardTokenStrategy } from './helpers/balancer-v2.reward-token-strategy';
 import { BalancerV2SpotPriceHelper } from './helpers/balancer-v2.spot-price.helper';
-import { BalancerV2TheGraphPoolTokenDataStrategy } from './helpers/balancer-v2.the-graph.pool-token-address-strategy';
 
 @Register.AppModule({
   appId: BALANCER_V2_DEFINITION.id,
@@ -31,18 +27,9 @@ import { BalancerV2TheGraphPoolTokenDataStrategy } from './helpers/balancer-v2.t
     // PolygonBalancerV2StakedContractPositionFetcher,
     // Helpers
     BalancerV2CacheManager,
-    BalancerV2PoolTokensHelper,
     BalancerV2SpotPriceHelper,
-    BalancerV2TheGraphPoolTokenDataStrategy,
     BalancerV2ClaimableContractPositionBalanceHelper,
-    BalancerV2EventsPoolTokenDataStrategy,
-    BalancerV2GaugeRewardTokenStrategy,
   ],
-  exports: [
-    BalancerV2SpotPriceHelper,
-    BalancerV2PoolTokensHelper,
-    BalancerV2ContractFactory,
-    BalancerV2GaugeRewardTokenStrategy,
-  ],
+  exports: [BalancerV2SpotPriceHelper, BalancerV2ContractFactory],
 })
 export class BalancerV2AppModule extends AbstractApp() {}
--- src/apps/koyo/polygon/koyo.pool.token-fetcher.ts
+++ src/apps/koyo/polygon/koyo.pool.token-fetcher.ts
@@ -1,10 +1,5 @@
-import { Inject } from '@nestjs/common';
-
 import { Register } from '~app-toolkit/decorators';
-import { BalancerV2PoolTokensHelper } from '~apps/balancer-v2';
-import { BalancerV2TheGraphPoolTokenDataStrategy } from '~apps/balancer-v2/helpers/balancer-v2.the-graph.pool-token-address-strategy';
-import { PositionFetcher } from '~position/position-fetcher.interface';
-import { AppTokenPosition } from '~position/position.interface';
+import { BalancerV2PoolTokenFetcher } from '~apps/balancer-v2/common/balancer-v2.pool.token-fetcher';
 import { Network } from '~types/network.interface';
 
 import { KOYO_DEFINITION } from '../koyo.definition';
@@ -14,22 +9,10 @@ const groupId = KOYO_DEFINITION.groups.pool.id;
 const network = Network.POLYGON_MAINNET;
 
 @Register.TokenPositionFetcher({ appId, groupId, network })
-export class PolygonKoyoPoolTokenFetcher implements PositionFetcher<AppTokenPosition> {
-  constructor(
-    @Inject(BalancerV2PoolTokensHelper) private readonly poolTokensHelper: BalancerV2PoolTokensHelper,
-    @Inject(BalancerV2TheGraphPoolTokenDataStrategy)
-    private readonly theGraphPoolTokenDataStrategy: BalancerV2TheGraphPoolTokenDataStrategy,
-  ) {}
-
-  getPositions() {
-    return this.poolTokensHelper.getPositions({
-      network,
-      appId,
-      groupId,
-      vaultAddress: '0xacf8489ccb47da2d7306d827bbede05bfa6fea1b',
-      resolvePoolTokenAddresses: this.theGraphPoolTokenDataStrategy.build({
-        subgraphUrl: 'https://api.thegraph.com/subgraphs/name/koyo-finance/exchange-subgraph-matic',
-      }),
-    });
-  }
+export class PolygonKoyoPoolTokenFetcher extends BalancerV2PoolTokenFetcher {
+  appId = KOYO_DEFINITION.id;
+  groupId = KOYO_DEFINITION.groups.pool.id;
+  network = Network.POLYGON_MAINNET;
+  subgraphUrl = 'https://api.thegraph.com/subgraphs/name/koyo-finance/exchange-subgraph-matic';
+  vaultAddress = '0xacf8489ccb47da2d7306d827bbede05bfa6fea1b';
 }
