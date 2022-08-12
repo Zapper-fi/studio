f0fa0bf801f650d38cf22a3ac5384215fd522182
Karan Shahani <karan.shahani91@gmail.com>
2022-08-12 11:36:06 -0400

    feat(spool): Add missing logo and fix lint in Polynomial

--- src/apps/polynomial/polynomial.module.ts
+++ src/apps/polynomial/polynomial.module.ts
@@ -2,11 +2,11 @@ import { Register } from '~app-toolkit/decorators';
 import { AbstractApp } from '~app/app.dynamic-module';
 
 import { PolynomialContractFactory } from './contracts';
+import { PolynomialApiHelper } from './helpers/polynomial.api';
 import { OptimismPolynomialBalanceFetcher } from './optimism/polynomial.balance-fetcher';
 import { OptimismPolynomialVaultsContractPositionFetcher } from './optimism/polynomial.vaults.contract-position-fetcher';
 import { OptimismPolynomialVaultsTokenFetcher } from './optimism/polynomial.vaults.token-fetcher';
 import { PolynomialAppDefinition, POLYNOMIAL_DEFINITION } from './polynomial.definition';
-import { PolynomialApiHelper } from './helpers/polynomial.api'
 
 @Register.AppModule({
   appId: POLYNOMIAL_DEFINITION.id,
@@ -19,4 +19,4 @@ import { PolynomialApiHelper } from './helpers/polynomial.api'
     PolynomialContractFactory,
   ],
 })
-export class PolynomialAppModule extends AbstractApp() { }
+export class PolynomialAppModule extends AbstractApp() {}
