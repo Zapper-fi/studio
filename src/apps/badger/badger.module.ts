e3c853b8ecb9b620dac91a4c5c8f0cc68ff41aba
William Poulin <williampoulin01@gmail.com>
2022-08-16 13:42:40 -0400

    chore(badger): refactor badger using template

--- src/apps/badger/badger.module.ts
+++ src/apps/badger/badger.module.ts
@@ -1,17 +1,14 @@
 import { Register } from '~app-toolkit/decorators';
 import { AbstractApp } from '~app/app.dynamic-module';
 
-import { ArbitrumBadgerClaimableContractPositionBalanceFetcher } from './arbitrum/badger.claimable.contract-position-balance-fetcher';
 import { ArbitrumBadgerClaimableContractPositionFetcher } from './arbitrum/badger.claimable.contract-position-fetcher';
 import { ArbitrumBadgerVaultTokenFetcher } from './arbitrum/badger.vault.token-fetcher';
 import { BadgerAppDefinition, BADGER_DEFINITION } from './badger.definition';
 import { BadgerContractFactory } from './contracts';
-import { EthereumBadgerClaimableContractPositionBalanceFetcher } from './ethereum/badger.claimable.contract-position-balance-fetcher';
 import { EthereumBadgerClaimableContractPositionFetcher } from './ethereum/badger.claimable.contract-position-fetcher';
 import { EthereumBadgerVaultTokenFetcher } from './ethereum/badger.vault.token-fetcher';
-import { BadgerClaimableContractPositionBalanceHelper } from './helpers/badger.claimable.balance-helper';
-import { BadgerVaultTokenHelper } from './helpers/badger.vault.token-helper';
-import { PolygonBadgerClaimableContractPositionBalanceFetcher } from './polygon/badger.claimable.contract-position-balance-fetcher';
+import { BadgerClaimableRewardsResolver } from './helpers/badger.claimable.rewards-resolver';
+import { BadgerVaultTokenDefinitionsResolver } from './helpers/badger.vault.token-definition-resolver';
 import { PolygonBadgerClaimableContractPositionFetcher } from './polygon/badger.claimable.contract-position-fetcher';
 import { PolygonBadgerVaultTokenFetcher } from './polygon/badger.vault.token-fetcher';
 
@@ -20,20 +17,17 @@ import { PolygonBadgerVaultTokenFetcher } from './polygon/badger.vault.token-fet
   providers: [
     BadgerAppDefinition,
     BadgerContractFactory,
-    BadgerClaimableContractPositionBalanceHelper,
-    BadgerVaultTokenHelper,
+    BadgerVaultTokenDefinitionsResolver,
+    BadgerClaimableRewardsResolver,
     // Arbitrum
     ArbitrumBadgerVaultTokenFetcher,
     ArbitrumBadgerClaimableContractPositionFetcher,
-    ArbitrumBadgerClaimableContractPositionBalanceFetcher,
     // Ethereum
     EthereumBadgerVaultTokenFetcher,
     EthereumBadgerClaimableContractPositionFetcher,
-    EthereumBadgerClaimableContractPositionBalanceFetcher,
     // Polygon
     PolygonBadgerVaultTokenFetcher,
     PolygonBadgerClaimableContractPositionFetcher,
-    PolygonBadgerClaimableContractPositionBalanceFetcher,
   ],
 })
 export class BadgerAppModule extends AbstractApp() {}
