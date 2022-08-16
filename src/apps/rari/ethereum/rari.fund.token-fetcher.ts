4d2c98f63b1c850dfe015b5296972580b1ccbb6b
immasandwich <karan.shahani91@gmail.com>
2022-08-16 11:20:23 -0400

    feat(rari): Extract to Studio (#1187)

---
+++ src/apps/rari/ethereum/rari.fund.token-fetcher.ts
@@ -0,0 +1,108 @@
+import { Inject } from '@nestjs/common';
+
+import { APP_TOOLKIT, IAppToolkit } from '~app-toolkit/app-toolkit.interface';
+import { ZERO_ADDRESS } from '~app-toolkit/constants/address';
+import { Register } from '~app-toolkit/decorators';
+import { Erc20 } from '~contract/contracts';
+import { DefaultDataProps } from '~position/display.interface';
+import {
+  AppTokenTemplatePositionFetcher,
+  DisplayPropsStageParams,
+  PricePerShareStageParams,
+  UnderlyingTokensStageParams,
+} from '~position/template/app-token.template.position-fetcher';
+import { Network } from '~types';
+
+import { RariContractFactory } from '../contracts';
+import { RARI_DEFINITION } from '../rari.definition';
+
+const SYMBOL_TO_ADDRESS = {
+  DAI: '0x6b175474e89094c44da98b954eedeac495271d0f',
+  USDC: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+  USDT: '0xdac17f958d2ee523a2206206994597c13d831ec7',
+  mUSD: '0xe2f2a5c287993345a840db3b0845fbc70f5935a5',
+};
+
+const RARI_POOL_DEFINITIONS = [
+  {
+    label: 'USDC Pool',
+    poolManagerAddress: '0xc6bf8c8a55f77686720e0a88e2fd1feef58ddf4a',
+    poolTokenAddress: '0x016bf078abcacb987f0589a6d3beadd4316922b0',
+  },
+  {
+    label: 'Ether Pool',
+    isEther: true,
+    poolManagerAddress: '0xd6e194af3d9674b62d1b30ec676030c23961275e',
+    poolTokenAddress: '0xcda4770d65b4211364cb870ad6be19e7ef1d65f4',
+  },
+  {
+    label: 'Yield Pool',
+    poolManagerAddress: '0x59fa438cd0731ebf5f4cdcaf72d4960efd13fce6',
+    poolTokenAddress: '0x3baa6b7af0d72006d3ea770ca29100eb848559ae',
+  },
+  {
+    label: 'DAI Pool',
+    poolManagerAddress: '0xb465baf04c087ce3ed1c266f96ca43f4847d9635',
+    poolTokenAddress: '0x0833cfcb11a5ba89fbaf73a407831c98ad2d7648',
+  },
+];
+
+const appId = RARI_DEFINITION.id;
+const groupId = RARI_DEFINITION.groups.fund.id;
+const network = Network.ETHEREUM_MAINNET;
+
+@Register.TokenPositionFetcher({ appId, groupId, network })
+export class EthereumRariFundTokenFetcher extends AppTokenTemplatePositionFetcher<Erc20> {
+  appId = appId;
+  groupId = groupId;
+  network = network;
+
+  constructor(
+    @Inject(APP_TOOLKIT) protected readonly appToolkit: IAppToolkit,
+    @Inject(RariContractFactory) protected readonly contractFactory: RariContractFactory,
+  ) {
+    super(appToolkit);
+  }
+
+  getContract(address: string): Erc20 {
+    return this.contractFactory.erc20({ address, network: this.network });
+  }
+
+  async getAddresses() {
+    return RARI_POOL_DEFINITIONS.map(v => v.poolTokenAddress);
+  }
+
+  async getUnderlyingTokenAddresses({ multicall, address }: UnderlyingTokensStageParams<Erc20>) {
+    const { poolManagerAddress, isEther } = RARI_POOL_DEFINITIONS.find(v => v.poolTokenAddress === address)!;
+    if (isEther) return [ZERO_ADDRESS];
+
+    const managerContract = this.contractFactory.rariFundManager({
+      address: poolManagerAddress,
+      network: this.network,
+    });
+
+    const symbols = await multicall.wrap(managerContract).callStatic.getAcceptedCurrencies();
+    return symbols.map(v => SYMBOL_TO_ADDRESS[v]!);
+  }
+
+  async getPricePerShare({ appToken }: PricePerShareStageParams<Erc20>) {
+    const { poolManagerAddress } = RARI_POOL_DEFINITIONS.find(v => v.poolTokenAddress === appToken.address)!;
+
+    const managerContract = this.contractFactory.rariFundManager({
+      address: poolManagerAddress,
+      network: this.network,
+    });
+
+    const liquidityRaw = await managerContract.callStatic.getFundBalance();
+    const liquidity = Number(liquidityRaw) / 10 ** 18;
+    const reserves = appToken.tokens.map(() => liquidity / appToken.tokens.length);
+    const pricePerShare = reserves.map(v => v / appToken.supply);
+
+    return pricePerShare;
+  }
+
+  async getLabel({ appToken }: DisplayPropsStageParams<Erc20, DefaultDataProps>) {
+    const { label } = RARI_POOL_DEFINITIONS.find(v => v.poolTokenAddress === appToken.address)!;
+    return label;
+  }
+}
