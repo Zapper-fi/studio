33b716999b7d29273b2732544bf4df00bb873a33
William Poulin <williampoulin01@gmail.com>
2022-08-17 12:54:55 -0400

    chore(badger): refactor badger using template (#1190)

--- src/apps/badger/arbitrum/badger.vault.token-fetcher.ts
+++ src/apps/badger/arbitrum/badger.vault.token-fetcher.ts
@@ -1,62 +1,33 @@
 import { Inject } from '@nestjs/common';
-import Axios from 'axios';
-import { values } from 'lodash';
 
+import { APP_TOOLKIT, IAppToolkit } from '~app-toolkit/app-toolkit.interface';
 import { Register } from '~app-toolkit/decorators';
-import { CURVE_DEFINITION } from '~apps/curve/curve.definition';
-import { CacheOnInterval } from '~cache/cache-on-interval.decorator';
-import { PositionFetcher } from '~position/position-fetcher.interface';
-import { AppTokenPosition } from '~position/position.interface';
 import { Network } from '~types/network.interface';
 
 import { BADGER_DEFINITION } from '../badger.definition';
+import { BadgerContractFactory, BadgerSett } from '../contracts';
+import { BadgerVaultTokenDefinitionsResolver } from '../helpers/badger.vault.token-definition-resolver';
 import { BadgerVaultTokenHelper } from '../helpers/badger.vault.token-helper';
 
 const appId = BADGER_DEFINITION.id;
 const groupId = BADGER_DEFINITION.groups.vault.id;
 const network = Network.ARBITRUM_MAINNET;
 
-export type BadgerApiTokensResponseEntry = {
-  address: string;
-  decimals: number;
-  name: string;
-  symbol: string;
-  type: string;
-  vaultToken: {
-    address: string;
-    network: string;
-  };
-};
-
-export type BadgerApiTokensResponse = Record<string, BadgerApiTokensResponseEntry>;
-
 @Register.TokenPositionFetcher({ appId, groupId, network })
-export class ArbitrumBadgerVaultTokenFetcher implements PositionFetcher<AppTokenPosition> {
-  constructor(@Inject(BadgerVaultTokenHelper) private readonly badgerVaultTokenHelper: BadgerVaultTokenHelper) {}
+export class ArbitrumBadgerVaultTokenFetcher extends BadgerVaultTokenHelper<BadgerSett> {
+  appId = appId;
+  groupId = groupId;
+  network = network;
 
-  @CacheOnInterval({
-    key: `studio:${appId}:${groupId}:${network}:definitions`,
-    timeout: 15 * 60 * 1000,
-  })
-  async getVaultDefinitions() {
-    const { data } = await Axios.get<BadgerApiTokensResponse>('https://api.badger.com/v2/tokens?chain=arbitrum');
-    const vaultDefinitions = values(data)
-      .filter(t => t.type === 'Vault')
-      .map(t => ({ address: t.address.toLowerCase(), underlyingAddress: t.vaultToken.address.toLowerCase() }));
-    return vaultDefinitions;
+  constructor(
+    @Inject(APP_TOOLKIT) protected readonly appToolkit: IAppToolkit,
+    @Inject(BadgerContractFactory) protected readonly contractFactory: BadgerContractFactory,
+    @Inject(BadgerVaultTokenDefinitionsResolver) tokenDefinitionsResolver: BadgerVaultTokenDefinitionsResolver,
+  ) {
+    super(appToolkit, tokenDefinitionsResolver, contractFactory);
   }
 
-  async getPositions() {
-    const vaultDefinitions = await this.getVaultDefinitions();
-
-    return this.badgerVaultTokenHelper.getTokens({
-      network,
-      definitions: vaultDefinitions,
-      dependencies: [
-        { appId: CURVE_DEFINITION.id, groupIds: [CURVE_DEFINITION.groups.pool.id], network },
-        { appId: 'sushiswap', groupIds: ['pool'], network },
-        { appId: 'swapr', groupIds: ['pool'], network },
-      ],
-    });
+  getContract(address: string): BadgerSett {
+    return this.contractFactory.badgerSett({ network: this.network, address });
   }
 }
