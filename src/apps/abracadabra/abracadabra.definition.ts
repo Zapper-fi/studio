f5ac9776fa39415983fb94818a50966cfbd8f82f
Karan Shahani <karan.shahani91@gmail.com>
2022-08-17 17:12:45 -0400

    feat(abracadabra): Migrate to templates

--- src/apps/abracadabra/abracadabra.definition.ts
+++ src/apps/abracadabra/abracadabra.definition.ts
@@ -5,16 +5,10 @@ import { Network } from '~types/network.interface';
 
 export const ABRACADABRA_DEFINITION = appDefinition({
   id: 'abracadabra',
-  groups: {
-    stakedSpell: { id: 'staked-spell', type: GroupType.TOKEN, label: 'Staked SPELL' },
-    mSpell: { id: 'm-spell', type: GroupType.POSITION, label: 'mSpell' },
-    cauldron: { id: 'cauldron', type: GroupType.POSITION, label: 'Cauldrons' },
-    farm: { id: 'farm', type: GroupType.POSITION, label: 'Farms' },
-    degenbox: { id: 'degenbox', type: GroupType.POSITION, label: 'Abracadabra Degenbox' },
-  },
   name: 'Abracadabra',
   description: `Abracadabra is a lending platform that uses interest-bearing tokens as collateral to borrow a USD pegged stable coin that can be used as any other stablecoin. Abracadabra provides the opportunity for users to unlock the capital of their yield.`,
   url: 'https://abracadabra.money/',
+  tags: [AppTag.LENDING],
   links: {
     github: 'https://github.com/Abracadabra-money',
     twitter: 'https://twitter.com/MIM_Spell',
@@ -22,7 +16,39 @@ export const ABRACADABRA_DEFINITION = appDefinition({
     telegram: 'https://t.me/abracadabramoney',
     medium: 'https://abracadabramoney.medium.com/',
   },
-  tags: [AppTag.LENDING],
+
+  groups: {
+    stakedSpell: {
+      id: 'staked-spell',
+      type: GroupType.TOKEN,
+      label: 'Staked SPELL',
+    },
+
+    mSpell: {
+      id: 'm-spell',
+      type: GroupType.POSITION,
+      label: 'mSpell',
+    },
+
+    cauldron: {
+      id: 'cauldron',
+      type: GroupType.POSITION,
+      label: 'Cauldrons',
+    },
+
+    farm: {
+      id: 'farm',
+      type: GroupType.POSITION,
+      label: 'Farms',
+    },
+
+    degenbox: {
+      id: 'degenbox',
+      type: GroupType.POSITION,
+      label: 'Abracadabra Degenbox',
+    },
+  },
+
   supportedNetworks: {
     [Network.ARBITRUM_MAINNET]: [AppAction.VIEW],
     [Network.AVALANCHE_MAINNET]: [AppAction.VIEW],
