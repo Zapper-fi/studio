edc0c1a2b682facd9a955fd2ed64be40d051acdd
William Poulin <williampoulin01@gmail.com>
2022-08-18 10:23:20 -0400

    chore(yield-yak): renamed yield-yak to match id (#1200)

--- src/apps/yield-yak/yield-yak.module.ts
+++ src/apps/yield-yak/yield-yak.module.ts
@@ -1,9 +1,9 @@
 import { Register } from '~app-toolkit/decorators';
 import { AbstractApp } from '~app/app.dynamic-module';
 
-import { AvalancheYieldyakBalanceFetcher } from './avalanche/yieldyak.balance-fetcher';
-import { AvalancheYieldyakFarmContractPositionFetcher } from './avalanche/yieldyak.farm.contract-position-fetcher';
-import { AvalancheYieldyakVaultTokenFetcher } from './avalanche/yieldyak.vault.token-fetcher';
+import { AvalancheYieldyakBalanceFetcher } from './avalanche/yield-yak.balance-fetcher';
+import { AvalancheYieldyakFarmContractPositionFetcher } from './avalanche/yield-yak.farm.contract-position-fetcher';
+import { AvalancheYieldyakVaultTokenFetcher } from './avalanche/yield-yak.vault.token-fetcher';
 import { YieldYakContractFactory } from './contracts';
 import { YieldYakVaultTokenDefinitionsResolver } from './helpers/yield-yak.vault.token-definitions-resolver';
 import { YieldYakAppDefinition, YIELD_YAK_DEFINITION } from './yield-yak.definition';
