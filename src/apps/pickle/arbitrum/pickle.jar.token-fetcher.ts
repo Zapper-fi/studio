2f37589b75dcaf264cc0215b20d6423f16ea6cdd
Karan Shahani <karan.shahani91@gmail.com>
2022-08-22 16:40:20 -0400

    feat(pickle): Remove unused types

--- src/apps/pickle/arbitrum/pickle.jar.token-fetcher.ts
+++ src/apps/pickle/arbitrum/pickle.jar.token-fetcher.ts
@@ -8,11 +8,6 @@ const appId = PICKLE_DEFINITION.id;
 const groupId = PICKLE_DEFINITION.groups.jar.id;
 const network = Network.ARBITRUM_MAINNET;
 
-export type PickleJarDataProps = {
-  liquidity: number;
-  reserve: number;
-};
-
 @Register.TokenPositionFetcher({ appId, groupId, network })
 export class ArbitrumPickleJarTokenFetcher extends PickleJarTokenFetcher {
   appId = appId;
