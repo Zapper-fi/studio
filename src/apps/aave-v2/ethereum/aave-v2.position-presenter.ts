07c6f881238c0739933d4b15c1984cfa62818d31
Justin D'Errico <justin.derrico1991@gmail.com>
2022-08-23 11:57:39 -0400

    fix(aave-v2): Apply template position presenter for Aave V2 forks (#1237)

--- src/apps/aave-v2/ethereum/aave-v2.position-presenter.ts
+++ src/apps/aave-v2/ethereum/aave-v2.position-presenter.ts
@@ -1,19 +1,18 @@
 import { Inject, Injectable } from '@nestjs/common';
 
-import { AaveV2ContractFactory } from '~apps/aave-v2';
 import { AaveV2PositionPresenter } from '~apps/aave-v2/common/aave-v2.position-presenter';
 import { Network } from '~types';
 
-import { AAVE_AMM_DEFINITION } from '../aave-amm.definition';
+import { AAVE_V2_DEFINITION } from '../aave-v2.definition';
+import { AaveV2ContractFactory } from '../contracts';
 
 @Injectable()
-export class EthereumAaveAmmPositionPresenter extends AaveV2PositionPresenter {
+export class EthereumAaveV2PositionPresenter extends AaveV2PositionPresenter {
   constructor(@Inject(AaveV2ContractFactory) contractFactory: AaveV2ContractFactory) {
     super(contractFactory);
   }
 
-  appId = AAVE_AMM_DEFINITION.id;
+  appId = AAVE_V2_DEFINITION.id;
   network = Network.ETHEREUM_MAINNET;
   lendingPoolAddress = '0x7d2768de32b0b80b7a3454c06bdac94a69ddc7a9';
-  explorePresentationConfig = undefined;
 }
