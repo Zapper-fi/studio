0d534e376f7f7523188ec41ab7356b159f2653bf
Karan Shahani <karan.shahani91@gmail.com>
2022-08-23 10:01:29 -0400

    feat(botto): Extract to Studio and templates

---
+++ src/apps/botto/botto.module.ts
@@ -0,0 +1,17 @@
+import { Register } from '~app-toolkit/decorators';
+
+import { BottoAppDefinition, BOTTO_DEFINITION } from './botto.definition';
+import { BottoContractFactory } from './contracts';
+import { EthereumBottoFarmContractPositionFetcher } from './ethereum/botto.farm.contract-position-fetcher';
+import { EthereumBottoGovernanceContractPositionFetcher } from './ethereum/botto.governance.contract-position-fetcher';
+
+@Register.AppModule({
+  appId: BOTTO_DEFINITION.id,
+  providers: [
+    BottoAppDefinition,
+    BottoContractFactory,
+    EthereumBottoFarmContractPositionFetcher,
+    EthereumBottoGovernanceContractPositionFetcher,
+  ],
+})
+export class BottoAppModule {}
---
+++ src/apps/botto/ethereum/botto.farm.contract-position-fetcher.ts
@@ -0,0 +1,70 @@
+import { Inject } from '@nestjs/common';
+import { BigNumberish } from 'ethers';
+
+import { APP_TOOLKIT, IAppToolkit } from '~app-toolkit/app-toolkit.interface';
+import { Register } from '~app-toolkit/decorators';
+import { GetTokenBalancesPerPositionParams } from '~position/template/contract-position.template.position-fetcher';
+import {
+  SingleStakingFarmDataProps,
+  SingleStakingFarmTemplateContractPositionFetcher,
+} from '~position/template/single-staking.template.contract-position-fetcher';
+import { Network } from '~types';
+
+import { BOTTO_DEFINITION } from '../botto.definition';
+import { BottoContractFactory, BottoLiquidityMining } from '../contracts';
+
+const appId = BOTTO_DEFINITION.id;
+const groupId = BOTTO_DEFINITION.groups.farm.id;
+const network = Network.ETHEREUM_MAINNET;
+
+@Register.ContractPositionFetcher({ appId, groupId, network })
+export class EthereumBottoFarmContractPositionFetcher extends SingleStakingFarmTemplateContractPositionFetcher<BottoLiquidityMining> {
+  appId = appId;
+  groupId = groupId;
+  network = network;
+  groupLabel = 'Farms';
+
+  constructor(
+    @Inject(APP_TOOLKIT) protected readonly appToolkit: IAppToolkit,
+    @Inject(BottoContractFactory) protected readonly contractFactory: BottoContractFactory,
+  ) {
+    super(appToolkit);
+  }
+
+  async getFarmDefinitions() {
+    return [
+      {
+        address: '0xf8515cae6915838543bcd7756f39268ce8f853fd',
+        stakedTokenAddress: '0x9dfad1b7102d46b1b197b90095b5c4e9f5845bba',
+        rewardTokenAddresses: [],
+      },
+    ];
+  }
+
+  getContract(address: string): BottoLiquidityMining {
+    return this.contractFactory.bottoLiquidityMining({ address, network: this.network });
+  }
+
+  async getRewardRates() {
+    return [0];
+  }
+
+  async getStakedTokenBalance({
+    address,
+    contract,
+  }: GetTokenBalancesPerPositionParams<BottoLiquidityMining, SingleStakingFarmDataProps>) {
+    return contract.totalUserStake(address);
+  }
+
+  getRewardTokenBalances({
+    address,
+    contract,
+  }: GetTokenBalancesPerPositionParams<BottoLiquidityMining, SingleStakingFarmDataProps>): Promise<
+    BigNumberish | BigNumberish[]
+  > {
+    return contract.callStatic
+      .withdraw({ from: address })
+      .then(res => res.reward)
+      .catch(() => 0);
+  }
+}
