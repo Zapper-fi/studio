584d3b351a598263b9f7f789bba100864befe16f
Karan Shahani <karan.shahani91@gmail.com>
2022-08-24 13:48:22 -0400

    feat(templates): Revert Rari Fuse

--- src/apps/rari-fuse/contracts/index.ts
+++ src/apps/rari-fuse/contracts/index.ts
@@ -4,10 +4,8 @@ import { IAppToolkit, APP_TOOLKIT } from '~app-toolkit/app-toolkit.interface';
 import { ContractFactory } from '~contract/contracts';
 import { Network } from '~types/network.interface';
 
-import { RariFuseComptroller__factory } from './ethers';
 import { RariFusePoolLens__factory } from './ethers';
 import { RariFusePoolsDirectory__factory } from './ethers';
-import { RariFuseToken__factory } from './ethers';
 
 // eslint-disable-next-line
 type ContractOpts = { address: string; network: Network };
@@ -18,21 +16,13 @@ export class RariFuseContractFactory extends ContractFactory {
     super((network: Network) => appToolkit.getNetworkProvider(network));
   }
 
-  rariFuseComptroller({ address, network }: ContractOpts) {
-    return RariFuseComptroller__factory.connect(address, this.appToolkit.getNetworkProvider(network));
-  }
   rariFusePoolLens({ address, network }: ContractOpts) {
     return RariFusePoolLens__factory.connect(address, this.appToolkit.getNetworkProvider(network));
   }
   rariFusePoolsDirectory({ address, network }: ContractOpts) {
     return RariFusePoolsDirectory__factory.connect(address, this.appToolkit.getNetworkProvider(network));
   }
-  rariFuseToken({ address, network }: ContractOpts) {
-    return RariFuseToken__factory.connect(address, this.appToolkit.getNetworkProvider(network));
-  }
 }
 
-export type { RariFuseComptroller } from './ethers';
 export type { RariFusePoolLens } from './ethers';
 export type { RariFusePoolsDirectory } from './ethers';
-export type { RariFuseToken } from './ethers';
--- src/apps/rari-fuse/ethereum/rari-fuse.supply.token-fetcher.ts
+++ src/apps/rari-fuse/ethereum/rari-fuse.supply.token-fetcher.ts
@@ -1,18 +1,51 @@
+import { Inject } from '@nestjs/common';
+
 import { Register } from '~app-toolkit/decorators';
+import { getLabelFromToken } from '~app-toolkit/helpers/presentation/image.present';
+import { CompoundContractFactory } from '~apps/compound';
+import { PositionFetcher } from '~position/position-fetcher.interface';
+import { AppTokenPosition } from '~position/position.interface';
 import { Network } from '~types/network.interface';
 
-import { RariFuseSupplyTokenFetcher } from '../common/rari-fuse.supply.token-fetcher';
+import { RariFuseContractFactory } from '../contracts';
+import { RariFuseSupplyTokenHelper } from '../helpers/rari-fuse.supply.token-helper';
 import { RARI_FUSE_DEFINITION } from '../rari-fuse.definition';
 
 const appId = RARI_FUSE_DEFINITION.id;
 const groupId = RARI_FUSE_DEFINITION.groups.supply.id;
 const network = Network.ETHEREUM_MAINNET;
-const groupLabel = 'Lending';
 
 @Register.TokenPositionFetcher({ appId, groupId, network })
-export class EthereumRariFuseSupplyTokenFetcher extends RariFuseSupplyTokenFetcher {
-  appId = appId;
-  groupId = groupId;
-  network = network;
-  groupLabel = groupLabel;
+export class EthereumRariFuseSupplyTokenFetcher implements PositionFetcher<AppTokenPosition> {
+  constructor(
+    @Inject(CompoundContractFactory) private readonly compoundContractFactory: CompoundContractFactory,
+    @Inject(RariFuseContractFactory) private readonly contractFactory: RariFuseContractFactory,
+    @Inject(RariFuseSupplyTokenHelper) private readonly rariFuseSupplyTokenHelper: RariFuseSupplyTokenHelper,
+  ) {}
+
+  async getPositions() {
+    return this.rariFuseSupplyTokenHelper.getTokens({
+      network,
+      appId,
+      groupId,
+      poolDirectoryAddress: '0x835482fe0532f169024d5e9410199369aad5c77e',
+      getRariFusePoolsDirectory: ({ address, network }) =>
+        this.contractFactory.rariFusePoolsDirectory({ address, network }),
+      getComptrollerContract: ({ address, network }) =>
+        this.compoundContractFactory.compoundComptroller({ address, network }),
+      getTokenContract: ({ address, network }) => this.compoundContractFactory.compoundCToken({ address, network }),
+      getAllMarkets: ({ contract, multicall }) => multicall.wrap(contract).getAllMarkets(),
+      getExchangeRate: async ({ contract, multicall }) =>
+        multicall
+          .wrap(contract)
+          .exchangeRateCurrent()
+          .catch(() => 0),
+      getSupplyRate: ({ contract, multicall }) => multicall.wrap(contract).supplyRatePerBlock(),
+      getBorrowRate: ({ contract, multicall }) => multicall.wrap(contract).borrowRatePerBlock(),
+      getUnderlyingAddress: ({ contract, multicall }) => multicall.wrap(contract).underlying(),
+      getExchangeRateMantissa: ({ underlyingTokenDecimals, tokenDecimals }) =>
+        18 + underlyingTokenDecimals - tokenDecimals,
+      getDisplayLabel: async ({ underlyingToken }) => getLabelFromToken(underlyingToken),
+    });
+  }
 }
