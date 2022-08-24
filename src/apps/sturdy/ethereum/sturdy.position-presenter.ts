76d502ca05c59e5511b47cc55bf7c43f1d079126
immasandwich <karan.shahani91@gmail.com>
2022-08-24 13:33:51 -0400

    feat(aave-v2): Remove unnecessary constructors in derived classes (#1251)

--- src/apps/sturdy/ethereum/sturdy.position-presenter.ts
+++ src/apps/sturdy/ethereum/sturdy.position-presenter.ts
@@ -1,6 +1,5 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 
-import { AaveV2ContractFactory } from '~apps/aave-v2';
 import { AaveV2PositionPresenter } from '~apps/aave-v2/common/aave-v2.position-presenter';
 import { Network } from '~types';
 
@@ -8,10 +7,6 @@ import STURDY_DEFINITION from '../sturdy.definition';
 
 @Injectable()
 export class EthereumSturdyPositionPresenter extends AaveV2PositionPresenter {
-  constructor(@Inject(AaveV2ContractFactory) contractFactory: AaveV2ContractFactory) {
-    super(contractFactory);
-  }
-
   appId = STURDY_DEFINITION.id;
   network = Network.ETHEREUM_MAINNET;
   lendingPoolAddress = '0xa422ca380bd70eef876292839222159e41aaee17';
