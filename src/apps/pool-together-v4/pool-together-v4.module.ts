ed8e5bef6781543b0f39ad3071168f77154ee42b
Justin D'Errico <justin.derrico1991@gmail.com>
2022-08-30 11:48:32 -0400

    fix

--- src/apps/pool-together-v4/pool-together-v4.module.ts
+++ src/apps/pool-together-v4/pool-together-v4.module.ts
@@ -1,10 +1,12 @@
 import { Register } from '~app-toolkit/decorators';
 import { AbstractApp } from '~app/app.dynamic-module';
 
+import { AvalanchePoolTogetherV4ClaimableContractPositionFetcher } from './avalanche/pool-together-v4.claimable.contract-position-fetcher';
 import { AvalanchePoolTogetherV4TicketTokenFetcher } from './avalanche/pool-together-v4.ticket-token-fetcher';
 import { PoolTogetherV4ApiPrizePoolRegistry } from './common/pool-together-v4.api.prize-pool-registry';
 import { PoolTogetherV4ContractFactory } from './contracts';
 import { EthereumPoolTogetherV4TicketTokenFetcher } from './ethereum/pool-together-v4.ticket-token-fetcher';
+import { OptimismPoolTogetherV4ClaimableContractPositionFetcher } from './optimism/pool-together-v4.claimable.contract-position-fetcher';
 import { OptimismPoolTogetherV4TicketTokenFetcher } from './optimism/pool-together-v4.ticket-token-fetcher';
 import { PolygonPoolTogetherV4TicketTokenFetcher } from './polygon/pool-together-v4.ticket-token-fetcher';
 import POOL_TOGETHER_V4_DEFINITION, { PoolTogetherV4AppDefinition } from './pool-together-v4.definition';
@@ -13,8 +15,10 @@ import POOL_TOGETHER_V4_DEFINITION, { PoolTogetherV4AppDefinition } from './pool
   appId: POOL_TOGETHER_V4_DEFINITION.id,
   providers: [
     AvalanchePoolTogetherV4TicketTokenFetcher,
+    AvalanchePoolTogetherV4ClaimableContractPositionFetcher,
     EthereumPoolTogetherV4TicketTokenFetcher,
     OptimismPoolTogetherV4TicketTokenFetcher,
+    OptimismPoolTogetherV4ClaimableContractPositionFetcher,
     PolygonPoolTogetherV4TicketTokenFetcher,
     PoolTogetherV4ApiPrizePoolRegistry,
     PoolTogetherV4AppDefinition,
