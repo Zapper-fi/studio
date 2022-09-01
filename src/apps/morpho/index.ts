7d0ea27b91442ae6b84b6a0d3946bfcea8c5d27a
Karan Shahani <karan.shahani91@gmail.com>
2022-09-01 10:57:13 -0400

    feat(morpho): Move to templates, fix position

--- src/apps/morpho/index.ts
+++
@@ -1,3 +0,0 @@
-export { MORPHO_DEFINITION, MorphoAppDefinition } from './morpho.definition';
-export { MorphoAppModule } from './morpho.module';
-export { MorphoContractFactory } from './contracts';
--- src/apps/morpho/morpho.module.ts
+++ src/apps/morpho/morpho.module.ts
@@ -1,29 +1,19 @@
 import { Register } from '~app-toolkit/decorators';
 import { AbstractApp } from '~app/app.dynamic-module';
-import { MorphoBalanceHelper } from '~apps/morpho/helpers/morpho.balance-helper';
-import { MorphoCompoundLendingMetaHelper } from '~apps/morpho/helpers/morpho.compound-lending.meta-helper';
-import { MorphoMarketsHelper } from '~apps/morpho/helpers/morpho.markets-helper';
-import { MorphoCompoundContractPositionHelper } from '~apps/morpho/helpers/morpho.morpho-compound.contract-position-helper';
-import { MorphoRateHelper } from '~apps/morpho/helpers/morpho.rate-helper';
 
 import { MorphoContractFactory } from './contracts';
-import { EthereumMorphoBalanceFetcher } from './ethereum/morpho.balance-fetcher';
 import { EthereumMorphoCompoundSupplyContractPositionFetcher } from './ethereum/morpho.morpho-compound.contract-position-fetcher';
+import { EthereumMorphoPositionPresenter } from './ethereum/morpho.position-presenter';
 import { MorphoAppDefinition, MORPHO_DEFINITION } from './morpho.definition';
 
 @Register.AppModule({
   appId: MORPHO_DEFINITION.id,
   providers: [
-    EthereumMorphoBalanceFetcher,
-    EthereumMorphoCompoundSupplyContractPositionFetcher,
     MorphoAppDefinition,
-    MorphoCompoundContractPositionHelper,
     MorphoContractFactory,
-    MorphoMarketsHelper,
-    MorphoRateHelper,
-    MorphoCompoundLendingMetaHelper,
-    MorphoBalanceHelper,
+    // Ethereum
+    EthereumMorphoCompoundSupplyContractPositionFetcher,
+    EthereumMorphoPositionPresenter,
   ],
-  exports: [MorphoCompoundContractPositionHelper],
 })
 export class MorphoAppModule extends AbstractApp() {}
