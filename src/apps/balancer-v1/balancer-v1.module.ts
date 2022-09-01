02ee5858335b2f2a4f578631ef686d4b1e333796
Karan Shahani <karan.shahani91@gmail.com>
2022-09-01 15:54:19 -0400

    feat(balancer-v1): Move Balancer V1 to templates

--- src/apps/balancer-v1/balancer-v1.module.ts
+++ src/apps/balancer-v1/balancer-v1.module.ts
@@ -2,11 +2,10 @@ import { Register } from '~app-toolkit/decorators';
 import { AbstractApp } from '~app/app.dynamic-module';
 
 import BALANCER_V1_DEFINITION, { BalancerV1AppDefinition } from './balancer-v1.definition';
-import { EthereumBalancerV1BalanceFetcher } from './ethereum/balancer-v1.balance-fetcher';
-import { EthereumBalancerV1PoolTokenFetcher } from './ethereum/balancer-v1.token-fetcher';
+import { EthereumBalancerV1PoolTokenFetcher } from './ethereum/balancer-v1.pool.token-fetcher';
 
 @Register.AppModule({
   appId: BALANCER_V1_DEFINITION.id,
-  providers: [BalancerV1AppDefinition, EthereumBalancerV1PoolTokenFetcher, EthereumBalancerV1BalanceFetcher],
+  providers: [BalancerV1AppDefinition, EthereumBalancerV1PoolTokenFetcher],
 })
 export class BalancerV1AppModule extends AbstractApp() {}
