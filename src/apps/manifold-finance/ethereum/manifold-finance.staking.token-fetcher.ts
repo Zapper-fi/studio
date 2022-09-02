c06aca45b4f9a295e0700cd54efea0832312f8b2
Clonescody <clonescody@protonmail.com>
2022-09-01 19:42:37 -0700

    switch to new vault template

--- src/apps/manifold-finance/ethereum/manifold-finance.staking.token-fetcher.ts
+++ src/apps/manifold-finance/ethereum/manifold-finance.staking.token-fetcher.ts
@@ -1,13 +1,7 @@
-import { Inject } from '@nestjs/common';
-
-import { IAppToolkit, APP_TOOLKIT } from '~app-toolkit/app-toolkit.interface';
 import { Register } from '~app-toolkit/decorators';
-import { PositionFetcher } from '~position/position-fetcher.interface';
-import { AppTokenPosition } from '~position/position.interface';
+import { VaultTokenFetcher } from '~position/template/vault.template.token-fetcher';
 import { Network } from '~types/network.interface';
 
-import { ManifoldFinanceContractFactory } from '../contracts';
-import { XFold } from '../contracts/ethers';
 import { MANIFOLD_FINANCE_DEFINITION } from '../manifold-finance.definition';
 
 const appId = MANIFOLD_FINANCE_DEFINITION.id;
@@ -15,28 +9,17 @@ const groupId = MANIFOLD_FINANCE_DEFINITION.groups.staking.id;
 const network = Network.ETHEREUM_MAINNET;
 
 @Register.TokenPositionFetcher({ appId, groupId, network })
-export class EthereumManifoldFinanceStakingTokenFetcher implements PositionFetcher<AppTokenPosition> {
-  constructor(
-    @Inject(APP_TOOLKIT) private readonly appToolkit: IAppToolkit,
-    @Inject(ManifoldFinanceContractFactory)
-    private readonly manifoldFinanceContractFactory: ManifoldFinanceContractFactory,
-  ) {}
+export class EthereumManifoldFinanceStakingTokenFetcher extends VaultTokenFetcher {
+  appId = appId;
+  groupId = groupId;
+  network = network;
+  groupLabel = 'xFOLD';
 
-  async getPositions() {
-    return this.appToolkit.helpers.vaultTokenHelper.getTokens<XFold>({
-      appId: MANIFOLD_FINANCE_DEFINITION.id,
-      groupId: MANIFOLD_FINANCE_DEFINITION.groups.staking.id,
-      network: Network.ETHEREUM_MAINNET,
-      resolveContract: ({ network }) =>
-        this.manifoldFinanceContractFactory.xFold({ address: '0x454bd9e2b29eb5963048cc1a8bd6fd44e89899cb', network }),
-      resolveVaultAddresses: () => ['0x454bd9e2b29eb5963048cc1a8bd6fd44e89899cb'],
-      resolveUnderlyingTokenAddress: ({ contract, multicall }) => multicall.wrap(contract).token(),
-      resolveReserve: async ({ underlyingToken, multicall, address }) =>
-        multicall
-          .wrap(this.appToolkit.globalContracts.erc20(underlyingToken))
-          .balanceOf(address)
-          .then(v => Number(v) / 10 ** underlyingToken.decimals),
-      resolvePricePerShare: () => 1,
-    });
+  vaultAddress = '0x454bd9e2b29eb5963048cc1a8bd6fd44e89899cb';
+  underlyingTokenAddress = '0xc0d4ceb216b3ba9c3701b291766fdcba977cec3a';
+  reserveAddress = '0x454bd9e2b29eb5963048cc1a8bd6fd44e89899cb';
+
+  async getPricePerShare() {
+    return 1;
   }
 }
--- src/apps/manifold-finance/manifold-finance.module.ts
+++ src/apps/manifold-finance/manifold-finance.module.ts
@@ -2,17 +2,11 @@ import { Register } from '~app-toolkit/decorators';
 import { AbstractApp } from '~app/app.dynamic-module';
 
 import { ManifoldFinanceContractFactory } from './contracts';
-import { EthereumManifoldFinanceBalanceFetcher } from './ethereum/manifold-finance.balance-fetcher';
 import { EthereumManifoldFinanceStakingTokenFetcher } from './ethereum/manifold-finance.staking.token-fetcher';
 import { ManifoldFinanceAppDefinition, MANIFOLD_FINANCE_DEFINITION } from './manifold-finance.definition';
 
 @Register.AppModule({
   appId: MANIFOLD_FINANCE_DEFINITION.id,
-  providers: [
-    EthereumManifoldFinanceBalanceFetcher,
-    EthereumManifoldFinanceStakingTokenFetcher,
-    ManifoldFinanceAppDefinition,
-    ManifoldFinanceContractFactory,
-  ],
+  providers: [EthereumManifoldFinanceStakingTokenFetcher, ManifoldFinanceAppDefinition, ManifoldFinanceContractFactory],
 })
 export class ManifoldFinanceAppModule extends AbstractApp() {}
