cdec9fd29deee505176696ef42c24583616a4469
Julien <julien@morpho.xyz>
2022-09-05 16:19:23 -0400

    feat(morpho): add Morpho-Aave position fetcher

--- src/apps/morpho/morpho.module.ts
+++ src/apps/morpho/morpho.module.ts
@@ -2,6 +2,7 @@ import { Register } from '~app-toolkit/decorators';
 import { AbstractApp } from '~app/app.dynamic-module';
 
 import { MorphoContractFactory } from './contracts';
+import { EthereumMorphoAaveV2SupplyContractPositionFetcher } from './ethereum/morpho.morpho-aave-v2.contract-position-fetcher';
 import { EthereumMorphoCompoundSupplyContractPositionFetcher } from './ethereum/morpho.morpho-compound.contract-position-fetcher';
 import { EthereumMorphoPositionPresenter } from './ethereum/morpho.position-presenter';
 import { MorphoAppDefinition, MORPHO_DEFINITION } from './morpho.definition';
@@ -13,6 +14,7 @@ import { MorphoAppDefinition, MORPHO_DEFINITION } from './morpho.definition';
     MorphoContractFactory,
     // Ethereum
     EthereumMorphoCompoundSupplyContractPositionFetcher,
+    EthereumMorphoAaveV2SupplyContractPositionFetcher,
     EthereumMorphoPositionPresenter,
   ],
 })
