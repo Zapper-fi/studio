26266fe706b52fbb419f9556514594de9505f430
Justin D'Errico <justin.derrico1991@gmail.com>
2022-09-06 17:09:04 -0400

    lint

--- src/apps/granary-finance/granary-finance.module.ts
+++ src/apps/granary-finance/granary-finance.module.ts
@@ -2,17 +2,16 @@ import { Register } from '~app-toolkit/decorators';
 import { AbstractApp } from '~app/app.dynamic-module';
 import { AaveV2AppModule } from '~apps/aave-v2';
 
+import { GranaryFinanceContractFactory } from './contracts';
 import { EthereumGranaryFinancePositionPresenter } from './ethereum/granary-finance.position-presenter';
 import { EthereumGranaryFinanceStableDebtTokenFetcher } from './ethereum/granary-finance.stable-debt.token-fetcher';
 import { EthereumGranaryFinanceSupplyTokenFetcher } from './ethereum/granary-finance.supply.token-fetcher';
 import { EthereumGranaryFinanceVariableDebtTokenFetcher } from './ethereum/granary-finance.variable-debt.token-fetcher';
+import { GranaryFinanceAppDefinition, GRANARY_FINANCE_DEFINITION } from './granary-finance.definition';
 import { OptimismGranaryFinancePositionPresenter } from './optimism/granary-finance.position-presenter';
 import { OptimismGranaryFinanceStableDebtTokenFetcher } from './optimism/granary-finance.stable-debt.token-fetcher';
 import { OptimismGranaryFinanceSupplyTokenFetcher } from './optimism/granary-finance.supply.token-fetcher';
 import { OptimismGranaryFinanceVariableDebtTokenFetcher } from './optimism/granary-finance.variable-debt.token-fetcher';
-
-import { GranaryFinanceContractFactory } from './contracts';
-import { GranaryFinanceAppDefinition, GRANARY_FINANCE_DEFINITION } from './granary-finance.definition';
 
 @Register.AppModule({
   appId: GRANARY_FINANCE_DEFINITION.id,
@@ -26,6 +25,8 @@ import { GranaryFinanceAppDefinition, GRANARY_FINANCE_DEFINITION } from './grana
     OptimismGranaryFinanceStableDebtTokenFetcher,
     OptimismGranaryFinanceSupplyTokenFetcher,
     OptimismGranaryFinanceVariableDebtTokenFetcher,
-    GranaryFinanceAppDefinition, GranaryFinanceContractFactory],
+    GranaryFinanceAppDefinition,
+    GranaryFinanceContractFactory,
+  ],
 })
-export class GranaryFinanceAppModule extends AbstractApp() { }
+export class GranaryFinanceAppModule extends AbstractApp() {}
