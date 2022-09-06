ca0e8d3c44389746a1e7d2a75d802b6f7e375bd1
immasandwich <karan.shahani91@gmail.com>
2022-09-06 16:15:42 -0400

    feat(rocket-pool): Move to templates (#1358)

--- src/apps/rocket-pool/ethereum/rocket-pool.minipool.contract-position-fetcher.ts
+++ src/apps/rocket-pool/ethereum/rocket-pool.minipool.contract-position-fetcher.ts
@@ -1,21 +1,22 @@
 import { Inject, Injectable } from '@nestjs/common';
 
 import { IAppToolkit, APP_TOOLKIT } from '~app-toolkit/app-toolkit.interface';
+import { ZERO_ADDRESS } from '~app-toolkit/constants/address';
 import { getLabelFromToken } from '~app-toolkit/helpers/presentation/image.present';
 import { MetaType } from '~position/position.interface';
 import { ContractPositionTemplatePositionFetcher } from '~position/template/contract-position.template.position-fetcher';
 import { GetDisplayPropsParams, GetTokenBalancesParams } from '~position/template/contract-position.template.types';
 import { Network } from '~types/network.interface';
 
-import { RocketNodeStaking, RocketPoolContractFactory } from '../contracts';
+import { RocketMinipoolManager, RocketPoolContractFactory } from '../contracts';
 import { ROCKET_POOL_DEFINITION } from '../rocket-pool.definition';
 
 @Injectable()
-export class EthereumRocketPoolStakingContractPositionFetcher extends ContractPositionTemplatePositionFetcher<RocketNodeStaking> {
+export class EthereumRocketPoolMinipoolContractPositionFetcher extends ContractPositionTemplatePositionFetcher<RocketMinipoolManager> {
   appId = ROCKET_POOL_DEFINITION.id;
-  groupId = ROCKET_POOL_DEFINITION.groups.staking.id;
+  groupId = ROCKET_POOL_DEFINITION.groups.minipool.id;
   network = Network.ETHEREUM_MAINNET;
-  groupLabel = 'Staking';
+  groupLabel = 'Minipools';
 
   constructor(
     @Inject(APP_TOOLKIT) protected readonly appToolkit: IAppToolkit,
@@ -24,23 +25,26 @@ export class EthereumRocketPoolStakingContractPositionFetcher extends ContractPo
     super(appToolkit);
   }
 
-  getContract(address: string): RocketNodeStaking {
-    return this.contractFactory.rocketNodeStaking({ address, network: this.network });
+  getContract(address: string): RocketMinipoolManager {
+    return this.contractFactory.rocketMinipoolManager({ address, network: this.network });
   }
 
   async getDefinitions() {
-    return [{ address: '0x3019227b2b8493e45bf5d25302139c9a2713bf15' }];
+    return [{ address: '0x6293b8abc1f36afb22406be5f96d893072a8cf3a' }];
   }
 
   async getTokenDefinitions() {
-    return [{ metaType: MetaType.SUPPLIED, address: '0xd33526068d116ce69f19a9ee46f0bd304f21a51f' }];
+    return [{ metaType: MetaType.SUPPLIED, address: ZERO_ADDRESS }];
   }
 
-  async getLabel({ contractPosition }: GetDisplayPropsParams<RocketNodeStaking>) {
+  async getLabel({ contractPosition }: GetDisplayPropsParams<RocketMinipoolManager>) {
     return `Staked ${getLabelFromToken(contractPosition.tokens[0])}`;
   }
 
-  async getTokenBalancesPerPosition({ address, contract }: GetTokenBalancesParams<RocketNodeStaking>) {
-    return [await contract.getNodeRPLStake(address)];
+  async getTokenBalancesPerPosition({ address, contract }: GetTokenBalancesParams<RocketMinipoolManager>) {
+    const minipoolCount = (await contract.getNodeActiveMinipoolCount(address)).toNumber();
+    const minipoolDepositSize = 16 * 10 ** 18; // 16 ETH
+    const balanceRaw = minipoolCount * minipoolDepositSize;
+    return [balanceRaw.toString()];
   }
 }
