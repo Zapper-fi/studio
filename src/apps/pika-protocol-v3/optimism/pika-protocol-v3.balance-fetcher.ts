ff1e37a86aa37c7da269263c3b62b87ff73857cf
The Ol' Dirty Bubble <106809061+theonepichael@users.noreply.github.com>
2022-09-07 09:43:08 -0400

    fix(pika-protocol-v3): Updates vault position calculation logic (#1360)

--- src/apps/pika-protocol-v3/optimism/pika-protocol-v3.balance-fetcher.ts
+++ src/apps/pika-protocol-v3/optimism/pika-protocol-v3.balance-fetcher.ts
@@ -5,10 +5,11 @@ import { IAppToolkit, APP_TOOLKIT } from '~app-toolkit/app-toolkit.interface';
 import { Register } from '~app-toolkit/decorators';
 import { presentBalanceFetcherResponse } from '~app-toolkit/helpers/presentation/balance-fetcher-response.present';
 import { BalanceFetcher } from '~balance/balance-fetcher.interface';
+import { IMulticallWrapper } from '~multicall';
 import { isClaimable, isSupplied } from '~position/position.utils';
 import { Network } from '~types/network.interface';
 
-import { PikaProtocolV3ContractFactory } from '../contracts';
+import { PikaProtocolV3ContractFactory, PikaProtocolV3Vault } from '../contracts';
 import { PIKA_PROTOCOL_V_3_DEFINITION } from '../pika-protocol-v3.definition';
 
 const network = Network.OPTIMISM_MAINNET;
@@ -18,11 +19,19 @@ export class OptimismPikaProtocolV3BalanceFetcher implements BalanceFetcher {
   constructor(
     @Inject(APP_TOOLKIT) private readonly appToolkit: IAppToolkit,
     @Inject(PikaProtocolV3ContractFactory) private readonly contractFactory: PikaProtocolV3ContractFactory,
-  ) {}
+  ) { }
 
   async getBalances(address: string) {
     const [vaultBalances] = await Promise.all([this.getFarmBalances(address)]);
     return presentBalanceFetcherResponse([{ label: 'Vaults', assets: vaultBalances }]);
+  }
+
+  private async getBalanceInVault(multicall: IMulticallWrapper, contract: PikaProtocolV3Vault, address: string) {
+    const contractInst = await multicall.wrap(contract);
+    const userShare = await contractInst.getShare(address);
+    const vaultBalance = (await contractInst.getVault()).balance;
+    const totalShare = await contractInst.getTotalShare();
+    return Number(userShare) * Number(vaultBalance) / Number(totalShare);
   }
 
   async getFarmBalances(address: string) {
@@ -47,7 +56,7 @@ export class OptimismPikaProtocolV3BalanceFetcher implements BalanceFetcher {
         const rewardToken = contractPosition.tokens.find(isClaimable)!;
 
         const [stakedBalanceRaw, rewardBalanceRaw] = await Promise.all([
-          multicall.wrap(contract).getShare(address),
+          this.getBalanceInVault(multicall, contract, address),
           multicall.wrap(rewardsContract).getClaimableReward(address),
         ]);
 
