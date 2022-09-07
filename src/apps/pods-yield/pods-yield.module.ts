635c1caa6dc0d2dde5ad19b5f1770d0828897cd4
Razvan Gabriel <razvan.gabriel.apostu@gmail.com>
2022-09-07 22:26:18 +0300

    feat(pods-yield): balance fetcher for user position in strategy and in queue

--- src/apps/pods-yield/pods-yield.module.ts
+++ src/apps/pods-yield/pods-yield.module.ts
@@ -2,6 +2,7 @@ import { Register } from '~app-toolkit/decorators';
 import { AbstractApp } from '~app/app.dynamic-module';
 
 import { PodsYieldContractFactory } from './contracts';
+import { EthereumPodsYieldBalanceFetcher } from './ethereum/pods-yield.balance-fetcher';
 import { EthereumPodsYieldQueueContractPositionFetcher } from './ethereum/pods-yield.queue.contract-position-fetcher';
 import { EthereumPodsYieldStrategyTokenFetcher } from './ethereum/pods-yield.strategy.token-fetcher';
 import { PodsYieldAppDefinition, PODS_YIELD_DEFINITION } from './pods-yield.definition';
@@ -9,6 +10,7 @@ import { PodsYieldAppDefinition, PODS_YIELD_DEFINITION } from './pods-yield.defi
 @Register.AppModule({
   appId: PODS_YIELD_DEFINITION.id,
   providers: [
+    EthereumPodsYieldBalanceFetcher,
     EthereumPodsYieldQueueContractPositionFetcher,
     EthereumPodsYieldStrategyTokenFetcher,
     PodsYieldAppDefinition,
