ac01a66159cf8177fac627c215560182185e29d8
immasandwich <karan.shahani91@gmail.com>
2022-09-09 12:44:14 -0400

    fix(pickle): Fix Master Chef V2 position fetchers (#1385)

--- src/position/template/master-chef-v2.template.contract-position-fetcher.ts
+++ src/position/template/master-chef-v2.template.contract-position-fetcher.ts
@@ -82,6 +82,7 @@ export abstract class MasterChefV2TemplateContractPositionFetcher<
     const { contract, definition, multicall } = params;
     const extraRewarderAddress = await this.getExtraRewarder(contract, definition.poolIndex);
     const rewarderContract = multicall.wrap(this.getExtraRewarderContract(extraRewarderAddress));
+    if (extraRewarderAddress === ZERO_ADDRESS) return rewardRates;
 
     const extraRewardRates = await this.getExtraRewardTokenRewardRates({ ...params, rewarderContract });
     return [...rewardRates, ...(Array.isArray(extraRewardRates) ? extraRewardRates : [extraRewardRates])];
