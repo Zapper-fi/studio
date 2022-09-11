69a9d4846d2e4fbcfccdda811c4a6eda07d28da3
William Poulin <williampoulin01@gmail.com>
2022-09-11 00:14:26 -0400

    chore(argo-finance): migrate argo-finance to template

--- src/apps/argo-finance/cronos/argo-finance.x-argo.token-fetcher.ts
+++ src/apps/argo-finance/cronos/argo-finance.x-argo.token-fetcher.ts
@@ -1,70 +1,26 @@
-import { Inject } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 
-import { IAppToolkit, APP_TOOLKIT } from '~app-toolkit/app-toolkit.interface';
-import { Register } from '~app-toolkit/decorators';
-import { buildDollarDisplayItem } from '~app-toolkit/helpers/presentation/display-item.present';
-import { getAppAssetImage } from '~app-toolkit/helpers/presentation/image.present';
-import { ContractType } from '~position/contract.interface';
-import { PositionFetcher } from '~position/position-fetcher.interface';
-import { AppTokenPosition } from '~position/position.interface';
 import { Network } from '~types/network.interface';
 
 import { ARGO_FINANCE_DEFINITION } from '../argo-finance.definition';
-import { ArgoFinanceContractFactory } from '../contracts';
+import { ArgoFinanceXargoTokenFetcher } from '../common/argo-finance.x-argo.token-fetcher';
 
-import { ADDRESSES } from './consts';
+@Injectable()
+export class CronosArgoFinanceXArgoTokenFetcher extends ArgoFinanceXargoTokenFetcher {
+  appId = ARGO_FINANCE_DEFINITION.id;
+  groupId = ARGO_FINANCE_DEFINITION.groups.xArgo.id;
+  network = Network.CRONOS_MAINNET;
+  groupLabel = 'XArgo';
 
-const appId = ARGO_FINANCE_DEFINITION.id;
-const groupId = ARGO_FINANCE_DEFINITION.groups.xArgo.id;
-const network = Network.CRONOS_MAINNET;
+  isExcludedFromBalances = true;
+  isExcludedFromExplore = true;
+  isExcludedFromTvl = true;
 
-@Register.TokenPositionFetcher({ appId, groupId, network })
-export class CronosArgoFinanceXArgoTokenFetcher implements PositionFetcher<AppTokenPosition> {
-  constructor(
-    @Inject(APP_TOOLKIT) private readonly appToolkit: IAppToolkit,
-    @Inject(ArgoFinanceContractFactory) private readonly argoFinanceContractFactory: ArgoFinanceContractFactory,
-  ) {}
-
-  async getVePosition(address: string, baseAddress: string) {
-    const multicall = this.appToolkit.getMulticall(network);
-    const baseTokens = await this.appToolkit.getBaseTokenPrices(network);
-    const baseToken = baseTokens.find(t => t.address === baseAddress)!;
-    const veToken = multicall.wrap(this.appToolkit.globalContracts.erc20({ address, network }));
-    const [supplyRaw, decimals, symbol] = await Promise.all([
-      veToken.totalSupply(),
-      veToken.decimals(),
-      veToken.symbol(),
-    ]);
-    const supply = Number(supplyRaw) / 10 ** decimals;
-    const pricePerShare = 1; // Note: Consult liquidity pools for peg once set up
-    const price = baseToken.price * pricePerShare;
-    const liquidity = supply * price;
-
-    const token: AppTokenPosition = {
-      type: ContractType.APP_TOKEN,
-      appId,
-      groupId,
-      address,
-      network,
-      symbol,
-      decimals,
-      supply,
-      price,
-      pricePerShare,
-      tokens: [baseToken],
-      dataProps: { liquidity },
-      displayProps: {
-        label: symbol,
-        secondaryLabel: buildDollarDisplayItem(price),
-        images: [getAppAssetImage(appId, address)],
-        statsItems: [{ label: 'Liquidity', value: buildDollarDisplayItem(liquidity) }],
-      },
-    };
-    return token;
+  async getAddresses(): Promise<string[]> {
+    return ['0xb966b5d6a0fcd5b373b180bbe072bbfbbee10552'];
   }
 
-  async getPositions() {
-    const [argo] = await Promise.all([this.getVePosition(ADDRESSES.xargo, ADDRESSES.argo)]);
-    return [argo];
+  async getUnderlyingTokenAddresses(): Promise<string> {
+    return '0x47a9d630dc5b28f75d3af3be3aaa982512cd89aa';
   }
 }
--- src/apps/argo-finance/cronos/consts.ts
+++
@@ -1,5 +0,0 @@
-export const ADDRESSES = {
-  argo: '0x47a9d630dc5b28f75d3af3be3aaa982512cd89aa'.toLowerCase(),
-  xargo: '0xb966b5d6a0fcd5b373b180bbe072bbfbbee10552'.toLowerCase(),
-  pledging: '0x1de93ce995d1bc763c2422ba30b1e73de4a45a01'.toLowerCase(),
-};
