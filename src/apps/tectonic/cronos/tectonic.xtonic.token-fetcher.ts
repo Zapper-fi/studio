0b1bde110a86a4a731e56230599140652d481284
Justin D'Errico <justin.derrico1991@gmail.com>
2022-09-12 17:53:29 -0400

    feat(tectonic): Migrate to template

--- src/apps/tectonic/cronos/tectonic.xtonic.token-fetcher.ts
+++ src/apps/tectonic/cronos/tectonic.xtonic.token-fetcher.ts
@@ -1,43 +1,39 @@
-import { Inject } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 
 import { IAppToolkit, APP_TOOLKIT } from '~app-toolkit/app-toolkit.interface';
-import { Register } from '~app-toolkit/decorators';
-import { PositionFetcher } from '~position/position-fetcher.interface';
-import { AppTokenPosition } from '~position/position.interface';
+import { Erc20 } from '~contract/contracts';
+import { GetPricePerShareParams } from '~position/template/app-token.template.types';
+import { VaultTokenDataProps, VaultTokenFetcher } from '~position/template/vault.template.token-fetcher';
 import { Network } from '~types/network.interface';
 
-import { TectonicContractFactory, TectonicXtonic } from '../contracts';
+import { TectonicContractFactory } from '../contracts';
 import { TECTONIC_DEFINITION } from '../tectonic.definition';
 
-const appId = TECTONIC_DEFINITION.id;
-const groupId = TECTONIC_DEFINITION.groups.xtonic.id;
-const network = Network.CRONOS_MAINNET;
-const TONIC_ADDRESS = '0xdd73dea10abc2bff99c60882ec5b2b81bb1dc5b2';
+@Injectable()
+export class CronosTectonicXTonicTokenFetcher extends VaultTokenFetcher {
+  appId = TECTONIC_DEFINITION.id;
+  groupId = TECTONIC_DEFINITION.groups.xtonic.id;
+  network = Network.CRONOS_MAINNET;
+  groupLabel = 'xTONIC';
 
-@Register.TokenPositionFetcher({ appId, groupId, network })
-export class CronosTectonicXTonicTokenFetcher implements PositionFetcher<AppTokenPosition> {
+  vaultAddress = '0x1bc9b7d4be47b76965a3f8e910b9ddd83150840f';
+  underlyingTokenAddress = '0xdd73dea10abc2bff99c60882ec5b2b81bb1dc5b2'; // Tonic
+
   constructor(
-    @Inject(APP_TOOLKIT) private readonly appToolkit: IAppToolkit,
-    @Inject(TectonicContractFactory) private readonly tectonicContractFactory: TectonicContractFactory,
-  ) {}
+    @Inject(APP_TOOLKIT) readonly appToolkit: IAppToolkit,
+    @Inject(TectonicContractFactory) private readonly contractFactory: TectonicContractFactory,
+  ) {
+    super(appToolkit);
+  }
 
-  async getPositions() {
-    return this.appToolkit.helpers.vaultTokenHelper.getTokens<TectonicXtonic>({
-      appId,
-      groupId,
-      network,
-      resolveContract: ({ address, network }) => this.tectonicContractFactory.tectonicXtonic({ address, network }),
-      resolveVaultAddresses: () => ['0x1bc9b7d4be47b76965a3f8e910b9ddd83150840f'],
-      resolveUnderlyingTokenAddress: () => TONIC_ADDRESS, // TONIC
-      resolveReserve: async () => {
-        const tonic = this.tectonicContractFactory.tectonicTToken({ address: TONIC_ADDRESS, network });
-        const [stakedTonic, decimals] = await Promise.all([
-          tonic.balanceOf('0xe165132fda537fa89ca1b52a647240c2b84c8f89'), // staking pool address
-          tonic.decimals(),
-        ]);
-        return Number(stakedTonic) / 10 ** decimals;
-      },
-      resolvePricePerShare: ({ reserve, supply }) => reserve / supply,
-    });
+  async getReserve({ multicall }: GetPricePerShareParams<Erc20, VaultTokenDataProps>) {
+    const tonic = multicall.wrap(
+      this.contractFactory.tectonicTToken({ address: this.underlyingTokenAddress, network: this.network }),
+    );
+    const [stakedTonic, decimals] = await Promise.all([
+      tonic.balanceOf('0xe165132fda537fa89ca1b52a647240c2b84c8f89'), // staking pool address
+      tonic.decimals(),
+    ]);
+    return Number(stakedTonic) / 10 ** decimals;
   }
 }
