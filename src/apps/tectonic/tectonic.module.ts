301c27db2100f8a9fb52e47d3cdefc296deb3ade
Justin D'Errico <justin.derrico1991@gmail.com>
2022-09-12 17:55:54 -0400

    position presenter

--- src/apps/tectonic/tectonic.module.ts
+++ src/apps/tectonic/tectonic.module.ts
@@ -3,6 +3,7 @@ import { AbstractApp } from '~app/app.dynamic-module';
 
 import { TectonicContractFactory } from './contracts';
 import { CronosTectonicBorrowContractPositionFetcher } from './cronos/tectonic.borrow.contract-position-fetcher';
+import { CronosTectonicPositionPresenter } from './cronos/tectonic.position-presenter';
 import { CronosTectonicSupplyTokenFetcher } from './cronos/tectonic.supply.token-fetcher';
 import { CronosTectonicXTonicTokenFetcher } from './cronos/tectonic.xtonic.token-fetcher';
 import { TectonicAppDefinition, TECTONIC_DEFINITION } from './tectonic.definition';
@@ -13,6 +14,7 @@ import { TectonicAppDefinition, TECTONIC_DEFINITION } from './tectonic.definitio
     CronosTectonicBorrowContractPositionFetcher,
     CronosTectonicSupplyTokenFetcher,
     CronosTectonicXTonicTokenFetcher,
+    CronosTectonicPositionPresenter,
     TectonicAppDefinition,
     TectonicContractFactory,
   ],
