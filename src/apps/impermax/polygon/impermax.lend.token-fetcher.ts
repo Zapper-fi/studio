df5e38b6ac44b7992b8caba33ae5c21e726bb720
Justin D'Errico <justin.derrico1991@gmail.com>
2022-09-14 11:22:03 -0400

    feat(impermax): Migrate to template

--- src/apps/impermax/polygon/impermax.lend.token-fetcher.ts
+++ src/apps/impermax/polygon/impermax.lend.token-fetcher.ts
@@ -1,24 +1,16 @@
-import { Inject } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 
-import { Register } from '~app-toolkit/decorators';
-import { PositionFetcher } from '~position/position-fetcher.interface';
-import { AppTokenPosition } from '~position/position.interface';
 import { Network } from '~types/network.interface';
 
-import { ImpermaxLendTokenHelper } from '../helpers/impermax.lend.token-fetcher-helper';
+import { ImpermaxLendTokenFetcher } from '../helpers/impermax.lend.token-fetcher';
 import { IMPERMAX_DEFINITION } from '../impermax.definition';
 
-const appId = IMPERMAX_DEFINITION.id;
-const groupId = IMPERMAX_DEFINITION.groups.lend.id;
-const network = Network.POLYGON_MAINNET;
+@Injectable()
+export class PolygonImpermaxLendTokenFetcher extends ImpermaxLendTokenFetcher {
+  appId = IMPERMAX_DEFINITION.id;
+  groupId = IMPERMAX_DEFINITION.groups.lend.id;
+  network = Network.POLYGON_MAINNET;
+  groupLabel = 'Lend';
 
-export const address = '0xbb92270716c8c424849f17ccc12f4f24ad4064d6';
-
-@Register.TokenPositionFetcher({ appId, groupId, network })
-export class PolygonImpermaxLendTokenFetcher implements PositionFetcher<AppTokenPosition> {
-  constructor(@Inject(ImpermaxLendTokenHelper) private readonly impermaxLendTokenHelper: ImpermaxLendTokenHelper) {}
-
-  async getPositions() {
-    return this.impermaxLendTokenHelper.getPositions({ address, network });
-  }
+  factoryAddress = '0xbb92270716c8c424849f17ccc12f4f24ad4064d6';
 }
