17de489480b5248f4b29fd2377b67b5cbc060587
Justin D'Errico <justin.derrico1991@gmail.com>
2022-09-15 10:00:30 -0400

    cleanup

--- src/utils/stack.ts
+++ src/utils/stack.ts
@@ -5,21 +5,14 @@ export function getStack() {
   const origPrepareStackTrace = Error.prepareStackTrace;
 
   // Override with function that just returns `stack`
-  Error.prepareStackTrace = function (_, stack) {
-    return stack;
-  };
+  Error.prepareStackTrace = (_, stack) => stack;
 
-  // Create a new `Error`, which automatically gets `stack`
-  const err = new Error();
-
-  // Evaluate `err.stack`, which calls our new `Error.prepareStackTrace`
-  const stack = err.stack as unknown as CallSite[];
+  // Evaluate `Error.stack`, which calls our new `Error.prepareStackTrace`
+  const stack = new Error().stack as unknown as CallSite[];
 
   // Restore original `Error.prepareStackTrace`
   Error.prepareStackTrace = origPrepareStackTrace;
 
   // Remove current stack call since redundant
-  stack.shift();
-
-  return stack;
+  return stack.slice(1);
 }
