6e447b5db494f1736379e9d3c225a112b4701eed
gaulois <gaulois.eth@gmail.com>
2022-09-16 09:29:44 +0200

    feat(synthetix): add support for loans

--- src/apps/synthetix/contracts/index.ts
+++ src/apps/synthetix/contracts/index.ts
@@ -5,6 +5,7 @@ import { ContractFactory } from '~contract/contracts';
 import { Network } from '~types/network.interface';
 
 import { SynthetixAddressResolver__factory } from './ethers';
+import { SynthetixLoan__factory } from './ethers';
 import { SynthetixNetworkToken__factory } from './ethers';
 import { SynthetixRewards__factory } from './ethers';
 import { SynthetixSummaryUtil__factory } from './ethers';
@@ -21,6 +22,9 @@ export class SynthetixContractFactory extends ContractFactory {
   synthetixAddressResolver({ address, network }: ContractOpts) {
     return SynthetixAddressResolver__factory.connect(address, this.appToolkit.getNetworkProvider(network));
   }
+  synthetixLoan({ address, network }: ContractOpts) {
+    return SynthetixLoan__factory.connect(address, this.appToolkit.getNetworkProvider(network));
+  }
   synthetixNetworkToken({ address, network }: ContractOpts) {
     return SynthetixNetworkToken__factory.connect(address, this.appToolkit.getNetworkProvider(network));
   }
@@ -33,6 +37,7 @@ export class SynthetixContractFactory extends ContractFactory {
 }
 
 export type { SynthetixAddressResolver } from './ethers';
+export type { SynthetixLoan } from './ethers';
 export type { SynthetixNetworkToken } from './ethers';
 export type { SynthetixRewards } from './ethers';
 export type { SynthetixSummaryUtil } from './ethers';
---
+++ src/apps/synthetix/helpers/synthetix.loan.contract-position-helper.ts
@@ -0,0 +1,55 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { IAppToolkit, APP_TOOLKIT } from '~app-toolkit/app-toolkit.interface';
+import { Network } from '~types/network.interface';
+import { borrowed, supplied } from '~position/position.utils';
+import { getTokenImg } from '~app-toolkit/helpers/presentation/image.present';
+import { ZERO_ADDRESS } from '~app-toolkit/constants/address';
+import { ContractPosition } from '~position/position.interface';
+import { SYNTHETIX_DEFINITION } from '../synthetix.definition';
+import { ContractType } from '~position/contract.interface';
+
+export type SynthetixLoanContractPositionHelperParams = {
+    loanContractAddress: string;
+    network: Network;
+};
+
+@Injectable()
+export class SynthetixLoanContractPositionHelper {
+    constructor(@Inject(APP_TOOLKIT) private readonly appToolkit: IAppToolkit) { }
+
+    async getPositions({ loanContractAddress, network }: SynthetixLoanContractPositionHelperParams) {
+        const baseTokens = await this.appToolkit.getBaseTokenPrices(network);
+
+        const ethToken = baseTokens.find(p => p.address === ZERO_ADDRESS);
+        const susdToken = baseTokens.find(p => p.symbol === 'sUSD')!;
+        const sethToken = baseTokens.find(p => p.symbol === 'sETH')!;
+        const tokens = [
+            supplied(ethToken),
+            borrowed(susdToken),
+            borrowed(sethToken)];
+
+        // Display Props
+        const label = SYNTHETIX_DEFINITION.groups.loan.label;
+        const images = [
+            getTokenImg(ethToken.address, network),
+            getTokenImg(susdToken.address, network),
+            getTokenImg(sethToken.address, network)];
+
+        const position: ContractPosition = {
+            type: ContractType.POSITION,
+            address: loanContractAddress,
+            appId: SYNTHETIX_DEFINITION.id,
+            groupId: SYNTHETIX_DEFINITION.groups.loan.id,
+            network,
+            tokens,
+            dataProps: {
+            },
+            displayProps: {
+                label,
+                images,
+            },
+        };
+
+        return [position];
+    }
+}
