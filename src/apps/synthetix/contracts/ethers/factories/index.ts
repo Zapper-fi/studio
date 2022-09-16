6db2334229424c8013522a6d244a51a6a66d9d42
gauloiseth <70271293+gdeschryver@users.noreply.github.com>
2022-09-16 19:08:23 +0200

    feat(synthetix): Add support for loans (#1428)

--- src/apps/synthetix/contracts/ethers/factories/index.ts
+++ src/apps/synthetix/contracts/ethers/factories/index.ts
@@ -2,6 +2,7 @@
 /* tslint:disable */
 /* eslint-disable */
 export { SynthetixAddressResolver__factory } from './SynthetixAddressResolver__factory';
+export { SynthetixLoan__factory } from './SynthetixLoan__factory';
 export { SynthetixNetworkToken__factory } from './SynthetixNetworkToken__factory';
 export { SynthetixRewards__factory } from './SynthetixRewards__factory';
 export { SynthetixSummaryUtil__factory } from './SynthetixSummaryUtil__factory';
