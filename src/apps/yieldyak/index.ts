d3afe46d0656cbf4c2ec5ce249627267eaf80299
0xdapper <0xdapp@protonmail.com>
2022-09-27 07:09:04 +0000

    chore: remove duplicate yieldyak app

--- src/apps/yieldyak/index.ts
+++
@@ -1,3 +0,0 @@
-export { YIELDYAK_DEFINITION, YieldyakAppDefinition } from './yieldyak.definition';
-export { YieldyakAppModule } from './yieldyak.module';
-export { YieldyakContractFactory } from './contracts';
