6853147cd76e8cdd31bed5ffb36baca45db134f3
immasandwich <karan@zapper.fi>
2022-09-28 08:56:20 -0400

    feat(maker): Convert to templates, fix key by including CDP ID (#1462)

--- src/apps/maker/ethereum/maker.vault.contract-position-fetcher.ts
+++ src/apps/maker/ethereum/maker.vault.contract-position-fetcher.ts
@@ -1,55 +1,65 @@
 import { Inject } from '@nestjs/common';
+import BigNumber from 'bignumber.js';
 import { ethers } from 'ethers';
-import _ from 'lodash';
-import { range } from 'lodash';
+import { compact, omit, range, sumBy } from 'lodash';
 
+import { drillBalance } from '~app-toolkit';
 import { APP_TOOLKIT, IAppToolkit } from '~app-toolkit/app-toolkit.interface';
-import { Register } from '~app-toolkit/decorators';
-import { getImagesFromToken, getTokenImg } from '~app-toolkit/helpers/presentation/image.present';
-import { UNISWAP_V2_DEFINITION } from '~apps/uniswap-v2';
+import { ZERO_ADDRESS } from '~app-toolkit/constants/address';
+import { PositionTemplate } from '~app-toolkit/decorators/position-template.decorator';
 import { ContractType } from '~position/contract.interface';
-import { PositionFetcher } from '~position/position-fetcher.interface';
-import { ContractPosition } from '~position/position.interface';
-import { borrowed, supplied } from '~position/position.utils';
-import { Network } from '~types/network.interface';
+import { ContractPositionBalance } from '~position/position-balance.interface';
+import { MetaType } from '~position/position.interface';
+import { isBorrowed, isSupplied } from '~position/position.utils';
+import { ContractPositionTemplatePositionFetcher } from '~position/template/contract-position.template.position-fetcher';
+import {
+  GetDataPropsParams,
+  GetDefinitionsParams,
+  GetDisplayPropsParams,
+  GetTokenBalancesParams,
+  GetTokenDefinitionsParams,
+  UnderlyingTokenDefinition,
+} from '~position/template/contract-position.template.types';
 
-import { MakerContractFactory } from '../contracts';
-import { MAKER_DEFINITION } from '../maker.definition';
+import { MakerContractFactory, MakerGemJoin } from '../contracts';
 
-const appId = MAKER_DEFINITION.id;
-const groupId = MAKER_DEFINITION.groups.vault.id;
-const network = Network.ETHEREUM_MAINNET;
-
-export type MakerVaultContractPositionDataProps = {
+export type MakerVaultDefinition = {
+  address: string;
   ilkName: string;
+  collateralTokenAddress: string;
 };
 
-@Register.ContractPositionFetcher({ appId, groupId, network })
-export class EthereumMakerVaultContractPositionFetcher implements PositionFetcher<ContractPosition> {
+export type MakerVaultDataProps = {
+  ilkName: string;
+  cRatio?: number;
+  cdpId?: number;
+};
+
+@PositionTemplate()
+export class EthereumMakerVaultContractPositionFetcher extends ContractPositionTemplatePositionFetcher<
+  MakerGemJoin,
+  MakerVaultDataProps,
+  MakerVaultDefinition
+> {
+  groupLabel = 'Vaults';
+
   constructor(
-    @Inject(APP_TOOLKIT)
-    private readonly appToolkit: IAppToolkit,
-    @Inject(MakerContractFactory)
-    private readonly makerContractFactory: MakerContractFactory,
-  ) {}
+    @Inject(APP_TOOLKIT) protected readonly appToolkit: IAppToolkit,
+    @Inject(MakerContractFactory) protected readonly contractFactory: MakerContractFactory,
+  ) {
+    super(appToolkit);
+  }
 
-  async getPositions() {
-    const multicall = this.appToolkit.getMulticall(network);
-    const baseTokens = await this.appToolkit.getBaseTokenPrices(network);
-    const appTokens = await this.appToolkit.getAppTokenPositions({
-      appId: UNISWAP_V2_DEFINITION.id,
-      groupIds: [UNISWAP_V2_DEFINITION.groups.pool.id],
-      network,
-    });
-    const allTokens = [...appTokens, ...baseTokens];
+  getContract(address: string) {
+    return this.contractFactory.makerGemJoin({ address, network: this.network });
+  }
 
-    // Gather the number of registered ilks
+  async getDefinitions({ multicall }: GetDefinitionsParams) {
     const ilkRegAddress = '0x5a464c28d19848f44199d003bef5ecc87d090f87';
-    const ilkRegContract = this.makerContractFactory.makerIlkRegistry({ address: ilkRegAddress, network });
+    const ilkRegContract = this.contractFactory.makerIlkRegistry({ address: ilkRegAddress, network: this.network });
     const numIlks = await ilkRegContract.count();
 
-    // Gather contract positions
-    const contractPositions = await Promise.all(
+    const definitions = await Promise.all(
       range(0, Number(numIlks)).map(async ilkIndex => {
         const ilk = await multicall.wrap(ilkRegContract).get(ilkIndex);
         const [gem, join] = await Promise.all([
@@ -59,43 +69,133 @@ export class EthereumMakerVaultContractPositionFetcher implements PositionFetche
 
         const ilkName = ethers.utils.parseBytes32String(ilk);
 
-        const contractAddress = join.toLowerCase();
+        const address = join.toLowerCase();
         const collateralTokenAddress = gem.toLowerCase();
-        const collateralToken = allTokens.find(v => v.address === collateralTokenAddress);
-        const debtToken = baseTokens.find(v => v.symbol === 'DAI');
-        if (!collateralToken || !debtToken) return null;
-
-        // Data Props
-        const tokens = [supplied(collateralToken), borrowed(debtToken)];
-
-        // Display Props
-        const label = `${ilkName} Vault`;
-        const collateralImages = getImagesFromToken(collateralToken);
-        const images = [...collateralImages, getTokenImg(debtToken.address, network)];
-
-        const position: ContractPosition<MakerVaultContractPositionDataProps> = {
-          type: ContractType.POSITION,
-          address: contractAddress,
-          appId: MAKER_DEFINITION.id,
-          groupId: MAKER_DEFINITION.groups.vault.id,
-          network,
-          tokens,
-
-          dataProps: {
-            ilkName,
-          },
-
-          displayProps: {
-            label,
-            images,
-          },
-        };
-
-        const key = this.appToolkit.getPositionKey(position, ['ilkName']);
-        return { key, ...position };
+        return { address, ilkName, collateralTokenAddress };
       }),
     );
 
-    return _.compact(contractPositions);
+    return definitions;
+  }
+
+  async getTokenDefinitions({
+    definition,
+  }: GetTokenDefinitionsParams<MakerGemJoin, MakerVaultDefinition>): Promise<UnderlyingTokenDefinition[] | null> {
+    return [
+      { metaType: MetaType.SUPPLIED, address: definition.collateralTokenAddress }, // Vault Collateral
+      { metaType: MetaType.BORROWED, address: '0x6b175474e89094c44da98b954eedeac495271d0f' }, // DAI
+    ];
+  }
+
+  async getDataProps({ definition }: GetDataPropsParams<MakerGemJoin, MakerVaultDataProps, MakerVaultDefinition>) {
+    return { ilkName: definition.ilkName };
+  }
+
+  async getLabel({ contractPosition }: GetDisplayPropsParams<MakerGemJoin, MakerVaultDataProps, MakerVaultDefinition>) {
+    return `${contractPosition.dataProps.ilkName} Vault`;
+  }
+
+  async getTokenBalancesPerPosition(_params: GetTokenBalancesParams<MakerGemJoin>): Promise<ethers.BigNumberish[]> {
+    throw new Error('Method not implemented.');
+  }
+
+  async getBalances(address: string): Promise<ContractPositionBalance<MakerVaultDataProps>[]> {
+    const multicall = this.appToolkit.getMulticall(this.network);
+    const positions = await this.appToolkit.getAppContractPositions<MakerVaultDataProps>({
+      appId: this.appId,
+      groupIds: [this.groupId],
+      network: this.network,
+    });
+
+    // Get the user's proxy address
+    const proxyRegAddress = '0x4678f0a6958e4d2bc4f1baf7bc52e8f3564f3fe4';
+    const proxyRegContract = this.contractFactory.makerProxyRegistry({
+      address: proxyRegAddress,
+      network: this.network,
+    });
+    const proxyAddress = await proxyRegContract.proxies(address);
+    if (proxyAddress === ZERO_ADDRESS) return [];
+
+    // Get the user's urn
+    const cdpManagerAddress = '0x5ef30b9986345249bc32d8928b7ee64de9435e39';
+    const cdpManagerContract = this.contractFactory.makerCdpManager({
+      address: cdpManagerAddress,
+      network: this.network,
+    });
+
+    // Retrieve all CDPs
+    const cdps: number[] = [];
+    let next = await cdpManagerContract.first(proxyAddress).then(v => Number(v));
+    while (next !== 0) {
+      cdps.push(next);
+      next = await cdpManagerContract.list(next).then(v => Number(v.next));
+    }
+
+    // Build balances across all CDPs
+    const allPositions = await Promise.all(
+      cdps.map(async cdp => {
+        const urn = await cdpManagerContract.urns(cdp);
+
+        // Gather balances
+        const vatAddress = '0x35d1b3f3d7966a1dfe207aa4514c12a259a0492b';
+        const vatContract = this.contractFactory.makerVat({ address: vatAddress, network: this.network });
+        const balances = await Promise.all(
+          positions.map(async position => {
+            const ilk = ethers.utils.formatBytes32String(position.dataProps.ilkName);
+            const { ink, art } = await multicall.wrap(vatContract).urns(ilk, urn.toLowerCase());
+
+            const collateralToken = position.tokens.find(isSupplied);
+            const debtToken = position.tokens.find(isBorrowed);
+            if (!collateralToken || !debtToken) return null;
+
+            // Data Props
+            const collateralRaw = new BigNumber(ink.toString())
+              .div(10 ** 18)
+              .times(10 ** collateralToken.decimals)
+              .toFixed(0);
+            const debtRaw = new BigNumber(art.toString())
+              .div(10 ** 18)
+              .times(10 ** debtToken.decimals)
+              .toFixed(0);
+            const collateral = drillBalance(collateralToken, collateralRaw);
+            const debt = drillBalance(debtToken, debtRaw, { isDebt: true });
+            const tokens = [collateral, debt];
+            const balanceUSD = sumBy(tokens, v => v.balanceUSD);
+            const cRatio = debt.balanceUSD === 0 ? 0 : (collateral.balanceUSD / Math.abs(debt.balanceUSD)) * 100;
+            const secondaryLabel = `C-Ratio: ${(cRatio * 100).toFixed(2)}%`;
+
+            const positionBalance: ContractPositionBalance<MakerVaultDataProps> = {
+              type: ContractType.POSITION,
+              key: position.key,
+              address: position.address,
+              appId: position.appId,
+              groupId: position.groupId,
+              network: position.network,
+              tokens,
+              balanceUSD,
+
+              dataProps: {
+                ilkName: position.dataProps.ilkName,
+                cdpId: cdp,
+                cRatio,
+              },
+
+              displayProps: {
+                label: position.displayProps.label,
+                secondaryLabel: secondaryLabel,
+                images: position.displayProps.images,
+              },
+            };
+
+            positionBalance.key = this.appToolkit.getPositionKey(omit(positionBalance, 'key'), ['cdpId']);
+            return positionBalance;
+          }),
+        );
+
+        return compact(balances);
+      }),
+    );
+
+    return compact(allPositions.flat());
   }
 }
