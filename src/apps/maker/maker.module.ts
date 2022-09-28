97d6768bdffd12503dfb71922eac69f03c9cd537
Karan Shahani <karan.shahani91@gmail.com>
2022-09-28 08:51:18 -0400

    feat(maker): Convert to templates, fix key by including CDP ID

--- src/apps/maker/maker.module.ts
+++ src/apps/maker/maker.module.ts
@@ -1,20 +1,16 @@
 import { Register } from '~app-toolkit/decorators';
 import { AbstractApp } from '~app/app.dynamic-module';
-import { SynthetixAppModule } from '~apps/synthetix';
 
 import { MakerContractFactory } from './contracts';
-import { EthereumMakerBalanceFetcher } from './ethereum/maker.balance-fetcher';
 import { EthereumMakerGovernanceContractPositionFetcher } from './ethereum/maker.governance.contract-position-fetcher';
 import { EthereumMakerVaultContractPositionFetcher } from './ethereum/maker.vault.contract-position-fetcher';
 import { MakerAppDefinition, MAKER_DEFINITION } from './maker.definition';
 
 @Register.AppModule({
   appId: MAKER_DEFINITION.id,
-  imports: [SynthetixAppModule],
   providers: [
     MakerAppDefinition,
     MakerContractFactory,
-    EthereumMakerBalanceFetcher,
     EthereumMakerGovernanceContractPositionFetcher,
     EthereumMakerVaultContractPositionFetcher,
   ],
