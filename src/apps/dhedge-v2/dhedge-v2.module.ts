511927e62e16d8e37a2e105455b368fc479a7fd4
gaulois <gaulois.eth@gmail.com>
2022-09-29 13:11:07 +0200

    feat(dhedge-v2): refactor and add logos

--- src/apps/dhedge-v2/dhedge-v2.module.ts
+++ src/apps/dhedge-v2/dhedge-v2.module.ts
@@ -7,16 +7,18 @@ import { OptimismDhedgeV2BalanceFetcher } from './optimism/dhedge-v2.balance-fet
 import { OptimismDhedgeV2PoolTokenFetcher } from './optimism/dhedge-v2.pool.token-fetcher';
 import { PolygonDhedgeV2BalanceFetcher } from './polygon/dhedge-v2.balance-fetcher';
 import { PolygonDhedgeV2PoolTokenFetcher } from './polygon/dhedge-v2.pool.token-fetcher';
+import { DhedgeV2PoolTokenFetcherHelper } from './helpers/dhedge-v2.pool.token-fetcher-helper'
 
 @Register.AppModule({
   appId: DHEDGE_V_2_DEFINITION.id,
   providers: [
     DhedgeV2AppDefinition,
     DhedgeV2ContractFactory,
+    DhedgeV2PoolTokenFetcherHelper,
     OptimismDhedgeV2BalanceFetcher,
     OptimismDhedgeV2PoolTokenFetcher,
     PolygonDhedgeV2BalanceFetcher,
     PolygonDhedgeV2PoolTokenFetcher,
   ],
 })
-export class DhedgeV2AppModule extends AbstractApp() {}
+export class DhedgeV2AppModule extends AbstractApp() { }
