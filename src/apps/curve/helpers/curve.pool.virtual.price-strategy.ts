b9db2bb841422a1aa1f1ad4092e53453cbdd9f7d
Karan Shahani <karan.shahani91@gmail.com>
2022-10-05 18:17:28 -0400

    feat(curve): Probably the simplest

--- src/apps/curve/helpers/curve.pool.virtual.price-strategy.ts
+++ src/apps/curve/helpers/curve.pool.virtual.price-strategy.ts
@@ -1,6 +1,5 @@
 import { BigNumberish } from 'ethers';
 
-import { isMulticallUnderlyingError } from '~multicall/multicall.ethers';
 import { IMulticallWrapper } from '~multicall/multicall.interface';
 
 import { CurvePoolTokenHelperParams } from './curve.pool.token-helper';
@@ -10,19 +9,11 @@ export type CurvePoolVirtualPriceStrategyParams<T> = {
 };
 
 export class CurvePoolVirtualPriceStrategy {
-  build<T>({
-    resolveVirtualPrice,
-  }: CurvePoolVirtualPriceStrategyParams<T>): CurvePoolTokenHelperParams<T>['resolvePoolTokenPrice'] {
-    return async ({ tokens, multicall, poolContract, supply, reserves }) => {
-      const virtualPriceRaw = await resolveVirtualPrice({ multicall, poolContract }).catch(err => {
-        if (isMulticallUnderlyingError(err)) return '0';
-        throw err;
-      });
-
-      const virtualPrice = Number(virtualPriceRaw) / 10 ** 18;
+  build<T>(_params: CurvePoolVirtualPriceStrategyParams<T>): CurvePoolTokenHelperParams<T>['resolvePoolTokenPrice'] {
+    return async ({ tokens, supply, reserves }) => {
       const reservesUSD = tokens.map((t, i) => reserves[i] * t.price);
       const liquidity = reservesUSD.reduce((total, r) => total + r, 0);
-      return virtualPrice > 0 ? virtualPrice * (liquidity / supply) : liquidity / supply;
+      return liquidity / supply;
     };
   }
 }
