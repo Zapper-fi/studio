0c6040425065f6f47be9b3f0c3f1b03e5974e7a7
tonzgao <tonyzgao@gmail.com>
2022-10-06 04:41:18 +0200

    feat(clever): Implement AladdinDAO CLever (#1476)

---
+++ src/apps/clever/ethereum/clever.furnace.contract-position-fetcher.ts
@@ -0,0 +1,48 @@
+import { Inject } from '@nestjs/common';
+
+import { APP_TOOLKIT, IAppToolkit } from '~app-toolkit/app-toolkit.interface';
+import { PositionTemplate } from '~app-toolkit/decorators/position-template.decorator';
+import { MetaType } from '~position/position.interface';
+import { ContractPositionTemplatePositionFetcher } from '~position/template/contract-position.template.position-fetcher';
+import { GetTokenBalancesParams } from '~position/template/contract-position.template.types';
+
+import { CleverContractFactory, CleverFurnace } from '../contracts';
+
+import { CVX, CLEVCVX } from './addresses';
+
+@PositionTemplate()
+export class EthereumCleverFurnaceContractPositionFetcher extends ContractPositionTemplatePositionFetcher<CleverFurnace> {
+  groupLabel = 'Furnace';
+
+  constructor(
+    @Inject(APP_TOOLKIT) protected readonly appToolkit: IAppToolkit,
+    @Inject(CleverContractFactory) protected readonly contractFactory: CleverContractFactory,
+  ) {
+    super(appToolkit);
+  }
+
+  getContract(address: string): CleverFurnace {
+    return this.contractFactory.cleverFurnace({ address, network: this.network });
+  }
+
+  async getDefinitions() {
+    return [{ address: '0xce4dcc5028588377e279255c0335effe2d7ab72a' }];
+  }
+
+  async getTokenDefinitions() {
+    return [
+      { metaType: MetaType.LOCKED, address: CLEVCVX },
+      { metaType: MetaType.CLAIMABLE, address: CVX },
+    ];
+  }
+
+  async getLabel() {
+    return `Clever Furnace`;
+  }
+
+  async getTokenBalancesPerPosition({ address, contract }: GetTokenBalancesParams<CleverFurnace>) {
+    const userInfo = await contract.userInfo(address);
+    console.warn('here', userInfo);
+    return [userInfo.unrealised, userInfo.realised];
+  }
+}
---
+++ src/apps/clever/ethereum/clever.lock.contract-position-fetcher.ts
@@ -0,0 +1,48 @@
+import { Inject } from '@nestjs/common';
+
+import { APP_TOOLKIT, IAppToolkit } from '~app-toolkit/app-toolkit.interface';
+import { PositionTemplate } from '~app-toolkit/decorators/position-template.decorator';
+import { MetaType } from '~position/position.interface';
+import { ContractPositionTemplatePositionFetcher } from '~position/template/contract-position.template.position-fetcher';
+import { GetTokenBalancesParams } from '~position/template/contract-position.template.types';
+
+import { CleverContractFactory, CleverLocker } from '../contracts';
+
+import { CVX, CLEVCVX } from './addresses';
+
+@PositionTemplate()
+export class EthereumCleverLockContractPositionFetcher extends ContractPositionTemplatePositionFetcher<CleverLocker> {
+  groupLabel = 'Lock';
+
+  constructor(
+    @Inject(APP_TOOLKIT) protected readonly appToolkit: IAppToolkit,
+    @Inject(CleverContractFactory) protected readonly contractFactory: CleverContractFactory,
+  ) {
+    super(appToolkit);
+  }
+
+  getContract(address: string): CleverLocker {
+    return this.contractFactory.cleverLocker({ address, network: this.network });
+  }
+
+  async getDefinitions() {
+    return [{ address: '0x96c68d861ada016ed98c30c810879f9df7c64154' }];
+  }
+
+  async getTokenDefinitions() {
+    return [
+      { metaType: MetaType.LOCKED, address: CVX },
+      { metaType: MetaType.CLAIMABLE, address: CVX },
+      { metaType: MetaType.BORROWED, address: CLEVCVX },
+    ];
+  }
+
+  async getLabel() {
+    return `CVX Lock`;
+  }
+
+  async getTokenBalancesPerPosition({ address, contract }: GetTokenBalancesParams<CleverLocker>) {
+    const userInfo = await contract.userInfo(address);
+    return [userInfo.totalLocked, userInfo.totalUnlocked, userInfo.totalDebt];
+  }
+}
