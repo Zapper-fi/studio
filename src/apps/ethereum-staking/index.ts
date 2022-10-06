a4ca1fd143c43e9a362a216b27c71da5e8d3781d
Karan Shahani <karan.shahani91@gmail.com>
2022-10-06 12:02:05 -0400

    fix(ethereum-staking): Fix typing error

--- src/apps/ethereum-staking/index.ts
+++
@@ -1,6 +0,0 @@
-export {
-  ETHEREUM_STAKING_DEFINITION as ETHEREUM_DEFINITION,
-  EthereumStakingAppDefinition as EthereumAppDefinition,
-} from './ethereum-staking.definition';
-export { EthereumStakingAppModule as EthereumAppModule } from './ethereum-staking.module';
-export { EthereumContractFactory } from './contracts';
