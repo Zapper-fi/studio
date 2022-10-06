bc0a7aa55aad3b7314e4af9600ac8bb0f5eefad4
Karan Shahani <karan.shahani91@gmail.com>
2022-10-05 22:29:35 -0400

    feat(curve): Done

--- src/apps/curve/curve.module.ts
+++ src/apps/curve/curve.module.ts
@@ -1,6 +1,12 @@
 import { Register } from '~app-toolkit/decorators';
 import { AbstractApp } from '~app/app.dynamic-module';
 
+import { ArbitrumCurveBalanceFetcher } from './arbitrum/curve.balance-fetcher';
+import { ArbitrumCurveGaugeContractPositionFetcher } from './arbitrum/curve.gauge.contract-position-fetcher';
+import { ArbitrumCurvePoolTokenFetcher } from './arbitrum/curve.pool.token-fetcher';
+import { AvalancheCurveBalanceFetcher } from './avalanche/curve.balance-fetcher';
+import { AvalancheCurveGaugeContractPositionFetcher } from './avalanche/curve.gauge.contract-position-fetcher';
+import { AvalancheCurvePoolTokenFetcher } from './avalanche/curve.pool.token-fetcher';
 import { CurveContractFactory } from './contracts';
 import { CurveAppDefinition, CURVE_DEFINITION } from './curve.definition';
 import { EthereumCurveBalanceFetcher } from './ethereum/curve.balance-fetcher';
@@ -8,6 +14,12 @@ import { EthereumCurveGaugeContractPositionFetcher } from './ethereum/curve.gaug
 import { EthereumCurvePoolTokenFetcher } from './ethereum/curve.pool.token-fetcher';
 import { EthereumCurveVestingEscrowContractPositionFetcher } from './ethereum/curve.vesting-escrow.contract-position-fetcher';
 import { EthereumCurveVotingEscrowContractPositionFetcher } from './ethereum/curve.voting-escrow.contract-position-fetcher';
+import { FantomCurveBalanceFetcher } from './fantom/curve.balance-fetcher';
+import { FantomCurveGaugeContractPositionFetcher } from './fantom/curve.gauge.contract-position-fetcher';
+import { FantomCurvePoolTokenFetcher } from './fantom/curve.pool.token-fetcher';
+import { GnosisCurveBalanceFetcher } from './gnosis/curve.balance-fetcher';
+import { GnosisCurveGaugeContractPositionFetcher } from './gnosis/curve.gauge.contract-position-fetcher';
+import { GnosisCurvePoolTokenFetcher } from './gnosis/curve.pool.token-fetcher';
 import { CurveApiClient } from './helpers/curve.api.client';
 import { CurveChildLiquidityGaugeRoiStrategy } from './helpers/curve.child-liquidity-gauge.roi-strategy';
 import { CurveGaugeDefaultContractPositionBalanceHelper } from './helpers/curve.gauge.default.contract-position-balance-helper';
@@ -25,6 +37,12 @@ import { CurveVestingEscrowContractPositionBalanceHelper } from './helpers/curve
 import { CurveVestingEscrowContractPositionHelper } from './helpers/curve.vesting-escrow.contract-position-helper';
 import { CurveVotingEscrowContractPositionBalanceHelper } from './helpers/curve.voting-escrow.contract-position-balance-helper';
 import { CurveVotingEscrowContractPositionHelper } from './helpers/curve.voting-escrow.contract-position-helper';
+import { OptimismCurveBalanceFetcher } from './optimism/curve.balance-fetcher';
+import { OptimismCurveGaugeContractPositionFetcher } from './optimism/curve.gauge.contract-position-fetcher';
+import { OptimismCurvePoolTokenFetcher } from './optimism/curve.pool.token-fetcher';
+import { PolygonCurveBalanceFetcher } from './polygon/curve.balance-fetcher';
+import { PolygonCurveGaugeContractPositionFetcher } from './polygon/curve.gauge.contract-position-fetcher';
+import { PolygonCurvePoolTokenFetcher } from './polygon/curve.pool.token-fetcher';
 
 @Register.AppModule({
   appId: CURVE_DEFINITION.id,
@@ -32,13 +50,13 @@ import { CurveVotingEscrowContractPositionHelper } from './helpers/curve.voting-
     CurveAppDefinition,
     CurveContractFactory,
     // Arbitrum
-    // ArbitrumCurveBalanceFetcher,
-    // ArbitrumCurvePoolTokenFetcher,
-    // ArbitrumCurveGaugeContractPositionFetcher,
-    // // Avalanche
-    // AvalancheCurveBalanceFetcher,
-    // AvalancheCurvePoolTokenFetcher,
-    // AvalancheCurveGaugeContractPositionFetcher,
+    ArbitrumCurveBalanceFetcher,
+    ArbitrumCurvePoolTokenFetcher,
+    ArbitrumCurveGaugeContractPositionFetcher,
+    // Avalanche
+    AvalancheCurveBalanceFetcher,
+    AvalancheCurvePoolTokenFetcher,
+    AvalancheCurveGaugeContractPositionFetcher,
     // Ethereum
     EthereumCurveBalanceFetcher,
     EthereumCurvePoolTokenFetcher,
@@ -46,21 +64,21 @@ import { CurveVotingEscrowContractPositionHelper } from './helpers/curve.voting-
     EthereumCurveVotingEscrowContractPositionFetcher,
     EthereumCurveVestingEscrowContractPositionFetcher,
     // Fantom
-    // FantomCurveBalanceFetcher,
-    // FantomCurvePoolTokenFetcher,
-    // FantomCurveGaugeContractPositionFetcher,
-    // // Gnosis
-    // GnosisCurveBalanceFetcher,
-    // GnosisCurvePoolTokenFetcher,
-    // GnosisCurveGaugeContractPositionFetcher,
-    // // Optimism
-    // OptimismCurveBalanceFetcher,
-    // OptimismCurvePoolTokenFetcher,
-    // OptimismCurveGaugeContractPositionFetcher,
-    // // Polygon
-    // PolygonCurveBalanceFetcher,
-    // PolygonCurvePoolTokenFetcher,
-    // PolygonCurveGaugeContractPositionFetcher,
+    FantomCurveBalanceFetcher,
+    FantomCurvePoolTokenFetcher,
+    FantomCurveGaugeContractPositionFetcher,
+    // Gnosis
+    GnosisCurveBalanceFetcher,
+    GnosisCurvePoolTokenFetcher,
+    GnosisCurveGaugeContractPositionFetcher,
+    // Optimism
+    OptimismCurveBalanceFetcher,
+    OptimismCurvePoolTokenFetcher,
+    OptimismCurveGaugeContractPositionFetcher,
+    // Polygon
+    PolygonCurveBalanceFetcher,
+    PolygonCurvePoolTokenFetcher,
+    PolygonCurveGaugeContractPositionFetcher,
     // API Helpers
     CurveApiClient,
     // Pool Token Helpers
--- src/apps/curve/helpers/curve.pool.virtual.price-strategy.ts
+++ src/apps/curve/helpers/curve.pool.virtual.price-strategy.ts
@@ -1,6 +1,9 @@
 import { BigNumberish } from 'ethers';
 
+import { isMulticallUnderlyingError } from '~multicall/multicall.ethers';
 import { IMulticallWrapper } from '~multicall/multicall.interface';
+
+import { CurvePoolType } from '../curve.types';
 
 import { CurvePoolTokenHelperParams } from './curve.pool.token-helper';
 
@@ -9,11 +12,33 @@ export type CurvePoolVirtualPriceStrategyParams<T> = {
 };
 
 export class CurvePoolVirtualPriceStrategy {
-  build<T>(_params: CurvePoolVirtualPriceStrategyParams<T>): CurvePoolTokenHelperParams<T>['resolvePoolTokenPrice'] {
-    return async ({ tokens, supply, reserves }) => {
-      const reservesUSD = tokens.map((t, i) => reserves[i] * t.price);
-      const liquidity = reservesUSD.reduce((total, r) => total + r, 0);
-      return liquidity / supply;
+  build<T>({
+    resolveVirtualPrice,
+  }: CurvePoolVirtualPriceStrategyParams<T>): CurvePoolTokenHelperParams<T>['resolvePoolTokenPrice'] {
+    return async ({ tokens, multicall, poolContract, supply, reserves, poolType }) => {
+      // Curve UI calculates pool token prices as follows:
+      // 1. Factory V2 pools are calculated as reserves in USD divided by supply times virtual price
+      // 2. Crypto pools are calculated as reserves in USD divided by supply
+      // 3. Stable pools are calculated simply as the price of their reference asset
+
+      // Given that pools like 4Pool with UST have a "failed" asset, the reference asset isn't enough alone
+      // So we'll calculate these pool token prices as the reserves in USD divided by the supply
+
+      if ([CurvePoolType.CRYPTO, CurvePoolType.FACTORY_CRYPTO].includes(poolType)) {
+        const virtualPriceRaw = await resolveVirtualPrice({ multicall, poolContract }).catch(err => {
+          if (isMulticallUnderlyingError(err)) return '0';
+          throw err;
+        });
+
+        const virtualPrice = Number(virtualPriceRaw) / 10 ** 18;
+        const reservesUSD = tokens.map((t, i) => reserves[i] * t.price);
+        const liquidity = reservesUSD.reduce((total, r) => total + r, 0);
+        return virtualPrice > 0 ? virtualPrice * (liquidity / supply) : liquidity / supply;
+      } else {
+        const reservesUSD = tokens.map((t, i) => reserves[i] * t.price);
+        const liquidity = reservesUSD.reduce((total, r) => total + r, 0);
+        return liquidity / supply;
+      }
     };
   }
 }
