f31e798b4d2a814a0d9feeae19bc005a19771e9f
corey-wombat <93235348+corey-wombat@users.noreply.github.com>
2022-10-10 08:16:28 +0800

    feat(wombat): Add Wombat Exchange integration (#1466)

---
+++ src/apps/wombat-exchange/index.ts
@@ -0,0 +1,3 @@
+export { WOMBAT_EXCHANGE_DEFINITION, WombatExchangeAppDefinition } from './wombat-exchange.definition';
+export { WombatExchangeAppModule } from './wombat-exchange.module';
+export { WombatExchangeContractFactory } from './contracts';
