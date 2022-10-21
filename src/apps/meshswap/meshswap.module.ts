f6a4c0265dd027a409c8ee099e06baa04849e9e8
William Poulin <will@zapper.fi>
2022-10-21 10:21:49 -0400

    chore(meshswap): Migrate Meshswap to template (#1605)
    * chore(meshswap): Migrate Meshswap to template
    
    * chore(meshswap): Fix duplicate label (primary and secondary)

--- src/apps/meshswap/meshswap.module.ts
+++ src/apps/meshswap/meshswap.module.ts
@@ -3,9 +3,7 @@ import { AbstractApp } from '~app/app.dynamic-module';
 import { UniswapV2AppModule } from '~apps/uniswap-v2';
 
 import { MeshswapContractFactory } from './contracts';
-import { MeshswapSupplyBalanceHelper } from './helpers/meshpool.supply.balance-helper';
 import { MeshswapAppDefinition, MESHSWAP_DEFINITION } from './meshswap.definition';
-import { PolygonMeshswapBalanceFetcher } from './polygon/meshswap.balance-fetcher';
 import { PolygonMeshswapPoolTokenFetcher } from './polygon/meshswap.pool.token-fetcher';
 import { PolygonMeshswapSupplyTokenFetcher } from './polygon/meshswap.supply.token-fetcher';
 
@@ -15,12 +13,9 @@ import { PolygonMeshswapSupplyTokenFetcher } from './polygon/meshswap.supply.tok
   providers: [
     MeshswapAppDefinition,
     MeshswapContractFactory,
-    // helpers
-    MeshswapSupplyBalanceHelper,
     // Polygon
     PolygonMeshswapPoolTokenFetcher,
     PolygonMeshswapSupplyTokenFetcher,
-    PolygonMeshswapBalanceFetcher,
   ],
   exports: [],
 })
