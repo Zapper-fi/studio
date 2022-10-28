6ad3d37ad342527f6c7020cd260124f5ec0cc69a
squirt11e <coloradobites@gmail.com>
2022-10-28 13:22:59 -0600

    Add pouch balances

--- src/apps/wolf-game/wolf-game.module.ts
+++ src/apps/wolf-game/wolf-game.module.ts
@@ -2,10 +2,11 @@ import { Register } from '~app-toolkit/decorators';
 import { AbstractApp } from '~app/app.dynamic-module';
 
 import { WolfGameContractFactory } from './contracts';
+import { EthereumWolfGameWoolPouchContractPositionFetcher } from './ethereum/wolf-game.wool-pouch.contract-position-fetcher';
 import WOLF_GAME_DEFINITION, { WolfGameAppDefinition } from './wolf-game.definition';
 
 @Register.AppModule({
   appId: WOLF_GAME_DEFINITION.id,
-  providers: [WolfGameAppDefinition, WolfGameContractFactory],
+  providers: [WolfGameAppDefinition, WolfGameContractFactory, EthereumWolfGameWoolPouchContractPositionFetcher],
 })
 export class WolfGameAppModule extends AbstractApp() {}
