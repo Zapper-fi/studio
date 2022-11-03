29ba8838a75a7c406f5e06912a37a98343268d14
gaulois <gaulois.eth@gmail.com>
2022-11-03 17:10:30 +0100

    feat(kwenta): cross margin

--- src/apps/kwenta/kwenta.module.ts
+++ src/apps/kwenta/kwenta.module.ts
@@ -3,10 +3,11 @@ import { AbstractApp } from '~app/app.dynamic-module';
 
 import { KwentaContractFactory } from './contracts';
 import { KwentaAppDefinition, KWENTA_DEFINITION } from './kwenta.definition';
-import { OptimismKwentaPerpContractPositionFetcher } from './optimism/kwenta.perp.contract-position-fetcher';
+import { OptimismKwentaIsolatedContractPositionFetcher } from './optimism/kwenta.isolated.contract-position-fetcher';
+import { OptimismKwentaCrossContractPositionFetcher } from './optimism/kwenta.cross.contract-position-fetcher';
 
 @Register.AppModule({
   appId: KWENTA_DEFINITION.id,
-  providers: [KwentaAppDefinition, KwentaContractFactory, OptimismKwentaPerpContractPositionFetcher],
+  providers: [KwentaAppDefinition, KwentaContractFactory, OptimismKwentaIsolatedContractPositionFetcher, OptimismKwentaCrossContractPositionFetcher],
 })
-export class KwentaAppModule extends AbstractApp() {}
+export class KwentaAppModule extends AbstractApp() { }
--- src/apps/kwenta/optimism/kwenta.perp.contract-position-fetcher.ts
+++ src/apps/kwenta/optimism/kwenta.perp.contract-position-fetcher.ts
@@ -1,15 +1,7 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject } from '@nestjs/common';
 import { parseBytes32String } from 'ethers/lib/utils';
 import { gql } from 'graphql-request';
-import { sumBy } from 'lodash';
-
-import { drillBalance } from '~app-toolkit';
 import { APP_TOOLKIT, IAppToolkit } from '~app-toolkit/app-toolkit.interface';
-import {
-  buildDollarDisplayItem,
-  buildNumberDisplayItem,
-  buildStringDisplayItem,
-} from '~app-toolkit/helpers/presentation/display-item.present';
 import { getAppAssetImage } from '~app-toolkit/helpers/presentation/image.present';
 import { SYNTHETIX_DEFINITION } from '~apps/synthetix/synthetix.definition';
 import { MetaType } from '~position/position.interface';
@@ -38,19 +30,15 @@ const getContractsQuery = gql`
   }
 `;
 
-@Injectable()
-export class OptimismKwentaPerpContractPositionFetcher extends ContractPositionTemplatePositionFetcher<KwentaFutures> {
-  appId = KWENTA_DEFINITION.id;
-  groupId = KWENTA_DEFINITION.groups.perp.id;
-  network = Network.OPTIMISM_MAINNET;
-  groupLabel = 'Perp';
-
+export abstract class OptimismKwentaPerpContractPositionFetcher extends ContractPositionTemplatePositionFetcher<KwentaFutures> {
   constructor(
     @Inject(APP_TOOLKIT) protected readonly appToolkit: IAppToolkit,
     @Inject(KwentaContractFactory) protected readonly contractFactory: KwentaContractFactory,
   ) {
     super(appToolkit);
   }
+
+  abstract getAccountAddress(address: string): Promise<string> | string;
 
   getContract(address: string): KwentaFutures {
     return this.contractFactory.kwentaFutures({ address, network: this.network });
@@ -84,14 +72,8 @@ export class OptimismKwentaPerpContractPositionFetcher extends ContractPositionT
 
   async getLabel({ contractPosition }: GetDisplayPropsParams<KwentaFutures>): Promise<string> {
     const baseAsset = await this.getBaseAsset({ contractPosition });
-    return `${baseAsset}-PERP`;
-  }
-
-  async getStatsItems({ contractPosition }) {
-    const multicall = this.appToolkit.getMulticall(this.network);
-    const contract = multicall.wrap(this.getContract(contractPosition.address));
-    const currentFundingRateRaw = await contract.currentFundingRate();
-    return [{ label: 'Funding Rate', value: buildNumberDisplayItem(Number(currentFundingRateRaw) / 10 ** 18) }];
+    const marginType = this.groupId;
+    return `${baseAsset}-PERP (${marginType})`;
   }
 
   async getImages({ contractPosition }: GetDisplayPropsParams<KwentaFutures>) {
@@ -100,52 +82,12 @@ export class OptimismKwentaPerpContractPositionFetcher extends ContractPositionT
   }
 
   async getTokenBalancesPerPosition({ address, contract }: GetTokenBalancesParams<KwentaFutures>) {
-    const remainingMargin = await contract.remainingMargin(address);
+    const accountAddress = await this.getAccountAddress(address);
+    if (accountAddress === "") {
+      return [];
+    }
+    const remainingMargin = await contract.remainingMargin(accountAddress);
     return [remainingMargin.marginRemaining];
   }
 
-  async getBalances(address: string) {
-    const multicall = this.appToolkit.getMulticall(this.network);
-    const contractPositions = await this.appToolkit.getAppContractPositions({
-      appId: this.appId,
-      network: this.network,
-      groupIds: [this.groupId],
-    });
-
-    const balances = await Promise.all(
-      contractPositions.map(async contractPosition => {
-        const contract = multicall.wrap(this.getContract(contractPosition.address));
-        const balancesRaw = await this.getTokenBalancesPerPosition({ address, contract, contractPosition, multicall });
-        const allTokens = contractPosition.tokens.map((cp, idx) =>
-          drillBalance(cp, balancesRaw[idx]?.toString() ?? '0', { isDebt: cp.metaType === MetaType.BORROWED }),
-        );
-
-        const tokens = allTokens.filter(v => Math.abs(v.balanceUSD) > 0.01);
-        const balanceUSD = sumBy(tokens, t => t.balanceUSD);
-        const enrichedDisplayItems = await this.getEnrichedDisplayItems({ address, contract });
-        return { ...contractPosition, tokens, balanceUSD, displayItems: enrichedDisplayItems };
-      }),
-    );
-
-    return balances;
-  }
-
-  private async getEnrichedDisplayItems({ address, contract }) {
-    const position = await contract.positions(address);
-    const positionSizeRaw = Number(position.size);
-    if (positionSizeRaw === 0) {
-      return [];
-    }
-    const liquidationPriceRaw = await contract.liquidationPrice(address);
-    const pnlRaw = await contract.profitLoss(address);
-    const notionalValueRaw = await contract.notionalValue(address);
-    return [
-      { label: 'Side', ...buildStringDisplayItem(positionSizeRaw > 0 ? 'LONG' : 'SHORT') },
-      { label: 'Size', ...buildNumberDisplayItem(positionSizeRaw / 10 ** 18) },
-      { label: 'Notional Value', ...buildDollarDisplayItem(Number(notionalValueRaw.value) / 10 ** 18) },
-      { label: 'PnL', ...buildDollarDisplayItem(Number(pnlRaw.pnl) / 10 ** 18) },
-      { label: 'Last Price', ...buildDollarDisplayItem(Number(position.lastPrice) / 10 ** 18) },
-      { label: 'Liquidation Price', ...buildDollarDisplayItem(Number(liquidationPriceRaw.price) / 10 ** 18) },
-    ];
-  }
 }
