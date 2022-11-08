e8a15b2ed96defc0542fc198f189f92a40c2eba7
Karan Shahani <karan.shahani91@gmail.com>
2022-11-08 12:31:36 -0500

    Another

--- src/apps/qi-dao/qi-dao.module.ts
+++ src/apps/qi-dao/qi-dao.module.ts
@@ -1,12 +1,12 @@
 import { Register } from '~app-toolkit/decorators';
 import { AbstractApp } from '~app/app.dynamic-module';
 
-import { ArbitrumQiDaoVaultPositionFetcher } from './arbitrum/qi-dao.vault.position-fetcher';
+import { ArbitrumQiDaoVaultPositionFetcher } from './arbitrum/qi-dao.vault.contract-position-fetcher';
 import { QiDaoContractFactory } from './contracts';
 import { FantomQiDaoFarmContractPositionFetcher } from './fantom/qi-dao.farm.contract-position-fetcher';
-import { FantomQiDaoVaultPositionFetcher } from './fantom/qi-dao.vault.position-fetcher';
-import { GnosisQiDaoVaultPositionFetcher } from './gnosis/qi-dao.vault.position-fetcher';
-import { OptimismQiDaoVaultPositionFetcher } from './optimism/qi-dao.vault.position-fetcher';
+import { FantomQiDaoVaultPositionFetcher } from './fantom/qi-dao.vault.contract-position-fetcher';
+import { GnosisQiDaoVaultPositionFetcher } from './gnosis/qi-dao.vault.contract-position-fetcher';
+import { OptimismQiDaoVaultPositionFetcher } from './optimism/qi-dao.vault.contract-position-fetcher';
 import { PolygonQiDaoEscrowedQiContractPositionFetcher } from './polygon/qi-dao.escrowed-qi.contract-position-fetcher';
 import { PolygonQiDaoFarmContractPositionFetcher } from './polygon/qi-dao.farm.contract-position-fetcher';
 import { PolygonQiDaoVaultPositionFetcher } from './polygon/qi-dao.vault.contract-position-fetcher';
