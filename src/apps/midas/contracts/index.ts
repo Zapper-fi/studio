8136b4b3b42dd7f482eb30e0155d31acf360c927
smartdevcom <smart.dev.cop@gmail.com>
2022-11-10 11:25:47 +1000

    generate a contract factory

--- src/apps/midas/contracts/index.ts
+++ src/apps/midas/contracts/index.ts
@@ -3,6 +3,9 @@ import { Injectable, Inject } from '@nestjs/common';
 import { IAppToolkit, APP_TOOLKIT } from '~app-toolkit/app-toolkit.interface';
 import { ContractFactory } from '~contract/contracts';
 import { Network } from '~types/network.interface';
+
+import { MidasPool__factory } from './ethers';
+
 // eslint-disable-next-line
 type ContractOpts = { address: string; network: Network };
 
@@ -11,4 +14,10 @@ export class MidasContractFactory extends ContractFactory {
   constructor(@Inject(APP_TOOLKIT) protected readonly appToolkit: IAppToolkit) {
     super((network: Network) => appToolkit.getNetworkProvider(network));
   }
+
+  midasPool({ address, network }: ContractOpts) {
+    return MidasPool__factory.connect(address, this.appToolkit.getNetworkProvider(network));
+  }
 }
+
+export type { MidasPool } from './ethers';
