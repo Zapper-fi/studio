94699266ba590e884c14e9ef5185d3600bdc8bd1
Karan Shahani <karan.shahani91@gmail.com>
2022-11-16 12:02:29 -0500

    feat(vendor-finance): To templates

--- src/apps/vendor-finance/index.ts
+++
@@ -1,3 +0,0 @@
-export { VENDOR_FINANCE_DEFINITION, VendorFinanceAppDefinition } from './vendor-finance.definition';
-export { VendorFinanceAppModule } from './vendor-finance.module';
-export { VendorFinanceContractFactory } from './contracts';
