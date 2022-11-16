011ed75703227ef27b481e9b7885edf245a924e2
William Poulin <will@zapper.fi>
2022-11-16 17:42:08 -0500

    fix(chicken-bond): Fix bLUSD price + filter out claimed and cancelled positions (#1742)

--- src/apps/chicken-bond/contracts/ethers/factories/index.ts
+++ src/apps/chicken-bond/contracts/ethers/factories/index.ts
@@ -4,3 +4,4 @@
 export { ChickenBondBlusd__factory } from './ChickenBondBlusd__factory';
 export { ChickenBondBondNft__factory } from './ChickenBondBondNft__factory';
 export { ChickenBondManager__factory } from './ChickenBondManager__factory';
+export { CurvePool__factory } from './CurvePool__factory';
