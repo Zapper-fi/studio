5bebca393918434e092401196934c2466f959234
Maxime Bedard <maxim3.bedard@gmail.com>
2022-11-16 13:31:56 -0500

    fix(schedule): Use only 1 metadata attribute for @Schedule (#1724)

--- src/scheduler/scheduler.decorator.ts
+++ src/scheduler/scheduler.decorator.ts
@@ -1,14 +1,11 @@
 import { applyDecorators, SetMetadata } from '@nestjs/common';
 
-import { SCHEDULER_INTERVAL, SCHEDULER_IS_SCHEDULED_OPERATION } from './scheduler.constants';
+export const SCHEDULER_OPTIONS = 'SCHEDULER_OPTIONS';
 
 export type ScheduleOptions = {
   every: number;
 };
 
 export const Schedule = (options: ScheduleOptions) => {
-  return applyDecorators(
-    SetMetadata(SCHEDULER_IS_SCHEDULED_OPERATION, true),
-    SetMetadata(SCHEDULER_INTERVAL, options.every),
-  );
+  return applyDecorators(SetMetadata(SCHEDULER_OPTIONS, options));
 };
