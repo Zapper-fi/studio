6335c2bf95a128c79216d7c3d5390b74ab7c45dc
sunnyRK <radadiyasunny970@gmail.com>
2022-11-17 13:10:18 +0530

    chore: Revert wrong commit  "build a setup for lemmafinace and added setip for usdl token"
    This reverts commit 6d69c4c39520e26612abda6ef3eebfee762f22b9.

--- src/apps/lemmafinance/index.ts
+++
@@ -1,3 +0,0 @@
-export { LEMMAFINANCE_DEFINITION, LemmafinanceAppDefinition } from './lemmafinance.definition';
-export { LemmafinanceAppModule } from './lemmafinance.module';
-export { LemmafinanceContractFactory } from './contracts';
