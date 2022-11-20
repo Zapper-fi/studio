c6837f43d0e8c0043a99b21cf0e8188bdd298e2b
gaulois <gaulois.eth@gmail.com>
2022-11-20 18:29:47 +0100

    feat(kwenta): WETH/KWENTA LP staking

--- src/apps/kwenta/kwenta.module.ts
+++ src/apps/kwenta/kwenta.module.ts
@@ -6,6 +6,7 @@ import { KwentaAppDefinition, KWENTA_DEFINITION } from './kwenta.definition';
 import { OptimismKwentaCrossContractPositionFetcher } from './optimism/kwenta.cross.contract-position-fetcher';
 import { OptimismKwentaIsolatedContractPositionFetcher } from './optimism/kwenta.isolated.contract-position-fetcher';
 import { OptimismKwentaStakingContractPositionFetcher } from './optimism/kwenta.staking.contract-position-fetcher';
+import { OptimismKwentaLpStakingContractPositionFetcher } from './optimism/kwenta.lp-staking.contract-position-fetcher';
 
 @Register.AppModule({
   appId: KWENTA_DEFINITION.id,
@@ -15,6 +16,7 @@ import { OptimismKwentaStakingContractPositionFetcher } from './optimism/kwenta.
     OptimismKwentaIsolatedContractPositionFetcher,
     OptimismKwentaCrossContractPositionFetcher,
     OptimismKwentaStakingContractPositionFetcher,
+    OptimismKwentaLpStakingContractPositionFetcher
   ],
 })
-export class KwentaAppModule extends AbstractApp() {}
+export class KwentaAppModule extends AbstractApp() { }
