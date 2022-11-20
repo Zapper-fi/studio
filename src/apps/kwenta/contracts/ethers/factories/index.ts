e25a4d5e71b2971ef1fdb80bab1c89c7c19ecfdf
gauloiseth <70271293+gdeschryver@users.noreply.github.com>
2022-11-20 23:33:33 +0100

    feat(kwenta): WETH/KWENTA LP staking (#1761)
    Co-authored-by: gaulois <gaulois.eth@gmail.com>

--- src/apps/kwenta/contracts/ethers/factories/index.ts
+++ src/apps/kwenta/contracts/ethers/factories/index.ts
@@ -2,4 +2,5 @@
 /* tslint:disable */
 /* eslint-disable */
 export { KwentaFutures__factory } from './KwentaFutures__factory';
+export { KwentaLpStaking__factory } from './KwentaLpStaking__factory';
 export { KwentaStaking__factory } from './KwentaStaking__factory';
