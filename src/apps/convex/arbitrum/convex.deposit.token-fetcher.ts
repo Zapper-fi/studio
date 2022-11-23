103bc25dc840e79b4f0898e5ec9e0ad6b9e5be47
immasandwich <karan@zapper.fi>
2022-11-23 16:38:26 -0500

    feat(convex): Support Arbitrum (#1775)

--- src/apps/convex/arbitrum/convex.deposit.token-fetcher.ts
+++ src/apps/convex/arbitrum/convex.deposit.token-fetcher.ts
@@ -1,9 +1,9 @@
 import { PositionTemplate } from '~app-toolkit/decorators/position-template.decorator';
 
-import { ConvexDepositTokenFetcher } from '../common/convex.deposit.token-fetcher';
+import { ConvexDepositSidechainTokenFetcher } from '../common/convex.deposit-sidechain.token-fetcher';
 
 @PositionTemplate()
-export class EthereumConvexDepositTokenFetcher extends ConvexDepositTokenFetcher {
+export class ArbitrumConvexDepositTokenFetcher extends ConvexDepositSidechainTokenFetcher {
   groupLabel = 'Liqudity Pool Staking';
   boosterAddress = '0xf403c135812408bfbe8713b5a23a04b3d48aae31';
 }
