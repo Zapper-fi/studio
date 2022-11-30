1ca9c03fbca13a37806380aefbd21ac6643fa810
Karan Shahani <karan.shahani91@gmail.com>
2022-11-30 11:06:40 -0500

    feat(curve): Remove helpers from index

--- src/apps/curve/index.ts
+++ src/apps/curve/index.ts
@@ -3,9 +3,5 @@ export { CurveAppModule } from './curve.module';
 export { CurveContractFactory } from './contracts';
 
 /* Helpers */
-export { CurvePoolTokenHelper } from './helpers/curve.pool.token-helper';
-export { CurvePoolVirtualPriceStrategy } from './helpers/curve.pool.virtual.price-strategy';
-export { CurvePoolOnChainCoinStrategy } from './helpers/curve.pool.on-chain.coin-strategy';
-export { CurvePoolOnChainReserveStrategy } from './helpers/curve.pool.on-chain.reserve-strategy';
 export { CurveVotingEscrowContractPositionBalanceHelper } from './helpers/curve.voting-escrow.contract-position-balance-helper';
 export { CurveVotingEscrowContractPositionHelper } from './helpers/curve.voting-escrow.contract-position-helper';
