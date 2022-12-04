637fa7f0753ca3dbf9ca7f3e6fd8440173cf150a
immasandwich <karan@zapper.fi>
2022-12-04 18:28:05 -0500

    feat(klima, jpegd): To templates (#1857)

--- src/apps/klima/klima.module.ts
+++ src/apps/klima/klima.module.ts
@@ -1,22 +1,18 @@
 import { Register } from '~app-toolkit/decorators';
 import { AbstractApp } from '~app/app.dynamic-module';
-import { OlympusAppModule } from '~apps/olympus';
 
 import { KlimaContractFactory } from './contracts';
 import { KlimaAppDefinition, KLIMA_DEFINITION } from './klima.definition';
-import { PolygonKlimaBalanceFetcher } from './polygon/klima.balance-fetcher';
-import { PolygonKlimaBondContractPositionFetcher } from './polygon/klima.bond.contract-position';
+import { PolygonKlimaBondContractPositionFetcher } from './polygon/klima.bond.contract-position-fetcher';
 import { PolygonKlimaSTokenFetcher } from './polygon/klima.s-klima.token-fetcher';
 import { PolygonKlimaWsTokenFetcher } from './polygon/klima.ws-klima.token-fetcher';
 
 @Register.AppModule({
   appId: KLIMA_DEFINITION.id,
-  imports: [OlympusAppModule],
   providers: [
     KlimaAppDefinition,
     KlimaContractFactory,
     // Polygon
-    PolygonKlimaBalanceFetcher,
     PolygonKlimaBondContractPositionFetcher,
     PolygonKlimaSTokenFetcher,
     PolygonKlimaWsTokenFetcher,
---
+++ src/apps/klima/polygon/klima.bond.contract-position-fetcher.ts
@@ -0,0 +1,80 @@
+import { Inject } from '@nestjs/common';
+import { BigNumberish } from 'ethers';
+
+import { IAppToolkit, APP_TOOLKIT } from '~app-toolkit/app-toolkit.interface';
+import { PositionTemplate } from '~app-toolkit/decorators/position-template.decorator';
+import {
+  OlympusBondContractPositionFetcher,
+  ResolveClaimableBalanceParams,
+  ResolveVestingBalanceParams,
+} from '~apps/olympus/common/olympus.bond.contract-position-fetcher';
+
+import { KlimaBondDepository, KlimaContractFactory } from '../contracts';
+
+@PositionTemplate()
+export class PolygonKlimaBondContractPositionFetcher extends OlympusBondContractPositionFetcher<KlimaBondDepository> {
+  groupLabel = 'Bonds';
+  bondDefinitions = [
+    {
+      address: '0x7de627c56d26529145a5f9d85948ecbeaf9a4b34',
+      bondedTokenAddress: '0x2f800db0fdb5223b3c3f354886d907a671414a7f',
+      mintedTokenAddress: '0x4e78011ce80ee02d2c3e649fb657e45898257815',
+    },
+    {
+      address: '0x27217c3f5bec4c12fa506a101bc4bd15417aeaa8',
+      bondedTokenAddress: '0xfc98e825a2264d890f9a1e68ed50e1526abccacd',
+      mintedTokenAddress: '0x4e78011ce80ee02d2c3e649fb657e45898257815',
+    },
+    {
+      address: '0x1e0dd93c81ac7af2974cdb326c85b87dd879389b',
+      bondedTokenAddress: '0x9803c7ae526049210a1725f7487af26fe2c24614',
+      mintedTokenAddress: '0x4e78011ce80ee02d2c3e649fb657e45898257815',
+    },
+    {
+      address: '0xbf2a35efcd85e790f02458db4a3e2f29818521c5',
+      bondedTokenAddress: '0x1e67124681b402064cd0abe8ed1b5c79d2e02f64',
+      mintedTokenAddress: '0x4e78011ce80ee02d2c3e649fb657e45898257815',
+    },
+    {
+      address: '0xb5af101742ecae095944f60c384d09453006bfde',
+      bondedTokenAddress: '0x5786b267d35f9d011c4750e0b0ba584e1fdbead1',
+      mintedTokenAddress: '0x4e78011ce80ee02d2c3e649fb657e45898257815',
+    },
+    {
+      address: '0xf9c3fc299de5f86d9cd6a724e6b44933720f5e6d',
+      bondedTokenAddress: '0x64a3b8ca5a7e406a78e660ae10c7563d9153a739',
+      mintedTokenAddress: '0x4e78011ce80ee02d2c3e649fb657e45898257815',
+    },
+  ];
+
+  constructor(
+    @Inject(APP_TOOLKIT) protected readonly appToolkit: IAppToolkit,
+    @Inject(KlimaContractFactory) protected readonly contractFactory: KlimaContractFactory,
+  ) {
+    super(appToolkit);
+  }
+
+  getContract(address: string): KlimaBondDepository {
+    return this.contractFactory.klimaBondDepository({ address, network: this.network });
+  }
+
+  async resolveVestingBalance({
+    address,
+    contract,
+  }: ResolveVestingBalanceParams<KlimaBondDepository>): Promise<BigNumberish> {
+    const [bondInfo, pendingPayout] = await Promise.all([
+      contract.bondInfo(address),
+      contract.pendingPayoutFor(address),
+    ]);
+
+    const vestingBalanceRaw = bondInfo.payout.gt(pendingPayout) ? bondInfo.payout.sub(pendingPayout) : 0;
+    return vestingBalanceRaw;
+  }
+
+  resolveClaimableBalance({
+    address,
+    contract,
+  }: ResolveClaimableBalanceParams<KlimaBondDepository>): Promise<BigNumberish> {
+    return contract.pendingPayoutFor(address);
+  }
+}
