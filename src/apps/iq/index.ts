e8bdd5ca3201649b0875ecf7cb73441255d2e4c5
Adebayo Olamilekan <75235148+OleanjiKingCode@users.noreply.github.com>
2022-12-06 23:38:46 +0100

    feat(Iq): add Iq app integration  (#1847)
    feat(Iq): add Iq app integration  (#1847)
    
    Co-authored-by: William Poulin <will@zapper.fi>

---
+++ src/apps/iq/index.ts
@@ -0,0 +1,3 @@
+export { IQ_DEFINITION, IqAppDefinition } from './iq.definition';
+export { IqAppModule } from './iq.module';
+export { IqContractFactory } from './contracts';
---
+++ src/apps/iq/iq.module.ts
@@ -0,0 +1,13 @@
+import { Register } from '~app-toolkit/decorators';
+import { AbstractApp } from '~app/app.dynamic-module';
+
+import { IqContractFactory } from './contracts';
+
+import { EthereumIqHiiqContractPositionFetcher } from './ethereum/iq.hiiq.contract-position-fetcher';
+import { IqAppDefinition, IQ_DEFINITION } from './iq.definition';
+
+@Register.AppModule({
+  appId: IQ_DEFINITION.id,
+  providers: [EthereumIqHiiqContractPositionFetcher, IqAppDefinition, IqContractFactory],
+})
+export class IqAppModule extends AbstractApp() {}
