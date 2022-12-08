39b8e82244c95692570d58146af9d878b64177a8
Karan Shahani <karan.shahani91@gmail.com>
2022-12-08 15:02:50 -0500

    fix(idle): Simplify vault token fetcher

--- src/apps/idle/ethereum/idle.vault.token-fetcher.ts
+++ src/apps/idle/ethereum/idle.vault.token-fetcher.ts
@@ -4,28 +4,15 @@ import { APP_TOOLKIT, IAppToolkit } from '~app-toolkit/app-toolkit.interface';
 import { PositionTemplate } from '~app-toolkit/decorators/position-template.decorator';
 import { AppTokenTemplatePositionFetcher } from '~position/template/app-token.template.position-fetcher';
 import {
-  DefaultAppTokenDataProps,
-  GetAddressesParams,
   GetDataPropsParams,
-  GetDefinitionsParams,
-  GetPriceParams,
   GetPricePerShareParams,
   GetUnderlyingTokensParams,
 } from '~position/template/app-token.template.types';
 
 import { IdleContractFactory, IdleToken } from '../contracts';
 
-export type IdleVaultTokenDefinition = {
-  address: string;
-  underlyingTokenAddress: string;
-};
-
 @PositionTemplate()
-export class EthereumIdleVaultTokenFetcher extends AppTokenTemplatePositionFetcher<
-  IdleToken,
-  DefaultAppTokenDataProps,
-  IdleVaultTokenDefinition
-> {
+export class EthereumIdleVaultTokenFetcher extends AppTokenTemplatePositionFetcher<IdleToken> {
   groupLabel = 'Vault';
   isExcludedFromBalances = true;
   isExcludedFromExplore = true;
@@ -42,39 +29,17 @@ export class EthereumIdleVaultTokenFetcher extends AppTokenTemplatePositionFetch
     return this.contractFactory.idleToken({ network: this.network, address });
   }
 
-  async getDefinitions({ multicall }: GetDefinitionsParams): Promise<IdleVaultTokenDefinition[]> {
+  async getAddresses() {
     const controller = this.contractFactory.idleController({
       address: '0x275da8e61ea8e02d51edd8d0dc5c0e62b4cdb0be',
       network: this.network,
     });
-    const marketTokenAddresses = await multicall.wrap(controller).getAllMarkets();
 
-    const definitions = await Promise.all(
-      marketTokenAddresses.map(async address => {
-        const idleTokenContract = this.contractFactory.idleToken({ address, network: this.network });
-        const underlyingTokenAddressRaw = await multicall.wrap(idleTokenContract).token();
-
-        return {
-          address,
-          underlyingTokenAddress: underlyingTokenAddressRaw.toLowerCase(),
-        };
-      }),
-    );
-
-    return definitions;
+    return controller.getAllMarkets();
   }
 
-  async getAddresses({ definitions }: GetAddressesParams) {
-    return definitions.map(v => v.address);
-  }
-
-  async getUnderlyingTokenAddresses({ definition }: GetUnderlyingTokensParams<IdleToken, IdleVaultTokenDefinition>) {
-    return definition.underlyingTokenAddress;
-  }
-
-  async getPrice({ appToken, contract }: GetPriceParams<IdleToken>): Promise<number> {
-    const priceRaw = await contract.tokenPrice();
-    return Number(priceRaw) / 10 ** appToken.tokens[0].decimals;
+  async getUnderlyingTokenAddresses({ contract }: GetUnderlyingTokensParams<IdleToken>) {
+    return contract.token();
   }
 
   async getPricePerShare({ appToken, contract }: GetPricePerShareParams<IdleToken>) {
