612161a9cbf8e8c35ac97c0cd95234031b8087a4
Pranav Raut <Pranavraut033@gmail.com>
2022-12-11 11:30:14 +0530

    fix: refactored code
    * moved external call to helper function
    * using getBaseTokenPrices for token object
    * removed try catch

--- src/apps/defiedge/common/defiedge.strategy.token-fetcher.ts
+++ src/apps/defiedge/common/defiedge.strategy.token-fetcher.ts
@@ -1,5 +1,4 @@
 import { Inject } from '@nestjs/common';
-import axios from 'axios';
 import { BigNumber } from 'ethers';
 import { formatEther, parseEther } from 'ethers/lib/utils';
 import _ from 'lodash';
@@ -11,14 +10,12 @@ import {
 } from '~app-toolkit/helpers/presentation/display-item.present';
 import { getImagesFromToken } from '~app-toolkit/helpers/presentation/image.present';
 import { ContractType } from '~position/contract.interface';
-import { WithMetaType } from '~position/display.interface';
 import { PositionFetcher } from '~position/position-fetcher.interface';
-import { AppTokenPosition, Token } from '~position/position.interface';
+import { AppTokenPosition } from '~position/position.interface';
 import { Network } from '~types';
 
 import { DefiedgeContractFactory } from '../contracts';
-import { Strategy } from '../types/defiedge.types';
-import { DEFIEDGE_BASE_URL, expandTo18Decimals, filterNulls, getTokenPrice } from '../utils';
+import { expandTo18Decimals, filterNulls, getDefiStrategies } from '../utils';
 
 export type StrategyTokenDataProps = {
   sinceInception: number;
@@ -37,112 +34,85 @@ export abstract class DefiedgeStrategyTokenFetcher implements PositionFetcher<Ap
   ) {}
 
   async getPositions() {
-    const networkParam = this.network === Network.ETHEREUM_MAINNET ? 'mainnet' : this.network;
-    const endpoint = `${DEFIEDGE_BASE_URL}/${networkParam}/strategies`;
     const multicall = this.appToolkit.getMulticall(this.network);
 
-    const strategies = await axios.get<Strategy[]>(endpoint).then(({ data }) => data);
+    const [strategies, baseTokenAddressToDetails] = await Promise.all([
+      getDefiStrategies(this.network),
+      this.appToolkit.getBaseTokenPrices(this.network).then(baseTokens => {
+        return _.keyBy(baseTokens, e => e.address.toLowerCase());
+      }),
+    ]);
 
-    const baseTokenAddressToDetails = await this.appToolkit.getBaseTokenPrices(this.network).then(baseTokens => {
-      return _.keyBy(baseTokens, e => e.address.toLowerCase());
-    });
-
-    const appTokens = await Promise.all(
+    const appTokens = await Promise.allSettled(
       strategies.map(async strategy => {
-        const erc20Contract = this.defiedgeContractFactory.erc20({ address: strategy.address, network: this.network });
+        const erc20Contract = this.defiedgeContractFactory.erc20({ address: strategy.id, network: this.network });
         const strategyContract = this.defiedgeContractFactory.strategy({
-          address: strategy.address,
+          address: strategy.id,
           network: this.network,
         });
 
-        try {
-          const [decimals, totalSupplyBN, aumWithFee, [token0, token1]] = await Promise.all([
-            multicall.wrap(erc20Contract).decimals(),
-            multicall.wrap(erc20Contract).totalSupply(),
-            multicall.wrap(strategyContract).callStatic.getAUMWithFees(false),
-            Promise.all(
-              [strategy.token0, strategy.token1].map(async (t): Promise<WithMetaType<Token>> => {
-                const baseToken = baseTokenAddressToDetails[t.id.toLowerCase()];
-                const price = baseToken?.price ? baseToken.price : await getTokenPrice(t.symbol);
+        const [decimals, totalSupplyBN, aumWithFee] = await Promise.all([
+          multicall.wrap(erc20Contract).decimals(),
+          multicall.wrap(erc20Contract).totalSupply(),
+          multicall.wrap(strategyContract).callStatic.getAUMWithFees(false),
+        ]);
 
-                return {
-                  address: t.id,
-                  decimals: +t.decimals,
-                  network: this.network,
-                  type: ContractType.BASE_TOKEN,
-                  price: price ?? 0,
-                  symbol: t.symbol,
-                };
-              }),
-            ),
-          ]);
+        const token0 = baseTokenAddressToDetails[strategy.token0.id.toLowerCase()];
+        const token1 = baseTokenAddressToDetails[strategy.token1.id.toLowerCase()];
 
-          const { amount0, amount1 } = aumWithFee;
-          const tokens = [token0, token1];
+        if (!token0 || !token1 || totalSupplyBN.lte(BigNumber.from(0))) return null;
 
-          const supply = Number(totalSupplyBN) / 10 ** decimals;
+        const tokens = [token0, token1];
+        const supply = Number(totalSupplyBN) / 10 ** decimals;
+        const { amount0, amount1 } = aumWithFee;
+        const t0Price = parseEther(token0.price.toString());
+        const t1Price = parseEther(token1.price.toString());
 
-          let sharePrice = 100;
-          let aum = 0;
-          let pricePerShare: number[] = [0, 0];
+        const aumBN = expandTo18Decimals(amount0, +token0.decimals)
+          .mul(t0Price)
+          .add(expandTo18Decimals(amount1, +token1.decimals).mul(t1Price));
 
-          if (token0.price && token1.price && totalSupplyBN.gt(BigNumber.from(0))) {
-            try {
-              const t0Price = parseEther(token0.price.toString());
-              const t1Price = parseEther(token1.price.toString());
+        const sharePrice = +Number(+formatEther(aumBN.div(totalSupplyBN))).toFixed(8) || 100;
+        const aum = +formatEther(aumBN) / 1e18;
 
-              const aumBN = expandTo18Decimals(amount0, +token0.decimals)
-                .mul(t0Price)
-                .add(expandTo18Decimals(amount1, +token1.decimals).mul(t1Price));
+        const pricePerShare = [
+          +formatEther(expandTo18Decimals(amount0, +token0.decimals).mul(t0Price)) / 1e18 / aum,
+          +formatEther(expandTo18Decimals(amount1, +token1.decimals).mul(t1Price)) / 1e18 / aum,
+        ];
 
-              sharePrice = +Number(+formatEther(aumBN.div(totalSupplyBN))).toFixed(8) || 100;
-              aum = +formatEther(aumBN) / 1e18;
-
-              pricePerShare = [
-                +formatEther(expandTo18Decimals(amount0, +token0.decimals).mul(t0Price)) / 1e18 / aum,
-                +formatEther(expandTo18Decimals(amount1, +token1.decimals).mul(t1Price)) / 1e18 / aum,
-              ];
-            } catch (e) {
-              // ignored
-            }
-          }
-
-          const appToken: AppTokenPosition<StrategyTokenDataProps> = {
-            type: ContractType.APP_TOKEN,
-            address: strategy.address,
-            appId: this.appId,
-            tokens,
-            decimals,
-            groupId: this.groupId,
-            network: this.network,
-            supply,
-            symbol: strategy.title,
-            pricePerShare,
-            price: sharePrice,
-            dataProps: { aum, sharePrice, sinceInception: sharePrice - 100 },
-            displayProps: {
-              label: strategy.subTitle || strategy.title,
-              secondaryLabel: strategy.subTitle ? undefined : strategy.title,
-              images: tokens.map(getImagesFromToken).flat(),
-              statsItems: [
-                {
-                  label: 'AUM',
-                  value: buildDollarDisplayItem(aum),
-                },
-                {
-                  label: 'Since inception',
-                  value: buildPercentageDisplayItem(sharePrice - 100),
-                },
-              ],
-            },
-          };
-          return appToken;
-        } catch {
-          return null;
-        }
+        const appToken: AppTokenPosition<StrategyTokenDataProps> = {
+          type: ContractType.APP_TOKEN,
+          address: strategy.id,
+          appId: this.appId,
+          tokens,
+          decimals,
+          groupId: this.groupId,
+          network: this.network,
+          supply,
+          symbol: strategy.title,
+          pricePerShare,
+          price: sharePrice,
+          dataProps: { aum, sharePrice, sinceInception: sharePrice - 100 },
+          displayProps: {
+            label: strategy.subTitle || strategy.title,
+            secondaryLabel: strategy.subTitle ? undefined : strategy.title,
+            images: tokens.map(getImagesFromToken).flat(),
+            statsItems: [
+              {
+                label: 'AUM',
+                value: buildDollarDisplayItem(aum),
+              },
+              {
+                label: 'Since inception',
+                value: buildPercentageDisplayItem(sharePrice - 100),
+              },
+            ],
+          },
+        };
+        return appToken;
       }),
     );
 
-    return appTokens.filter(filterNulls);
+    return appTokens.map(e => (e.status == 'fulfilled' ? e.value : null)).filter(filterNulls);
   }
 }
--- src/apps/defiedge/utils/index.ts
+++ src/apps/defiedge/utils/index.ts
@@ -1,24 +1,11 @@
 import axios from 'axios';
 import { BigNumber } from 'ethers';
 
+import { Network } from '~types';
+
+import { Strategy } from '../types/defiedge.types';
+
 export const DEFIEDGE_BASE_URL = 'https://api.defiedge.io';
-export const DEFIEDGE_SUBGRAPH_BASE_URL = 'https://api.thegraph.com/subgraphs/name/unbound-finance';
-
-const promises: Record<string, Promise<number> | undefined> = {};
-
-export function getTokenPrice(symbol: string): Promise<number> {
-  let promise = promises[symbol];
-
-  if (promise) return promise;
-
-  promise = axios
-    .get<{ USD: number }>(`${DEFIEDGE_BASE_URL}/${symbol}/price`)
-    .then(({ data }) => data.USD)
-    .catch(() => 0)
-    .finally(() => setTimeout(() => delete promises[symbol], 15000));
-
-  return promise;
-}
 
 export function expandTo18Decimals(value: BigNumber, decimals?: number): BigNumber {
   return value.mul(BigNumber.from(10).pow(BigNumber.from(18).sub(BigNumber.from(decimals ?? 0))));
@@ -26,4 +13,11 @@ export function expandTo18Decimals(value: BigNumber, decimals?: number): BigNumb
 
 export function filterNulls<T>(a: T | null): a is T {
   return Boolean(a);
+}
+
+export async function getDefiStrategies(network: Network) {
+  const networkParam = network === Network.ETHEREUM_MAINNET ? 'mainnet' : network;
+  const endpoint = `${DEFIEDGE_BASE_URL}/${networkParam}/strategies`;
+
+  return await axios.get<Strategy[]>(endpoint).then(({ data }) => data);
 }
