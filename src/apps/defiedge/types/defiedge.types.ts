8560991eceb628b66e092a9d2d24d4a6c7be4b49
Pranav Raut <Pranavraut033@gmail.com>
2022-12-11 11:30:21 +0530

    chore: removed used types

--- src/apps/defiedge/types/defiedge.types.ts
+++ src/apps/defiedge/types/defiedge.types.ts
@@ -1,76 +1,13 @@
 export interface Strategy {
   id: string;
-  createdAtTimestamp: string;
-  amount0: string;
-  amount1: string;
-  shares: string;
-  txCount: string;
-  name: string;
-  owner: string;
-  userCount: string;
-  type: DataFeed;
-  hash: string;
   pool: string;
-  unusedAmount0: string;
-  unusedAmount1: string;
-  feeTier: string;
   token0: Token;
   token1: Token;
-  adds: Add[];
-  removes: Add[];
-  since_inception: { USD: number };
-  collectedFeesToken0: string;
-  collectedFeesToken1: string;
-  onHold: boolean;
-  address: string;
-  dataFeed: DataFeed;
-  subTitle: null | string;
+  subTitle: string | null;
   title: string;
-  aum: number;
-  apy: number;
 }
 
-interface Add {
-  id: string;
-  timestamp: string;
-  amount0: string;
-  amount1: string;
-  __typename: AddTypename;
-}
-
-enum AddTypename {
-  Add = 'Add',
-  Remove = 'Remove',
-}
-
-enum DataFeed {
-  Chainlink = 'Chainlink',
-  Twap = 'Twap',
-}
-
-export interface User {
-  id: string;
-  positions: Position[];
-}
-
-interface Position {
-  collectedFeesToken0: string;
-  collectedFeesToken1: string;
-  strategy: Strategy;
-  amount0: string;
-  amount1: string;
-}
-
-export interface PositionStrategy {
-  id: string;
-  name: string;
-  pool: string;
-  hash: string;
-  type: string;
-  token0: Token;
-  token1: Token;
-}
-interface Token {
+export interface Token {
   id: string;
   totalSupply: string;
   name: string;
