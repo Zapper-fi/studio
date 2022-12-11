0124ec20d0b26e7139640d07c471f14d853db723
William Poulin <will@zapper.fi>
2022-12-10 22:10:44 -0500

    fix(iq): Linting

--- src/apps/iq/iq.module.ts
+++ src/apps/iq/iq.module.ts
@@ -2,7 +2,6 @@ import { Register } from '~app-toolkit/decorators';
 import { AbstractApp } from '~app/app.dynamic-module';
 
 import { IqContractFactory } from './contracts';
-
 import { EthereumIqHiiqContractPositionFetcher } from './ethereum/iq.hiiq.contract-position-fetcher';
 import { IqAppDefinition, IQ_DEFINITION } from './iq.definition';
 
