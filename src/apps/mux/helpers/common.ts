570a1c9d83aa88d75a11066658feb14517d18796
xionyk <82797250+xionyk@users.noreply.github.com>
2022-12-13 21:02:10 +0800

    feat(mux): Add lev-trades ContractPositionFetcher and add BalanceFetcher

--- src/apps/mux/helpers/common.ts
+++ src/apps/mux/helpers/common.ts
@@ -2,6 +2,7 @@ import Axios, { AxiosInstance } from 'axios';
 import { ethers } from 'ethers';
 
 import { IAppToolkit } from '~app-toolkit/app-toolkit.interface';
+import { ZERO_ADDRESS } from '~app-toolkit/constants/address';
 import { MuxContractFactory } from '~apps/mux';
 import { LiquidityAsset } from '~apps/mux/helpers/mux.mlp.token-helper';
 import { ContractType } from '~position/contract.interface';
@@ -29,8 +30,6 @@ export const ASSET_IS_SHORTABLE = 0x00000001000000; // allow shorting this asset
 export const ASSET_USE_STABLE_TOKEN_FOR_PROFIT = 0x00000100000000; // take profit will get stable coin
 export const ASSET_IS_ENABLED = 0x00010000000000; // allowed to be assetId and collateralId
 export const ASSET_IS_STRICT_STABLE = 0x01000000000000; // assetPrice is always 1 unless volatility exceeds strictStableDeviation
-
-const invalidAddress = '0x0000000000000000000000000000000000000000';
 
 export const READER_ADDRESS = {
   [Network.ARBITRUM_MAINNET]: '0x6e29c4e8095b2885b8d30b17790924f33ecd7b33',
@@ -88,7 +87,7 @@ async function getReaderAssets(network: Network, appToolkit: IAppToolkit): Promi
   const storage = await multicall.wrap(readerContract).callStatic.getChainStorage();
 
   return storage[1]
-    .filter(item => item.tokenAddress !== invalidAddress)
+    .filter(item => item.tokenAddress !== ZERO_ADDRESS)
     .map(a => {
       return {
         symbol: ethers.utils.parseBytes32String(a.symbol),
