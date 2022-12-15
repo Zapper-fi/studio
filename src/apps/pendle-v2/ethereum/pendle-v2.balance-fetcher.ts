22f2f7a462783b496467755c3ae16ab4eb9a3cb7
Le Duy Thuc <leduythuc.cs@gmail.com>
2022-12-15 17:44:25 +0700

    feat: implement balance fetcher

---
+++ src/apps/pendle-v2/ethereum/pendle-v2.balance-fetcher.ts
@@ -0,0 +1,56 @@
+import { Inject } from '@nestjs/common';
+
+import { IAppToolkit, APP_TOOLKIT } from '~app-toolkit/app-toolkit.interface';
+import { Register } from '~app-toolkit/decorators';
+import { presentBalanceFetcherResponse } from '~app-toolkit/helpers/presentation/balance-fetcher-response.present';
+import { BalanceFetcher } from '~balance/balance-fetcher.interface';
+import { Network } from '~types/network.interface';
+
+import { PENDLE_V_2_DEFINITION } from '../pendle-v2.definition';
+
+const network = Network.ETHEREUM_MAINNET;
+
+@Register.BalanceFetcher(PENDLE_V_2_DEFINITION.id, network)
+export class EthereumPendleV2BalanceFetcher implements BalanceFetcher {
+  constructor(@Inject(APP_TOOLKIT) private readonly appToolkit: IAppToolkit) { }
+
+  private async fetchBalanceInGroup(address: string, groupId: string) {
+    return this.appToolkit.helpers.tokenBalanceHelper.getTokenBalances({
+      address,
+      appId: PENDLE_V_2_DEFINITION.id,
+      network,
+      groupId: groupId,
+    });
+  }
+  async getBalances(address: string) {
+    const [
+      ptBalances,
+      ytBalances,
+      syBalances,
+      lpBalances,
+    ] = await Promise.all([
+      this.fetchBalanceInGroup(address, PENDLE_V_2_DEFINITION.groups.principalToken.label),
+      this.fetchBalanceInGroup(address, PENDLE_V_2_DEFINITION.groups.yieldToken.label),
+      this.fetchBalanceInGroup(address, PENDLE_V_2_DEFINITION.groups.standardizedYieldToken.label),
+      this.fetchBalanceInGroup(address, PENDLE_V_2_DEFINITION.groups.pool.label),
+    ]);
+    return presentBalanceFetcherResponse([
+      {
+        label: 'Principal Tokens',
+        assets: ptBalances,
+      },
+      {
+        label: 'Yield Tokens',
+        assets: ytBalances,
+      },
+      {
+        label: 'Standardized Yield Tokens',
+        assets: syBalances,
+      },
+      {
+        label: 'Pools',
+        assets: lpBalances,
+      },
+    ]);
+  }
+}
