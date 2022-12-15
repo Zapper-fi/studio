8f5d734d12f28fcfdbc4276037030d59f2c32254
sand0man <sand0man@proton.me>
2022-12-15 10:21:36 -0800

    add cask

---
+++ src/apps/cask-protocol/celo/cask-protocol.wallet.token-fetcher.ts
@@ -0,0 +1,15 @@
+import { Register } from '~app-toolkit/decorators';
+import { CaskProtocolWalletTokenFetcher } from '../common/cask-protocol.wallet.token-fetcher';
+import { Network } from '~types/network.interface';
+
+import { CASK_PROTOCOL_DEFINITION } from '../cask-protocol.definition';
+
+const appId = CASK_PROTOCOL_DEFINITION.id;
+const groupId = CASK_PROTOCOL_DEFINITION.groups.wallet.id;
+const network = Network.CELO_MAINNET;
+
+@Register.TokenPositionFetcher({ appId, groupId, network })
+export class CeloCaskProtocolWalletTokenFetcher extends CaskProtocolWalletTokenFetcher {
+  caskVaultContractAddress = '0xBCcDbB0806Acc914F6746DE592f924B374190710';
+  caskNetwork = Network.CELO_MAINNET;
+}
---
+++ src/apps/cask-protocol/index.ts
@@ -0,0 +1,3 @@
+export { CASK_PROTOCOL_DEFINITION, CaskProtocolAppDefinition } from './cask-protocol.definition';
+export { CaskProtocolAppModule } from './cask-protocol.module';
+export { CaskProtocolContractFactory } from './contracts';
