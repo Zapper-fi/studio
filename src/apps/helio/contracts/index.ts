65f9757f31fdb450463f3562bbe3780fdbbeccb4
William Poulin <will@zapper.fi>
2022-12-19 16:40:22 -0500

    fix(helio): Migration to template (#1950)

--- src/apps/helio/contracts/index.ts
+++ src/apps/helio/contracts/index.ts
@@ -4,7 +4,6 @@ import { IAppToolkit, APP_TOOLKIT } from '~app-toolkit/app-toolkit.interface';
 import { ContractFactory } from '~contract/contracts';
 import { Network } from '~types/network.interface';
 
-import { HelioHay__factory } from './ethers';
 import { HelioJar__factory } from './ethers';
 
 // eslint-disable-next-line
@@ -16,13 +15,9 @@ export class HelioContractFactory extends ContractFactory {
     super((network: Network) => appToolkit.getNetworkProvider(network));
   }
 
-  helioHay({ address, network }: ContractOpts) {
-    return HelioHay__factory.connect(address, this.appToolkit.getNetworkProvider(network));
-  }
   helioJar({ address, network }: ContractOpts) {
     return HelioJar__factory.connect(address, this.appToolkit.getNetworkProvider(network));
   }
 }
 
-export type { HelioHay } from './ethers';
 export type { HelioJar } from './ethers';
