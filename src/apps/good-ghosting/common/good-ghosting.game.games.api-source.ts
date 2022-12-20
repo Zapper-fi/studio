f49d50b6b9f19bd06c5e109ff1b0795ca2723946
meetkosiso <kosiso.gaxion@gmail.com>
2022-12-20 20:57:44 +0100

    fix: resolved an incorrect balance bug

--- src/apps/good-ghosting/common/good-ghosting.game.games.api-source.ts
+++ src/apps/good-ghosting/common/good-ghosting.game.games.api-source.ts
@@ -56,10 +56,10 @@ export class GoodGhostingGameGamesApiSource {
       const isPolygonGame = NetworkId.PolygonMainnet === networkId;
       const isCeloGame = NetworkId.CeloMainnet === networkId;
 
-      if (isV2Game && rewards) {
-        rewards.map(reward => {
+      if (isV2Game) {
+        rewardTokens[RewardType.Deposit] = depositTokenAddress;
+        rewards?.map(reward => {
           rewardTokens[reward.type] = reward.address;
-          rewardTokens[RewardType.Deposit] = depositTokenAddress;
         });
 
         const rewardTokenAddress = Object.values(rewardTokens);
