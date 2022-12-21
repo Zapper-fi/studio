049f6c612f84ee34a4d49fe51b9acb3284899b6e
William Poulin <will@zapper.fi>
2022-12-21 12:15:19 -0500

    fix(sushiswap-bentobox): Migration to template

--- src/apps/sushiswap-bentobox/binance-smart-chain/sushiswap-bentobox.vault.contract-position-fetcher.ts
+++ src/apps/sushiswap-bentobox/binance-smart-chain/sushiswap-bentobox.vault.contract-position-fetcher.ts
@@ -1,29 +1,10 @@
-import { Inject } from '@nestjs/common';
+import { PositionTemplate } from '~app-toolkit/decorators/position-template.decorator';
 
-import { Register } from '~app-toolkit/decorators';
-import { PositionFetcher } from '~position/position-fetcher.interface';
-import { ContractPosition } from '~position/position.interface';
-import { Network } from '~types/network.interface';
+import { SushiswapBentoboxVaultContractPositionFetcher } from '../common/sushiswap-bentobox.vault.contract-position-fetcher';
 
-import { SushiSwapBentoBoxContractPositionHelper } from '../helpers/sushiswap-bentobox.vault.contract-position-helper';
-import { SUSHISWAP_BENTOBOX_DEFINITION } from '../sushiswap-bentobox.definition';
-
-const appId = SUSHISWAP_BENTOBOX_DEFINITION.id;
-const groupId = SUSHISWAP_BENTOBOX_DEFINITION.groups.vault.id;
-const network = Network.BINANCE_SMART_CHAIN_MAINNET;
-
-@Register.ContractPositionFetcher({ appId, groupId, network })
-export class BscSushiSwapBentoBoxContractPositionFetcher implements PositionFetcher<ContractPosition> {
-  constructor(
-    @Inject(SushiSwapBentoBoxContractPositionHelper)
-    private readonly positionHelper: SushiSwapBentoBoxContractPositionHelper,
-  ) {}
-
-  async getPositions() {
-    return this.positionHelper.getPositions({
-      bentoBoxAddress: '0xf5bce5077908a1b7370b9ae04adc565ebd643966',
-      network,
-      subgraphUrl: 'https://api.thegraph.com/subgraphs/name/sushiswap/bsc-bentobox',
-    });
-  }
+@PositionTemplate()
+export class BinanceSmartChainSushiSwapBentoBoxContractPositionFetcher extends SushiswapBentoboxVaultContractPositionFetcher {
+  groupLabel = 'BentoBox';
+  subgraphUrl = 'https://api.thegraph.com/subgraphs/name/sushiswap/bentobox-bsc';
+  bentoboxAddress = '0xf5bce5077908a1b7370b9ae04adc565ebd643966';
 }
