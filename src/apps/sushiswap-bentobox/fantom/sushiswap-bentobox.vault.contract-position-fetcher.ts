b722434ec15b3bdc5e786edf4a343724817460fb
William Poulin <will@zapper.fi>
2022-12-21 14:49:27 -0500

    fix(sushiswap-bentobox): Migrate to template (#1968)
    * fix(sushiswap-bentobox): Migration to template
    
    * feat(sushiswap-bentobox): Add bentobox vaults on avalanche and fantom

--- src/apps/sushiswap-bentobox/fantom/sushiswap-bentobox.vault.contract-position-fetcher.ts
+++ src/apps/sushiswap-bentobox/fantom/sushiswap-bentobox.vault.contract-position-fetcher.ts
@@ -3,8 +3,8 @@ import { PositionTemplate } from '~app-toolkit/decorators/position-template.deco
 import { SushiswapBentoboxVaultContractPositionFetcher } from '../common/sushiswap-bentobox.vault.contract-position-fetcher';
 
 @PositionTemplate()
-export class BinanceSmartChainSushiSwapBentoBoxContractPositionFetcher extends SushiswapBentoboxVaultContractPositionFetcher {
+export class FantomSushiSwapBentoBoxContractPositionFetcher extends SushiswapBentoboxVaultContractPositionFetcher {
   groupLabel = 'BentoBox';
-  subgraphUrl = 'https://api.thegraph.com/subgraphs/name/sushiswap/bentobox-bsc';
+  subgraphUrl = 'https://api.thegraph.com/subgraphs/name/sushiswap/bentobox-fantom';
   bentoboxAddress = '0xf5bce5077908a1b7370b9ae04adc565ebd643966';
 }
