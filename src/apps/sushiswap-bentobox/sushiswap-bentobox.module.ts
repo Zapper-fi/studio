818555c1bc4ea64ca03d047fd13ea365468ecda7
William Poulin <will@zapper.fi>
2022-12-21 12:19:11 -0500

    feat(sushiswap-bentobox): Add bentobox vaults on avalanche and fantom

--- src/apps/sushiswap-bentobox/sushiswap-bentobox.module.ts
+++ src/apps/sushiswap-bentobox/sushiswap-bentobox.module.ts
@@ -2,10 +2,12 @@ import { Register } from '~app-toolkit/decorators';
 import { AbstractApp } from '~app/app.dynamic-module';
 
 import { ArbitrumSushiSwapBentoBoxContractPositionFetcher } from './arbitrum/sushiswap-bentobox.vault.contract-position-fetcher';
+import { AvalancheSushiSwapBentoBoxContractPositionFetcher } from './avalanche/sushiswap-bentobox.vault.contract-position-fetcher';
 import { BinanceSmartChainSushiSwapBentoBoxContractPositionFetcher } from './binance-smart-chain/sushiswap-bentobox.vault.contract-position-fetcher';
 import { SushiswapBentoboxVaultTokensResolver } from './common/sushiswap-bentobox.vault-tokens-resolver';
 import { SushiswapBentoboxContractFactory } from './contracts';
 import { EthereumSushiSwapBentoBoxContractPositionFetcher } from './ethereum/sushiswap-bentobox.vault.contract-position-fetcher';
+import { FantomSushiSwapBentoBoxContractPositionFetcher } from './fantom/sushiswap-bentobox.vault.contract-position-fetcher';
 import { PolygonSushiSwapBentoBoxContractPositionFetcher } from './polygon/sushiswap-bentobox.vault.contract-position-fetcher';
 import { SushiSwapBentoBoxAppDefinition, SUSHISWAP_BENTOBOX_DEFINITION } from './sushiswap-bentobox.definition';
 
@@ -17,10 +19,14 @@ import { SushiSwapBentoBoxAppDefinition, SUSHISWAP_BENTOBOX_DEFINITION } from '.
     SushiswapBentoboxVaultTokensResolver,
     // Arbitrum
     ArbitrumSushiSwapBentoBoxContractPositionFetcher,
+    // Avalanche
+    AvalancheSushiSwapBentoBoxContractPositionFetcher,
     // Binance Smart Chain
     BinanceSmartChainSushiSwapBentoBoxContractPositionFetcher,
     // Ethereum
     EthereumSushiSwapBentoBoxContractPositionFetcher,
+    // Fantom
+    FantomSushiSwapBentoBoxContractPositionFetcher,
     // Polygon
     PolygonSushiSwapBentoBoxContractPositionFetcher,
   ],
