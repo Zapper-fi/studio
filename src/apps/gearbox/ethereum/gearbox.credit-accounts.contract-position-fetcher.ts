41d2b4dbff77a45351c412f08d891438e970976b
0xdapper <94534135+0xdapper@users.noreply.github.com>
2022-12-25 23:30:38 +0530

    feat(gearbox): add credit accounts tracking (#1991)
    * feat(gearbox): Add credit accounts tracking
    
    * fix(gearbox): Remove duplicate of underlying as supply token to remove double counting
    
    * fix(gearbox): Fix label
    
    Co-authored-by: William Poulin <will@zapper.fi>

---
+++ src/apps/gearbox/ethereum/gearbox.credit-accounts.contract-position-fetcher.ts
@@ -0,0 +1,124 @@
+import { Inject } from '@nestjs/common';
+import { BigNumberish } from 'ethers';
+import _ from 'lodash';
+
+import { IAppToolkit, APP_TOOLKIT } from '~app-toolkit/app-toolkit.interface';
+import { PositionTemplate } from '~app-toolkit/decorators/position-template.decorator';
+import { getLabelFromToken } from '~app-toolkit/helpers/presentation/image.present';
+import { DefaultDataProps } from '~position/display.interface';
+import { MetaType } from '~position/position.interface';
+import { ContractPositionTemplatePositionFetcher } from '~position/template/contract-position.template.position-fetcher';
+import {
+  GetDefinitionsParams,
+  DefaultContractPositionDefinition,
+  GetTokenDefinitionsParams,
+  UnderlyingTokenDefinition,
+  GetDisplayPropsParams,
+  GetTokenBalancesParams,
+} from '~position/template/contract-position.template.types';
+
+import { CreditManagerV2, GearboxContractFactory } from '../contracts';
+
+@PositionTemplate()
+export class EthereumGearboxCreditAccountsContractPositionFetcher extends ContractPositionTemplatePositionFetcher<CreditManagerV2> {
+  constructor(
+    @Inject(APP_TOOLKIT) readonly appToolkit: IAppToolkit,
+    @Inject(GearboxContractFactory) private readonly gearboxContractFactory: GearboxContractFactory,
+  ) {
+    super(appToolkit);
+  }
+
+  groupLabel = 'Credit Account';
+
+  getContract(address: string): CreditManagerV2 {
+    return this.gearboxContractFactory.creditManagerV2({ address, network: this.network });
+  }
+
+  async getTokenDefinitions(
+    params: GetTokenDefinitionsParams<CreditManagerV2, DefaultContractPositionDefinition>,
+  ): Promise<UnderlyingTokenDefinition[] | null> {
+    const contract = params.contract;
+    const multicall = params.multicall;
+
+    const underlying = await contract.underlying();
+    const collateralTokensCount = await contract.collateralTokensCount();
+    const collateralTokens = await Promise.all(
+      _.range(collateralTokensCount.toNumber()).map(idx =>
+        multicall
+          .wrap(contract)
+          .collateralTokens(idx)
+          .then(([token, _]) => token),
+      ),
+    );
+
+    return [
+      {
+        address: underlying,
+        metaType: MetaType.BORROWED,
+        network: this.network,
+      },
+      ...collateralTokens.map(token => ({
+        address: token,
+        metaType: MetaType.SUPPLIED,
+        network: this.network,
+      })),
+    ];
+  }
+
+  async getLabel(
+    params: GetDisplayPropsParams<CreditManagerV2, DefaultDataProps, DefaultContractPositionDefinition>,
+  ): Promise<string> {
+    return getLabelFromToken(params.contractPosition.tokens[0]);
+  }
+
+  async getDefinitions(params: GetDefinitionsParams): Promise<DefaultContractPositionDefinition[]> {
+    const contractsRegister = this.gearboxContractFactory.contractsRegister({
+      address: '0xa50d4e7d8946a7c90652339cdbd262c375d54d99',
+      network: this.network,
+    });
+
+    const creditManagers = await contractsRegister.getCreditManagers();
+    const creditManagerContracts = creditManagers.map(addr =>
+      this.gearboxContractFactory.creditManagerV2({ address: addr, network: this.network }),
+    );
+
+    const multicall = params.multicall;
+    const creditManagerVersions = await Promise.all(
+      creditManagerContracts.map(contract => multicall.wrap(contract).version()),
+    );
+
+    // only v2 credit managers
+    return creditManagers
+      .filter((_, idx) => creditManagerVersions[idx].toNumber() === 2)
+      .map((address, idx) => ({ address, contract: creditManagerContracts[idx] }));
+  }
+
+  async getTokenBalancesPerPosition({
+    address,
+    contractPosition,
+    contract,
+    multicall,
+  }: GetTokenBalancesParams<CreditManagerV2, DefaultDataProps>): Promise<BigNumberish[]> {
+    let creditAccountAddress = '';
+
+    try {
+      creditAccountAddress = await contract.getCreditAccountOrRevert(address);
+    } catch (err) {
+      return Array(contractPosition.tokens.length).fill(0);
+    }
+
+    const balances = await Promise.all(
+      contractPosition.tokens.map(async token => {
+        if (token.metaType === MetaType.BORROWED) {
+          const debt = await contract.calcCreditAccountAccruedInterest(creditAccountAddress);
+          return debt.borrowedAmountWithInterestAndFees;
+        }
+
+        const tokenContract = this.gearboxContractFactory.erc20({ address: token.address, network: this.network });
+        return multicall.wrap(tokenContract).balanceOf(creditAccountAddress);
+      }),
+    );
+
+    return balances;
+  }
+}
--- src/apps/gearbox/gearbox.module.ts
+++ src/apps/gearbox/gearbox.module.ts
@@ -2,11 +2,19 @@ import { Register } from '~app-toolkit/decorators';
 import { AbstractApp } from '~app/app.dynamic-module';
 
 import { GearboxContractFactory } from './contracts';
+import { EthereumGearboxCreditAccountsContractPositionFetcher } from './ethereum/gearbox.credit-accounts.contract-position-fetcher';
 import { EthereumGearboxLendingTokenFetcher } from './ethereum/gearbox.lending.token-fetcher';
+import { EthereumGearboxPhantomTokenFetcher } from './ethereum/gearbox.phantom.token-fetcher';
 import { GearboxAppDefinition, GEARBOX_DEFINITION } from './gearbox.definition';
 
 @Register.AppModule({
   appId: GEARBOX_DEFINITION.id,
-  providers: [EthereumGearboxLendingTokenFetcher, GearboxAppDefinition, GearboxContractFactory],
+  providers: [
+    EthereumGearboxCreditAccountsContractPositionFetcher,
+    EthereumGearboxLendingTokenFetcher,
+    EthereumGearboxPhantomTokenFetcher,
+    GearboxAppDefinition,
+    GearboxContractFactory,
+  ],
 })
 export class GearboxAppModule extends AbstractApp() {}
