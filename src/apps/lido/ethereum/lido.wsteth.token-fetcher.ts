8cab9af24a4b22499559610dd510ce26c903d7d5
William Poulin <will@zapper.fi>
2023-01-03 21:55:55 -0500

    fix(lido): Migration to template (#2024)

--- src/apps/lido/ethereum/lido.wsteth.token-fetcher.ts
+++ src/apps/lido/ethereum/lido.wsteth.token-fetcher.ts
@@ -1,67 +1,48 @@
 import { Inject } from '@nestjs/common';
+import 'moment-duration-format';
 
-import { IAppToolkit, APP_TOOLKIT } from '~app-toolkit/app-toolkit.interface';
-import { Register } from '~app-toolkit/decorators';
-import { buildDollarDisplayItem } from '~app-toolkit/helpers/presentation/display-item.present';
-import { getTokenImg } from '~app-toolkit/helpers/presentation/image.present';
-import { ContractType } from '~position/contract.interface';
-import { PositionFetcher } from '~position/position-fetcher.interface';
-import { AppTokenPosition } from '~position/position.interface';
-import { Network } from '~types/network.interface';
+import { APP_TOOLKIT, IAppToolkit } from '~app-toolkit/app-toolkit.interface';
+import { PositionTemplate } from '~app-toolkit/decorators/position-template.decorator';
+import { AppTokenTemplatePositionFetcher } from '~position/template/app-token.template.position-fetcher';
+import {
+  GetPricePerShareParams,
+  DefaultAppTokenDataProps,
+  DefaultAppTokenDefinition,
+  GetUnderlyingTokensParams,
+} from '~position/template/app-token.template.types';
 
-import { LidoContractFactory } from '../contracts';
-import { LIDO_DEFINITION } from '../lido.definition';
+import { LidoContractFactory, Wsteth } from '../contracts';
 
-const appId = LIDO_DEFINITION.id;
-const groupId = LIDO_DEFINITION.groups.wsteth.id;
-const network = Network.ETHEREUM_MAINNET;
+@PositionTemplate()
+export class EthereumLidoWstethTokenFetcher extends AppTokenTemplatePositionFetcher<Wsteth> {
+  groupLabel = 'wstETH';
+  isExcludedFromBalances = true;
 
-@Register.TokenPositionFetcher({ appId, groupId, network, options: { excludeFromTvl: true } })
-export class EthereumLidoWstethTokenFetcher implements PositionFetcher<AppTokenPosition> {
   constructor(
-    @Inject(APP_TOOLKIT) private readonly appToolkit: IAppToolkit,
-    @Inject(LidoContractFactory) private readonly lidoContractFactory: LidoContractFactory,
-  ) {}
+    @Inject(APP_TOOLKIT) protected readonly appToolkit: IAppToolkit,
+    @Inject(LidoContractFactory) protected readonly contractFactory: LidoContractFactory,
+  ) {
+    super(appToolkit);
+  }
 
-  async getPositions() {
-    const address = '0x7f39c581f595b53c5cb19bd0b3f8da6c935e2ca0';
-    const multicall = this.appToolkit.getMulticall(network);
-    const contract = this.lidoContractFactory.steth({ address: address, network });
-    const baseTokenDependencies = await this.appToolkit.getBaseTokenPrices(network);
-    const wstethToken = baseTokenDependencies.find(v => v.symbol === 'wstETH')!;
-    const [symbol, decimals, totalSupply] = await Promise.all([
-      multicall.wrap(contract).symbol(),
-      multicall.wrap(contract).decimals(),
-      multicall.wrap(contract).totalSupply(),
-    ]);
-    const supply = Number(totalSupply) / 10 ** decimals;
-    const liquidity = wstethToken.price * supply;
-    const token: AppTokenPosition = {
-      address,
-      network,
-      appId,
-      groupId,
-      symbol,
-      decimals,
-      supply,
-      tokens: [],
-      dataProps: {},
-      pricePerShare: 1,
-      price: wstethToken.price,
-      type: ContractType.APP_TOKEN,
-      displayProps: {
-        label: symbol,
-        secondaryLabel: buildDollarDisplayItem(wstethToken.price),
-        images: [getTokenImg(address, network)],
-        statsItems: [
-          {
-            label: 'Liquidity',
-            value: buildDollarDisplayItem(liquidity),
-          },
-        ],
-      },
-    };
+  getContract(address: string): Wsteth {
+    return this.contractFactory.wsteth({ network: this.network, address });
+  }
 
-    return [token];
+  async getAddresses() {
+    return ['0x7f39c581f595b53c5cb19bd0b3f8da6c935e2ca0'];
+  }
+
+  async getUnderlyingTokenDefinitions({ contract }: GetUnderlyingTokensParams<Wsteth>) {
+    return [{ address: await contract.stETH(), network: this.network }];
+  }
+
+  async getPricePerShare({
+    appToken,
+    contract,
+  }: GetPricePerShareParams<Wsteth, DefaultAppTokenDataProps, DefaultAppTokenDefinition>) {
+    const pricePerShareRaw = await contract.stEthPerToken();
+
+    return Number(pricePerShareRaw) / 10 ** appToken.decimals;
   }
 }
