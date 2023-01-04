6eecf2dd6055caf64e3716b273533f886b405371
danilo neves cruz <cruzdanilo@gmail.com>
2023-01-03 22:07:29 -0500

    feat(exactly): ✨ add block number and timestamp to definitions

--- src/apps/exactly/common/exactly.definitions-resolver.ts
+++ src/apps/exactly/common/exactly.definitions-resolver.ts
@@ -1,31 +1,41 @@
-import { Inject, Injectable } from '@nestjs/common';
-import { constants } from 'ethers';
+import { Injectable } from '@nestjs/common';
+import { type BigNumber, constants } from 'ethers';
 
 import type { IMulticallWrapper } from '~multicall/multicall.interface';
 import type { DefaultAppTokenDefinition } from '~position/template/app-token.template.types';
 import { Network } from '~types/network.interface';
 
-import { ExactlyContractFactory } from '../contracts';
-import type { Previewer } from '../contracts';
+import { type Previewer, Previewer__factory } from '../contracts/ethers';
 
 export const PREVIEWER_ADDRESSES = {
   [Network.ETHEREUM_MAINNET]: '0x37ac9c4a26db589ee35215c7053009ee645585bb',
 } as Partial<Record<Network, string>>;
 
-export type ExactlyMarketDefinition = DefaultAppTokenDefinition & Previewer.MarketAccountStructOutput;
+export type ExactlyMarketDefinition = DefaultAppTokenDefinition &
+  Previewer.MarketAccountStructOutput & { blockNumber: number; timestamp: number };
 export type GetMarketDefinitionsParams = { network: Network; multicall: IMulticallWrapper; account?: string };
 
 @Injectable()
 export class ExactlyDefinitionsResolver {
-  constructor(@Inject(ExactlyContractFactory) protected readonly contractFactory: ExactlyContractFactory) {}
-
   async getDefinitions({ multicall, network, account }: GetMarketDefinitionsParams) {
     const address = PREVIEWER_ADDRESSES[network];
     if (!address) throw new Error(`missing previewer on ${network}`);
-    const exactly = await multicall
-      .wrap(this.contractFactory.previewer({ address, network }))
-      .exactly(account ?? constants.AddressZero);
-    return exactly.map(m => ({ address: m.market.toLowerCase(), ...m } as ExactlyMarketDefinition));
+    const previewer = Previewer__factory.createInterface();
+    const { address: multicallAddress, interface: multicallInterface } = multicall.contract;
+    const [block, [{ data: exactly }, { data: ts }]] = await multicall.wrap(multicall.contract).callStatic.aggregate(
+      [
+        { target: address, callData: previewer.encodeFunctionData('exactly', [account ?? constants.AddressZero]) },
+        { target: multicallAddress, callData: multicallInterface.encodeFunctionData('getCurrentBlockTimestamp') },
+      ],
+      true,
+    );
+    const blockNumber = block.toNumber();
+    const timestamp = (
+      multicall.contract.interface.decodeFunctionResult('getCurrentBlockTimestamp', ts)[0] as BigNumber
+    ).toNumber();
+    return (previewer.decodeFunctionResult('exactly', exactly)[0] as Previewer.MarketAccountStructOutput[]).map(
+      m => ({ address: m.market.toLowerCase(), blockNumber, timestamp, ...m } as ExactlyMarketDefinition),
+    );
   }
 
   async getDefinition(params: GetMarketDefinitionsParams & { market: string }) {
