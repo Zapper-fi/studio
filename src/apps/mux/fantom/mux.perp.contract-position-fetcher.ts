eb7e320211eb742604edcb67ac341fda784b6280
Karan Shahani <karan.shahani91@gmail.com>
2023-01-05 15:21:43 -0500

    feat(mux): Fix reader address

--- src/apps/mux/fantom/mux.perp.contract-position-fetcher.ts
+++ src/apps/mux/fantom/mux.perp.contract-position-fetcher.ts
@@ -5,7 +5,7 @@ import { MuxPerpContractPositionFetcher } from '../common/mux.perp.contract-posi
 @PositionTemplate()
 export class FantomMuxPerpContractPositionFetcher extends MuxPerpContractPositionFetcher {
   groupLabel = 'Perpetuals';
-  readerAddress = '0x2981bb8f9c7f7c5b9d8ca5e41c0d9cbbd89c7489';
+  readerAddress = '0xfb0dcdc30bf892ec981255e7133aecb8ea642b76';
   usdcAddress = '0x04068da6c83afcfa0e13ba15a6696662335d5b75';
   vaultAddress = '0xdaf2064f52f123ee1d410e97c2df549c23a99683';
 }
