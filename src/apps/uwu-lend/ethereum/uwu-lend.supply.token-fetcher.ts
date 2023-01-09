7a1913361f8860a2ef9d68d3df0b0199083f6437
William Poulin <will@zapper.fi>
2023-01-09 17:28:29 -0500

    feat(uwu-lend): Implement Uwu-lend (#2072)
    * feat(uwu-lend): Add supply/borrow and platform fees positions

---
+++ src/apps/uwu-lend/ethereum/uwu-lend.supply.token-fetcher.ts
@@ -0,0 +1,28 @@
+import { PositionTemplate } from '~app-toolkit/decorators/position-template.decorator';
+import { GetDisplayPropsParams } from '~position/template/app-token.template.types';
+
+import {
+  UwuLendLendingTokenFetcher,
+  UwuLendReserveApyData,
+  UwuLendReserveTokenAddressesData,
+} from '../common/uwu-lend.lending.token-fetcher';
+import { UwuLendUToken } from '../contracts';
+
+@PositionTemplate()
+export class EthereumUwuLendSupplyTokenFetcher extends UwuLendLendingTokenFetcher {
+  groupLabel = 'Lending';
+  providerAddress = '0x2409af0251dcb89ee3dee572629291f9b087c668';
+  isDebt = false;
+
+  getTokenAddress(reserveTokenAddressesData: UwuLendReserveTokenAddressesData): string {
+    return reserveTokenAddressesData.aTokenAddress;
+  }
+
+  getApyFromReserveData(reserveApyData: UwuLendReserveApyData): number {
+    return reserveApyData.supplyApy;
+  }
+
+  async getTertiaryLabel({ appToken }: GetDisplayPropsParams<UwuLendUToken>) {
+    return `${appToken.dataProps.apy.toFixed(3)}% APY`;
+  }
+}
---
+++ src/apps/uwu-lend/uwu-lend.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+
+import { AbstractApp } from '~app/app.dynamic-module';
+
+import { UwuLendContractFactory } from './contracts';
+import { EthereumUwuLendPlatformFeesV1PositionFetcher } from './ethereum/uwu-lend.platform-fees-v1.contract-position-fetcher';
+import { EthereumUwuLendPlatformFeesV2PositionFetcher } from './ethereum/uwu-lend.platform-fees-v2.contract-position-fetcher';
+import { EthereumUwuLendSupplyTokenFetcher } from './ethereum/uwu-lend.supply.token-fetcher';
+import { EthereumUwuLendVariableDebtTokenFetcher } from './ethereum/uwu-lend.variable-debt.token-fetcher';
+import { UwuLendAppDefinition } from './uwu-lend.definition';
+
+@Module({
+  providers: [
+    UwuLendAppDefinition,
+    UwuLendContractFactory,
+    EthereumUwuLendSupplyTokenFetcher,
+    EthereumUwuLendVariableDebtTokenFetcher,
+    EthereumUwuLendPlatformFeesV1PositionFetcher,
+    EthereumUwuLendPlatformFeesV2PositionFetcher,
+  ],
+})
+export class UwuLendAppModule extends AbstractApp() {}
