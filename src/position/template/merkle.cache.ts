dd655ed39e1af702ae7bdf87b9f0c3f6ce32dbca
Karan Shahani <karan.shahani91@gmail.com>
2023-01-10 11:59:31 -0500

    More fixes

---
+++ src/position/template/merkle.cache.ts
@@ -0,0 +1,50 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { entries } from 'lodash';
+import moment from 'moment';
+
+import { APP_TOOLKIT, IAppToolkit } from '~app-toolkit/app-toolkit.interface';
+import { Schedule } from '~scheduler/scheduler.decorator';
+import { Network } from '~types/network.interface';
+
+export type VotiumActiveTokenData = {
+  value: string;
+  label: string;
+  symbol: string;
+  decimals: number;
+};
+
+@Injectable()
+export abstract class MerkleCache<T = any> {
+  protected abstract readonly appId: string;
+  protected abstract readonly groupId: string;
+  protected abstract readonly network: Network;
+
+  protected abstract resolveMerkleData(): Promise<Record<string, Record<string, T>>>;
+
+  constructor(@Inject(APP_TOOLKIT) private readonly appToolkit: IAppToolkit) {}
+
+  private getKey(rewardTokenAddress: string, walletAddress: string) {
+    const fields = [this.appId, this.groupId, this.network, rewardTokenAddress, walletAddress];
+    const key = fields.map(v => v.toLowerCase()).join(':');
+    return `studio:merkle:${key}`;
+  }
+
+  @Schedule({
+    every: moment.duration('5', 'minutes').asMilliseconds(),
+  })
+  async cacheMerkleData() {
+    const data = await this.resolveMerkleData();
+
+    const cacheable = entries(data).flatMap(([rewardTokenAddress, claims]) => {
+      return entries(claims).map(([walletAddress, claim]) => {
+        return [this.getKey(rewardTokenAddress, walletAddress), claim] as [string, T];
+      });
+    });
+
+    await this.appToolkit.setManyToCache(cacheable, moment.duration('30', 'minutes').asSeconds());
+  }
+
+  async getClaim(rewardTokenAddress: string, walletAddress: string) {
+    return this.appToolkit.getFromCache<T>(this.getKey(rewardTokenAddress, walletAddress));
+  }
+}
