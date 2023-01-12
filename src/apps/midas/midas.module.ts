8a4f2843e6ee15533b2cd21d4b52d73e6522ef50
smartdevcom <45715420+smartdevcom@users.noreply.github.com>
2023-01-13 01:14:16 +1000

    feat: further Integration midas on polygon, evmos (#2107)
    * feat: further integration midas
    
    Co-authored-by: peetzweg/ <839848+peetzweg@users.noreply.github.com>

--- src/apps/midas/midas.module.ts
+++ src/apps/midas/midas.module.ts
@@ -4,9 +4,17 @@ import { AbstractApp } from '~app/app.dynamic-module';
 
 import { BinanceSmartChainMidasMarketTokenFetcher } from './binance-smart-chain/midas.market.token-fetcher';
 import { MidasContractFactory } from './contracts';
+import { EvmosMidasMarketTokenFetcher } from './evmos/midas.market.token-fetcher';
 import { MidasAppDefinition } from './midas.definition';
+import { PolygonMidasMarketTokenFetcher } from './polygon/midas.market.token-fetcher';
 
 @Module({
-  providers: [BinanceSmartChainMidasMarketTokenFetcher, MidasAppDefinition, MidasContractFactory],
+  providers: [
+    BinanceSmartChainMidasMarketTokenFetcher,
+    EvmosMidasMarketTokenFetcher,
+    MidasAppDefinition,
+    MidasContractFactory,
+    PolygonMidasMarketTokenFetcher,
+  ],
 })
 export class MidasAppModule extends AbstractApp() {}
