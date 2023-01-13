72d88e5e3f62df91efe345c2e11aa7e57110021a
Karan Shahani <karan.shahani91@gmail.com>
2023-01-13 14:26:32 -0500

    feat(euler): Fix folder of euler position presenter

--- src/apps/euler/euler.module.ts
+++ src/apps/euler/euler.module.ts
@@ -2,12 +2,12 @@ import { Module } from '@nestjs/common';
 
 import { AbstractApp } from '~app/app.dynamic-module';
 
-import { EthereumEulerPositionPresenter } from './common/euler.position-presenter';
 import { EulerTokenDefinitionsResolver } from './common/euler.token-definition-resolver';
 import { EulerContractFactory } from './contracts';
 import { EthereumEulerDTokenTokenFetcher } from './ethereum/euler.d-token.token-fetcher';
 import { EthereumEulerETokenTokenFetcher } from './ethereum/euler.e-token.token-fetcher';
 import { EthereumEulerPTokenTokenFetcher } from './ethereum/euler.p-token.token-fetcher';
+import { EthereumEulerPositionPresenter } from './ethereum/euler.position-presenter';
 
 @Module({
   providers: [
