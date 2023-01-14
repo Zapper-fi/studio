7d83a1603eac56017cc5fdd641311438cc6d7148
William Poulin <will@zapper.fi>
2023-01-14 16:04:03 -0500

    fix(illuvium): Add sIlv2 app token position (#2125)

--- src/apps/illuvium/illuvium.module.ts
+++ src/apps/illuvium/illuvium.module.ts
@@ -5,12 +5,14 @@ import { AbstractApp } from '~app/app.dynamic-module';
 import { IlluviumContractFactory } from './contracts';
 import { EthereumIlluviumFarmV2ContractPositionFetcher } from './ethereum/illuvium.farm-v2.contract-position-fetcher';
 import { EthereumIlluviumFarmContractPositionFetcher } from './ethereum/illuvium.farm.contract-position-fetcher';
+import { EthereumIlluviumSIlv2TokenFetcher } from './ethereum/illuvium.s-ilv2.token-fetcher';
 
 @Module({
   providers: [
     IlluviumContractFactory,
     EthereumIlluviumFarmContractPositionFetcher,
     EthereumIlluviumFarmV2ContractPositionFetcher,
+    EthereumIlluviumSIlv2TokenFetcher,
   ],
 })
 export class IlluviumAppModule extends AbstractApp() {}
