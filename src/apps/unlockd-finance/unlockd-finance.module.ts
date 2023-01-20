aa6326b86aed368579babf0a78ede19e7c2dd21c
William Poulin <will@zapper.fi>
2023-01-20 13:43:54 -0500

    fix(unlockd-finance): Add position presenter

--- src/apps/unlockd-finance/unlockd-finance.module.ts
+++ src/apps/unlockd-finance/unlockd-finance.module.ts
@@ -3,14 +3,16 @@ import { Module } from '@nestjs/common';
 import { AbstractApp } from '~app/app.dynamic-module';
 
 import { UnlockdFinanceContractFactory } from './contracts';
+import { EthereumUnlockdFinancePositionPresenter } from './ethereum/unlockd-finance.position-presenter';
 import { EthereumUnlockdFinanceSupplyTokenFetcher } from './ethereum/unlockd-finance.supply.token-fetcher';
 import { EthereumUnlockdFinanceVariableDebtTokenFetcher } from './ethereum/unlockd-finance.variable-debt.token-fetcher';
 
 @Module({
   providers: [
+    UnlockdFinanceContractFactory,
+    EthereumUnlockdFinancePositionPresenter,
     EthereumUnlockdFinanceSupplyTokenFetcher,
     EthereumUnlockdFinanceVariableDebtTokenFetcher,
-    UnlockdFinanceContractFactory,
   ],
 })
 export class UnlockdFinanceAppModule extends AbstractApp() {}
