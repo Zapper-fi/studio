8c23416a5e2589b6e373a0007c473e25d40c04ed
William Poulin <will@zapper.fi>
2023-01-26 16:45:42 -0500

    fix(thales): Fix vault definitions

--- src/apps/thales/optimism/thales.vault.contract-position-fetcher.ts
+++ src/apps/thales/optimism/thales.vault.contract-position-fetcher.ts
@@ -1,22 +1,23 @@
 import { Inject } from '@nestjs/common';
 import { BigNumberish } from 'ethers';
+
 import { APP_TOOLKIT, IAppToolkit } from '~app-toolkit/app-toolkit.interface';
 import { PositionTemplate } from '~app-toolkit/decorators/position-template.decorator';
+import { DefaultDataProps } from '~position/display.interface';
 import { MetaType } from '~position/position.interface';
 import { ContractPositionTemplatePositionFetcher } from '~position/template/contract-position.template.position-fetcher';
-import { GetDisplayPropsParams, GetTokenBalancesParams } from '~position/template/contract-position.template.types';
+import {
+  GetDisplayPropsParams,
+  GetTokenBalancesParams,
+  GetTokenDefinitionsParams,
+} from '~position/template/contract-position.template.types';
 
 import { Vaults, ThalesContractFactory } from '../contracts';
 
-
-let vaultAddressToName = new Map<string, string>([
-  ['0xb484027cb0c538538bad2be492714154f9196f93', 'Thales Discount'],
-  ['0x43318de9e8f65b591598f17add87ae7247649c83', 'Thales Degen Discount'],
-  ['0x6c7fd4321183b542e81bcc7de4dfb88f9dbca29f', 'Thales Safu Discount'],
-  ['0xc922f4cde42dd658a7d3ea852caf7eae47f6cecd', 'Overtime Discount'],
-  ['0xbaac5464bf6e767c9af0e8d4677c01be2065fd5f', 'Overtime Degen Discount'],
-  ['0x43d19841d818b2ccc63a8b44ce8c7def8616d98e', 'Overtime Safu Discount'],
-]);
+export type ThalesVaultDefinition = {
+  address: string;
+  name: string;
+};
 
 @PositionTemplate()
 export class OptimismThalesVaultContractPositionFetcher extends ContractPositionTemplatePositionFetcher<Vaults> {
@@ -33,35 +34,36 @@ export class OptimismThalesVaultContractPositionFetcher extends ContractPosition
     return this.contractFactory.vaults({ network: this.network, address });
   }
 
-  async getDefinitions() {
-    let definitions = new Array;
-    for (let vaultAddress of vaultAddressToName.keys()) {
-      definitions.push({ address: vaultAddress })
-    }
-    return definitions;
+  async getDefinitions(): Promise<ThalesVaultDefinition[]> {
+    return [
+      { address: '0xb484027cb0c538538bad2be492714154f9196f93', name: 'Thales Discount' },
+      { address: '0x43318de9e8f65b591598f17add87ae7247649c83', name: 'Thales Degen Discount' },
+      { address: '0x6c7fd4321183b542e81bcc7de4dfb88f9dbca29f', name: 'Thales Safu Discount' },
+      { address: '0xc922f4cde42dd658a7d3ea852caf7eae47f6cecd', name: 'Overtime Discount' },
+      { address: '0xbaac5464bf6e767c9af0e8d4677c01be2065fd5f', name: 'Overtime Degen Discount' },
+      { address: '0x43d19841d818b2ccc63a8b44ce8c7def8616d98e', name: 'Overtime Safu Discount' },
+    ];
   }
 
-  async getTokenDefinitions() {
+  async getTokenDefinitions({ contract }: GetTokenDefinitionsParams<Vaults>) {
     return [
       {
-        address: '0x8c6f28f2f1a3c87f0f938b96d27520d9751ec8d9', // sUSD
+        address: await contract.sUSD(),
         metaType: MetaType.SUPPLIED,
         network: this.network,
       },
     ];
   }
 
-  async getLabel({ contractPosition }: GetDisplayPropsParams<Vaults>) {
-    return `${vaultAddressToName.get(contractPosition.address)} Vault`;
+  async getLabel({ definition }: GetDisplayPropsParams<Vaults, DefaultDataProps, ThalesVaultDefinition>) {
+    return `${definition.name}`;
   }
 
-  async getTokenBalancesPerPosition({
-    address,
-    contract,
-  }: GetTokenBalancesParams<Vaults>): Promise<BigNumberish[]> {
+  async getTokenBalancesPerPosition({ address, contract }: GetTokenBalancesParams<Vaults>): Promise<BigNumberish[]> {
     const currentRound = await contract.round();
     const currentBalance = await contract.getBalancesPerRound(Number(currentRound), address);
-    const pendingdeposit = await contract.getBalancesPerRound(Number(currentRound) + 1, address);
-    return [Number(currentBalance) + Number(pendingdeposit)];
+    const pendingDeposit = await contract.getBalancesPerRound(Number(currentRound) + 1, address);
+
+    return [currentBalance.add(pendingDeposit)];
   }
 }
