820fdbae483333286baa6d8026a603317edb28f5
gaulois <gaulois.eth@gmail.com>
2023-01-26 08:41:17 +0100

    feat(velodrome): claimable bribes

--- src/apps/velodrome/velodrome.module.ts
+++ src/apps/velodrome/velodrome.module.ts
@@ -6,14 +6,16 @@ import { VelodromeContractFactory } from './contracts';
 import { OptimismVelodromeStakingContractPositionFetcher } from './optimism/velodrome.farm.contract-position-fetcher';
 import { OptimismVelodromePoolsTokenFetcher } from './optimism/velodrome.pool.token-fetcher';
 import { OptimismVelodromeVotingEscrowContractPositionFetcher } from './optimism/velodrome.voting-escrow.contract-position-fetcher';
+import { OptimismVelodromeBribeContractPositionFetcher } from './optimism/velodrome.bribe.contract-position-fetcher';
 
 @Module({
   providers: [
     OptimismVelodromePoolsTokenFetcher,
     OptimismVelodromeStakingContractPositionFetcher,
     OptimismVelodromeVotingEscrowContractPositionFetcher,
+    OptimismVelodromeBribeContractPositionFetcher,
 
     VelodromeContractFactory,
   ],
 })
-export class VelodromeAppModule extends AbstractApp() {}
+export class VelodromeAppModule extends AbstractApp() { }
