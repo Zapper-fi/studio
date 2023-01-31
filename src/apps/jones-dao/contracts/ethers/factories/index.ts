5fa9dd871af1b7b915835f633514e09c66f28ae1
William Poulin <will@zapper.fi>
2023-01-31 18:14:45 -0500

    feat(jones-dao): Add advanced strategies (jGLP + jUSDC)

--- src/apps/jones-dao/contracts/ethers/factories/index.ts
+++ src/apps/jones-dao/contracts/ethers/factories/index.ts
@@ -5,4 +5,6 @@ export { JonesMetavault__factory } from './JonesMetavault__factory';
 export { JonesMillinerV2__factory } from './JonesMillinerV2__factory';
 export { JonesStakingRewards__factory } from './JonesStakingRewards__factory';
 export { JonesStakingRewardsFactory__factory } from './JonesStakingRewardsFactory__factory';
+export { JonesStrategyToken__factory } from './JonesStrategyToken__factory';
+export { JonesStrategyVault__factory } from './JonesStrategyVault__factory';
 export { JonesVault__factory } from './JonesVault__factory';
