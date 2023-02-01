abffeeca34e32cbd811c0a11d3f583c1d817c282
William Poulin <will@zapper.fi>
2023-02-01 11:52:26 -0500

    fix(velodrome): Filter out not supported underlying tokens on bribe contract position (#2227)

--- src/apps/velodrome/optimism/velodrome.bribe.contract-position-fetcher.ts
+++ src/apps/velodrome/optimism/velodrome.bribe.contract-position-fetcher.ts
@@ -1,136 +1,153 @@
-import axios from 'axios';
 import { Inject, NotImplementedException } from '@nestjs/common';
-import { MetaType } from '~position/position.interface';
+import axios from 'axios';
 import _, { range, sumBy } from 'lodash';
-import { drillBalance } from '~app-toolkit/helpers/drill-balance.helper';
+
 import { APP_TOOLKIT, IAppToolkit } from '~app-toolkit/app-toolkit.interface';
 import { PositionTemplate } from '~app-toolkit/decorators/position-template.decorator';
-import { ContractPositionTemplatePositionFetcher } from '~position/template/contract-position.template.position-fetcher';
+import { drillBalance } from '~app-toolkit/helpers/drill-balance.helper';
+import { DefaultDataProps } from '~position/display.interface';
 import { ContractPositionBalance } from '~position/position-balance.interface';
-import {
-    GetDisplayPropsParams,
-} from '~position/template/contract-position.template.types';
+import { MetaType } from '~position/position.interface';
+import { ContractPositionTemplatePositionFetcher } from '~position/template/contract-position.template.position-fetcher';
+import { GetDisplayPropsParams, GetTokenDefinitionsParams } from '~position/template/contract-position.template.types';
 
 import { VelodromeContractFactory, VelodromeBribe } from '../contracts';
-import { DefaultDataProps } from '~position/display.interface';
 
 export type VelodromeBribeDefinition = {
-    address: string;
-    name: string;
+  address: string;
+  name: string;
 };
 
 interface VelodromeApiPairData {
-    gauge: GaugeData;
-    symbol: string;
-    address: string;
+  gauge: GaugeData;
+  symbol: string;
+  address: string;
 }
 
 interface GaugeData {
-    wrapped_bribe_address: string;
+  wrapped_bribe_address: string;
 }
+
+// Pools which contains optiDoge
+const notSupportedPoolTokenAddresses = [
+  '0xce9accfbb25eddce91845c3a7c3d1613d1d7081f',
+  '0x22bc9c46b72b2e92f0539d18d1f2273ee0e7f3fc',
+  '0x763cbb83cb837114fded11d562bcbf3d58a682ac',
+  '0x4ecafe1a9798e4e874f5df212377615ae357b566',
+  '0x415c6f07757ab86d902eeca5055fcb3ca974b880',
+  '0xb22cd502a49e90e4e6200921a41a7f065e9a1b9e',
+  '0x6a938eddf290ca29a0865ae613d52c160ce4554b',
+  '0xc23ab0245e23c22bf306a62dc9fe2958cdcf37b0',
+  '0x103f2556cb47eaf2161e700f15716f65711ed983',
+  '0xce57d093bf2ce0bd02e83c364c9bd766be2212b2',
+];
 
 @PositionTemplate()
 export class OptimismVelodromeBribeContractPositionFetcher extends ContractPositionTemplatePositionFetcher<VelodromeBribe> {
-    constructor(
-        @Inject(APP_TOOLKIT) protected readonly appToolkit: IAppToolkit,
-        @Inject(VelodromeContractFactory) protected readonly contractFactory: VelodromeContractFactory,
-    ) {
-        super(appToolkit);
-    }
-    veTokenAddress = '0x9c7305eb78a432ced5c4d14cac27e8ed569a2e26';
-    veVoteAddress = '0x09236cff45047dbee6b921e00704bed6d6b8cf7e';
-    groupLabel = 'Bribe';
+  constructor(
+    @Inject(APP_TOOLKIT) protected readonly appToolkit: IAppToolkit,
+    @Inject(VelodromeContractFactory) protected readonly contractFactory: VelodromeContractFactory,
+  ) {
+    super(appToolkit);
+  }
+  veTokenAddress = '0x9c7305eb78a432ced5c4d14cac27e8ed569a2e26';
+  veVoteAddress = '0x09236cff45047dbee6b921e00704bed6d6b8cf7e';
+  groupLabel = 'Bribe';
 
-    getContract(address: string): VelodromeBribe {
-        return this.contractFactory.velodromeBribe({ address, network: this.network });
-    }
+  getContract(address: string): VelodromeBribe {
+    return this.contractFactory.velodromeBribe({ address, network: this.network });
+  }
 
-    async getDefinitions(): Promise<VelodromeBribeDefinition[]> {
-        const { data } = await axios.get<{ data: VelodromeApiPairData[] }>('https://api.velodrome.finance/api/v1/pairs');
-        var definitions = new Array;
-        data.data.filter(v => !!v).filter(v => !!v.gauge).forEach(pool => {
-            const wBribeAddress = pool.gauge.wrapped_bribe_address;
-            if (wBribeAddress != null) {
-                definitions.push({ address: wBribeAddress, name: pool.symbol });
-            }
-        }
-        );
-        return definitions;
-    }
+  async getDefinitions(): Promise<VelodromeBribeDefinition[]> {
+    const { data } = await axios.get<{ data: VelodromeApiPairData[] }>('https://api.velodrome.finance/api/v1/pairs');
+    const definitions = data.data
+      .filter(v => !!v)
+      .filter(v => !!v.gauge)
+      .map(pool => {
+        const wBribeAddress = pool.gauge.wrapped_bribe_address;
+        return wBribeAddress != null ? { address: wBribeAddress, name: pool.symbol } : null;
+      });
 
-    async getTokenDefinitions({ contract }) {
-        const numRewards = Number(await contract.rewardsListLength());
-        const bribeTokens = await Promise.all(range(numRewards).map(async n => await contract.rewards(n)));
-        const baseTokens = await this.appToolkit.getBaseTokenPrices(this.network);
-        var tokenDefinitions = new Array();
-        for (let token of bribeTokens) {
-            const tokenFound = baseTokens.find(p => p.address === token);
-            if (tokenFound) {
-                tokenDefinitions.push({
-                    metaType: MetaType.CLAIMABLE,
-                    address: token,
-                    network: this.network,
-                });
-            }
-        }
-        return tokenDefinitions;
-    }
+    return _.compact(definitions);
+  }
 
-    async getLabel({ definition }: GetDisplayPropsParams<VelodromeBribe, DefaultDataProps, VelodromeBribeDefinition>): Promise<string> {
-        return `${definition.name} Bribe`;
-    }
+  async getTokenDefinitions({ contract }: GetTokenDefinitionsParams<VelodromeBribe>) {
+    const numRewards = Number(await contract.rewardsListLength());
+    const bribeTokens = await Promise.all(range(numRewards).map(async n => await contract.rewards(n)));
+    const tokenDefinitions = bribeTokens.map(address => {
+      if (notSupportedPoolTokenAddresses.includes(address)) return null;
 
-    getTokenBalancesPerPosition(): never {
-        throw new NotImplementedException();
-    }
+      return {
+        metaType: MetaType.CLAIMABLE,
+        address,
+        network: this.network,
+      };
+    });
 
-    async getBalances(address: string): Promise<ContractPositionBalance<DefaultDataProps>[]> {
-        const multicall = this.appToolkit.getMulticall(this.network);
-        const escrow = multicall.wrap(this.contractFactory.velodromeVe({ address: this.veTokenAddress, network: this.network }));
-        const veCount = Number(await escrow.balanceOf(address));
-        if (veCount === 0) {
-            return [];
-        };
+    return _.compact(tokenDefinitions);
+  }
 
-        const veTokenIds = await Promise.all(range(veCount).map(async i => {
-            const tokenId = await escrow.tokenOfOwnerByIndex(address, i);
-            return tokenId;
-        }))
+  async getLabel({
+    definition,
+  }: GetDisplayPropsParams<VelodromeBribe, DefaultDataProps, VelodromeBribeDefinition>): Promise<string> {
+    return `${definition.name} Bribe`;
+  }
 
-        const contractPositions = await this.appToolkit.getAppContractPositions({
-            appId: this.appId,
-            network: this.network,
-            groupIds: [this.groupId],
-        });
+  getTokenBalancesPerPosition(): never {
+    throw new NotImplementedException();
+  }
 
-        const balances = await Promise.all(contractPositions.map(async contractPosition => {
-            const bribeTokens = contractPosition.tokens;
-            const bribeContract = multicall.wrap(this.getContract(contractPosition.address));
+  async getBalances(address: string): Promise<ContractPositionBalance<DefaultDataProps>[]> {
+    const multicall = this.appToolkit.getMulticall(this.network);
+    const escrow = multicall.wrap(
+      this.contractFactory.velodromeVe({ address: this.veTokenAddress, network: this.network }),
+    );
+    const veCount = Number(await escrow.balanceOf(address));
+    if (veCount === 0) return [];
 
-            var tokenBalances = new Array;
-            for (let bribeToken of bribeTokens) {
-                var tokenBalance = 0;
-                for (let veTokenId of veTokenIds) {
-                    const balance = await multicall.wrap(bribeContract).earned(bribeToken.address, veTokenId);
-                    tokenBalance += Number(balance);
-                }
-                tokenBalances.push(tokenBalance);
-            }
+    const veTokenIds = await Promise.all(
+      range(veCount).map(async i => {
+        const tokenId = await escrow.tokenOfOwnerByIndex(address, i);
+        return tokenId;
+      }),
+    );
 
-            const nonZeroBalancesRaw = tokenBalances.filter(balance => balance > 0);
-            const allTokens = contractPosition.tokens.map((cp, idx) =>
-                drillBalance(cp, nonZeroBalancesRaw[idx]?.toString() ?? '0'),
+    const contractPositions = await this.appToolkit.getAppContractPositions({
+      appId: this.appId,
+      network: this.network,
+      groupIds: [this.groupId],
+    });
+
+    const balances = await Promise.all(
+      contractPositions.map(async contractPosition => {
+        const bribeTokens = contractPosition.tokens;
+        const bribeContract = multicall.wrap(this.getContract(contractPosition.address));
+
+        const tokenBalancesRaw = await Promise.all(
+          bribeTokens.map(async bribeToken => {
+            return Promise.all(
+              veTokenIds.map(async veTokenId => {
+                return await multicall.wrap(bribeContract).earned(bribeToken.address, veTokenId);
+              }),
             );
-
-            const tokens = allTokens.filter(v => Math.abs(v.balanceUSD) > 0.01);
-            const balanceUSD = sumBy(tokens, t => t.balanceUSD);
-
-            const balance: ContractPositionBalance = { ...contractPosition, tokens, balanceUSD };
-
-            return balance;
-        }),
+          }),
         );
-        return _.compact(balances);
-    }
-}
 
+        const tokenBalances = tokenBalancesRaw.map(x => Number(x)).flat();
+
+        const nonZeroBalancesRaw = tokenBalances.filter(balance => balance > 0);
+        const allTokens = contractPosition.tokens.map((cp, idx) =>
+          drillBalance(cp, nonZeroBalancesRaw[idx]?.toString() ?? '0'),
+        );
+
+        const tokens = allTokens.filter(v => Math.abs(v.balanceUSD) > 0.01);
+        const balanceUSD = sumBy(tokens, t => t.balanceUSD);
+
+        const balance: ContractPositionBalance = { ...contractPosition, tokens, balanceUSD };
+
+        return balance;
+      }),
+    );
+    return _.compact(balances);
+  }
+}
