615dfe18e6a3bb6e58225130a228043ecdd2c3cd
ALECKS <0xalecks@gmail.com>
2023-02-09 10:26:12 -0500

    feat(redacted-cartel): add new fetcher

---
+++ src/apps/redacted-cartel/ethereum/redacted-cartel.revenue-lock.contract-position-fetcher.ts
@@ -0,0 +1,86 @@
+import { Inject } from '@nestjs/common';
+import axios from 'axios';
+import { BigNumberish, Contract } from 'ethers';
+
+import { APP_TOOLKIT, IAppToolkit } from '~app-toolkit/app-toolkit.interface';
+import { ZERO_ADDRESS } from '~app-toolkit/constants/address';
+import { PositionTemplate } from '~app-toolkit/decorators/position-template.decorator';
+import { getLabelFromToken } from '~app-toolkit/helpers/presentation/image.present';
+import { DefaultDataProps } from '~position/display.interface';
+import { MetaType } from '~position/position.interface';
+import { isSupplied } from '~position/position.utils';
+import { ContractPositionTemplatePositionFetcher } from '~position/template/contract-position.template.position-fetcher';
+import {
+  GetDefinitionsParams,
+  DefaultContractPositionDefinition,
+  GetTokenDefinitionsParams,
+  UnderlyingTokenDefinition,
+  GetDisplayPropsParams,
+  GetTokenBalancesParams,
+} from '~position/template/contract-position.template.types';
+
+import { RedactedCartelContractFactory, RedactedRevenueLock } from '../contracts';
+
+@PositionTemplate()
+export class EthereumRedactedCartelRevenueLockContractPositionFetcher extends ContractPositionTemplatePositionFetcher<RedactedRevenueLock> {
+  groupLabel = 'Revenue Lock';
+  rlBTRFLY = '0x742b70151cd3bc7ab598aaff1d54b90c3ebc6027';
+  BTRFLYv2 = '0xc55126051b22ebb829d00368f4b12bde432de5da';
+
+  constructor(
+    @Inject(APP_TOOLKIT) protected readonly appToolkit: IAppToolkit,
+    @Inject(RedactedCartelContractFactory)
+    protected readonly contractFactory: RedactedCartelContractFactory,
+  ) {
+    super(appToolkit);
+  }
+
+  getContract(address: string): RedactedRevenueLock {
+    return this.contractFactory.redactedRevenueLock({ address, network: this.network });
+  }
+
+  async getDefinitions(_params: GetDefinitionsParams): Promise<DefaultContractPositionDefinition[]> {
+    return [{ address: this.rlBTRFLY }];
+  }
+
+  async getTokenDefinitions(
+    _params: GetTokenDefinitionsParams<Contract, DefaultContractPositionDefinition>,
+  ): Promise<UnderlyingTokenDefinition[] | null> {
+    return [
+      {
+        metaType: MetaType.SUPPLIED,
+        address: this.BTRFLYv2,
+        network: this.network,
+      },
+      {
+        metaType: MetaType.CLAIMABLE,
+        address: this.BTRFLYv2,
+        network: this.network,
+      },
+      {
+        metaType: MetaType.CLAIMABLE,
+        address: ZERO_ADDRESS,
+        network: this.network,
+      },
+    ];
+  }
+
+  async getLabel({
+    contractPosition,
+  }: GetDisplayPropsParams<Contract, DefaultDataProps, DefaultContractPositionDefinition>): Promise<string> {
+    const suppliedToken = contractPosition.tokens.find(isSupplied)!;
+    return `Voting Escrow ${getLabelFromToken(suppliedToken)}`;
+  }
+
+  async getTokenBalancesPerPosition({
+    address,
+    contract,
+  }: GetTokenBalancesParams<Contract, DefaultDataProps>): Promise<BigNumberish[]> {
+    const lockedBalances = await contract.lockedBalances(address);
+    const { data } = await axios.get(`https://app.redacted.finance/api/rewards/1/${address}`, { headers: {} });
+    const ethRewards = data?.data?.find(t => t.symbol === 'ETH');
+    const btrRewards = data?.data?.find(t => t.symbol === 'BTRFLY');
+
+    return [lockedBalances.total, btrRewards?.claimable, ethRewards?.claimable];
+  }
+}
