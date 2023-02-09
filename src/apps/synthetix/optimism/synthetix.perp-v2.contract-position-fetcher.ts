31efe575ca54213e80334ad43099aa94f942df5d
gaulois <gaulois.eth@gmail.com>
2023-02-09 17:42:12 +0100

    feat(synthetix): perp v2

--- src/apps/synthetix/optimism/synthetix.perp-v2.contract-position-fetcher.ts
+++ src/apps/synthetix/optimism/synthetix.perp-v2.contract-position-fetcher.ts
@@ -22,14 +22,37 @@ export class OptimismSynthetixPerpV2ContractPositionFetcher extends ContractPosi
   ) {
     super(appToolkit);
   }
-  groupLabel = 'Perp v2';
+  groupLabel = 'Isolated';
 
   getContract(address: string): SynthetixPerpV2 {
     return this.contractFactory.synthetixPerpV2({ address, network: this.network });
   }
 
   async getDefinitions(): Promise<DefaultContractPositionDefinition[]> {
-    return [{ address: '0xa1245e5b52555e40aa8a3ffb95c5c426c7c7ef12' }];
+    return [{ address: '0x8c14db69b1778c7bbb0683b2dea21f79b9b5f059' },
+    { address: '0x74dccf74644485a6920d0cfe1a4d34b2f4216181' },
+    { address: '0x407e5a7548869ce520354d8eca127754625cc9c8' },
+    { address: '0x002089829127a5769db0d53372d5d53a4d1a87a2' },
+    { address: '0x69d255473d0d15dc087cbf4962a65839aa28a2c5' },
+    { address: '0x637340ce915a6e2eee7c02521ac334c30c219d0d' },
+    { address: '0x857d91d6c63892b383cf10cd15285604ff9976b8' },
+    { address: '0xb0a1d2c68bf4d0980402dc220ca6ddeb8dbfbc56' },
+    { address: '0xea46a4dfa7d2767ff4bae2b76f5c6bd80057c723' },
+    { address: '0x6aa31707bd6acf24063a806570160978e62752a0' },
+    { address: '0x9363c080ca0b16ead12fd33aac65c8d0214e9d6d' },
+    { address: '0x156e18b355b36ff3b9cb36bcaaf3155d95d3319a' },
+    { address: '0x0965efeb0579c9bf8d15a77a4f14ec623421d902' },
+    { address: '0x0868d8421b6c4f13b392ea4b72f8012884c45b74' },
+    { address: '0x1fb664858da319b68fc5f6d47dd6ca0d994055a2' },
+    { address: '0xcd077eab8efbcb94aa04f2055d7a9216f23697a6' },
+    { address: '0xf23c5ec62ec4398302efd84587eb8ba26f21b155' },
+    { address: '0x1eaed534ee8d25da73a4e21cde96e4fca9c46187' },
+    { address: '0x423ddc17a01e3e44a0bd125ec2f645b8b9ad3259' },
+    { address: '0x6ee44d9e0f868833a5543bcabc3bd1a7d843edb8' },
+    { address: '0xda3a5e9502b23eaedc8cc048998893013e09787d' },
+    { address: '0x9ab1f9b25b312f162e815a027b4805516401093e' },
+    { address: '0xe05637e338b11640c51766304878b08181463413' }
+    ];
   }
 
   async getTokenDefinitions() {
