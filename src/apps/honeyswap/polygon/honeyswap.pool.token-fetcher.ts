2388909bf179e6e034690a146d926cb0249f2d99
William Poulin <will@zapper.fi>
2023-02-15 17:19:27 -0500

    fix(uniswap-v2): Add condition on skipVolume (#2307)
    * fix(honeyswap): Refactor pool token fetcher using default template
    
    * fix(uniswap-v2): Add condition on skipVolume
    
    * fix(honeyswap): Add UniswapV2ContractFactory to honeyswap's module

--- src/apps/honeyswap/polygon/honeyswap.pool.token-fetcher.ts
+++ src/apps/honeyswap/polygon/honeyswap.pool.token-fetcher.ts
@@ -1,38 +1,10 @@
-import { Inject } from '@nestjs/common';
-import { BigNumberish } from 'ethers';
-
-import { APP_TOOLKIT, IAppToolkit } from '~app-toolkit/app-toolkit.interface';
 import { PositionTemplate } from '~app-toolkit/decorators/position-template.decorator';
-import { UniswapV2PoolSubgraphTemplateTokenFetcher } from '~apps/uniswap-v2/common/uniswap-v2.pool.subgraph.template.token-fetcher';
-
-import { HoneyswapContractFactory, HoneyswapPair } from '../contracts';
+import { UniswapV2DefaultPoolSubgraphTemplateTokenFetcher } from '~apps/uniswap-v2/common/uniswap-v2.default.subgraph.template.token-fetcher';
 
 @PositionTemplate()
-export class PolygonHoneyswapPoolTokenFetcher extends UniswapV2PoolSubgraphTemplateTokenFetcher<HoneyswapPair> {
-  subgraphUrl = 'https://api.thegraph.com/subgraphs/name/1hive/honeyswap-polygon';
-  factoryAddress = '0x03daa61d8007443a6584e3d8f85105096543c19c';
+export class PolygonHoneyswapPoolTokenFetcher extends UniswapV2DefaultPoolSubgraphTemplateTokenFetcher {
   groupLabel = 'Pools';
 
-  constructor(
-    @Inject(APP_TOOLKIT) protected readonly appToolkit: IAppToolkit,
-    @Inject(HoneyswapContractFactory) protected readonly contractFactory: HoneyswapContractFactory,
-  ) {
-    super(appToolkit);
-  }
-
-  getPoolTokenContract(address: string): HoneyswapPair {
-    return this.contractFactory.honeyswapPair({ address, network: this.network });
-  }
-
-  getPoolToken0(contract: HoneyswapPair): Promise<string> {
-    return contract.token0();
-  }
-
-  getPoolToken1(contract: HoneyswapPair): Promise<string> {
-    return contract.token1();
-  }
-
-  getPoolReserves(contract: HoneyswapPair): Promise<BigNumberish[]> {
-    return contract.getReserves();
-  }
+  subgraphUrl = 'https://api.thegraph.com/subgraphs/name/1hive/honeyswap-polygon';
+  factoryAddress = '0x03daa61d8007443a6584e3d8f85105096543c19c';
 }
