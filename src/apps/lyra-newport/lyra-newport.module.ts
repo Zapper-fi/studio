169972bf4d7c04644ac937a23d607343fc0ac772
paulvaden <pbdonut@gmail.com>
2023-02-16 19:13:00 +0000

    Remove duplicate eth contracts

--- src/apps/lyra-newport/lyra-newport.module.ts
+++ src/apps/lyra-newport/lyra-newport.module.ts
@@ -3,8 +3,6 @@ import { Module } from '@nestjs/common';
 import { AbstractApp } from '~app/app.dynamic-module';
 
 import { LyraNewportContractFactory } from './contracts';
-import { EthereumLyraNewportStkLyraTokenFetcher } from './ethereum/lyra-newport.stk-lyra.token-fetcher';
-import { EthereumLyraNewportStakingContractPositionFetcher } from './ethereum/lyra-newport.staking.contract-position-fetcher';
 import { ArbitrumLyraNewportOptionsContractPositionFetcher } from './arbitrum/lyra-newport.options.contract-position-fetcher';
 import { ArbitrumLyraNewportPoolTokenFetcher } from './arbitrum/lyra-newport.pool.token-fetcher';
 
@@ -13,8 +11,6 @@ import { ArbitrumLyraNewportPoolTokenFetcher } from './arbitrum/lyra-newport.poo
     LyraNewportContractFactory,
     ArbitrumLyraNewportOptionsContractPositionFetcher,
     ArbitrumLyraNewportPoolTokenFetcher,
-    EthereumLyraNewportStkLyraTokenFetcher,
-    EthereumLyraNewportStakingContractPositionFetcher,
   ],
 })
 export class LyraNewportAppModule extends AbstractApp() { }
