12483946fcbe38c860e87e3484390e8c4d140fe3
0xTaiga <94946789+0xTaiga@users.noreply.github.com>
2023-02-16 10:35:38 -0500

    feat(vendor): Added mainnet support for vendor finance (#2311)

--- src/apps/vendor-finance/graphql/constants.ts
+++ src/apps/vendor-finance/graphql/constants.ts
@@ -1,1 +1,2 @@
-export const VENDOR_GRAPH_URL = 'https://api.thegraph.com/subgraphs/name/0xtaiga/vendor-finance';
+export const VENDOR_GRAPH_URL_ARBI = 'https://api.thegraph.com/subgraphs/name/0xtaiga/vendor-finance';
+export const VENDOR_GRAPH_URL_MAINNET = 'https://api.thegraph.com/subgraphs/name/0xtaiga/vendor-finance-mainnet';
