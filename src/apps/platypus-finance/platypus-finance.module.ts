8533c191b08afbeecc8a95bbf3b8b976b5284c23
William Poulin <will@zapper.fi>
2023-02-16 16:48:37 -0500

    fix(platypus-finance): Temporary pausing the app every position related to their pools

--- src/apps/platypus-finance/platypus-finance.module.ts
+++ src/apps/platypus-finance/platypus-finance.module.ts
@@ -2,9 +2,9 @@ import { Module } from '@nestjs/common';
 
 import { AbstractApp } from '~app/app.dynamic-module';
 
-import { AvalanchePlatypusFinanceChefContractPositionFetcher } from './avalanche/platypus-finance.chef.contract-position-fetcher';
-import { AvalanchePlatypusFinanceFarmContractPositionFetcher } from './avalanche/platypus-finance.farm.contract-position-fetcher';
-import { AvalanchePlatypusFinancePoolTokenFetcher } from './avalanche/platypus-finance.pool.token-fetcher';
+//import { AvalanchePlatypusFinanceChefContractPositionFetcher } from './avalanche/platypus-finance.chef.contract-position-fetcher';
+//import { AvalanchePlatypusFinanceFarmContractPositionFetcher } from './avalanche/platypus-finance.farm.contract-position-fetcher';
+//import { AvalanchePlatypusFinancePoolTokenFetcher } from './avalanche/platypus-finance.pool.token-fetcher';
 import { AvalanchePlatypusFinanceVotingEscrowContractPositionFetcher } from './avalanche/platypus-finance.voting-escrow.contract-position-fetcher';
 import { PlatypusFinanceContractFactory } from './contracts';
 
@@ -12,9 +12,9 @@ import { PlatypusFinanceContractFactory } from './contracts';
   providers: [
     PlatypusFinanceContractFactory,
     // Avalanche
-    AvalanchePlatypusFinancePoolTokenFetcher,
-    AvalanchePlatypusFinanceFarmContractPositionFetcher,
-    AvalanchePlatypusFinanceChefContractPositionFetcher,
+    //AvalanchePlatypusFinancePoolTokenFetcher,
+    //AvalanchePlatypusFinanceFarmContractPositionFetcher,
+    //AvalanchePlatypusFinanceChefContractPositionFetcher,
     AvalanchePlatypusFinanceVotingEscrowContractPositionFetcher,
   ],
 })
