c6c0513fc4e036d1f74b8ebf4e7674691de5913d
0xdapper <0xdapp@protonmail.com>
2023-02-28 15:39:31 +0000

    feat(gearbox): add rewards

--- src/apps/gearbox/gearbox.module.ts
+++ src/apps/gearbox/gearbox.module.ts
@@ -6,13 +6,17 @@ import { GearboxContractFactory } from './contracts';
 import { EthereumGearboxCreditAccountsContractPositionFetcher } from './ethereum/gearbox.credit-accounts.contract-position-fetcher';
 import { EthereumGearboxLendingTokenFetcher } from './ethereum/gearbox.lending.token-fetcher';
 import { EthereumGearboxPhantomTokenFetcher } from './ethereum/gearbox.phantom.token-fetcher';
+import { EthereumGearboxRewardsPositionFetcher } from './ethereum/gearbox.rewards.contract-position-fetcher';
+import { EthereumGearboxRewardsMerkleCache } from './ethereum/gearbox.rewards.merkle-cache';
 
 @Module({
   providers: [
-    GearboxContractFactory,
     EthereumGearboxCreditAccountsContractPositionFetcher,
     EthereumGearboxLendingTokenFetcher,
     EthereumGearboxPhantomTokenFetcher,
+    EthereumGearboxRewardsPositionFetcher,
+    GearboxContractFactory,
+    EthereumGearboxRewardsMerkleCache,
   ],
 })
 export class GearboxAppModule extends AbstractApp() {}
