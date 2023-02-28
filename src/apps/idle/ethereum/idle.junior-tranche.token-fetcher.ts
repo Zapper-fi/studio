f566d74717727c1d8b297ea4874121bda427f2cf
William Poulin <will@zapper.fi>
2023-02-28 15:23:31 -0500

    fix(idle): Testing renaming group + cleanup

--- src/apps/idle/ethereum/idle.junior-tranche.token-fetcher.ts
+++ src/apps/idle/ethereum/idle.junior-tranche.token-fetcher.ts
@@ -2,12 +2,6 @@ import { PositionTemplate } from '~app-toolkit/decorators/position-template.deco
 
 import { EthereumIdleTranchesPoolTokenFetcher } from '../common/idle.tranche.token-fetcher';
 
-export type IdleAppTokenDefinition = {
-  address: string;
-  cdoAddress: string;
-  underlyingTokenAddress: string;
-};
-
 @PositionTemplate()
 export class EthereumIdleJuniorTranchesPoolTokenFetcher extends EthereumIdleTranchesPoolTokenFetcher {
   groupLabel = 'Junior Tranches';
