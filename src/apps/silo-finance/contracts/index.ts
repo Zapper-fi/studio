7bf7806cb90d66af7ba2b010679f9cda24e4153c
pragneshmangukiya <83950378+pragneshmangukiya@users.noreply.github.com>
2023-03-06 21:00:21 +0530

    feat(silo-finance): Integrate new app (#2383)
    * feat: add silo-finance
    
    * feat(silo-finance): add logo
    
    * fix(silo-finance): rename to logo.png
    
    ---------
    
    Co-authored-by: William Poulin <will@zapper.fi>

---
+++ src/apps/silo-finance/contracts/index.ts
@@ -0,0 +1,27 @@
+import { Injectable, Inject } from '@nestjs/common';
+
+import { IAppToolkit, APP_TOOLKIT } from '~app-toolkit/app-toolkit.interface';
+import { ContractFactory } from '~contract/contracts';
+import { Network } from '~types/network.interface';
+
+import { Silo__factory, SiloLens__factory } from './ethers';
+
+// eslint-disable-next-line
+type ContractOpts = { address: string; network: Network };
+
+@Injectable()
+export class SiloFinanceContractFactory extends ContractFactory {
+  constructor(@Inject(APP_TOOLKIT) protected readonly appToolkit: IAppToolkit) {
+    super((network: Network) => appToolkit.getNetworkProvider(network));
+  }
+
+  silo({ address, network }: ContractOpts) {
+    return Silo__factory.connect(address, this.appToolkit.getNetworkProvider(network));
+  }
+  siloLens({ address, network }: ContractOpts) {
+    return SiloLens__factory.connect(address, this.appToolkit.getNetworkProvider(network));
+  }
+}
+
+export type { Silo } from './ethers';
+export type { SiloLens } from './ethers';
