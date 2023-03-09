369ef09d0b40496bfd8c93aa06a85ebadac24c9e
William Poulin <will@zapper.fi>
2023-03-09 16:16:33 -0500

    fix(silo-finance): Extract external API calls into a resolver

--- src/apps/silo-finance/contracts/index.ts
+++ src/apps/silo-finance/contracts/index.ts
@@ -4,7 +4,7 @@ import { IAppToolkit, APP_TOOLKIT } from '~app-toolkit/app-toolkit.interface';
 import { ContractFactory } from '~contract/contracts';
 import { Network } from '~types/network.interface';
 
-import { Silo__factory, SiloLens__factory } from './ethers';
+import { Silo__factory, SiloLens__factory, SiloMarketAsset__factory } from './ethers';
 
 // eslint-disable-next-line
 type ContractOpts = { address: string; network: Network };
@@ -21,7 +21,11 @@ export class SiloFinanceContractFactory extends ContractFactory {
   siloLens({ address, network }: ContractOpts) {
     return SiloLens__factory.connect(address, this.appToolkit.getNetworkProvider(network));
   }
+  siloMarketAsset({ address, network }: ContractOpts) {
+    return SiloMarketAsset__factory.connect(address, this.appToolkit.getNetworkProvider(network));
+  }
 }
 
 export type { Silo } from './ethers';
 export type { SiloLens } from './ethers';
+export type { SiloMarketAsset } from './ethers';
