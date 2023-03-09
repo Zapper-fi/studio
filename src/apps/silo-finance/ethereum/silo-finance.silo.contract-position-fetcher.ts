e5a7c508f374b800db48f89e0264985bb523eeac
William Poulin <will@zapper.fi>
2023-03-09 16:27:58 -0500

    fix(silo-finance): Extract external API calls into a resolver (#2404)

--- src/apps/silo-finance/ethereum/silo-finance.silo.contract-position-fetcher.ts
+++ src/apps/silo-finance/ethereum/silo-finance.silo.contract-position-fetcher.ts
@@ -1,19 +1,10 @@
-import { Network } from '~types';
+import { PositionTemplate } from '~app-toolkit/decorators/position-template.decorator';
 
 import { SiloFinanceSiloContractPositionFetcher } from '../common/silo-finance.silo.contract-position-fetcher';
-import { SiloLens } from '../contracts';
 
+@PositionTemplate()
 export class EthereumSiloFinanceSiloContractPositionFetcher extends SiloFinanceSiloContractPositionFetcher {
-  network = Network.ETHEREUM_MAINNET;
+  groupLabel = 'Silos';
 
-  getSubgraphUrl(): string {
-    return 'https://gateway.thegraph.com/api/fbf06f34dad21c4df6a9e1f647ba1d16/deployments/id/QmRMtCkaYsizfmoavcE1ULwc2DkG1GZjXDHTwHjXAAH9sp';
-  }
-
-  getSiloLens(): SiloLens {
-    return this.siloFinanceContractFactory.siloLens({
-      network: this.network,
-      address: '0xf12c3758c1ec393704f0db8537ef7f57368d92ea',
-    });
-  }
+  siloLensAddress = '0xf12c3758c1ec393704f0db8537ef7f57368d92ea';
 }
--- src/apps/silo-finance/silo-finance.module.ts
+++ src/apps/silo-finance/silo-finance.module.ts
@@ -2,15 +2,23 @@ import { Module } from '@nestjs/common';
 
 import { AbstractApp } from '~app/app.dynamic-module';
 
+//import { ArbitrumMarketAssetsTokenFetcher } from './arbitrum/silo-finance.market-asset.token-fetcher';
 import { ArbitrumSiloFinanceSiloContractPositionFetcher } from './arbitrum/silo-finance.silo.contract-position-fetcher';
+import { SiloFinanceDefinitionResolver } from './common/silo-finance.definition-resolver';
 import { SiloFinanceContractFactory } from './contracts';
+//import { EthereumMarketAssetsTokenFetcher } from './ethereum/silo-finance.market-asset.token-fetcher';
 import { EthereumSiloFinanceSiloContractPositionFetcher } from './ethereum/silo-finance.silo.contract-position-fetcher';
 
 @Module({
   providers: [
-    EthereumSiloFinanceSiloContractPositionFetcher,
-    ArbitrumSiloFinanceSiloContractPositionFetcher,
     SiloFinanceContractFactory,
+    SiloFinanceDefinitionResolver,
+    // Arbitrum
+    ArbitrumSiloFinanceSiloContractPositionFetcher,
+    //ArbitrumMarketAssetsTokenFetcher,
+    // Ethereum
+    EthereumSiloFinanceSiloContractPositionFetcher,
+    //EthereumMarketAssetsTokenFetcher,
   ],
 })
 export class SiloFinanceAppModule extends AbstractApp() {}
