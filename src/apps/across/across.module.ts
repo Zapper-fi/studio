96658410d4fe1d3dbad6dc5b09b7697e725a67e7
William Poulin <will@zapper.fi>
2023-03-13 17:06:36 -0400

    feat(across): Add Across staking/rewards positions (#2420)

--- src/apps/across/across.module.ts
+++ src/apps/across/across.module.ts
@@ -5,8 +5,14 @@ import { AbstractApp } from '~app/app.dynamic-module';
 import { AcrossContractFactory } from './contracts';
 import { EthereumAcrossPoolV1TokenFetcher } from './ethereum/across.pool-v1.token-fetcher';
 import { EthereumAcrossPoolV2TokenFetcher } from './ethereum/across.pool-v2.token-fetcher';
+import { EthereumStakingContractPositionFetcher } from './ethereum/across.staking.contract-position-fetcher';
 
 @Module({
-  providers: [AcrossContractFactory, EthereumAcrossPoolV1TokenFetcher, EthereumAcrossPoolV2TokenFetcher],
+  providers: [
+    AcrossContractFactory,
+    EthereumAcrossPoolV1TokenFetcher,
+    EthereumAcrossPoolV2TokenFetcher,
+    EthereumStakingContractPositionFetcher,
+  ],
 })
 export class AcrossAppModule extends AbstractApp() {}
