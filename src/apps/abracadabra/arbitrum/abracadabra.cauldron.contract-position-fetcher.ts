ba34ba668e3512b6d7ad87fd3f59fdf92afdc39b
0xmDreamy <0xmDreamy@proton.me>
2023-03-16 21:52:02 +0100

    refactor(abracadabra): Split getTokenDefinitions for cauldrons into three cauldron types

--- src/apps/abracadabra/arbitrum/abracadabra.cauldron.contract-position-fetcher.ts
+++ src/apps/abracadabra/arbitrum/abracadabra.cauldron.contract-position-fetcher.ts
@@ -2,10 +2,11 @@ import { PositionTemplate } from '~app-toolkit/decorators/position-template.deco
 
 import { AbracadabraCauldronContractPositionFetcher } from '../common/abracadabra.cauldron.contract-position-fetcher';
 
-import { ARBITRUM_CAULDRONS } from './abracadabra.arbitrum.constants';
+import { ARBITRUM_GLP_CAULDRONS, ARBITRUM_CAULDRONS } from './abracadabra.arbitrum.constants';
 
 @PositionTemplate()
 export class ArbitrumAbracadabraCauldronContractPositionFetcher extends AbracadabraCauldronContractPositionFetcher {
   groupLabel = 'Cauldrons';
   cauldrons = ARBITRUM_CAULDRONS;
+  glpCauldrons = ARBITRUM_GLP_CAULDRONS;
 }
--- src/apps/abracadabra/common/abracadabra.cauldron.contract-position-fetcher.ts
+++ src/apps/abracadabra/common/abracadabra.cauldron.contract-position-fetcher.ts
@@ -2,10 +2,12 @@ import { Inject } from '@nestjs/common';
 
 import { APP_TOOLKIT, IAppToolkit } from '~app-toolkit/app-toolkit.interface';
 import { getLabelFromToken } from '~app-toolkit/helpers/presentation/image.present';
+import { DefaultDataProps } from '~position/display.interface';
 import { MetaType } from '~position/position.interface';
 import { isSupplied } from '~position/position.utils';
 import { ContractPositionTemplatePositionFetcher } from '~position/template/contract-position.template.position-fetcher';
 import {
+  DefaultContractPositionDefinition,
   GetDisplayPropsParams,
   GetTokenBalancesParams,
   GetTokenDefinitionsParams,
@@ -13,8 +15,18 @@ import {
 
 import { AbracadabraContractFactory, AbracadabraCauldron } from '../contracts';
 
-export abstract class AbracadabraCauldronContractPositionFetcher extends ContractPositionTemplatePositionFetcher<AbracadabraCauldron> {
-  abstract cauldrons: string[];
+interface AbracadabraCauldronContractPositionDefinition extends DefaultContractPositionDefinition {
+  type: 'REGULAR' | 'CONVEX' | 'GLP';
+}
+
+export abstract class AbracadabraCauldronContractPositionFetcher extends ContractPositionTemplatePositionFetcher<
+  AbracadabraCauldron,
+  DefaultDataProps,
+  AbracadabraCauldronContractPositionDefinition
+> {
+  cauldrons: string[] = [];
+  convexCauldrons: string[] = [];
+  glpCauldrons: string[] = [];
 
   constructor(
     @Inject(APP_TOOLKIT) protected readonly appToolkit: IAppToolkit,
@@ -28,63 +40,129 @@ export abstract class AbracadabraCauldronContractPositionFetcher extends Contrac
   }
 
   async getDefinitions() {
-    return this.cauldrons.map(address => ({ address }));
+    const regularCauldronDefinitions: AbracadabraCauldronContractPositionDefinition[] = this.cauldrons.map(address => ({
+      address,
+      type: 'REGULAR',
+    }));
+    const convexCauldronDefinitions: AbracadabraCauldronContractPositionDefinition[] = this.convexCauldrons.map(
+      address => ({
+        address,
+        type: 'CONVEX',
+      }),
+    );
+    const glpCauldronDefinitions: AbracadabraCauldronContractPositionDefinition[] = this.glpCauldrons.map(address => ({
+      address,
+      type: 'GLP',
+    }));
+    return [...regularCauldronDefinitions, ...convexCauldronDefinitions, ...glpCauldronDefinitions];
   }
 
-  async getTokenDefinitions({ address, contract, multicall }: GetTokenDefinitionsParams<AbracadabraCauldron>) {
+  async getTokenDefinitions(
+    params: GetTokenDefinitionsParams<AbracadabraCauldron, AbracadabraCauldronContractPositionDefinition>,
+  ) {
+    const {
+      definition: { type },
+    } = params;
+    switch (type) {
+      case 'REGULAR':
+        return this.getRegularTokenDefinitions(params);
+      case 'CONVEX':
+        return this.getConvexTokenDefinitions(params);
+      case 'GLP':
+        return this.getGlpTokenDefinitions(params);
+    }
+  }
+
+  private async getRegularTokenDefinitions({
+    contract,
+  }: GetTokenDefinitionsParams<AbracadabraCauldron, AbracadabraCauldronContractPositionDefinition>) {
     const [collateralAddressRaw, debtAddressRaw] = await Promise.all([
       contract.collateral(),
       contract.magicInternetMoney(),
     ]);
 
-    let realCollateralAddressRaw = collateralAddressRaw;
-
-    // Abracadabra wraps Convex pools in its own wrapper ERC20 token
-    const convexCauldrons = [
-      '0x4eaed76c3a388f4a841e9c765560bbe7b3e4b3a0',
-      '0x35a0dd182e4bca59d5931eae13d0a2332fa30321',
-      '0x806e16ec797c69afa8590a55723ce4cc1b54050e',
-      '0x6371efe5cd6e3d2d7c477935b7669401143b7985',
-      '0x257101f20cb7243e2c7129773ed5dbbcef8b34e0',
-    ];
-
-    if (convexCauldrons.includes(address)) {
-      const convexWrapper = this.contractFactory.abracadabraConvexWrapper({
-        address: collateralAddressRaw.toLowerCase(),
-        network: this.network,
-      });
-
-      realCollateralAddressRaw = await multicall.wrap(convexWrapper).convexToken();
-    }
-
-    // Abracadabra wraps GLP pools in its own wrapper ERC20 token
-    const glpCauldrons = ['0x5698135ca439f21a57bddbe8b582c62f090406d5'];
-
-    if (glpCauldrons.includes(address)) {
-      const glpWrapper = this.contractFactory.abracadabraGlpWrapper({
-        address: collateralAddressRaw.toLowerCase(),
-        network: this.network,
-      });
-
-      const sGlpAddressRaw = await multicall.wrap(glpWrapper).underlying();
-      const sGlp = this.contractFactory.abracadabraGmxSGlp({
-        address: sGlpAddressRaw.toLowerCase(),
-        network: this.network,
-      });
-
-      const glpAddress = await multicall.wrap(sGlp).glp();
-      realCollateralAddressRaw = glpAddress;
-    }
-
     return [
       {
         metaType: MetaType.SUPPLIED,
-        address: realCollateralAddressRaw,
+        address: collateralAddressRaw,
         network: this.network,
       },
       {
         metaType: MetaType.BORROWED,
         address: debtAddressRaw,
+        network: this.network,
+      },
+    ];
+  }
+
+  private async getConvexTokenDefinitions({
+    contract,
+    multicall,
+  }: GetTokenDefinitionsParams<AbracadabraCauldron, AbracadabraCauldronContractPositionDefinition>) {
+    const [collateralTokenAddress, debtTokenAddress] = await Promise.all([
+      contract.collateral().then(collateralTokenAddress => {
+        const convexWrapper = multicall.wrap(
+          this.contractFactory.abracadabraConvexWrapper({
+            address: collateralTokenAddress.toLowerCase(),
+            network: this.network,
+          }),
+        );
+
+        return convexWrapper.convexToken();
+      }),
+      contract.magicInternetMoney(),
+    ]);
+
+    return [
+      {
+        metaType: MetaType.SUPPLIED,
+        address: collateralTokenAddress,
+        network: this.network,
+      },
+      {
+        metaType: MetaType.BORROWED,
+        address: debtTokenAddress,
+        network: this.network,
+      },
+    ];
+  }
+
+  private async getGlpTokenDefinitions({
+    contract,
+    multicall,
+  }: GetTokenDefinitionsParams<AbracadabraCauldron, AbracadabraCauldronContractPositionDefinition>) {
+    const [collateralTokenAddress, debtTokenAddress] = await Promise.all([
+      contract.collateral(),
+      contract.magicInternetMoney(),
+    ]);
+
+    const glpWrapper = multicall.wrap(
+      this.contractFactory.abracadabraGlpWrapper({
+        address: collateralTokenAddress,
+        network: this.network,
+      }),
+    );
+
+    const sGlpAddress = await glpWrapper.underlying();
+
+    const sGlp = multicall.wrap(
+      this.contractFactory.abracadabraGmxSGlp({
+        address: sGlpAddress,
+        network: this.network,
+      }),
+    );
+
+    const glpAddress = await sGlp.glp();
+
+    return [
+      {
+        metaType: MetaType.SUPPLIED,
+        address: glpAddress,
+        network: this.network,
+      },
+      {
+        metaType: MetaType.BORROWED,
+        address: debtTokenAddress,
         network: this.network,
       },
     ];
@@ -101,26 +179,28 @@ export abstract class AbracadabraCauldronContractPositionFetcher extends Contrac
     contract,
     multicall,
   }: GetTokenBalancesParams<AbracadabraCauldron>) {
-    const [borrowPartRaw, totalBorrowRaw, collateralShareRaw, bentoBoxAddressRaw] = await Promise.all([
+    const [borrowPartRaw, totalBorrowRaw, suppliedBalanceRaw] = await Promise.all([
       contract.userBorrowPart(address),
       contract.totalBorrow(),
-      contract.userCollateralShare(address),
-      contract.bentoBox(),
+      Promise.all([contract.userCollateralShare(address), contract.bentoBox()]).then(
+        ([collateralShareRaw, bentoBoxAddress]) => {
+          const bentoBoxContract = multicall.wrap(
+            this.contractFactory.abracadabraBentoBoxTokenContract({
+              address: bentoBoxAddress,
+              network: this.network,
+            }),
+          );
+
+          const suppliedToken = contractPosition.tokens.find(isSupplied)!;
+
+          return bentoBoxContract.toAmount(suppliedToken.address, collateralShareRaw, false);
+        },
+      ),
     ]);
 
-    const bentoBoxTokenContract = multicall.wrap(
-      this.contractFactory.abracadabraBentoBoxTokenContract({
-        address: bentoBoxAddressRaw,
-        network: this.network,
-      }),
-    );
-
-    const suppliedToken = contractPosition.tokens.find(isSupplied)!;
-    const suppliedBalanceRaw = await bentoBoxTokenContract.toAmount(suppliedToken.address, collateralShareRaw, false);
-    let borrowedBalanceRaw = totalBorrowRaw.base;
-    if (!borrowedBalanceRaw.eq(0)) {
-      borrowedBalanceRaw = borrowPartRaw.mul(totalBorrowRaw.elastic).div(totalBorrowRaw.base);
-    }
+    const borrowedBalanceRaw = totalBorrowRaw.base.eq(0)
+      ? 0
+      : borrowPartRaw.mul(totalBorrowRaw.elastic).div(totalBorrowRaw.base);
 
     return [suppliedBalanceRaw, borrowedBalanceRaw];
   }
