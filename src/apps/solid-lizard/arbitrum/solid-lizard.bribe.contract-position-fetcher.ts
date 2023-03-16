97a27c9ee3e3c0ac87f7945f79b2c16f907b86cc
TonyG <tonyzgao@gmail.com>
2023-03-16 17:18:53 +0100

    feat(solid-lizard): basic implementation

---
+++ src/apps/solid-lizard/arbitrum/solid-lizard.bribe.contract-position-fetcher.ts
@@ -0,0 +1,50 @@
+import _, { range } from 'lodash';
+
+import { PositionTemplate } from '~app-toolkit/decorators/position-template.decorator';
+import { DefaultDataProps } from '~position/display.interface';
+import { MetaType } from '~position/position.interface';
+import { GetDisplayPropsParams, GetTokenDefinitionsParams } from '~position/template/contract-position.template.types';
+
+import { VotingRewardsContractPositionFetcher } from '../common/solid-lizard.voting-rewards.contract-position-fetcher';
+import { SolidLizardBribe } from '../contracts';
+
+export type SolidLizardBribeDefinition = {
+  address: string;
+  name: string;
+};
+
+@PositionTemplate()
+export class ArbitrumSolidLizardBribeContractPositionFetcher extends VotingRewardsContractPositionFetcher<SolidLizardBribe> {
+  groupLabel = 'Bribe';
+
+  getContract(address: string): SolidLizardBribe {
+    return this.contractFactory.solidLizardBribe({ address, network: this.network });
+  }
+
+  async getDefinitions(): Promise<SolidLizardBribeDefinition[]> {
+    return this.definitionsResolver.getBribeDefinitions();
+  }
+
+  async getTokenDefinitions({ contract }: GetTokenDefinitionsParams<SolidLizardBribe>) {
+    const numRewards = Number(await contract.rewardTokensLength());
+    const bribeTokens = await Promise.all(range(numRewards).map(async n => await contract.rewardTokens(n)));
+    const baseTokens = await this.appToolkit.getBaseTokenPrices(this.network);
+    const tokenDefinitions = bribeTokens.map(token => {
+      const tokenFound = baseTokens.find(p => p.address === token.toLowerCase());
+      if (!tokenFound) return null;
+      return {
+        metaType: MetaType.CLAIMABLE,
+        address: token,
+        network: this.network,
+      };
+    });
+
+    return _.compact(tokenDefinitions);
+  }
+
+  async getLabel({
+    definition,
+  }: GetDisplayPropsParams<SolidLizardBribe, DefaultDataProps, SolidLizardBribeDefinition>): Promise<string> {
+    return `${definition.name} Bribe`;
+  }
+}
