9c5585a67308a891179e53bfa72dbfb453f3670b
Pranav Raut <pranavraut033@gmail.com>
2023-03-31 14:40:38 +0530

    feat(defiedge): added bsc network

--- src/apps/defiedge/defiedge.module.ts
+++ src/apps/defiedge/defiedge.module.ts
@@ -3,6 +3,7 @@ import { Module } from '@nestjs/common';
 import { AbstractApp } from '~app/app.dynamic-module';
 
 import { ArbitrumDefiedgeStrategyTokenFetcher } from './arbitrum/defiedge.strategy.token-fetcher';
+import { BinanceSmartChainDefiedgeStrategyTokenFetcher } from './binance-smart-chain/defiedge.strategy.token-fetcher';
 import { DefiedgeStrategyDefinitionsResolver } from './common/defiedge.strategy.definitions-resolver';
 import { DefiedgeContractFactory } from './contracts';
 import { EthereumDefiedgeStrategyTokenFetcher } from './ethereum/defiedge.strategy.token-fetcher';
@@ -15,6 +16,7 @@ import { POLYGON_DEFIEDGE_PROVIDERS } from './polygon';
     DefiedgeStrategyDefinitionsResolver,
     ArbitrumDefiedgeStrategyTokenFetcher,
     EthereumDefiedgeStrategyTokenFetcher,
+    BinanceSmartChainDefiedgeStrategyTokenFetcher,
     ...POLYGON_DEFIEDGE_PROVIDERS,
     ...OPTIMISM_DEFIEDGE_PROVIDERS,
   ],
