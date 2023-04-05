03a4c68d4e2e76a3957a2f2e091369347201e55a
William Poulin <will@zapper.fi>
2023-04-04 23:54:47 -0400

    fix(lyra-avalon): Remove staked Lyra claimables position on Arbitrum (#2515)

--- src/apps/lyra-avalon/lyra-avalon.module.ts
+++ src/apps/lyra-avalon/lyra-avalon.module.ts
@@ -3,7 +3,6 @@ import { Module } from '@nestjs/common';
 import { AbstractApp } from '~app/app.dynamic-module';
 
 import { ArbitrumLyraAvalonOptionsContractPositionFetcher } from './arbitrum/lyra-avalon.options.contract-position-fetcher';
-import { ArbitrumLyraAvalonStkLyraClaimableContractPositionFetcher } from './arbitrum/lyra-avalon.stk-lyra-claimable.contract-position-fetcher';
 import { ArbitrumLyraAvalonStkLyraTokenFetcher } from './arbitrum/lyra-avalon.stk-lyra.token-fetcher';
 import { LyraAvalonContractFactory } from './contracts';
 import { EthereumLyraAvalonStakingContractPositionFetcher } from './ethereum/lyra-avalon.staking.contract-position-fetcher';
@@ -20,7 +19,6 @@ import { OptimismLyraAvalonStkLyraTokenFetcher } from './optimism/lyra-avalon.st
     // Arbitrum
     ArbitrumLyraAvalonOptionsContractPositionFetcher,
     ArbitrumLyraAvalonStkLyraTokenFetcher,
-    ArbitrumLyraAvalonStkLyraClaimableContractPositionFetcher,
     // Optimism
     OptimismLyraAvalonOptionsContractPositionFetcher,
     OptimismLyraAvalonPoolTokenFetcher,
