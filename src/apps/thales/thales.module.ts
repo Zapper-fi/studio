6a4a9b3143dbb00d3c6783e43b836b35e182cd3d
William Poulin <will@zapper.fi>
2023-04-05 23:59:47 -0400

    fix(thales): Linting (#2526)

--- src/apps/thales/thales.module.ts
+++ src/apps/thales/thales.module.ts
@@ -2,16 +2,16 @@ import { Module } from '@nestjs/common';
 
 import { AbstractApp } from '~app/app.dynamic-module';
 
+import { ArbitrumThalesEscrowContractPositionFetcher } from './arbitrum/thales.escrow.contract-position-fetcher';
+import { ArbitrumThalesOvertimeAmmLpContractPositionFetcher } from './arbitrum/thales.overtime-amm-lp.contract-position-fetcher';
+import { ArbitrumThalesStakingContractPositionFetcher } from './arbitrum/thales.staking.contract-position-fetcher';
+import { ArbitrumThalesVaultContractPositionFetcher } from './arbitrum/thales.vault.contract-position-fetcher';
 import { ThalesContractFactory } from './contracts';
 import { OptimismThalesEscrowContractPositionFetcher } from './optimism/thales.escrow.contract-position-fetcher';
-import { ArbitrumThalesEscrowContractPositionFetcher } from './arbitrum/thales.escrow.contract-position-fetcher';
+import { OptimismThalesOvertimeAmmLpContractPositionFetcher } from './optimism/thales.overtime-amm-lp.contract-position-fetcher';
 import { OptimismThalesPool2ContractPositionFetcher } from './optimism/thales.pool2.contract-position-fetcher';
 import { OptimismThalesStakingContractPositionFetcher } from './optimism/thales.staking.contract-position-fetcher';
-import { ArbitrumThalesStakingContractPositionFetcher } from './arbitrum/thales.staking.contract-position-fetcher';
 import { OptimismThalesVaultContractPositionFetcher } from './optimism/thales.vault.contract-position-fetcher';
-import { ArbitrumThalesVaultContractPositionFetcher } from './arbitrum/thales.vault.contract-position-fetcher';
-import { OptimismThalesOvertimeAmmLpContractPositionFetcher } from './optimism/thales.overtime-amm-lp.contract-position-fetcher';
-import { ArbitrumThalesOvertimeAmmLpContractPositionFetcher } from './arbitrum/thales.overtime-amm-lp.contract-position-fetcher';
 
 @Module({
   providers: [
@@ -27,4 +27,4 @@ import { ArbitrumThalesOvertimeAmmLpContractPositionFetcher } from './arbitrum/t
     ArbitrumThalesOvertimeAmmLpContractPositionFetcher,
   ],
 })
-export class ThalesAppModule extends AbstractApp() { }
+export class ThalesAppModule extends AbstractApp() {}
