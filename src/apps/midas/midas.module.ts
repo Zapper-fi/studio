b0af1e745470e7c2ecd2ddaf8a0d67606bf29035
William Poulin <will@zapper.fi>
2023-04-06 12:44:22 -0400

    fix(apps): Remove app integrations on Cronos and Evmos (#2529)

--- src/apps/midas/midas.module.ts
+++ src/apps/midas/midas.module.ts
@@ -4,16 +4,9 @@ import { AbstractApp } from '~app/app.dynamic-module';
 
 import { BinanceSmartChainMidasMarketTokenFetcher } from './binance-smart-chain/midas.market.token-fetcher';
 import { MidasContractFactory } from './contracts';
-import { EvmosMidasMarketTokenFetcher } from './evmos/midas.market.token-fetcher';
 import { PolygonMidasMarketTokenFetcher } from './polygon/midas.market.token-fetcher';
 
 @Module({
-  providers: [
-    BinanceSmartChainMidasMarketTokenFetcher,
-    EvmosMidasMarketTokenFetcher,
-
-    MidasContractFactory,
-    PolygonMidasMarketTokenFetcher,
-  ],
+  providers: [MidasContractFactory, BinanceSmartChainMidasMarketTokenFetcher, PolygonMidasMarketTokenFetcher],
 })
 export class MidasAppModule extends AbstractApp() {}
