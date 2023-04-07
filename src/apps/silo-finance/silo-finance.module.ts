7c92b42e795698f740705c966834246c09c118dd
Karan Shahani <karan.shahani91@gmail.com>
2023-04-07 14:19:58 -0400

    Done

--- src/apps/silo-finance/silo-finance.module.ts
+++ src/apps/silo-finance/silo-finance.module.ts
@@ -2,8 +2,14 @@ import { Module } from '@nestjs/common';
 
 import { AbstractApp } from '~app/app.dynamic-module';
 
+import { ArbitrumDTokenTokenFetcher } from './arbitrum/silo-finance.d-token.token-fetcher';
+import { ArbitrumSiloFinanceIncentivesContractPositionfetcher } from './arbitrum/silo-finance.incentives.contract-position-fetcher';
+import { ArbitrumSTokenTokenFetcher } from './arbitrum/silo-finance.s-token.token-fetcher';
+import { ArbitrumSPTokenTokenFetcher } from './arbitrum/silo-finance.sp-token.token-fetcher';
 import { SiloFinanceDefinitionResolver } from './common/silo-finance.definition-resolver';
 import { SiloFinanceContractFactory } from './contracts';
+import { EthereumDTokenTokenFetcher } from './ethereum/silo-finance.d-token.token-fetcher';
+import { EthereumSiloFinanceIncentivesContractPositionfetcher } from './ethereum/silo-finance.incentives.contract-position-fetcher';
 import { EthereumSTokenTokenFetcher } from './ethereum/silo-finance.s-token.token-fetcher';
 import { EthereumSPTokenTokenFetcher } from './ethereum/silo-finance.sp-token.token-fetcher';
 
@@ -12,17 +18,15 @@ import { EthereumSPTokenTokenFetcher } from './ethereum/silo-finance.sp-token.to
     SiloFinanceContractFactory,
     SiloFinanceDefinitionResolver,
     // Arbitrum
-    // ArbitrumSTokenTokenFetcher,
-    // ArbitrumSPTokenTokenFetcher,
-    // ArbitrumDTokenTokenFetcher,
-    // ArbitrumSiloFinanceSiloContractPositionFetcher,
-    // ArbitrumSiloFinanceIncentivesContractPositionfetcher,
+    ArbitrumSTokenTokenFetcher,
+    ArbitrumSPTokenTokenFetcher,
+    ArbitrumDTokenTokenFetcher,
+    ArbitrumSiloFinanceIncentivesContractPositionfetcher,
     // Ethereum
     EthereumSTokenTokenFetcher,
     EthereumSPTokenTokenFetcher,
-    // EthereumDTokenTokenFetcher,
-    // EthereumSiloFinanceSiloContractPositionFetcher,
-    // EthereumSiloFinanceIncentivesContractPositionfetcher,
+    EthereumDTokenTokenFetcher,
+    EthereumSiloFinanceIncentivesContractPositionfetcher,
   ],
 })
 export class SiloFinanceAppModule extends AbstractApp() {}
