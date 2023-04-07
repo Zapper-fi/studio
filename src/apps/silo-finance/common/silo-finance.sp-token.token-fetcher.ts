df65dab9300a48a996634bccc866e11e6218f427
Karan Shahani <karan.shahani91@gmail.com>
2023-04-07 14:16:02 -0400

    Fix SP token balances

--- src/apps/silo-finance/common/silo-finance.sp-token.token-fetcher.ts
+++ src/apps/silo-finance/common/silo-finance.sp-token.token-fetcher.ts
@@ -1,8 +1,6 @@
 import { Inject } from '@nestjs/common';
-import BigNumber from 'bignumber.js';
 
 import { APP_TOOLKIT, IAppToolkit } from '~app-toolkit/app-toolkit.interface';
-import { RawTokenBalance } from '~position/position-balance.interface';
 import { AppTokenTemplatePositionFetcher } from '~position/template/app-token.template.position-fetcher';
 import {
   DefaultAppTokenDataProps,
@@ -29,10 +27,6 @@ export abstract class SiloFinanceSpTokenTokenFetcher extends AppTokenTemplatePos
   SpTokenDataProps,
   SpTokenDefinition
 > {
-  isExcludedFromBalances = true;
-  isExcludedFromExplore = true;
-  isExcludedFromTvl = true;
-
   constructor(
     @Inject(APP_TOOLKIT) protected readonly appToolkit: IAppToolkit,
     @Inject(SiloFinanceContractFactory) protected readonly contractFactory: SiloFinanceContractFactory,
@@ -74,38 +68,5 @@ export abstract class SiloFinanceSpTokenTokenFetcher extends AppTokenTemplatePos
     const defaultDataProps = await super.getDataProps(params);
     const siloAddress = params.definition.siloAddress;
     return { ...defaultDataProps, siloAddress };
-  }
-
-  async getRawBalances(address: string): Promise<RawTokenBalance[]> {
-    const multicall = this.appToolkit.getMulticall(this.network);
-
-    const appTokens = await this.appToolkit.getAppTokenPositions<SpTokenDataProps>({
-      appId: this.appId,
-      network: this.network,
-      groupIds: [this.groupId],
-    });
-
-    return Promise.all(
-      appTokens.map(async appToken => {
-        const balanceRaw = await multicall.wrap(this.getContract(appToken.address)).balanceOf(address);
-
-        const siloContract = this.contractFactory.silo({
-          address: appToken.dataProps.siloAddress,
-          network: this.network,
-        });
-
-        const assetStorage = await multicall.wrap(siloContract).assetStorage(appToken.tokens[0].address);
-        const collateralOnlyDeposits = assetStorage.collateralOnlyDeposits;
-
-        const balance = new BigNumber(balanceRaw.toString())
-          .times(collateralOnlyDeposits.toString())
-          .div(appToken.supply * 10 ** appToken.decimals);
-
-        return {
-          key: this.appToolkit.getPositionKey(appToken),
-          balance: balance.toString(),
-        };
-      }),
-    );
   }
 }
