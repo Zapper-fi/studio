705bbd3c208c172e3660118b3b26d4e0b02264f9
William Poulin <will@zapper.fi>
2023-04-08 19:46:16 -0400

    fix(reserve-protocol): Linting

--- src/apps/reserve-protocol/reserve-protocol.module.ts
+++ src/apps/reserve-protocol/reserve-protocol.module.ts
@@ -3,8 +3,8 @@ import { Module } from '@nestjs/common';
 import { AbstractApp } from '~app/app.dynamic-module';
 
 import { ReserveProtocolContractFactory } from './contracts';
-import { EthereumReserveProtocolStakedRsrTokenFetcher } from './ethereum/reserve-protocol.staked-rsr.token-fetcher';
 import { EthereumReserveProtocolCooldownContractPositionFetcher } from './ethereum/reserve-protocol.cooldown.contract-position-fetcher';
+import { EthereumReserveProtocolStakedRsrTokenFetcher } from './ethereum/reserve-protocol.staked-rsr.token-fetcher';
 
 @Module({
   providers: [
