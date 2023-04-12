f46286df9a1ab15520ea308e73542944dff86fcb
Pirex <diogo.fpmp.pires@gmail.com>
2023-04-12 22:08:49 +0100

    feat(zharta): fixed position values

--- src/apps/zharta/ethereum/zharta.lending-pool-core.contract-position-fetcher.ts
+++ src/apps/zharta/ethereum/zharta.lending-pool-core.contract-position-fetcher.ts
@@ -1,18 +1,14 @@
 import { Inject } from '@nestjs/common';
-import { BigNumberish, Contract } from 'ethers';
 
 import { APP_TOOLKIT, IAppToolkit } from '~app-toolkit/app-toolkit.interface';
 import { PositionTemplate } from '~app-toolkit/decorators/position-template.decorator';
 import { getLabelFromToken } from '~app-toolkit/helpers/presentation/image.present';
-import { DefaultDataProps } from '~position/display.interface';
 import { MetaType } from '~position/position.interface';
 import { isSupplied } from '~position/position.utils';
 import { ContractPositionTemplatePositionFetcher } from '~position/template/contract-position.template.position-fetcher';
 import {
-  GetDefinitionsParams,
   DefaultContractPositionDefinition,
   GetTokenDefinitionsParams,
-  UnderlyingTokenDefinition,
   GetDisplayPropsParams,
   GetTokenBalancesParams,
 } from '~position/template/contract-position.template.types';
@@ -95,6 +91,6 @@ export class EthereumZhartaLendingPoolCoreContractPositionFetcher extends Contra
       contract.computeWithdrawableAmount(address)
     ]);
 
-    return [lenderFundsRaw.currentAmountDeposited, withdrawableAmountRaw];
+    return [lenderFundsRaw.currentAmountDeposited, withdrawableAmountRaw.sub(lenderFundsRaw.currentAmountDeposited)];
   }
 }
