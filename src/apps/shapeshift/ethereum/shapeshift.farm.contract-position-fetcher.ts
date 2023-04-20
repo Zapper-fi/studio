621f012b792341dbdf85e75b65be941ef684dea9
Fireb0mb1 <firebomb1@gmail.com>
2023-04-20 19:20:58 +0200

    fix(shapeshift): update fox farms

--- src/apps/shapeshift/ethereum/shapeshift.farm.contract-position-fetcher.ts
+++ src/apps/shapeshift/ethereum/shapeshift.farm.contract-position-fetcher.ts
@@ -11,15 +11,39 @@ import {
 import { ShapeshiftContractFactory, ShapeshiftStakingRewards } from '../contracts';
 
 const FARMS = [
-  // UNI-V2 FOX / ETH (retired)
+  // UNI-V2 FOX / ETH v1 (inactive)
   {
     address: '0xdd80e21669a664bce83e3ad9a0d74f8dad5d9e72',
     stakedTokenAddress: '0x470e8de2ebaef52014a47cb5e6af86884947f08c',
     rewardTokenAddresses: ['0xc770eefad204b5180df6a14ee197d99d808ee52d'],
   },
-  // UNI-V2 FOX / ETH
+  // UNI-V2 FOX / ETH v2 (inactive)
   {
     address: '0xc54b9f82c1c54e9d4d274d633c7523f2299c42a0',
+    stakedTokenAddress: '0x470e8de2ebaef52014a47cb5e6af86884947f08c',
+    rewardTokenAddresses: ['0xc770eefad204b5180df6a14ee197d99d808ee52d'],
+  },
+  // UNI-V2 FOX / ETH v3 (inactive)
+  {
+    address: '0x212ebf9fd3c10f371557b08e993eaab385c3932b',
+    stakedTokenAddress: '0x470e8de2ebaef52014a47cb5e6af86884947f08c',
+    rewardTokenAddresses: ['0xc770eefad204b5180df6a14ee197d99d808ee52d'],
+  },
+  // UNI-V2 FOX / ETH v4 (inactive)
+  {
+    address: '0x24fd7fb95dc742e23dc3829d3e656feeb5f67fa0',
+    stakedTokenAddress: '0x470e8de2ebaef52014a47cb5e6af86884947f08c',
+    rewardTokenAddresses: ['0xc770eefad204b5180df6a14ee197d99d808ee52d'],
+  },
+  // UNI-V2 FOX / ETH v5 (inactive)
+  {
+    address: '0xc14eaa8284feff79edc118e06cadbf3813a7e555',
+    stakedTokenAddress: '0x470e8de2ebaef52014a47cb5e6af86884947f08c',
+    rewardTokenAddresses: ['0xc770eefad204b5180df6a14ee197d99d808ee52d'],
+  },
+  // UNI-V2 FOX / ETH v6 (active)
+  {
+    address: '0xebb1761ad43034fd7faa64d84e5bbd8cb5c40b68',
     stakedTokenAddress: '0x470e8de2ebaef52014a47cb5e6af86884947f08c',
     rewardTokenAddresses: ['0xc770eefad204b5180df6a14ee197d99d808ee52d'],
   },
