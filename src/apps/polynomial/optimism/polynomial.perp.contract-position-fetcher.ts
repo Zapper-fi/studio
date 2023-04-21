b4480b4b3129a9f6c0b1e915b4f05d2ace061853
gaulois <gaulois.eth@gmail.com>
2023-04-21 17:25:50 +0200

    feat(polynomial): Add trade tvl

--- src/apps/polynomial/optimism/polynomial.perp.contract-position-fetcher.ts
+++ src/apps/polynomial/optimism/polynomial.perp.contract-position-fetcher.ts
@@ -1,4 +1,5 @@
 import { Inject } from '@nestjs/common';
+import Axios from 'axios';
 
 import { APP_TOOLKIT, IAppToolkit } from '~app-toolkit/app-toolkit.interface';
 import { PositionTemplate } from '~app-toolkit/decorators/position-template.decorator';
@@ -7,6 +8,10 @@ import { SynthetixContractFactory } from '../../synthetix/contracts';
 import { OptimismSynthetixPerpV2ContractPositionFetcher } from '../../synthetix/optimism/synthetix.perp-v2.contract-position-fetcher';
 import { PolynomialContractFactory } from '../contracts';
 import { PolynomialAccountResolver } from '../common/polynomial.account-resolver';
+
+export type PolynomialPerpDataProp = {
+  liquidity: number;
+};
 
 @PositionTemplate()
 export class OptimismPolynomialPerpContractPositionFetcher extends OptimismSynthetixPerpV2ContractPositionFetcher {
@@ -23,5 +28,12 @@ export class OptimismPolynomialPerpContractPositionFetcher extends OptimismSynth
 
   async getAccountAddress(address: string): Promise<string> {
     return this.polynomialAccountResolver.getSmartWalletAddress(address);
+  }
+
+  async getDataProps(): Promise<PolynomialPerpDataProp> {
+    const { data } = await Axios.get<{ tvl: number }>('https://perps-api-experimental.polynomial.fi/snx-perps/tvl');
+    return {
+      liquidity: data.tvl
+    };
   }
 }
