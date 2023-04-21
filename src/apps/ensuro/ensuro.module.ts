f3d7048b7fc3426f0599d19c90aa99b1be472cbb
William Poulin <will@zapper.fi>
2023-04-21 16:01:34 -0400

    fix(ensuo): Linting

--- src/apps/ensuro/ensuro.module.ts
+++ src/apps/ensuro/ensuro.module.ts
@@ -2,11 +2,9 @@ import { Module } from '@nestjs/common';
 
 import { AbstractApp } from '~app/app.dynamic-module';
 
-import { EnsuroContractFactory } from './contracts';
-import {
-  PolygonEnsuroETokenTokenFetcher,
-} from './polygon/ensuro.etoken.token-fetcher';
 import { EnsuroApiRegistry } from './common/ensuro.api-registry';
+import { EnsuroContractFactory } from './contracts';
+import { PolygonEnsuroETokenTokenFetcher } from './polygon/ensuro.e-token.token-fetcher';
 
 @Module({
   providers: [EnsuroContractFactory, EnsuroApiRegistry, PolygonEnsuroETokenTokenFetcher],
