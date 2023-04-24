9b74085d55a2690474f3d394d212472cf1afa15b
William Poulin <will@zapper.fi>
2023-04-24 16:29:31 -0400

    fix(apps): Linting (#2612)

--- src/apps/polynomial/optimism/polynomial.smart-wallet.contract-position-fetcher.ts
+++ src/apps/polynomial/optimism/polynomial.smart-wallet.contract-position-fetcher.ts
@@ -4,13 +4,13 @@ import Axios from 'axios';
 import { APP_TOOLKIT, IAppToolkit } from '~app-toolkit/app-toolkit.interface';
 import { PositionTemplate } from '~app-toolkit/decorators/position-template.decorator';
 import { Erc20 } from '~contract/contracts';
-
+import { MetaType } from '~position/position.interface';
 import { ContractPositionTemplatePositionFetcher } from '~position/template/contract-position.template.position-fetcher';
 import {
   DefaultContractPositionDefinition,
   GetTokenBalancesParams,
 } from '~position/template/contract-position.template.types';
-import { MetaType } from '~position/position.interface';
+
 import { PolynomialAccountResolver } from '../common/polynomial.account-resolver';
 
 export type PolynomialSmartWalletDataProp = {
@@ -18,7 +18,7 @@ export type PolynomialSmartWalletDataProp = {
 };
 
 @PositionTemplate()
-export class OptimismPolynomialSmartWalletContractPositionFetcher extends ContractPositionTemplatePositionFetcher<Erc20>{
+export class OptimismPolynomialSmartWalletContractPositionFetcher extends ContractPositionTemplatePositionFetcher<Erc20> {
   sUSD = '0x8c6f28f2f1a3c87f0f938b96d27520d9751ec8d9';
   groupLabel = 'Smart Wallet';
 
@@ -62,8 +62,7 @@ export class OptimismPolynomialSmartWalletContractPositionFetcher extends Contra
   async getDataProps(): Promise<PolynomialSmartWalletDataProp> {
     const { data } = await Axios.get<{ tvl: number }>('https://perps-api-experimental.polynomial.fi/snx-perps/tvl');
     return {
-      liquidity: data.tvl
+      liquidity: data.tvl,
     };
   }
-
 }
