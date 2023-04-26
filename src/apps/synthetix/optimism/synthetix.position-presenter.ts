ba8fa57b819c2a727be8437ed0fa34b766e718d2
gauloiseth <70271293+gdeschryver@users.noreply.github.com>
2023-04-26 23:03:46 +0200

    fix(synthetix): OP position presenter (#2620)
    Co-authored-by: gaulois <gaulois.eth@gmail.com>

--- src/apps/synthetix/optimism/synthetix.position-presenter.ts
+++ src/apps/synthetix/optimism/synthetix.position-presenter.ts
@@ -5,4 +5,5 @@ import { SynthetixPositionPresenter } from '../common/synthetix.position-present
 @PresenterTemplate()
 export class OptimismSynthetixPositionPresenter extends SynthetixPositionPresenter {
   snxAddress = '0x8700daec35af8ff88c16bdf0418774cb3d7599b4';
+  sUSDAddress = '0x8c6f28f2f1a3c87f0f938b96d27520d9751ec8d9';
 }
