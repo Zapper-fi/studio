50faeab775405798cea546f310fbe4f521015682
Guilherme Guimarães <ggviana@hotmail.com.br>
2023-05-02 23:39:31 -0300

    feat(pods): Added new Pods Yield Vault

--- src/apps/pods-yield/ethereum/config.ts
+++ src/apps/pods-yield/ethereum/config.ts
@@ -1,6 +1,7 @@
 export const strategyAddresses = [
   `0xbab1e772d70300422312dff12daddcb60864bd41`,
   `0x463f9ed5e11764eb9029762011a03643603ad879`,
+  `0x5fe4b38520e856921978715c8579d2d7a4d2274f`,
 ];
 export const strategyDetails = {
   [strategyAddresses[0]]: {
@@ -8,6 +9,9 @@ export const strategyDetails = {
   },
   [strategyAddresses[1]]: {
     title: 'stETH Volatility Vault',
+  },
+  [strategyAddresses[2]]: {
+    title: 'ETHphoria Vault',
   },
   standard: {
     title: 'Pods Yield Vault',
