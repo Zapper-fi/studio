1e00fa1aacadbcc752622683b6080f3108da84cd
William Poulin <will@zapper.fi>
2023-05-03 18:00:12 -0400

    fix(synthetix): Revert Synthetix PERP balance updates

--- src/apps/synthetix/optimism/synthetix.perp.contract-position-fetcher.ts
+++ src/apps/synthetix/optimism/synthetix.perp.contract-position-fetcher.ts
@@ -1,18 +1,14 @@
 import { Inject } from '@nestjs/common';
-import { BigNumberish } from 'ethers';
 import { parseBytes32String } from 'ethers/lib/utils';
 import { gql } from 'graphql-request';
-import { sumBy } from 'lodash';
 
 import { APP_TOOLKIT, IAppToolkit } from '~app-toolkit/app-toolkit.interface';
-import { drillBalance } from '~app-toolkit/helpers/drill-balance.helper';
 import { getAppAssetImage } from '~app-toolkit/helpers/presentation/image.present';
 import { gqlFetch } from '~app-toolkit/helpers/the-graph.helper';
 import { DefaultDataProps } from '~position/display.interface';
-import { ContractPositionBalance, RawContractPositionBalance } from '~position/position-balance.interface';
 import { MetaType } from '~position/position.interface';
 import { ContractPositionTemplatePositionFetcher } from '~position/template/contract-position.template.position-fetcher';
-import { GetDisplayPropsParams } from '~position/template/contract-position.template.types';
+import { GetDisplayPropsParams, GetTokenBalancesParams } from '~position/template/contract-position.template.types';
 
 import { SynthetixContractFactory, SynthetixPerp } from '../contracts';
 
@@ -38,7 +34,6 @@ export const FUTURES_MARKETS_QUERY = gql`
 
 export type SynthetixPerpPositionDefinition = {
   address: string;
-  side: string;
   asset: string;
 };
 
@@ -47,8 +42,6 @@ export abstract class OptimismSynthetixPerpContractPositionFetcher extends Contr
   DefaultDataProps,
   SynthetixPerpPositionDefinition
 > {
-  useCustomMarketLogos = false;
-
   constructor(
     @Inject(APP_TOOLKIT) protected readonly appToolkit: IAppToolkit,
     @Inject(SynthetixContractFactory) protected readonly contractFactory: SynthetixContractFactory,
@@ -76,15 +69,11 @@ export abstract class OptimismSynthetixPerpContractPositionFetcher extends Contr
 
     const markets = futuresMarkets.filter(market => this.marketFilter(market.marketKey));
 
-    const definitions = ['SHORT', 'NEUTRAL', 'LONG']
-      .map(optionType =>
-        markets.map(market => {
-          const baseAssetRaw = parseBytes32String(market.asset);
-          const baseAsset = baseAssetRaw.charAt(0) === 's' ? baseAssetRaw.substring(1) : baseAssetRaw;
-          return { address: market.id.toLowerCase(), side: optionType, asset: baseAsset };
-        }),
-      )
-      .flat();
+    const definitions = markets.map(market => {
+      const baseAssetRaw = parseBytes32String(market.asset);
+      const baseAsset = baseAssetRaw.charAt(0) === 's' ? baseAssetRaw.substring(1) : baseAssetRaw;
+      return { address: market.id.toLowerCase(), asset: baseAsset };
+    });
 
     return definitions;
   }
@@ -102,113 +91,17 @@ export abstract class OptimismSynthetixPerpContractPositionFetcher extends Contr
   async getLabel({
     definition,
   }: GetDisplayPropsParams<SynthetixPerp, DefaultDataProps, SynthetixPerpPositionDefinition>): Promise<string> {
-    return `${definition.asset}-PERP ${definition.side}`;
-  }
-
-  async getDataProps({ definition }) {
-    return { side: definition.side };
+    return `${definition.asset}-PERP`;
   }
 
   async getImages({
     definition,
   }: GetDisplayPropsParams<SynthetixPerp, DefaultDataProps, SynthetixPerpPositionDefinition>) {
-    return [getAppAssetImage(this.useCustomMarketLogos ? this.appId : 'synthetix', `s${definition.asset}`)];
+    return [getAppAssetImage('synthetix', `s${definition.asset}`)];
   }
 
-  async getTokenBalancesPerPosition(): Promise<BigNumberish[]> {
-    throw new Error('Method not implemented.');
-  }
-
-  async getBalances(address: string): Promise<ContractPositionBalance<DefaultDataProps>[]> {
-    const multicall = this.appToolkit.getMulticall(this.network);
-
-    const contractPositions = await this.appToolkit.getAppContractPositions({
-      appId: this.appId,
-      network: this.network,
-      groupIds: [this.groupId],
-    });
-
-    if (!contractPositions) return [];
-
-    const allPositions = await Promise.all(
-      contractPositions.map(async contractPosition => {
-        const synthPerpContract = this.contractFactory.synthetixPerp({
-          address: contractPosition.address,
-          network: this.network,
-        });
-        const remainingMargin = await multicall.wrap(synthPerpContract).remainingMargin(address);
-        const marginRemaining = remainingMargin.marginRemaining;
-        if (Number(marginRemaining) === 0) return [];
-
-        const position = await multicall.wrap(synthPerpContract).positions(address);
-        const side = contractPosition.dataProps.side;
-        const size = Number(position.size);
-
-        const matchesSide =
-          (size > 0 && side === 'LONG') || (size < 0 && side === 'SHORT') || (size === 0 && side === 'NEUTRAL');
-        if (!matchesSide) return [];
-
-        const tokens = [drillBalance(contractPosition.tokens[0], marginRemaining.toString())];
-        const balanceUSD = sumBy(tokens, v => v.balanceUSD);
-
-        const contractPositionBalance: ContractPositionBalance = {
-          ...contractPosition,
-          tokens,
-          balanceUSD,
-        };
-
-        return contractPositionBalance;
-      }),
-    );
-
-    return allPositions.flat();
-  }
-
-  async getRawBalances(address: string): Promise<RawContractPositionBalance[]> {
-    const multicall = this.appToolkit.getMulticall(this.network);
-
-    const contractPositions = await this.appToolkit.getAppContractPositions({
-      appId: this.appId,
-      network: this.network,
-      groupIds: [this.groupId],
-    });
-
-    if (!contractPositions) return [];
-
-    return (
-      await Promise.all(
-        contractPositions
-          .map(async contractPosition => {
-            const synthPerpContract = this.contractFactory.synthetixPerp({
-              address: contractPosition.address,
-              network: this.network,
-            });
-            const remainingMargin = await multicall.wrap(synthPerpContract).remainingMargin(address);
-            const marginRemaining = remainingMargin.marginRemaining;
-            if (Number(marginRemaining) === 0) return [];
-
-            const position = await multicall.wrap(synthPerpContract).positions(address);
-            const side = contractPosition.dataProps.side;
-            const size = Number(position.size);
-
-            const matchesSide =
-              (size > 0 && side === 'LONG') || (size < 0 && side === 'SHORT') || (size === 0 && side === 'NEUTRAL');
-            if (!matchesSide) return [];
-
-            return [
-              {
-                key: this.appToolkit.getPositionKey(contractPosition),
-                tokens: [
-                  {
-                    key: this.appToolkit.getPositionKey(contractPosition.tokens[0]),
-                    balance: marginRemaining.toString(),
-                  },
-                ],
-              },
-            ];
-          })
-          .flat(),
-      )
-    ).flat();
+  async getTokenBalancesPerPosition({ address, contract }: GetTokenBalancesParams<SynthetixPerp>) {
+    const remainingMargin = await contract.remainingMargin(address);
+    return [remainingMargin.marginRemaining];
   }
 }
