8f9b5a0d008a0cdaac653ecc10046b8a1cac2b1d
Clonescody <clonescody@protonmail.com>
2023-05-14 20:24:17 -0700

    balances fetch logic

--- src/apps/sentiment/arbitrum/sentiment.account.contract-position-fetcher.ts
+++ src/apps/sentiment/arbitrum/sentiment.account.contract-position-fetcher.ts
@@ -1,33 +1,38 @@
 import { Inject } from '@nestjs/common';
-import { Contract } from 'ethers';
 
 import { APP_TOOLKIT, IAppToolkit } from '~app-toolkit/app-toolkit.interface';
 import { PositionTemplate } from '~app-toolkit/decorators/position-template.decorator';
 import { DefaultDataProps } from '~position/display.interface';
-import { ContractPositionBalance } from '~position/position-balance.interface';
 import { MetaType } from '~position/position.interface';
 import { ContractPositionTemplatePositionFetcher } from '~position/template/contract-position.template.position-fetcher';
 import {
-  GetDefinitionsParams,
+  GetTokenBalancesParams,
   GetTokenDefinitionsParams,
   UnderlyingTokenDefinition,
-  GetDisplayPropsParams,
-  GetTokenBalancesParams,
 } from '~position/template/contract-position.template.types';
 
 import { SentimentAccount, SentimentContractFactory } from '../contracts';
 
 import { SentimentDefinitionsResolver } from './sentiment.account.definitions-resolver';
+import { SENTIMENT_REGISTRY_IMPL_ADDRESS } from './sentiment.constants';
 
 export type SentimentAccountDefinition = {
   address: string;
   owner: string;
 };
 
+export type SentimentAccountDataProps = DefaultDataProps & {
+  address: string;
+  owner: string;
+};
+
 @PositionTemplate()
-export class ArbitrumSentimentAccountContractPositionFetcher extends ContractPositionTemplatePositionFetcher<Contract> {
+export class ArbitrumSentimentAccountContractPositionFetcher extends ContractPositionTemplatePositionFetcher<
+  SentimentAccount,
+  SentimentAccountDataProps,
+  SentimentAccountDefinition
+> {
   groupLabel: string;
-  subgraphUrl: string;
   investableAssets: string[];
   borrowableAssets: string[];
 
@@ -42,7 +47,7 @@ export class ArbitrumSentimentAccountContractPositionFetcher extends ContractPos
     this.borrowableAssets = [];
   }
 
-  async getDefinitions(_params: GetDefinitionsParams): Promise<SentimentAccountDefinition[]> {
+  async getDefinitions(): Promise<SentimentAccountDefinition[]> {
     const [sentimentAccounts, investableAssets, borrowableAssets] = await Promise.all([
       this.sentimentDefinitionsResolver.getAllAccountsDefinitions(),
       this.sentimentDefinitionsResolver.getAllInvestableAssetsFromSubgraph(),
@@ -78,28 +83,123 @@ export class ArbitrumSentimentAccountContractPositionFetcher extends ContractPos
     ];
   }
 
-  async getLabel(
-    _params: GetDisplayPropsParams<Contract, DefaultDataProps, SentimentAccountDefinition>,
-  ): Promise<string> {
-    return 'Sentiment account';
+  async getLabel(): Promise<string> {
+    return 'Sentiment accounts';
   }
 
-  async getTokenBalancesPerPosition(_params: GetTokenBalancesParams<SentimentAccount>) {
-    console.log('getTokenBalancesPerPosition');
-    return ['0'];
+  async getTokenBalancesPerPosition({ address, contractPosition }: GetTokenBalancesParams<SentimentAccount>) {
+    const allTokens = await this.appToolkit.getBaseTokenPrices(this.network);
+
+    const accountBalances = await Promise.all(
+      contractPosition.tokens.map(async (token, _index) => {
+        const tokenBase = allTokens.find(tokenBase => tokenBase.address === token.address);
+        if (!tokenBase) {
+          return '0';
+        }
+        if (token.metaType === MetaType.SUPPLIED) {
+          const balanceRaw = await this.sentimentContractFactory
+            .erc20({ address: token.address, network: this.network })
+            .balanceOf(address);
+          return balanceRaw;
+        }
+        if (token.metaType === MetaType.BORROWED) {
+          const lToken = await this.sentimentContractFactory
+            .sentimentRegistry({
+              address: SENTIMENT_REGISTRY_IMPL_ADDRESS,
+              network: this.network,
+            })
+            .LTokenFor(token.address);
+          const borrowBalanceRaw = await this.sentimentContractFactory
+            .sentimentLToken({ address: lToken, network: this.network })
+            .getBorrowBalance(address);
+
+          return borrowBalanceRaw;
+        }
+        return '0';
+      }),
+    );
+
+    return accountBalances;
   }
 
-  async getBalances(_address: string): Promise<ContractPositionBalance<DefaultDataProps>[]> {
-    console.log('this.appId : ', this.appId);
-    console.log('this.groupId : ', this.groupId);
-    const sentimentAccounts = await this.appToolkit.getAppContractPositions({
-      appId: this.appId,
-      groupIds: [this.groupId],
-      network: this.network,
-    });
-    console.log('accounts : ', sentimentAccounts);
-    const userAccounts = sentimentAccounts.filter(sentimentAccount => sentimentAccount.dataProps.owner === _address);
+  // async getBalances(_address: string): Promise<ContractPositionBalance<SentimentAccountDataProps>[]> {
+  //   const sentimentAccounts = await this.appToolkit.getAppContractPositions<SentimentAccountDataProps>({
+  //     appId: this.appId,
+  //     network: this.network,
+  //     groupIds: [this.groupId],
+  //   });
+  //   console.log('sentimentAccounts : ', sentimentAccounts);
+  //   const userAccounts = await this.sentimentContractFactory
+  //     .sentimentRegistry({
+  //       address: SENTIMENT_REGISTRY_IMPL_ADDRESS,
+  //       network: this.network,
+  //     })
+  //     .accountsOwnedBy(_address);
+  //   console.log('userAccounts from registry : ', userAccounts);
+  //   const allTokens = await this.appToolkit.getBaseTokenPrices(this.network);
 
-    return ['0'];
-  }
+  //   const accountsBalances: ContractPositionBalance<SentimentAccountDataProps>[] = await Promise.all(
+  //     sentimentAccounts.map(async accountPosition => {
+  //       const accountTokens: WithMetaType<TokenBalance>[] = [];
+
+  //       await Promise.all(
+  //         accountPosition.tokens.map(async (token, _index) => {
+  //           const tokenBase = allTokens.find(tokenBase => tokenBase.address === token.address);
+  //           if (!tokenBase) {
+  //             return;
+  //           }
+  //           if (token.metaType === MetaType.SUPPLIED) {
+  //             const balanceRaw = await this.sentimentContractFactory
+  //               .erc20({ address: token.address, network: this.network })
+  //               .balanceOf(_address);
+  //             const balance = balanceRaw.toNumber() / 10 ** tokenBase.decimals;
+  //             accountTokens[_index] = {
+  //               ...token,
+  //               ...tokenBase,
+  //               balanceRaw: balanceRaw.toString(),
+  //               balance,
+  //               balanceUSD: balance * tokenBase.price,
+  //             };
+  //           }
+  //           if (token.metaType === MetaType.BORROWED) {
+  //             const lToken = await this.sentimentContractFactory
+  //               .sentimentRegistry({
+  //                 address: SENTIMENT_REGISTRY_ADDRESS,
+  //                 network: this.network,
+  //               })
+  //               .LTokenFor(token.address);
+
+  //             const borrowBalanceRaw = await this.sentimentContractFactory
+  //               .sentimentLToken({ address: lToken, network: this.network })
+  //               .getBorrowBalance(_address);
+  //             const borrowBalance = borrowBalanceRaw.toNumber() / 10 ** tokenBase.decimals;
+
+  //             accountTokens[_index] = {
+  //               ...token,
+  //               ...tokenBase,
+  //               balanceRaw: borrowBalanceRaw.toString(),
+  //               balance: borrowBalance,
+  //               balanceUSD: borrowBalance * tokenBase.price,
+  //             };
+  //           }
+  //         }),
+  //       );
+  //       const totalDeposited = accountTokens
+  //         .filter(({ metaType }) => metaType === MetaType.SUPPLIED)
+  //         .reduce((acc, { balanceUSD }) => acc + balanceUSD, 0);
+  //       const totalBorrowed = accountTokens
+  //         .filter(({ metaType }) => metaType === MetaType.BORROWED)
+  //         .reduce((acc, { balanceUSD }) => acc + balanceUSD, 0);
+  //       console.log('total borrowed : ', totalBorrowed);
+  //       return {
+  //         ...accountPosition,
+  //         tokens: accountTokens,
+  //         balanceUSD: totalDeposited + totalBorrowed,
+  //         type: ContractType.POSITION,
+  //       };
+  //     }),
+  //   );
+
+  //   return accountsBalances;
+  // }
 }
