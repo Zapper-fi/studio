637d093b3932d19d51473f78e8f4de8757100121
William Poulin <will@zapper.fi>
2023-05-17 12:22:53 -0400

    feat(clever): Add veClev and platform fee positions and fix furnace and farms labels

--- src/apps/clever/clever.module.ts
+++ src/apps/clever/clever.module.ts
@@ -3,11 +3,13 @@ import { Module } from '@nestjs/common';
 import { AbstractApp } from '~app/app.dynamic-module';
 
 import { CleverContractFactory } from './contracts';
+import { EthereumCleverLockContractPositionFetcher } from './ethereum/clever.clever.contract-position-fetcher';
 import { EthereumCleverFarmingContractPositionFetcher } from './ethereum/clever.farming.contract-position-fetcher';
 import { EthereumCleverFurnaceContractPositionFetcher } from './ethereum/clever.furnace.contract-position-fetcher';
 import { EthereumCleverLeverTokenFetcher } from './ethereum/clever.lever.token-fetcher';
-import { EthereumCleverLockContractPositionFetcher } from './ethereum/clever.lock.contract-position-fetcher';
+import { EthereumCleverPlatformFeeContractPositionFetcher } from './ethereum/clever.platform-fee.contract-position-fetcher';
 import { EthereumCleverVestingContractPositionFetcher } from './ethereum/clever.vesting.contract-position-fetcher';
+import { EthereumCleverVotingEscrowContractPositionFetcher } from './ethereum/clever.voting-escrow.contract-position-fetcher';
 
 @Module({
   providers: [
@@ -17,6 +19,8 @@ import { EthereumCleverVestingContractPositionFetcher } from './ethereum/clever.
     EthereumCleverLockContractPositionFetcher,
     EthereumCleverFarmingContractPositionFetcher,
     EthereumCleverVestingContractPositionFetcher,
+    EthereumCleverVotingEscrowContractPositionFetcher,
+    EthereumCleverPlatformFeeContractPositionFetcher,
   ],
 })
 export class CleverAppModule extends AbstractApp() {}
