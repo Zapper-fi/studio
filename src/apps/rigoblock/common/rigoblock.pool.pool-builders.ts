45575407c74aef80da86e13566cb6c6763ae625c
gab <gabriele.rigo@gmail.com>
2023-05-24 20:46:29 +0200

    fix: L2 chain definitions

--- src/apps/rigoblock/common/rigoblock.pool.pool-builders.ts
+++ src/apps/rigoblock/common/rigoblock.pool.pool-builders.ts
@@ -18,14 +18,14 @@ export const POOL_BUILDERS: Partial<Record<Network, {
         blockNumber: 34751673,
       },
     ],
-    [Network.ARBITRUM]: [
+    [Network.ARBITRUM_MAINNET]: [
       {
         registryAddress: '0x06767e8090bA5c4Eca89ED00C3A719909D503ED6',
         tokenWhitelistAddress: '0xB43baD2638696F8bC82247B92bD56B8DF37d89aB',
         blockNumber: 32290603,
       },
     ],
-    [Network.OPTIMISM]: [
+    [Network.OPTIMISM_MAINNET]: [
       {
         registryAddress: '0x06767e8090bA5c4Eca89ED00C3A719909D503ED6',
         tokenWhitelistAddress: '0xB43baD2638696F8bC82247B92bD56B8DF37d89aB',
