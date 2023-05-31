2755182e031460e20705ceaaa6d661b59fb369cd
webmass <webmass@inverse.finance>
2023-05-31 16:21:08 +0200

    feat: Firm => add DBR rewards

--- src/apps/inverse-firm/ethereum/inverse-firm.loan.contract-position-fetcher.ts
+++ src/apps/inverse-firm/ethereum/inverse-firm.loan.contract-position-fetcher.ts
@@ -30,6 +30,11 @@ export class EthereumInverseFirmLoanContractPositionFetcher extends ContractPosi
   InverseFirmLoanContractPositionDefinition
 > {
   groupLabel = 'Lending';
+  dbrAddress = '0xad038eb671c44b853887a7e32528fab35dc5d710'
+  dbrDistributor = '0xdcd2D918511Ba39F2872EB731BB88681AE184244'
+  invAddress = '0x41D5D79431A913C4aE7d69a668ecdfE5fF9DFB68'
+  xinvAddress = '0x1637e4e9941D55703a7A5E7807d6aDA3f7DCD61B'
+  dolaAddress = '0x865377367054516e17014ccded1e7d814edc9ce4'
 
   constructor(
     @Inject(APP_TOOLKIT) protected readonly appToolkit: IAppToolkit,
@@ -46,7 +51,7 @@ export class EthereumInverseFirmLoanContractPositionFetcher extends ContractPosi
     const multicall = this.appToolkit.getMulticall(this.network);
 
     const dolaBorrowRightContract = this.contractFactory.dbr({
-      address: '0xad038eb671c44b853887a7e32528fab35dc5d710',
+      address: this.dbrAddress,
       network: this.network,
     });
 
@@ -61,7 +66,7 @@ export class EthereumInverseFirmLoanContractPositionFetcher extends ContractPosi
         return {
           address,
           suppliedTokenAddress: collateralTokenAddressRaw.toLowerCase(),
-          borrowedTokenAddress: '0x865377367054516e17014ccded1e7d814edc9ce4', // dola
+          borrowedTokenAddress: this.dolaAddress, // dola
         };
       }),
     );
@@ -107,5 +112,81 @@ export class EthereumInverseFirmLoanContractPositionFetcher extends ContractPosi
     const borrowed = await contract.debts(address);
 
     return [supplied, borrowed];
+  }
+
+  async getRewardTokenBalances({
+    address,
+    multicall,
+    contract,
+  }: GetTokenDefinitionsParams<SimpleMarket>): Promise<BigNumberish[]> {
+    const personalEscrow = await contract.escrows(address);
+    if (personalEscrow === ZERO_ADDRESS) return [];
+
+    const escrowContract = this.contractFactory.rewardableEscrow({
+      address: personalEscrow.toLowerCase(),
+      network: this.network,
+    });
+
+    let dbrClaimable: BigNumberish = 0;
+
+    try {
+      // escrow may or not be with the rewards feature
+      dbrClaimable = await multicall.wrap(escrowContract).claimable();
+    } catch (e) {
+      // no claimable rewards feature for this market
+    }
+
+    return [
+      dbrClaimable,
+    ];
+  }
+
+  async getRewardTokenAddresses() {
+    return [
+      this.dbrAddress,
+    ];
+  }
+
+  async getRewardRates({
+    multicall
+  }) {
+    const dbrDistributorContract = this.contractFactory.dbrDistributor({
+      address: this.dbrDistributor,
+      network: this.network,
+    });
+    const xinvContract = this.contractFactory.xinv({
+      address: this.xinvAddress,
+      network: this.network,
+    });
+
+    const [
+      rewardRate,
+      dbrDistributorSupply,
+      xinvExRate,
+    ] = await Promise.all([
+      multicall.wrap(dbrDistributorContract).rewardRate(),
+      multicall.wrap(dbrDistributorContract).totalSupply(),
+      multicall.wrap(xinvContract).exchangeRateStored(),
+    ]);
+
+    const [dbrtoken, invToken] = await Promise.all([
+      this.appToolkit.getBaseTokenPrice({
+        network: this.network,
+        address: this.dbrAddress,
+      }),
+      this.appToolkit.getBaseTokenPrice({
+        network: this.network,
+        address: this.invAddress,
+      }),
+    ]);
+
+    if (!dbrtoken || !invToken) return [0];
+
+    const invStakedViaDistributor = xinvExRate.mul(dbrDistributorSupply);
+    const dbrYearlyRewardRate = rewardRate.mul(31536000);
+    const dbrInvExRate = dbrtoken.price / invToken.price;
+    const dbrApr = Number(dbrYearlyRewardRate) * Number(dbrInvExRate) / Number(invStakedViaDistributor) / 1e18 * 100;
+
+    return [dbrApr];
   }
 }
