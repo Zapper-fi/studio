956a6181c786215b74d762f69f64b5d70f394916
William Poulin <will@zapper.fi>
2023-06-21 09:36:44 -0400

    fix(solace): Remove app integration(v1)

--- src/apps/solace/ethereum/solace.scp.token-fetcher.ts
+++
@@ -1,9 +0,0 @@
-import { PositionTemplate } from '~app-toolkit/decorators/position-template.decorator';
-
-import { SolaceScpTokenFetcher } from '../common/solace.scp.token-fetcher';
-
-@PositionTemplate()
-export class EthereumSolaceScpTokenFetcher extends SolaceScpTokenFetcher {
-  groupLabel = 'SCP';
-  scpAddress = '0x501acee83a6f269b77c167c6701843d454e2efa0';
-}
--- src/apps/solace/ethereum/solace.x-solace-v1.token-fetcher.ts
+++
@@ -1,9 +0,0 @@
-import { PositionTemplate } from '~app-toolkit/decorators/position-template.decorator';
-
-import { SolaceXSolacev1TokenFetcher } from '../common/solace.x-solace-v1.token-fetcher';
-
-@PositionTemplate()
-export class EthereumSolaceXsolacev1TokenFetcher extends SolaceXSolacev1TokenFetcher {
-  groupLabel = 'xSOLACEv1';
-  xSolaceV1Address = '0x501ace5ac3af20f49d53242b6d208f3b91cfc411';
-}
