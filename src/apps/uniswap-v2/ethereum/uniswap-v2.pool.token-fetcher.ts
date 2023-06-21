e451c10ef2ce0eca49a7e369cc5a83b721807e9b
William Poulin <will@zapper.fi>
2023-06-21 14:07:56 -0400

    fix(uniswap-v2): Temporary replace subgraph url until indexing issues are fixed

--- src/apps/uniswap-v2/ethereum/uniswap-v2.pool.token-fetcher.ts
+++ src/apps/uniswap-v2/ethereum/uniswap-v2.pool.token-fetcher.ts
@@ -38,7 +38,7 @@ export class EthereumUniswapV2PoolTokenFetcher extends UniswapV2DefaultPoolSubgr
   groupLabel = 'Pools';
 
   factoryAddress = '0x5c69bee701ef814a2b6a3edd4b1652cb9cc5aa6f';
-  subgraphUrl = 'https://api.thegraph.com/subgraphs/name/ianlapham/uniswapv2';
+  subgraphUrl = 'https://api.thegraph.com/subgraphs/name/ianlapham/uniswap-v2-dev';
   ignoredPools = ['0x3016a43b482d0480460f6625115bd372fe90c6bf'];
   first = 5000;
   // Todo: remove when subgraph isn't throwing when calling volumeUSD
