bfc81260dc50ac05a217e1d65768f59de1d22cae
William Poulin <will@zapper.fi>
2023-06-22 14:28:37 -0400

    feat(teahouse): Add vaults on Arbitrum

--- src/apps/teahouse/teahouse.module.ts
+++ src/apps/teahouse/teahouse.module.ts
@@ -2,11 +2,17 @@ import { Module } from '@nestjs/common';
 
 import { AbstractApp } from '~app/app.dynamic-module';
 
+import { ArbitrumTeahouseVaultsTokenFetcher } from './arbitrum/teahouse.vaults.token-fetcher';
 import { TeahouseContractFactory } from './contracts';
 import { EthereumTeahouseVaultsTokenFetcher } from './ethereum/teahouse.vaults.token-fetcher';
 import { OptimismTeahouseVaultsTokenFetcher } from './optimism/teahouse.vaults.token-fetcher';
 
 @Module({
-  providers: [TeahouseContractFactory, EthereumTeahouseVaultsTokenFetcher, OptimismTeahouseVaultsTokenFetcher],
+  providers: [
+    TeahouseContractFactory,
+    ArbitrumTeahouseVaultsTokenFetcher,
+    EthereumTeahouseVaultsTokenFetcher,
+    OptimismTeahouseVaultsTokenFetcher,
+  ],
 })
 export class TeahouseAppModule extends AbstractApp() {}
