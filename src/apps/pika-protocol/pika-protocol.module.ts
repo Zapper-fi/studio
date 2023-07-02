646e20151cfaf8bcbd0a7960d3c044a0bf651560
William Poulin <will@zapper.fi>
2023-07-02 17:22:29 -0400

    feat(pika-protocol): Add EsPika and Vest Escrow Pika position (#2794)

--- src/apps/pika-protocol/pika-protocol.module.ts
+++ src/apps/pika-protocol/pika-protocol.module.ts
@@ -3,9 +3,16 @@ import { Module } from '@nestjs/common';
 import { AbstractApp } from '~app/app.dynamic-module';
 
 import { PikaProtocolContractFactory } from './contracts';
+import { OptimismPikaProtocolEsPikaTokenFetcher } from './optimism/pika-protocol.es-pika.token-fetcher';
+import { OptimismPikaProtocolEscrowContractPositionFetcher } from './optimism/pika-protocol.escrowed.contract-position-fetcher';
 import { OptimismPikaProtocolVaultContractPositionFetcher } from './optimism/pika-protocol.vault.contract-position-fetcher';
 
 @Module({
-  providers: [PikaProtocolContractFactory, OptimismPikaProtocolVaultContractPositionFetcher],
+  providers: [
+    PikaProtocolContractFactory,
+    OptimismPikaProtocolVaultContractPositionFetcher,
+    OptimismPikaProtocolEsPikaTokenFetcher,
+    OptimismPikaProtocolEscrowContractPositionFetcher,
+  ],
 })
 export class PikaProtocolAppModule extends AbstractApp() {}
