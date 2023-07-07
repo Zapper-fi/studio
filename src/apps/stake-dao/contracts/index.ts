44274a067e9618a528a23f1dbbd150ca4611d217
William Poulin <will@zapper.fi>
2023-07-07 15:24:42 -0400

    feat(stake-dao): Add vault tokens, passive vault tokens, multi-gauge positions and add addresses to gauge position

--- src/apps/stake-dao/contracts/index.ts
+++ src/apps/stake-dao/contracts/index.ts
@@ -4,10 +4,15 @@ import { IAppToolkit, APP_TOOLKIT } from '~app-toolkit/app-toolkit.interface';
 import { ContractFactory } from '~contract/contracts';
 import { Network } from '~types/network.interface';
 
-import { StakeDaoCurvePool__factory } from './ethers';
-import { StakeDaoFarm__factory } from './ethers';
-import { StakeDaoGauge__factory } from './ethers';
-import { StakeDaoVotingEscrow__factory } from './ethers';
+import {
+  StakeDaoCurvePool__factory,
+  StakeDaoFarm__factory,
+  StakeDaoGauge__factory,
+  StakeDaoMultiGauge__factory,
+  StakeDaoPassiveVault__factory,
+  StakeDaoVault__factory,
+  StakeDaoVotingEscrow__factory,
+} from './ethers';
 
 // eslint-disable-next-line
 type ContractOpts = { address: string; network: Network };
@@ -27,6 +32,15 @@ export class StakeDaoContractFactory extends ContractFactory {
   stakeDaoGauge({ address, network }: ContractOpts) {
     return StakeDaoGauge__factory.connect(address, this.appToolkit.getNetworkProvider(network));
   }
+  stakeDaoMultiGauge({ address, network }: ContractOpts) {
+    return StakeDaoMultiGauge__factory.connect(address, this.appToolkit.getNetworkProvider(network));
+  }
+  stakeDaoPassiveVault({ address, network }: ContractOpts) {
+    return StakeDaoPassiveVault__factory.connect(address, this.appToolkit.getNetworkProvider(network));
+  }
+  stakeDaoVault({ address, network }: ContractOpts) {
+    return StakeDaoVault__factory.connect(address, this.appToolkit.getNetworkProvider(network));
+  }
   stakeDaoVotingEscrow({ address, network }: ContractOpts) {
     return StakeDaoVotingEscrow__factory.connect(address, this.appToolkit.getNetworkProvider(network));
   }
@@ -35,4 +49,7 @@ export class StakeDaoContractFactory extends ContractFactory {
 export type { StakeDaoCurvePool } from './ethers';
 export type { StakeDaoFarm } from './ethers';
 export type { StakeDaoGauge } from './ethers';
+export type { StakeDaoMultiGauge } from './ethers';
+export type { StakeDaoPassiveVault } from './ethers';
+export type { StakeDaoVault } from './ethers';
 export type { StakeDaoVotingEscrow } from './ethers';
