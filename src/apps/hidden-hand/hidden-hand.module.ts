45838beb0ea7ed88ff2b5f525f2c407773bdd8c3
rafael albquerque bezerra <bzrra.rafael@gmail.com>
2023-07-13 00:18:16 -0400

    feat(module): add arbitrum

--- src/apps/hidden-hand/hidden-hand.module.ts
+++ src/apps/hidden-hand/hidden-hand.module.ts
@@ -2,16 +2,18 @@ import { Module } from '@nestjs/common';
 
 import { AbstractApp } from '~app/app.dynamic-module';
 
+import { ArbitrumHiddenHandRewardsContractPositionFetcher } from './arbitrum/hidden-hand.rewards.contract-position-fetcher';
+import { HiddenHandRewardsResolver } from './common/hidden-hand.rewards-resolver';
 import { HiddenHandContractFactory } from './contracts';
 import { EthereumHiddenHandRewardsContractPositionFetcher } from './ethereum/hidden-hand.rewards.contract-position-fetcher';
 import { OptimismHiddenHandRewardsContractPositionFetcher } from './optimism/hidden-hand.rewards.contract-position-fetcher';
-import { HiddenHandRewardsResolver } from './common/hidden-hand.rewards-resolver';
 
 @Module({
   providers: [
     HiddenHandRewardsResolver,
     EthereumHiddenHandRewardsContractPositionFetcher,
     OptimismHiddenHandRewardsContractPositionFetcher,
+    ArbitrumHiddenHandRewardsContractPositionFetcher,
     HiddenHandContractFactory,
   ],
 })
