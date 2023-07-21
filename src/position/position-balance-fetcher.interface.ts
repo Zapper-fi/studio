2544869acfdac9ce70e864e97c9db8cbd06d031c
immasandwich <karan@zapper.fi>
2023-07-21 10:07:15 -0400

    feat(studio): Eliminate legacy code for position balance fetchers (#2834)

--- src/position/position-balance-fetcher.interface.ts
+++
@@ -1,6 +0,0 @@
-import { DefaultDataProps } from './display.interface';
-import { PositionBalance } from './position-balance.interface';
-
-export interface PositionBalanceFetcher<T extends PositionBalance<V>, V = DefaultDataProps> {
-  getBalances(address: string): Promise<T[]>;
-}
