d23de2488ec0f8e466d76bdc6e0530d016375c71
William Poulin <will@zapper.fi>
2023-07-28 20:52:53 -0400

    fix(vendor-finance): Fetch pool borrow position on chain (#2849)

--- src/apps/vendor-finance/vendor-finance.module.ts
+++ src/apps/vendor-finance/vendor-finance.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 
 import { AbstractApp } from '~app/app.dynamic-module';
 
-import { ArbitrumVendorFinancePoolContractPositionFetcher } from './arbitrum/vendor-finance.pool.contract-position-fetcher';
 import { ArbitrumVendorFinancePoolV2ContractPositionFetcher } from './arbitrum/vendor-finance.pool-v2.contract-position-fetcher';
+import { ArbitrumVendorFinancePoolContractPositionFetcher } from './arbitrum/vendor-finance.pool.contract-position-fetcher';
 import { VendorFinanceContractFactory } from './contracts';
 import { EthereumVendorFinancePoolContractPositionFetcher } from './ethereum/vendor-finance.pool.contract-position-fetcher';
 
