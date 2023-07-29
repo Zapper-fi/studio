6821ffd8779d33f5cd9b27e4d8d36e5be36311e1
William Poulin <will@zapper.fi>
2023-07-28 21:51:30 -0400

    fix(vela): Fix VLP and esVela app tokens

--- src/apps/vela/arbitrum/vela.es-vela.token-fetcher.ts
+++ src/apps/vela/arbitrum/vela.es-vela.token-fetcher.ts
@@ -4,6 +4,6 @@ import { VelaEsVelaTokenFetcher } from '../common/vela.es-vela.token-fetcher';
 
 @PositionTemplate()
 export class ArbitrumVelaEsVelaTokenFetcher extends VelaEsVelaTokenFetcher {
-  esVelaAddress = '0xa9f5606c3e6aab998fd4f4bc54a18d9fe13a0dd8';
+  esVelaAddress = '0xefd5a713c5bd85e9ced46070b2532e4a47a18102';
   velaAddress = '0x088cd8f5ef3652623c22d48b1605dcfe860cd704';
 }
--- src/apps/vela/arbitrum/vela.vlp.token-fetcher.ts
+++ src/apps/vela/arbitrum/vela.vlp.token-fetcher.ts
@@ -4,15 +4,7 @@ import { VelaVlpTokenFetcher } from '../common/vela.vlp.token-fetcher';
 
 @PositionTemplate()
 export class ArbitrumVelaVlpTokenFetcher extends VelaVlpTokenFetcher {
-  vlpAddress = '0x4e0d4a5a5b4faf5c2ecc1c63c8d19bb0804a96f1';
+  vlpAddress = '0xc5b2d9fda8a82e8dcecd5e9e6e99b78a9188eb05';
   usdcAddress = '0xff970a61a04b1ca14834a43f5de4533ebddb5cc8';
-  velaVaultAddress = '0x5957582f020301a2f732ad17a69ab2d8b2741241';
-
-  async getApy(): Promise<number> {
-    return 0;
-  }
-
-  async getPricePerShare(): Promise<number[]> {
-    return [0];
-  }
+  vaultAddress = '0xc4abade3a15064f9e3596943c699032748b13352';
 }
