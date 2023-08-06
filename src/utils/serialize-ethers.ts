26aa7ce6da2ee51cc605b69362b380f79f33539c
Karan Shahani <karan.shahani91@gmail.com>
2023-08-05 22:22:26 -0400

    feat(studio): Serialize exactly market

--- src/utils/serialize-ethers.ts
+++ src/utils/serialize-ethers.ts
@@ -1,8 +1,13 @@
 import { isBigNumberish } from '@ethersproject/bignumber/lib/bignumber';
 import _ from 'lodash';
 
+const isPureArray = (obj: any): obj is any[] => {
+  const props = Object.getOwnPropertyNames(obj);
+  return _.isArray(obj) && props.every(key => key === 'length' || _.isInteger(_.toNumber(key)));
+};
+
 export const serializeEthersResult = (obj: Record<string, any>) => {
-  if (_.isArray(obj)) {
+  if (isPureArray(obj)) {
     return obj.map(innerObj => serializeEthersResult(innerObj));
   } else if (_.isObject(obj) && !isBigNumberish(obj)) {
     return _.mapValues(obj, val => serializeEthersResult(val));
