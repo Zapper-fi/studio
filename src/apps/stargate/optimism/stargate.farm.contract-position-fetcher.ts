370a67bf6ab4e301b4423c411a54ba58853dd756
William Poulin <will@zapper.fi>
2023-08-15 13:07:51 -0400

    feat(stargate): Support Base

--- src/apps/stargate/optimism/stargate.farm.contract-position-fetcher.ts
+++ src/apps/stargate/optimism/stargate.farm.contract-position-fetcher.ts
@@ -1,9 +1,17 @@
 import { PositionTemplate } from '~app-toolkit/decorators/position-template.decorator';
 
 import { StargateFarmContractPositionFetcher } from '../common/stargate.farm.contract-position-fetcher';
+import { StargateChef } from '../contracts';
 
 @PositionTemplate()
-export class OptimismStargateFarmContractPositionFetcher extends StargateFarmContractPositionFetcher {
+export class OptimismStargateFarmContractPositionFetcher extends StargateFarmContractPositionFetcher<StargateChef> {
   groupLabel = 'Farms';
   chefAddress = '0x4a364f8c717caad9a442737eb7b8a55cc6cf18d8';
+
+  getStargateChefContract(address: string): StargateChef {
+    return this.contractFactory.stargateChef({ address, network: this.network });
+  }
+  getStargateTokenAddress(contract: StargateChef): Promise<string> {
+    return contract.stargate();
+  }
 }
