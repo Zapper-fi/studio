06c9eaef12930766f2c9e2898307a6d9c4401470
William Poulin <will@zapper.fi>
2023-09-30 22:44:57 -0400

    fix(balancer-v2): Filter out faulty reawrd address from Balancer-v2 farm position (#2959)

--- src/apps/balancer-v2/base/balancer-v2.farm.contract-position-fetcher.ts
+++ src/apps/balancer-v2/base/balancer-v2.farm.contract-position-fetcher.ts
@@ -6,4 +6,6 @@ import { BalancerV2FarmContractPositionFetcher } from '../common/balancer-v2.far
 export class BaseBalancerV2FarmContractPositionFetcher extends BalancerV2FarmContractPositionFetcher {
   groupLabel = 'Staked';
   subgraphUrl = 'https://api.studio.thegraph.com/query/24660/balancer-gauges-base/version/latest';
+
+  faultyRewardAddress = ['0x65226673f3d202e0f897c862590d7e1a992b2048'];
 }
