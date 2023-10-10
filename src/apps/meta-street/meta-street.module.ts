4e7b735ac3f8b5e47f90d7982822b8e019dfc08d
greg <wegory@users.noreply.github.com>
2023-10-10 22:17:51 +0800

    feat(metastreet): Add MetaStreet App (#2955)
    Co-authored-by: wegory <gregoryteo@hotmail.sg>

---
+++ src/apps/meta-street/meta-street.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+
+import { AbstractApp } from '~app/app.dynamic-module';
+
+import { MetaStreetContractFactory } from './contracts';
+import { EthereumMetaStreetLendingV2ContractPositionFetcher } from './ethereum/meta-street.lending-v2.contract-position-fetcher';
+import { EthereumMetaStreetLendingV2LegacyContractPositionFetcher } from './ethereum/meta-street.lending-v2-legacy.contract-position-fetcher';
+
+@Module({
+  providers: [
+    EthereumMetaStreetLendingV2ContractPositionFetcher,
+    EthereumMetaStreetLendingV2LegacyContractPositionFetcher,
+    MetaStreetContractFactory,
+  ],
+})
+export class MetaStreetAppModule extends AbstractApp() {}
