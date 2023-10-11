1784daf79f1f4af5a63f4aaf12001baeab58b2ee
William Poulin <will@zapper.fi>
2023-10-11 16:08:07 -0400

    fix(granary-finance): Add variable debt on Base

--- src/apps/granary-finance/fantom/granary-finance.position-presenter.ts
+++ src/apps/granary-finance/fantom/granary-finance.position-presenter.ts
@@ -1,7 +1,29 @@
 import { PresenterTemplate } from '~app-toolkit/decorators/presenter-template.decorator';
+import { PresentationConfig } from '~app/app.interface';
 import { AaveV2PositionPresenter } from '~apps/aave-v2/common/aave-v2.position-presenter';
 
 @PresenterTemplate()
 export class FantomGranaryFinancePositionPresenter extends AaveV2PositionPresenter {
   lendingPoolAddress = '0x7220ffd5dc173ba3717e47033a01d870f06e5284';
+
+  explorePresentationConfig?: PresentationConfig = {
+    tabs: [
+      {
+        label: 'Lending',
+        viewType: 'split',
+        views: [
+          {
+            viewType: 'list',
+            label: 'Supply',
+            groupIds: ['supply'],
+          },
+          {
+            viewType: 'list',
+            label: 'Borrow',
+            groupIds: ['variable-debt'],
+          },
+        ],
+      },
+    ],
+  };
 }
