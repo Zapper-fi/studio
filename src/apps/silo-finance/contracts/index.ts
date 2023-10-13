ef3805a60fc624dfea4c96a8569048fa930ab262
William Poulin <will@zapper.fi>
2023-10-13 13:18:55 -0400

    fix(silo-finance): Migrate app tokens to ATI

--- src/apps/silo-finance/contracts/index.ts
+++ src/apps/silo-finance/contracts/index.ts
@@ -4,7 +4,7 @@ import { IAppToolkit, APP_TOOLKIT } from '~app-toolkit/app-toolkit.interface';
 import { ContractFactory } from '~contract/contracts';
 import { Network } from '~types/network.interface';
 
-import { Silo__factory, SiloIncentives__factory, SiloLens__factory, SiloMarketAsset__factory } from './ethers';
+import { SiloIncentives__factory } from './ethers';
 
 // eslint-disable-next-line
 type ContractOpts = { address: string; network: Network };
@@ -15,21 +15,9 @@ export class SiloFinanceContractFactory extends ContractFactory {
     super((network: Network) => appToolkit.getNetworkProvider(network));
   }
 
-  silo({ address, network }: ContractOpts) {
-    return Silo__factory.connect(address, this.appToolkit.getNetworkProvider(network));
-  }
   siloIncentives({ address, network }: ContractOpts) {
     return SiloIncentives__factory.connect(address, this.appToolkit.getNetworkProvider(network));
   }
-  siloLens({ address, network }: ContractOpts) {
-    return SiloLens__factory.connect(address, this.appToolkit.getNetworkProvider(network));
-  }
-  siloMarketAsset({ address, network }: ContractOpts) {
-    return SiloMarketAsset__factory.connect(address, this.appToolkit.getNetworkProvider(network));
-  }
 }
 
-export type { Silo } from './ethers';
 export type { SiloIncentives } from './ethers';
-export type { SiloLens } from './ethers';
-export type { SiloMarketAsset } from './ethers';
