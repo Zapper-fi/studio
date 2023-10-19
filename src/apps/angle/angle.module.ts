ca830a69f3a32b8cdbd5a54a02ad499b50658a00
William Poulin <will@zapper.fi>
2023-10-19 15:56:49 -0400

    fix(angle): Remove Perps (#2988)

--- src/apps/angle/angle.module.ts
+++ src/apps/angle/angle.module.ts
@@ -4,7 +4,6 @@ import { AbstractApp } from '~app/app.dynamic-module';
 
 import { AngleApiHelper } from './common/angle.api';
 import { AngleContractFactory } from './contracts';
-import { EthereumAnglePerpetualsContractPositionFetcher } from './ethereum/angle.perpetual.contract-position-fetcher';
 import { EthereumAngleSanTokenTokenFetcher } from './ethereum/angle.san-token.token-fetcher';
 import { EthereumAngleVaultsContractPositionFetcher } from './ethereum/angle.vault.contract-position-fetcher';
 import { EthereumAngleVeAngleContractPositionFetcher } from './ethereum/angle.voting-escrow.contract-position-fetcher';
@@ -16,7 +15,6 @@ import { EthereumAngleVeAngleContractPositionFetcher } from './ethereum/angle.vo
     // Ethereum
     EthereumAngleSanTokenTokenFetcher,
     EthereumAngleVeAngleContractPositionFetcher,
-    EthereumAnglePerpetualsContractPositionFetcher,
     EthereumAngleVaultsContractPositionFetcher,
   ],
 })
