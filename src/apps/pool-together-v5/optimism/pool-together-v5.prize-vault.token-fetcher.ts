cc618ad53f4ceb6e7bf725ab689623508e1c598d
Ncookiez <ncukiert@gmail.com>
2023-10-22 14:00:14 -0400

    PT V5 Optimism Support

---
+++ src/apps/pool-together-v5/optimism/pool-together-v5.prize-vault.token-fetcher.ts
@@ -0,0 +1,7 @@
+import { PositionTemplate } from '~app-toolkit/decorators/position-template.decorator';
+import { PoolTogetherV5PrizeVaultTokenFetcher } from '../common/pool-together-v5.prize-vault.token-fetcher';
+
+@PositionTemplate()
+export class OptimismPoolTogetherV5PrizeVaultTokenFetcher extends PoolTogetherV5PrizeVaultTokenFetcher {
+  groupLabel: 'Prize Vaults';
+}
