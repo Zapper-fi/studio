0552a3ca4da8afcb81292aa8a05c1de0f10f9378
William Poulin <will@zapper.fi>
2023-10-23 13:42:40 -0400

    fix(yearn): Migrate all app tokens to ATI (#2993)

--- src/apps/yearn/yearn.module.ts
+++ src/apps/yearn/yearn.module.ts
@@ -2,34 +2,18 @@ import { Module } from '@nestjs/common';
 
 import { AbstractApp } from '~app/app.dynamic-module';
 
-import { ArbitrumYearnVaultTokenFetcher } from './arbitrum/yearn.vault.token-fetcher';
-import { YearnVaultTokenDefinitionsResolver } from './common/yearn.vault.token-definitions-resolver';
 import { YearnContractFactory } from './contracts';
 import { EthereumYearnGovernanceContractPositionFetcher } from './ethereum/yearn.governance.contract-position-fetcher';
-import { EthereumYearnVaultTokenFetcher } from './ethereum/yearn.vault.token-fetcher';
 import { EthereumYearnVeYfiContractPositionFetcher } from './ethereum/yearn.ve-yfi.contract-position-fetcher';
-import { EthereumYearnYCrvTokenTokenFetcher } from './ethereum/yearn.y-crv.token-fetcher';
-import { EthereumYearnYieldTokenFetcher } from './ethereum/yearn.yield.token-fetcher';
-import { FantomYearnVaultTokenFetcher } from './fantom/yearn.vault.token-fetcher';
 import { OptimismYearnSakingContractPositionFetcher } from './optimism/yearn.staking.contract-position-fetcher';
-import { OptimismYearnVaultTokenFetcher } from './optimism/yearn.vault.token-fetcher';
 
 @Module({
   providers: [
     YearnContractFactory,
-    YearnVaultTokenDefinitionsResolver,
-    // Arbitrum
-    ArbitrumYearnVaultTokenFetcher,
     // Ethereum
-    EthereumYearnVaultTokenFetcher,
     EthereumYearnGovernanceContractPositionFetcher,
-    EthereumYearnYieldTokenFetcher,
-    EthereumYearnYCrvTokenTokenFetcher,
     EthereumYearnVeYfiContractPositionFetcher,
-    // Fantom
-    FantomYearnVaultTokenFetcher,
     // Optimism
-    OptimismYearnVaultTokenFetcher,
     OptimismYearnSakingContractPositionFetcher,
   ],
 })
