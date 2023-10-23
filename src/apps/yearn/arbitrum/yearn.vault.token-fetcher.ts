0730a6f5f1e7a5d8a6d2a27a38d0664e26b1a495
William Poulin <will@zapper.fi>
2023-10-23 13:37:15 -0400

    fix(yearn): Migrate all app tokens to ATI

--- src/apps/yearn/arbitrum/yearn.vault.token-fetcher.ts
+++
@@ -1,8 +0,0 @@
-import { PositionTemplate } from '~app-toolkit/decorators/position-template.decorator';
-
-import { YearnVaultTokenFetcher } from '../common/yearn.vault.token-fetcher';
-
-@PositionTemplate()
-export class ArbitrumYearnVaultTokenFetcher extends YearnVaultTokenFetcher {
-  groupLabel = 'Vault';
-}
