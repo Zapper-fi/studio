7bc1dc7e10680a1ddc89b2c7e42f84e5d517f2d2
William Poulin <will@zapper.fi>
2023-10-23 22:36:33 -0400

    fix(pool-together-v5): Linting (#2997)

--- src/apps/pool-together-v5/pool-together-v5.module.ts
+++ src/apps/pool-together-v5/pool-together-v5.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 
 import { AbstractApp } from '~app/app.dynamic-module';
-import { OptimismPoolTogetherV5PrizeVaultTokenFetcher } from './optimism/pool-together-v5.prize-vault.token-fetcher';
+
 import { PoolTogetherV5ContractFactory } from './contracts';
+import { OptimismPoolTogetherV5PrizeVaultTokenFetcher } from './optimism/pool-together-v5.prize-vault.token-fetcher';
 
 @Module({
-  providers: [
-    OptimismPoolTogetherV5PrizeVaultTokenFetcher,
-    PoolTogetherV5ContractFactory
-  ],
+  providers: [PoolTogetherV5ContractFactory, OptimismPoolTogetherV5PrizeVaultTokenFetcher],
 })
 export class PoolTogetherV5AppModule extends AbstractApp() {}
