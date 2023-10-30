2d284d700ed409602017d3cce63d8915abae9469
William Poulin <will@zapper.fi>
2023-10-30 17:20:35 -0400

    fix(apps): Removed apps on Aurora and Moonriver

--- src/apps/beefy/moonriver/beefy.vault.token-fetcher.ts
+++
@@ -1,8 +0,0 @@
-import { PositionTemplate } from '~app-toolkit/decorators/position-template.decorator';
-
-import { BeefyVaultTokenFetcher } from '../common/beefy.vault.token-fetcher';
-
-@PositionTemplate()
-export class MoonriverBeefyVaultTokenFetcher extends BeefyVaultTokenFetcher {
-  groupLabel = 'Vaults';
-}
--- src/apps/trisolaris/aurora/trisolaris.pool.token-fetcher.ts
+++
@@ -1,8 +0,0 @@
-import { PositionTemplate } from '~app-toolkit/decorators/position-template.decorator';
-import { UniswapV2DefaultPoolOnChainTemplateTokenFetcher } from '~apps/uniswap-v2/common/uniswap-v2.default.pool.on-chain.template.token-fetcher';
-
-@PositionTemplate()
-export class AuroraTrisolarisPoolTokenFetcher extends UniswapV2DefaultPoolOnChainTemplateTokenFetcher {
-  factoryAddress = '0xc66f594268041db60507f00703b152492fb176e7';
-  groupLabel = 'Pools';
-}
