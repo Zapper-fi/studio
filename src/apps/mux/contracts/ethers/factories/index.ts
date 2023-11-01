bb2ec3226c9c171f450e325df945eef17e081a18
William Poulin <will@zapper.fi>
2023-11-01 13:06:25 -0400

    feat(mux): Add veMUX (#3008)

--- src/apps/mux/contracts/ethers/factories/index.ts
+++ src/apps/mux/contracts/ethers/factories/index.ts
@@ -6,3 +6,4 @@ export { MuxReader__factory } from './MuxReader__factory';
 export { MuxRewardRouter__factory } from './MuxRewardRouter__factory';
 export { MuxRewardTracker__factory } from './MuxRewardTracker__factory';
 export { MuxVault__factory } from './MuxVault__factory';
+export { MuxVeMux__factory } from './MuxVeMux__factory';
