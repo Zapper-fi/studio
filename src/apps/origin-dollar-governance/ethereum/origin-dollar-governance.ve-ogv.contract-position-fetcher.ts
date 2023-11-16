85f350d1d93822da0160fd1d273c9e2ac29757da
William Poulin <will@zapper.fi>
2023-11-15 19:30:27 -0500

    fix(origin-dollar-governance): Fix veOGV underlying balance and combined both veOGV balance and rewards into a veOGV cp (#3033)
    * fix(origin-dollar-governance): Fix veOGV underlying balance and combined both veOGV balance and rewards into a veOGV cp
    
    * feat(origin-dollar-governance): Bump veOGV max lockups to 20

--- src/apps/origin-dollar-governance/ethereum/origin-dollar-governance.ve-ogv.contract-position-fetcher.ts
+++ src/apps/origin-dollar-governance/ethereum/origin-dollar-governance.ve-ogv.contract-position-fetcher.ts
@@ -1,14 +1,18 @@
 import { Inject } from '@nestjs/common';
 import { BigNumberish } from 'ethers';
+import { compact, range, sum } from 'lodash';
 
 import { APP_TOOLKIT, IAppToolkit } from '~app-toolkit/app-toolkit.interface';
 import { PositionTemplate } from '~app-toolkit/decorators/position-template.decorator';
+import { getLabelFromToken } from '~app-toolkit/helpers/presentation/image.present';
 import { DefaultDataProps } from '~position/display.interface';
 import { MetaType } from '~position/position.interface';
 import { ContractPositionTemplatePositionFetcher } from '~position/template/contract-position.template.position-fetcher';
 import {
   DefaultContractPositionDefinition,
+  GetDisplayPropsParams,
   GetTokenBalancesParams,
+  GetTokenDefinitionsParams,
   UnderlyingTokenDefinition,
 } from '~position/template/contract-position.template.types';
 
@@ -16,8 +20,8 @@ import { OriginDollarGovernanceViemContractFactory } from '../contracts';
 import { Veogv } from '../contracts/viem';
 
 @PositionTemplate()
-export class EthereumOriginDollarGovernanceRewardsContractPositionFetcher extends ContractPositionTemplatePositionFetcher<Veogv> {
-  groupLabel = 'Staking Reward';
+export class EthereumOriginDollarGovernanceVeOgvContractPositionFetcher extends ContractPositionTemplatePositionFetcher<Veogv> {
+  groupLabel = 'Governance Staked';
 
   constructor(
     @Inject(APP_TOOLKIT) protected readonly appToolkit: IAppToolkit,
@@ -35,25 +39,45 @@ export class EthereumOriginDollarGovernanceRewardsContractPositionFetcher extend
     return [{ address: '0x0c4576ca1c365868e162554af8e385dc3e7c66d9' }];
   }
 
-  async getTokenDefinitions(): Promise<UnderlyingTokenDefinition[] | null> {
+  async getTokenDefinitions({
+    contract,
+  }: GetTokenDefinitionsParams<Veogv>): Promise<UnderlyingTokenDefinition[] | null> {
+    const address = await contract.read.ogv();
     return [
       {
-        address: '0x9c354503c38481a7a7a51629142963f98ecc12d0',
+        address,
+        metaType: MetaType.LOCKED,
+        network: this.network,
+      },
+      {
+        address,
         metaType: MetaType.CLAIMABLE,
         network: this.network,
       },
     ];
   }
 
-  async getLabel() {
-    return `Origin Dollar Governance Staking Rewards`;
+  async getLabel({ contractPosition }: GetDisplayPropsParams<Veogv>) {
+    return getLabelFromToken(contractPosition.tokens[0]);
   }
 
   async getTokenBalancesPerPosition({
     address,
     contract,
   }: GetTokenBalancesParams<Veogv, DefaultDataProps>): Promise<BigNumberish[]> {
+    const lockupsRaw = await Promise.all(
+      range(0, 30).map(async index => {
+        const amount = await contract.read
+          .lockups([address, BigInt(index)])
+          .then(x => x[0])
+          .catch(() => null);
+        return amount;
+      }),
+    );
+    const lockups = compact(lockupsRaw);
+    const locked = sum(lockups.map(x => Number(x)));
+
     const rewardBalance = await contract.read.previewRewards([address]);
-    return [rewardBalance];
+    return [locked, rewardBalance];
   }
 }
