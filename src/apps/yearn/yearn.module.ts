950b5c56bf8e61dc374fabf9b57d38ed2e787052
William Poulin <will@zapper.fi>
2023-11-28 13:55:46 -0500

    feat(yearn): Add yPrisma staking rewards

--- src/apps/yearn/yearn.module.ts
+++ src/apps/yearn/yearn.module.ts
@@ -4,6 +4,7 @@ import { AbstractApp } from '~app/app.dynamic-module';
 
 import { YearnViemContractFactory } from './contracts';
 import { EthereumYearnGovernanceContractPositionFetcher } from './ethereum/yearn.governance.contract-position-fetcher';
+import { EthereumYearnStakingRewardContractPositionFetcher } from './ethereum/yearn.staking-reward.contract-position-fetcher';
 import { EthereumYearnVeYfiContractPositionFetcher } from './ethereum/yearn.ve-yfi.contract-position-fetcher';
 import { OptimismYearnSakingContractPositionFetcher } from './optimism/yearn.staking.contract-position-fetcher';
 
@@ -13,6 +14,7 @@ import { OptimismYearnSakingContractPositionFetcher } from './optimism/yearn.sta
     // Ethereum
     EthereumYearnGovernanceContractPositionFetcher,
     EthereumYearnVeYfiContractPositionFetcher,
+    EthereumYearnStakingRewardContractPositionFetcher,
     // Optimism
     OptimismYearnSakingContractPositionFetcher,
   ],
