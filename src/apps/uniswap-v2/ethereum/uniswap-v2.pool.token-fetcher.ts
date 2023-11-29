4611f33e33952fb2ff975cac0877a951e73c4cda
Karan Shahani <karan.shahani91@gmail.com>
2023-11-29 12:16:51 -0500

    fix(uniswap-v2): Fix slow subgraph call

--- src/apps/uniswap-v2/ethereum/uniswap-v2.pool.token-fetcher.ts
+++ src/apps/uniswap-v2/ethereum/uniswap-v2.pool.token-fetcher.ts
@@ -38,9 +38,10 @@ export class EthereumUniswapV2PoolTokenFetcher extends UniswapV2DefaultPoolSubgr
   groupLabel = 'Pools';
 
   factoryAddress = '0x5c69bee701ef814a2b6a3edd4b1652cb9cc5aa6f';
-  subgraphUrl = 'https://api.thegraph.com/subgraphs/name/ianlapham/uniswapv2?source=zapper';
+  subgraphUrl = 'https://api.thegraph.com/subgraphs/name/ianlapham/uniswap-v2-dev?source=zapper';
   ignoredPools = ['0x3016a43b482d0480460f6625115bd372fe90c6bf'];
-  first = 5000;
+  orderBy = 'trackedReserveETH';
+  first = 1000;
   // Todo: remove when subgraph isn't throwing when calling volumeUSD
   skipVolume = true;
 
