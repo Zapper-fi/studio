1e2eae8c2755ca0cc7a13d4d79efd670e8fbcef3
William Poulin <will@zapper.fi>
2023-12-06 17:46:15 -0500

    fix(lyra-avalon): Update subgraph endpoints

--- src/apps/lyra-avalon/arbitrum/lyra-avalon.pool.token-fetcher.ts
+++ src/apps/lyra-avalon/arbitrum/lyra-avalon.pool.token-fetcher.ts
@@ -4,7 +4,7 @@ import { LyraAvalonPoolTokenFetcher } from '../common/lyra-avalon.pool.token-fet
 
 @PositionTemplate()
 export class ArbitrumLyraAvalonPoolTokenFetcher extends LyraAvalonPoolTokenFetcher {
-  subgraphUrl = 'https://api.lyra.finance/subgraph/arbitrum/v2/api';
+  subgraphUrl = 'https://subgraph.satsuma-prod.com/sw9vuxiQey3c/lyra/arbitrum-mainnet/api';
 
   registryContractAddress = '0x6c87e4364fd44b0d425adfd0328e56b89b201329';
   underlyingContractAddress = '0xff970a61a04b1ca14834a43f5de4533ebddb5cc8';
