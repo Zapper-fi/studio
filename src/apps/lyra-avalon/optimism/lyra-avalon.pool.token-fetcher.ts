88c4f119dc6afc71ae708783f349c68d6f4e8e11
William Poulin <will@zapper.fi>
2023-12-06 17:51:37 -0500

    fix(lyra-avalon): Update subgraph endpoints (#3103)

--- src/apps/lyra-avalon/optimism/lyra-avalon.pool.token-fetcher.ts
+++ src/apps/lyra-avalon/optimism/lyra-avalon.pool.token-fetcher.ts
@@ -4,7 +4,8 @@ import { LyraAvalonPoolTokenFetcher } from '../common/lyra-avalon.pool.token-fet
 
 @PositionTemplate()
 export class OptimismLyraAvalonPoolTokenFetcher extends LyraAvalonPoolTokenFetcher {
-  subgraphUrl = 'https://api.lyra.finance/subgraph/optimism/v2/api';
+  subgraphUrl = 'https://subgraph.satsuma-prod.com/sw9vuxiQey3c/lyra/optimism-mainnet-newport/api';
+
   registryContractAddress = '0x0fed189bcd4a680e05b153dc7c3dc87004e162fb';
   underlyingContractAddress = '0x7f5c764cbc14f9669b88837ca1490cca17c31607';
 }
