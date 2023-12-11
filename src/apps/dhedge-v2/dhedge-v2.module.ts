342f259551799178f863a956e1516494878ed647
William Poulin <will@zapper.fi>
2023-12-11 13:36:41 -0500

    feat(dhedge-v2): Add staking position

--- src/apps/dhedge-v2/dhedge-v2.module.ts
+++ src/apps/dhedge-v2/dhedge-v2.module.ts
@@ -4,9 +4,15 @@ import { AbstractApp } from '~app/app.dynamic-module';
 
 import { DhedgeV2ViemContractFactory } from './contracts';
 import { OptimismDhedgeV2PoolTokenFetcher } from './optimism/dhedge-v2.pool.token-fetcher';
+import { OptimismDhedgeV2StakingContractPositionFetcher } from './optimism/dhedge-v2.staking.contract-position-fetcher';
 import { PolygonDhedgeV2PoolTokenFetcher } from './polygon/dhedge-v2.pool.token-fetcher';
 
 @Module({
-  providers: [DhedgeV2ViemContractFactory, OptimismDhedgeV2PoolTokenFetcher, PolygonDhedgeV2PoolTokenFetcher],
+  providers: [
+    DhedgeV2ViemContractFactory,
+    OptimismDhedgeV2PoolTokenFetcher,
+    OptimismDhedgeV2StakingContractPositionFetcher,
+    PolygonDhedgeV2PoolTokenFetcher,
+  ],
 })
 export class DhedgeV2AppModule extends AbstractApp() {}
