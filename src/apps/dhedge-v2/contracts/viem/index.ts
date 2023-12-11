74580dba5037512450ba049a64be36a787e6343a
William Poulin <will@zapper.fi>
2023-12-11 14:06:28 -0500

    feat(dhedge-v2): Add staking position (#3125)

--- src/apps/dhedge-v2/contracts/viem/index.ts
+++ src/apps/dhedge-v2/contracts/viem/index.ts
@@ -3,7 +3,9 @@
 /* eslint-disable */
 
 export type { DhedgeV2Factory } from './DhedgeV2Factory';
+export type { DhedgeV2Staking } from './DhedgeV2Staking';
 export type { DhedgeV2Token } from './DhedgeV2Token';
 
 export { DhedgeV2Factory__factory } from './DhedgeV2Factory';
+export { DhedgeV2Staking__factory } from './DhedgeV2Staking';
 export { DhedgeV2Token__factory } from './DhedgeV2Token';
