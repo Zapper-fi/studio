60e37a70199924da1cc475f9e1fcd7e4cc1b55a0
William Poulin <will@zapper.fi>
2023-12-13 10:46:05 -0500

    feat(notional-finance-v3): Add claimables on Arbitrum

--- src/apps/notional-finance-v3/notional-finance-v3.module.ts
+++ src/apps/notional-finance-v3/notional-finance-v3.module.ts
@@ -3,6 +3,7 @@ import { Module } from '@nestjs/common';
 import { AbstractApp } from '~app/app.dynamic-module';
 
 import { ArbitrumNotionalFinanceV3BorrowContractPositionFetcher } from './arbitrum/notional-finance-v3.borrow.contract-position-fetcher';
+import { ArbitrumNotionalFinanceV3ClaimableContractPositionFetcher } from './arbitrum/notional-finance-v3.claimable.contract-position-fetcher';
 import { ArbitrumNotionalFinanceV3NTokenTokenFetcher } from './arbitrum/notional-finance-v3.n-token.token-fetcher';
 import { ArbitrumNotionalFinanceV3PCashTokenFetcher } from './arbitrum/notional-finance-v3.p-cash.token-fetcher';
 import { ArbitrumNotionalFinanceV3PDebtTokenFetcher } from './arbitrum/notional-finance-v3.p-debt.token-fetcher';
@@ -18,6 +19,7 @@ import { NotionalFinanceV3ViemContractFactory } from './contracts';
     ArbitrumNotionalFinanceV3NTokenTokenFetcher,
     ArbitrumNotionalFinanceV3SupplyContractPositionFetcher,
     ArbitrumNotionalFinanceV3BorrowContractPositionFetcher,
+    ArbitrumNotionalFinanceV3ClaimableContractPositionFetcher,
   ],
 })
 export class NotionalFinanceV3AppModule extends AbstractApp() {}
