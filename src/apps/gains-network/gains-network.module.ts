75c57685638dac9a2aef5a5c98b43179a5e5fb16
William Poulin <will@zapper.fi>
2023-12-14 15:15:23 -0500

    feat(gains-networks): Add staking positions (6.4.1)

--- src/apps/gains-network/gains-network.module.ts
+++ src/apps/gains-network/gains-network.module.ts
@@ -4,10 +4,12 @@ import { AbstractApp } from '~app/app.dynamic-module';
 
 import { ArbitrumGainsNetworkGTokenTokenFetcher } from './arbitrum/gains-network.g-token.token-fetcher';
 import { ArbitrumGainsNetworkLockedContractPositionFetcher } from './arbitrum/gains-network.locked.contract-position-fetcher';
+import { ArbitrumGainsNetworkStakingV2ContractPositionFetcher } from './arbitrum/gains-network.staking-v2.contract-position-fetcher';
 import { ArbitrumGainsNetworkStakingContractPositionFetcher } from './arbitrum/gains-network.staking.contract-position-fetcher';
 import { GainsNetworkViemContractFactory } from './contracts';
 import { PolygonGainsNetworkGTokenTokenFetcher } from './polygon/gains-network.g-token.token-fetcher';
 import { PolygonGainsNetworkLockedContractPositionFetcher } from './polygon/gains-network.locked.contract-position-fetcher';
+import { PolygonGainsNetworkStakingV2ContractPositionFetcher } from './polygon/gains-network.staking-v2.contract-position-fetcher';
 import { PolygonGainsNetworkStakingContractPositionFetcher } from './polygon/gains-network.staking.contract-position-fetcher';
 
 @Module({
@@ -17,10 +19,12 @@ import { PolygonGainsNetworkStakingContractPositionFetcher } from './polygon/gai
     ArbitrumGainsNetworkGTokenTokenFetcher,
     ArbitrumGainsNetworkStakingContractPositionFetcher,
     ArbitrumGainsNetworkLockedContractPositionFetcher,
+    ArbitrumGainsNetworkStakingV2ContractPositionFetcher,
     // Polygon
     PolygonGainsNetworkStakingContractPositionFetcher,
     PolygonGainsNetworkGTokenTokenFetcher,
     PolygonGainsNetworkLockedContractPositionFetcher,
+    PolygonGainsNetworkStakingV2ContractPositionFetcher,
   ],
 })
 export class GainsNetworkAppModule extends AbstractApp() {}
