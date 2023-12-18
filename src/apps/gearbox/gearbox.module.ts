e24222fd7f982498e6104eed9a93fb90468ee9c8
William Poulin <will@zapper.fi>
2023-12-18 14:01:50 -0500

    fix(gearbox): Fixed lending app tokens v2 and added lending app tokens v3

--- src/apps/gearbox/gearbox.module.ts
+++ src/apps/gearbox/gearbox.module.ts
@@ -4,19 +4,21 @@ import { AbstractApp } from '~app/app.dynamic-module';
 
 import { GearboxViemContractFactory } from './contracts';
 import { EthereumGearboxCreditAccountsContractPositionFetcher } from './ethereum/gearbox.credit-accounts.contract-position-fetcher';
-import { EthereumGearboxLendingTokenFetcher } from './ethereum/gearbox.lending.token-fetcher';
+import { EthereumGearboxLendingV2TokenFetcher } from './ethereum/gearbox.lending-v2.token-fetcher';
+import { EthereumGearboxLendingV3TokenFetcher } from './ethereum/gearbox.lending-v3.token-fetcher';
 import { EthereumGearboxPhantomTokenFetcher } from './ethereum/gearbox.phantom.token-fetcher';
 import { EthereumGearboxRewardsPositionFetcher } from './ethereum/gearbox.rewards.contract-position-fetcher';
 import { EthereumGearboxRewardsMerkleCache } from './ethereum/gearbox.rewards.merkle-cache';
 
 @Module({
   providers: [
-    EthereumGearboxCreditAccountsContractPositionFetcher,
-    EthereumGearboxLendingTokenFetcher,
-    EthereumGearboxPhantomTokenFetcher,
-    EthereumGearboxRewardsPositionFetcher,
     GearboxViemContractFactory,
     EthereumGearboxRewardsMerkleCache,
+    EthereumGearboxCreditAccountsContractPositionFetcher,
+    EthereumGearboxLendingV2TokenFetcher,
+    EthereumGearboxLendingV3TokenFetcher,
+    EthereumGearboxPhantomTokenFetcher,
+    EthereumGearboxRewardsPositionFetcher,
   ],
 })
 export class GearboxAppModule extends AbstractApp() {}
