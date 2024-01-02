651add4823237f594b01d1dc0d9478d212556937
William Poulin <will@zapper.fi>
2024-01-02 15:01:58 -0500

    more

--- src/apps/mstable/mstable.module.ts
+++ src/apps/mstable/mstable.module.ts
@@ -4,11 +4,9 @@ import { AbstractApp } from '~app/app.dynamic-module';
 
 import { MstableViemContractFactory } from './contracts';
 import { EthereumMstableEarnContractPositionFetcher } from './ethereum/mstable.earn.contract-position-fetcher';
-import { EthereumMstableMetaVaultTokenFetcher } from './ethereum/mstable.meta-vault.token-fetcher';
 import { EthereumMstableMtaV1FarmContractPositionFetcher } from './ethereum/mstable.mta-v1-farm.contract-position-fetcher';
 import { EthereumMstableMtaV2FarmContractPositionFetcher } from './ethereum/mstable.mta-v2-farm.contract-position-fetcher';
 import { EthereumMstableSavingsVaultContractPositionFetcher } from './ethereum/mstable.savings-vault.contract-position-fetcher';
-import { PolygonMstableImusdTokenFetcher } from './polygon/mstable.imusd.token-fetcher';
 import { PolygonMstableSavingsVaultContractPositionFetcher } from './polygon/mstable.savings-vault-farm.contract-position-fetcher';
 
 @Module({
@@ -17,9 +15,7 @@ import { PolygonMstableSavingsVaultContractPositionFetcher } from './polygon/mst
     EthereumMstableEarnContractPositionFetcher,
     EthereumMstableMtaV1FarmContractPositionFetcher,
     EthereumMstableMtaV2FarmContractPositionFetcher,
-    EthereumMstableMetaVaultTokenFetcher,
     EthereumMstableSavingsVaultContractPositionFetcher,
-    PolygonMstableImusdTokenFetcher,
     PolygonMstableSavingsVaultContractPositionFetcher,
   ],
 })
