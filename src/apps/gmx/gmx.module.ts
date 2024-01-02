cff438bee0c31ce2957c884b8954d9648fe14db5
William Poulin <will@zapper.fi>
2024-01-02 14:21:02 -0500

    more

--- src/apps/gmx/gmx.module.ts
+++ src/apps/gmx/gmx.module.ts
@@ -2,11 +2,9 @@ import { Module } from '@nestjs/common';
 
 import { AbstractApp } from '~app/app.dynamic-module';
 
-import { ArbitrumGmxEsGmxTokenFetcher } from './arbitrum/gmx.es-gmx.token-fetcher';
 import { ArbitrumGmxFarmContractPositionFetcher } from './arbitrum/gmx.farm.contract-position-fetcher';
 import { ArbitrumGmxGlpTokenFetcher } from './arbitrum/gmx.glp.token-fetcher';
 import { ArbitrumGmxPerpContractPositionFetcher } from './arbitrum/gmx.perp.contract-position-fetcher';
-import { AvalancheGmxEsGmxTokenFetcher } from './avalanche/gmx.es-gmx.token-fetcher';
 import { AvalancheGmxFarmContractPositionFetcher } from './avalanche/gmx.farm.contract-position-fetcher';
 import { AvalancheGmxGlpTokenFetcher } from './avalanche/gmx.glp.token-fetcher';
 import { AvalancheGmxPerpContractPositionFetcher } from './avalanche/gmx.perp.contract-position-fetcher';
@@ -16,12 +14,10 @@ import { GmxViemContractFactory } from './contracts';
   providers: [
     GmxViemContractFactory,
     // Arbitrum
-    ArbitrumGmxEsGmxTokenFetcher,
     ArbitrumGmxFarmContractPositionFetcher,
     ArbitrumGmxGlpTokenFetcher,
     ArbitrumGmxPerpContractPositionFetcher,
     // Avalanche
-    AvalancheGmxEsGmxTokenFetcher,
     AvalancheGmxFarmContractPositionFetcher,
     AvalancheGmxGlpTokenFetcher,
     AvalancheGmxPerpContractPositionFetcher,
