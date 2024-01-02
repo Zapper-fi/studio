39f8073a7c2bdf5268e742e29cb9977a4aa4c797
William Poulin <will@zapper.fi>
2024-01-02 17:28:41 -0500

    more

--- src/apps/tokemak/tokemak.module.ts
+++ src/apps/tokemak/tokemak.module.ts
@@ -4,19 +4,15 @@ import { AbstractApp } from '~app/app.dynamic-module';
 
 import { TokemakClaimableResolver } from './common/tokemak.claimable.resolver';
 import { TokemakViemContractFactory } from './contracts';
-import { EthereumTokemakAccTokeTokenFetcher } from './ethereum/tokemak.acc-toke.token-fetcher';
 import { EthereumTokemakClaimableContractPositionFetcher } from './ethereum/tokemak.claimable.contract-position-fetcher';
 import { EthereumTokemakFarmContractPositionFetcher } from './ethereum/tokemak.farm.contract-position-fetcher';
-import { EthereumTokemakReactorTokenFetcher } from './ethereum/tokemak.reactor.token-fetcher';
 
 @Module({
   providers: [
     TokemakViemContractFactory,
     TokemakClaimableResolver,
-    EthereumTokemakReactorTokenFetcher,
     EthereumTokemakFarmContractPositionFetcher,
     EthereumTokemakClaimableContractPositionFetcher,
-    EthereumTokemakAccTokeTokenFetcher,
   ],
 })
 export class TokemakAppModule extends AbstractApp() {}
