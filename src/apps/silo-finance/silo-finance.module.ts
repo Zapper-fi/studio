18b4058e50b187030e508870aab51ebee69723b6
William Poulin <will@zapper.fi>
2024-01-02 13:06:25 -0500

    feat(silo-finance): Added STIP claimables on Arbitrum (#3170)

--- src/apps/silo-finance/silo-finance.module.ts
+++ src/apps/silo-finance/silo-finance.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 
 import { AbstractApp } from '~app/app.dynamic-module';
 
+import { ArbitrumSiloFinanceClaimableContractPositionFetcher } from './arbitrum/silo-finance.claimable.contract-position-fetcher';
 import { SiloFinanceViemContractFactory } from './contracts';
 import { EthereumSiloFinanceIncentivesContractPositionfetcher } from './ethereum/silo-finance.incentives.contract-position-fetcher';
 
 @Module({
   providers: [
     SiloFinanceViemContractFactory,
-    // Ethereum
+    ArbitrumSiloFinanceClaimableContractPositionFetcher,
     EthereumSiloFinanceIncentivesContractPositionfetcher,
   ],
 })
