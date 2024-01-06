d6407bd5e168dae604debb8bd5f1a35e5cc4a2c3
Jibswap <hello@jibswap.com>
2024-01-06 21:53:17 +0700

    feat(apps): use uniswap v2 viem contract

--- src/apps/jibswap/jibswap.module.ts
+++ src/apps/jibswap/jibswap.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 
 import { AbstractApp } from '~app/app.dynamic-module';
 
-import { JibswapContractFactory } from './contracts';
+import { UniswapV2ViemContractFactory } from '~apps/uniswap-v2/contracts';
+
+import { JBCJibswapPoolTokenFetcher } from './jbc/jibswap.pool.token-fetcher';
+
 
 @Module({
-  providers: [JibswapContractFactory],
+  providers: [UniswapV2ViemContractFactory, JBCJibswapPoolTokenFetcher],
 })
 export class JibswapAppModule extends AbstractApp() {}
