77f161b72877532eafca57bb8fd9df0911477163
Jibswap <hello@jibswap.com>
2024-01-06 22:33:14 +0700

    feat(apps): add abi and formatted

--- src/apps/jibswap/contracts/viem.contract-factory.ts
+++ src/apps/jibswap/contracts/viem.contract-factory.ts
@@ -1,20 +1,23 @@
 
-        import { Injectable, Inject } from '@nestjs/common';
-        import { PublicClient } from 'viem';
+import { Injectable, Inject } from '@nestjs/common'
+import { PublicClient } from 'viem'
 
-        import { Network } from '~types/network.interface';
-        import { IAppToolkit, APP_TOOLKIT } from '~app-toolkit/app-toolkit.interface';
-        
-        
-        
-        
-        
-        
+import { Network } from '~types/network.interface'
+import { IAppToolkit, APP_TOOLKIT } from '~app-toolkit/app-toolkit.interface'
 
-        @Injectable()
-        export class JibswapViemContractFactory {
-          constructor(@Inject(APP_TOOLKIT) protected readonly appToolkit: IAppToolkit) {}
+import { JibswapFactory__factory, JibswapPair__factory } from './viem'
 
-          
-        }
-      
+type ContractOpts = { address: string; network: Network }
+
+@Injectable()
+export class JibswapViemContractFactory {
+  constructor(@Inject(APP_TOOLKIT) protected readonly appToolkit: IAppToolkit) { }
+
+  jibswapFactory({ address, network }: ContractOpts) {
+    return JibswapFactory__factory.connect(address, this.appToolkit.getViemNetworkProvider(network))
+  }
+
+  jibswapPair({ address, network }: ContractOpts) {
+    return JibswapPair__factory.connect(address, this.appToolkit.getViemNetworkProvider(network))
+  }
+}
--- src/apps/jibswap/jbc/jibswap.pool.token-fetcher.ts
+++ src/apps/jibswap/jbc/jibswap.pool.token-fetcher.ts
@@ -1,10 +1,10 @@
-import { PositionTemplate } from '~app-toolkit/decorators/position-template.decorator';
-import { UniswapV2DefaultPoolSubgraphTemplateTokenFetcher } from '~apps/uniswap-v2/common/uniswap-v2.default.subgraph.template.token-fetcher';
+import { PositionTemplate } from '~app-toolkit/decorators/position-template.decorator'
+import { UniswapV2DefaultPoolSubgraphTemplateTokenFetcher } from '~apps/uniswap-v2/common/uniswap-v2.default.subgraph.template.token-fetcher'
 
 @PositionTemplate()
 export class JBCJibswapPoolTokenFetcher extends UniswapV2DefaultPoolSubgraphTemplateTokenFetcher {
-  groupLabel = 'Pools';
+    groupLabel = 'Pools';
 
-  subgraphUrl = 'https://graph.jibswap.com/subgraphs/name/jibswap?source=zapper';
-  factoryAddress = '0x4bbda880c5a0cdcec6510f0450c6c8bc5773d499';
+    subgraphUrl = 'https://graph.jibswap.com/subgraphs/name/jibswap?source=zapper';
+    factoryAddress = '0x4bbda880c5a0cdcec6510f0450c6c8bc5773d499';
 }
