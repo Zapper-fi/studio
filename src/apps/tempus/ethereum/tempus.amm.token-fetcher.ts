7ea62230061dfbfe9f48c52e0f502499c4cb31db
William Poulin <will@zapper.fi>
2024-01-08 13:45:17 -0500

    chore(studio): Migrated Tempus to ATI

--- src/apps/tempus/ethereum/tempus.amm.token-fetcher.ts
+++
@@ -1,8 +0,0 @@
-import { PositionTemplate } from '~app-toolkit/decorators/position-template.decorator';
-
-import { TempusAmmTokenFetcher } from '../common/tempus.amm.token-fetcher';
-
-@PositionTemplate()
-export class EthereumTempusAmmTokenFetcher extends TempusAmmTokenFetcher {
-  groupLabel = 'AMM Liquidity';
-}
